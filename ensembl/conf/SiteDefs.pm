9e589f8e35106f487bb2e06e72c14fae99307c9e
Ensembl Account <ensembl@sanger.ac.uk>
2006-05-26 18:10:25 +0000

    Commented honey bee. Added Ciona savignyi. Changed Fugu to Takifugu. Added other aliases for 2 ciona. Commit on James request

--- ensembl/conf/SiteDefs.pm
+++ ensembl/conf/SiteDefs.pm
@@ -22,19 +22,20 @@ sub update_conf {
   $SiteDefs::__species_aliases{ 'Gallus_gallus'            } = [qw(gg chicken)];
 #-------------------- fish
   $SiteDefs::__species_aliases{ 'Danio_rerio'              } = [qw(dr zfish zebrafish)];
-  $SiteDefs::__species_aliases{ 'Fugu_rubripes'            } = [qw(fr ffish fugu takifugu takifugu_rubripes)];
+  $SiteDefs::__species_aliases{ 'Takifugu_rubripes'            } = [qw(fr ffish fugu takifugu takifugu_rubripes)];
   $SiteDefs::__species_aliases{ 'Tetraodon_nigroviridis'   } = [qw(tn tetraodon)];
 #  $SiteDefs::__species_aliases{ 'Oryzias_latipes'          } = [qw(ol medaka)];
 #-------------------- amphibians
   $SiteDefs::__species_aliases{ 'Xenopus_tropicalis'       } = [qw(xt xenopus frog)];
 #-------------------- flies
   $SiteDefs::__species_aliases{ 'Anopheles_gambiae'        } = [qw(ag mosquito mos anopheles)];
-  $SiteDefs::__species_aliases{ 'Apis_mellifera'           } = [qw(am honeybee bee)];
+#  $SiteDefs::__species_aliases{ 'Apis_mellifera'           } = [qw(am honeybee bee)];
   $SiteDefs::__species_aliases{ 'Drosophila_melanogaster'  } = [qw(dm fly)];
 #-------------------- worms
 # $SiteDefs::__species_aliases{ 'Caenorhabditis_briggsae'  } = [qw(cb briggsae)];
   $SiteDefs::__species_aliases{ 'Caenorhabditis_elegans'   } = [qw(ce worm elegans)];
-  $SiteDefs::__species_aliases{ 'Ciona_intestinalis'       } = [qw(ci seqsquirt ciona)];
+  $SiteDefs::__species_aliases{ 'Ciona_intestinalis'       } = [qw(ci seasquirti cionai)];
+  $SiteDefs::__species_aliases{ 'Ciona_savignyi'       } = [qw(cs seasquirts cionas)];
 #-------------------- yeast
   $SiteDefs::__species_aliases{ 'Saccharomyces_cerevisiae' } = [qw(sc yeast saccharomyces )];
 }
