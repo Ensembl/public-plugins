3d48be6f4328fa923fbe161d6319a3692f5bfaa5
Anne Parker <ap5@sanger.ac.uk>
2006-11-01 11:55:34 +0000

    Added edoc documentation

--- ensembl/modules/EnsEMBL/Ensembl/Document/Configure.pm
+++ ensembl/modules/EnsEMBL/Ensembl/Document/Configure.pm
@@ -1,5 +1,7 @@
 package EnsEMBL::Ensembl::Document::Configure;
 
+### Plugin menu configuration for a standard Ensembl website
+
 use CGI qw(escapeHTML);
 use EnsEMBL::Web::SpeciesDefs;
 
@@ -7,10 +9,12 @@ use EnsEMBL::Web::Root;
 our @ISA  = qw(EnsEMBL::Web::Root);
 
 sub common_menu_items {
+### Addition menu items for site-specific content
+### 1. Archive links
+### 2. Data download link
   my($self,$doc) = @_;
 
-# Archive stuff
-
+  ## Links to archive sites
   my $URL = CGI::escapeHTML($ENV{'REQUEST_URI'});
   my @archive_sites;
   my @releases = @{ $doc->species_defs->RELEASE_INFO || $doc->species_defs->anyother_species('RELEASE_INFO') || []};
@@ -49,6 +53,7 @@ sub common_menu_items {
       'icon'  => '/img/ensemblicon.gif',
   ) unless ($URL =~/familyview/ || !@archive_sites) ;
   
+  ## Stable archive link for current release
   my $stable_URL = sprintf "http://%s.archive.ensembl.org%s",
     CGI::escapeHTML($doc->species_defs->ARCHIVE_VERSION), CGI::escapeHTML($ENV{'REQUEST_URI'});
 
@@ -68,41 +73,46 @@ sub common_menu_items {
 }
 
 sub static_menu_items {
+### Addition static-content-only menu items for site-specific content
+### 1. Lists of current species
   my( $self, $doc ) = @_;
-    $doc->menu->add_block( 'species', 'bulleted', 'Select a species', 'priority' => 20 );
 
-  # do species popups from config
-    my @group_order = qw( Mammals Chordates Eukaryotes );
-    my %spp_tree = (
+  $doc->menu->add_block( 'species', 'bulleted', 'Select a species', 'priority' => 20 );
+
+  my @group_order = qw( Mammals Chordates Eukaryotes );
+  my %spp_tree = (
       'Mammals'   => { 'label'=>'Mammals',          'species' => [] },
       'Chordates' => { 'label'=>'Other chordates',  'species' => [] },
       'Eukaryotes'=> { 'label'=>'Other eukaryotes', 'species' => [] },
-    );
-    my @species_inconf = @{$doc->species_defs->ENSEMBL_SPECIES};
-    foreach my $sp ( @species_inconf) {
-      my $bio_name = $doc->species_defs->other_species($sp, "SPECIES_BIO_NAME");
-      my $group    = $doc->species_defs->other_species($sp, "SPECIES_GROUP") || 'default_group';
-      unless( $spp_tree{ $group } ) {
-        push @group_order, $group;
-        $spp_tree{ $group } = { 'label' => $group, 'species' => [] };
-      }
-      my $hash_ref = { 'href'=>"/$sp/", 'text'=>"<i>$bio_name</i>", 'raw'=>1 };
-      push @{ $spp_tree{$group}{'species'} }, $hash_ref;
+  );
+  ## get the info for all species
+  my @species_inconf = @{$doc->species_defs->ENSEMBL_SPECIES};
+  foreach my $sp ( @species_inconf) {
+    my $bio_name = $doc->species_defs->other_species($sp, "SPECIES_BIO_NAME");
+    my $group    = $doc->species_defs->other_species($sp, "SPECIES_GROUP") || 'default_group';
+    unless( $spp_tree{ $group } ) {
+      push @group_order, $group;
+      $spp_tree{ $group } = { 'label' => $group, 'species' => [] };
     }
-    foreach my $group (@group_order) {
-      next unless @{ $spp_tree{$group}{'species'} };
-      my $text = $spp_tree{$group}{'label'};
-      $doc->menu->add_entry(
+    my $hash_ref = { 'href'=>"/$sp/", 'text'=>"<i>$bio_name</i>", 'raw'=>1 };
+    push @{ $spp_tree{$group}{'species'} }, $hash_ref;
+  }
+  ## output the info grouped by taxa
+  foreach my $group (@group_order) {
+    next unless @{ $spp_tree{$group}{'species'} };
+    my $text = $spp_tree{$group}{'label'};
+    $doc->menu->add_entry(
         'species',
         'href'=>'/',
         'text'=>$text,
         'options'=>$spp_tree{$group}{'species'}
-      );
-    }
+    );
+  }
 }
 
 sub dynamic_menu_items {
-
+### Addition dynamic-content-only menu items for site-specific content
+### Stub - currently no items added
 }
 
 1;
