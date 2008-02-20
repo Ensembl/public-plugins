9b822a838b5dc1139d48e01a0f35cea62b0ecb64
Anne Parker <ap5@sanger.ac.uk>
2008-02-20 09:52:07 +0000

    Fixed some alphabetical ordering of species

--- ensembl/conf/SiteDefs.pm
+++ ensembl/conf/SiteDefs.pm
@@ -13,13 +13,12 @@ sub update_conf {
 #-------------------- mammals
   $SiteDefs::__species_aliases{ 'Bos_taurus'               } = [qw(bt btau cow moo)];
   $SiteDefs::__species_aliases{ 'Canis_familiaris'         } = [qw(cf cfam dog)]; 
+  $SiteDefs::__species_aliases{ 'Cavia_porcellus'          } = [qw(cp cpor guineapig)];
   $SiteDefs::__species_aliases{ 'Dasypus_novemcinctus'     } = [qw(dn dnov dasypus armadillo)];
   $SiteDefs::__species_aliases{ 'Echinops_telfairi'        } = [qw(et etel echinops tenrec)];
   $SiteDefs::__species_aliases{ 'Equus_caballus'           } = [qw(ec horse)];
   $SiteDefs::__species_aliases{ 'Felis_catus'              } = [qw(fc fcat cat)];
-  $SiteDefs::__species_aliases{ 'Tupaia_belangeri'         } = [qw(tb tbel treeshrew shrew)];
   $SiteDefs::__species_aliases{ 'Erinaceus_europaeus'      } = [qw(ee eeur hedgehog)];
-  $SiteDefs::__species_aliases{ 'Cavia_porcellus'          } = [qw(cp cpor guineapig)];
   $SiteDefs::__species_aliases{ 'Homo_sapiens'             } = [qw(hs hsap human man default)]; 
   $SiteDefs::__species_aliases{ 'Loxodonta_africana'       } = [qw(la lafr elephant africana loxodontai hathi nellie dumbo)];
   $SiteDefs::__species_aliases{ 'Oryctolagus_cuniculus'    } = [qw(oc ocun rabbit bugs bunny hutch harvey)];
@@ -36,13 +35,14 @@ sub update_conf {
   $SiteDefs::__species_aliases{ 'Rattus_norvegicus'        } = [qw(rn rnor rat)];
   $SiteDefs::__species_aliases{ 'Sorex_araneus'            } = [qw(sa shrew)];
   $SiteDefs::__species_aliases{ 'Spermophilus_tridecemlineatus'  } = [qw(st stri squirrel spermophilus)];
+  $SiteDefs::__species_aliases{ 'Tupaia_belangeri'         } = [qw(tb tbel treeshrew shrew)];
 #-------------------- birds
   $SiteDefs::__species_aliases{ 'Gallus_gallus'                  } = [qw(gg ggal chicken)];
 #-------------------- fish
   $SiteDefs::__species_aliases{ 'Danio_rerio'                    } = [qw(dr drer zfish zebrafish)];
   $SiteDefs::__species_aliases{ 'Gasterosteus_aculeatus'         } = [qw(ga gacu stickleback gasterosteus)];
-  $SiteDefs::__species_aliases{ 'Takifugu_rubripes'              } = [qw(fr trub ffish fugu takifugu f_rubripes fugu_rubripes)];
   $SiteDefs::__species_aliases{ 'Oryzias_latipes'                } = [qw(ol olat medaka)];
+  $SiteDefs::__species_aliases{ 'Takifugu_rubripes'              } = [qw(fr trub ffish fugu takifugu f_rubripes fugu_rubripes)];
   $SiteDefs::__species_aliases{ 'Tetraodon_nigroviridis'         } = [qw(tn tnig tetraodon)];
 #-------------------- amphibians
   $SiteDefs::__species_aliases{ 'Xenopus_tropicalis'             } = [qw(xt xtro xenopus frog)];
