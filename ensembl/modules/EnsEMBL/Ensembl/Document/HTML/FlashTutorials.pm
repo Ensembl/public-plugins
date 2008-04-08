4945dc4b1240e1b15e83cc6ea72829eac8b6d610
Anne Parker <ap5@sanger.ac.uk>
2008-04-08 15:04:10 +0000

    Added header and blurb into if block

--- ensembl/modules/EnsEMBL/Ensembl/Document/HTML/FlashTutorials.pm
+++ ensembl/modules/EnsEMBL/Ensembl/Document/HTML/FlashTutorials.pm
@@ -17,7 +17,11 @@ sub render {
 
   if (scalar(@movies)) {
  
-    $html = qq(
+    $html = qq(<h2>Online Workshops</h2>
+
+<p>The tutorials listed below are Flash animations of some of our training presentations, with added popup notes in place of a soundtrack. We are gradually adding to the list, so please check back regularly (the list will also be included in the bimonthly Release Email, which is sent to the <a href="/info/about/contact/mailing.html">ensembl-announce mailing list</a>).</p>
+<p>Please note that files are around 3MB per minute, so if you are on a dialup connection, playback may be jerky.</p>
+
 <table class="ss tint">
 <tr>
   <th style="width:60%">Title</th>
