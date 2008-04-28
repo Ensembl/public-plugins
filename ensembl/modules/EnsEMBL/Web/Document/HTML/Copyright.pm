b7ab3ab05732f59f88c8b1f4ae793714ba72bca2
Anne Parker <ap5@sanger.ac.uk>
2008-04-28 10:19:11 +0000

    Updated footer

--- ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -2,22 +2,36 @@ package EnsEMBL::Web::Document::HTML::Copyright;
 use strict;
 use CGI qw(escapeHTML);
 use EnsEMBL::Web::Document::HTML;
+use EnsEMBL::Web::RegObj;
 
 our @ISA = qw(EnsEMBL::Web::Document::HTML);
 
 sub render {
   my @time = localtime();
   my $year = @time[5] + 1900;
+
+  ## Stable archive link for current release
+  my $species_defs = $ENSEMBL_WEB_REGISTRY->species_defs;
+  my $stable_URL = sprintf "http://%s.archive.ensembl.org%s",
+      CGI::escapeHTML($species_defs->ARCHIVE_VERSION), CGI::escapeHTML($ENV{'REQUEST_URI'});
+
   $_[0]->print( qq(
-   <div id="copy"><div id="a1"><div id="a2">
-    <p class="center">
-    <a href="http://www.sanger.ac.uk"><img src="/img/wtsi_rev.png" alt="WTSI" title="Wellcome Trust Sanger Institute" class="float-left" style="padding-left:20px" /></a>
-    <a href="http://www.ebi.ac.uk"><img src="/img/ebi_new.gif" alt="EMBL-EBI" title="European BioInformatics Institute" class="float-right" style="padding-right:20px" /></a>
+    <table width="100%"><tr>
+    <td width="50%">
+    <a href="http://www.sanger.ac.uk"><img src="/img/wtsi_rev.png" alt="WTSI" title="Wellcome Trust Sanger Institute" /></a>
+    <a href="http://www.ebi.ac.uk"><img src="/img/ebi_new.gif" alt="EMBL-EBI" title="European BioInformatics Institute" /></a>
     &copy; $year <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
     <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>.
-    Ensembl is available to <a href="/info/downloads/index.html">download for public use</a> - please see the <a href="/info/code_licence.html">code licence</a> for details.
-    </p>
-    </div></div></div>
+    <br />Ensembl receives major funding from the Wellcome Trust.
+    <br />Our <a href="/info/about/credits.html">credits page</a> includes additional current and previous funding.
+    </td>
+    <td width="20%">&nbsp;</td>
+    <td class="right" width="30%" style="vertical-align:bottom">
+    <a href="http://www.ensembl.org/info/about/">About&nbsp;Ensembl</a> | 
+    <a href="http://www.ensembl.org/info/about/contact/">Contact&nbsp;Us</a> | 
+    <a href="/info/help/">Help</a> 
+    </td>
+    </tr></table>
     ) 
   );
 }
