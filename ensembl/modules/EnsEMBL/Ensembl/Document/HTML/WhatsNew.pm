4c3a00ce7e740f5e03b6f21c5a4fe76580330265
Anne Parker <ap5@sanger.ac.uk>
2008-05-15 09:25:21 +0000

    Temporary changes to home page until we get species_defs working

--- ensembl/modules/EnsEMBL/Ensembl/Document/HTML/WhatsNew.pm
+++ ensembl/modules/EnsEMBL/Ensembl/Document/HTML/WhatsNew.pm
@@ -18,15 +18,8 @@ sub render {
   my $user = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->get_user;
 
   my $html = qq(<div class="species-news">
-      <h2 class="first">What's New in Ensembl Release 49 (March 2008)</h2>
-      <ul>
-      <li><a href="http://ensweb-03.internal.sanger.ac.uk:10000/Multi/newsview?rel=49#cat2" style="text-decoration: none;">Release schedule</a> (<i>all species</i>)</li>
-      <li><a href="http://ensweb-03.internal.sanger.ac.uk:10000/Pongo_pygmaeus/newsview?rel=49#cat2" style="text-decoration: none;">New species - Orangutan</a> (<span class="latin">Pongo pygmaeus</span>)</li>
-      <li><a href="http://ensweb-03.internal.sanger.ac.uk:10000/Equus_caballus/newsview?rel=49#cat2" style="text-decoration: none;">New species - Horse</a> (<i>Equus caballus</i>)</li>
-      <li><a href="http://ensweb-03.internal.sanger.ac.uk:10000/Takifugu_rubripes/newsview?rel=49#cat2" style="text-decoration: none;">New genebuild - Fugu</a> (<i>Takifugu rubripes</i>)</li>
-      <li><a href="http://ensweb-03.internal.sanger.ac.uk:10000/Drosophila_melanogaster/newsview?rel=49#cat2" style="text-decoration: none;">New import of Fly database</a> (<i>Drosophila melanogaster</i>)</li>
-      <p><a href="http://ensweb-03.internal.sanger.ac.uk:10000/Multi/newsview?rel=current">More news &rarr;</a></p>
-      </ul>
+      <h2 class="first">What's New in Ensembl Release 50</h2>
+      <p class="red">No news yet!</p>
     </div>
 );
 
@@ -38,6 +31,7 @@ sub render {
         <p><a href="http://ensembl.blogspot.com/">Go to Ensembl blog &rarr;</a></p>
       </ul>);
 
+=pod
   if ($species_defs->ENSEMBL_LOGINS) {
     if ($user && $user->id) {
       #if (!$filtered) {
@@ -48,7 +42,7 @@ sub render {
       $html .= qq(<p><a href="javascript:login_link();">Log in</a> to see customised news &middot; <a href="/common/user/register">Register</a></p>);
     }
   }
-
+=cut
   return $html;
 }
 
