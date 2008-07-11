38e7e2b2c0c7f21da418561ab3b700e58e27bed8
Anne Parker <ap5@sanger.ac.uk>
2008-07-11 10:06:38 +0000

    Made more compact

--- ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -26,26 +26,22 @@ sub render {
     q(
     <div class="twocol-left left unpadded">
         %s release %d - %s - %s
+      &copy; <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
+      <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>
+    <br />
         <a class="modal_link" id="p_link" href="sorry.html">Permanent link</a> -
         <a class="modal_link" id="a_link" href="sorry.html">View in archive site</a>
+    </div>
       ),
     $sd->ENSEMBL_SITETYPE, $sd->ENSEMBL_VERSION,
     $sd->ENSEMBL_RELEASE_DATE,
     '',
-#    $sd->SPECIES_COMMON_NAME ? sprintf( '%s <i>%s</i> %s -', $sd->SPECIES_COMMON_NAME, $sd->SPECIES_BIO_NAME, $sd->ASSEMBLY_ID ): '',
-#    $sd->SPECIES_BIO_NAME,
-#    $sd->ASSEMBLY_ID,
     $URL,
     $URL,
     $URL,
     $URL
   );
 
-  $self->print( qq(<br />
-      &copy; $year <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
-      <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>
-    </div>) 
-  );
 }
 
 1;
