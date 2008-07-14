3515f3cecc15ebe70f6aa892982229e8db8b4de4
James Smith <js5@sanger.ac.uk>
2008-07-14 08:51:35 +0000

    moved blog entry link to make sure that it is XHTML compliant

--- ensembl/modules/EnsEMBL/Ensembl/Document/HTML/WhatsNew.pm
+++ ensembl/modules/EnsEMBL/Ensembl/Document/HTML/WhatsNew.pm
@@ -17,24 +17,26 @@ sub render {
   my $release_id = $species_defs->ENSEMBL_VERSION;
   my $user = $EnsEMBL::Web::RegObj::ENSEMBL_WEB_REGISTRY->get_user;
 
-  my $html = qq(<div class="species-news">
+  my $html = qq(
+    <div class="species-news">
       <h2 class="first">What's New in Ensembl Release 50</h2>
       <ul>
-      <li>Cleaner interface!</li>
-      <li>Exciting new navigation!</li>
-      <li>Faster!</li>
-      <li>Some other stuff we haven't done yet...</li>
+        <li>Cleaner interface!</li>
+        <li>Exciting new navigation!</li>
+        <li>Faster!</li>
+        <li>Some other stuff we haven't done yet...</li>
       </ul>
-    </div>
-);
+    </div>);
 
-  $html .= qq(<h2>Latest Blog Entries</h2>
+  $html .= qq(
+  
+      <h2>Latest Blog Entries</h2>
       <ul>
         <li>New Release notes and BAC clones in mouse</li>
         <li>Ensembl US East Coast Tour</li>
         <li>March workshops, release and down time</li>
-        <p><a href="http://ensembl.blogspot.com/">Go to Ensembl blog &rarr;</a></p>
-      </ul>);
+      </ul>
+      <p><a href="http://ensembl.blogspot.com/">Go to Ensembl blog &rarr;</a></p>);
 
 =pod
   if ($species_defs->ENSEMBL_LOGINS) {
