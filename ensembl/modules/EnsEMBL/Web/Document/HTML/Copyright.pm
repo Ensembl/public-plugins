237992d9122e9dd22473e4e75b5bedd86a8da2a3
Anne Parker <ap5@sanger.ac.uk>
2008-08-07 09:32:03 +0000

    Updated links to point to working archive popups

--- ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -3,7 +3,7 @@ package EnsEMBL::Web::Document::HTML::Copyright;
 ### Replacement copyright notice for www.ensembl.org
 
 use strict;
-use CGI qw(escapeHTML);
+use CGI qw(escape);
 use EnsEMBL::Web::Document::HTML;
 use EnsEMBL::Web::RegObj;
 
@@ -20,27 +20,33 @@ sub render {
 
   my $sd = $ENSEMBL_WEB_REGISTRY->species_defs;
 
-  my $URL = sprintf '/%s/%s/%s?%s', $ENV{'ENSEMBL_SPECIES'},$ENV{'ENSEMBL_TYPE'},$ENV{'ENSEMBL_ACTION'},$ENV{'QUERY_STRING'};
+  my $referer = CGI::escape($ENV{'REQUEST_URI'});
+  my $stable_URL = sprintf "http://%s.archive.ensembl.org%s",
+      CGI::escape($sd->ARCHIVE_VERSION), CGI::escape($ENV{'REQUEST_URI'});
 
   $self->printf(
     q(
     <div class="twocol-left left unpadded">
-        %s release %d - %s - %s
+        %s release %d - %s
       &copy; <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
       <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>
-    <br />
-        <a class="modal_link" id="p_link" href="sorry.html">Permanent link</a> -
-        <a class="modal_link" id="a_link" href="sorry.html">View in archive site</a>
-    </div>
-      ),
+    ),
     $sd->ENSEMBL_SITETYPE, $sd->ENSEMBL_VERSION,
     $sd->ENSEMBL_RELEASE_DATE,
-    '',
-    $URL,
-    $URL,
-    $URL,
-    $URL
+    );
+  
+  unless ($ENV{'ENSEMBL_TYPE'} ] =~ /Help|Account|UserData/) {
+    $self->printf(
+      q(
+      <br />
+        <a class="modal_link" id="p_link" href="%s">Permanent link</a> -
+        <a class="modal_link" id="a_link" href="%s">View in archive site</a>
+      ),
+    '/Help/Permalink?url='.$stable_URL,
+    '/Help/ArchiveList?url='.$referer,
   );
+  }
+  $self->print('</div>');
 
 }
 
