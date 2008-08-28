7a61dce8d25d3e003757ed25d31403877798aca2
Anne Parker <ap5@sanger.ac.uk>
2008-08-28 09:34:11 +0000

    Updated to use correct data from species defs

--- ensembl/modules/EnsEMBL/Ensembl/Document/HTML/ArchiveList.pm
+++ ensembl/modules/EnsEMBL/Ensembl/Document/HTML/ArchiveList.pm
@@ -12,14 +12,14 @@ sub render {
 
   my $SD = $ENSEMBL_WEB_REGISTRY->species_defs;
   my $species = $SD->ENSEMBL_PRIMARY_SPECIES;
-  my %archive_info = %{$SD->get_config($species, 'archive')};
-  my %archive_to_display = %{$archive_info{'online'}};
+  my %archive_info = %{$SD->ENSEMBL_ARCHIVES};
   my $html = qq(<h3 class="boxed">List of currently available archives</h3>
 <ul class="spaced">);
   my $count = 0;
 
-  foreach my $release (reverse sort keys %archive_to_display) {
-    my $subdomain = $archive_to_display{$release};
+  foreach my $release (reverse sort keys %archive_info) {
+    next if $release > $SD->ENSEMBL_VERSION; ## In case this is a dev site on a yet-to-be-released version
+    my $subdomain = $archive_info{$release};
     (my $date = $subdomain) =~ s/20/ 20/; 
     $html .= qq(<li><strong><a href="http://$subdomain.archive.ensembl.org">Ensembl $release: $date</a>);
     $html .= ' - currently www.ensembl.org' if $release == $SD->ENSEMBL_VERSION;
