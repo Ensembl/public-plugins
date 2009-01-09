bae48a7d5f28382863314837d657e27db7364344
Anne Parker <ap5@sanger.ac.uk>
2009-01-09 14:31:31 +0000

    Small fix to remove links on print version

--- ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -29,8 +29,9 @@ sub render {
     q(
     <div class="twocol-left left unpadded">
         %s release %d - %s
-      &copy; <a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
-      <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a>
+      &copy; <span class="print_hide"><a href="http://www.sanger.ac.uk/" class="nowrap">WTSI</a> /
+      <a href="http://www.ebi.ac.uk/" style="white-space:nowrap">EBI</a></span>
+      <span class="screen_hide_inline">WTSI / EBI</span>
     ),
     $sd->ENSEMBL_SITETYPE, $sd->ENSEMBL_VERSION,
     $sd->ENSEMBL_RELEASE_DATE,
