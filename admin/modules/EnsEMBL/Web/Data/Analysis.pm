7b9d56cde1eb46c016918bcbac1942be6158cd2d
Evgeny Bragin <eb4@sanger.ac.uk>
2009-03-30 12:47:28 +0000

    + dynamic db connection test

--- admin/modules/EnsEMBL/Web/Data/Analysis.pm
+++ admin/modules/EnsEMBL/Web/Data/Analysis.pm
@@ -25,6 +25,56 @@ __PACKAGE__->add_queriable_fields(
     gff_feature     => 'varchar(40)',
 );
 
-__PACKAGE__->might_have(analysis_description => 'EnsEMBL::Web::Data::AnalysisDescription, qw/display_label description/);
+__PACKAGE__->might_have(analysis_description => 'EnsEMBL::Web::Data::AnalysisDescription, qw/display_label description/');
+
+########################################################################
+## Several tweaks to make multiple database connections
+########################################################################
+
+use DBI;
+
+my %dbh;
+my $current_dbh;
+
+##
+## EnsEMBL::Web::Data::Analysis::connect($db_info) - dynamic db connection
+## $db_info = {NAME => 'dbname', HOST => ..., PORT => ...}
+##
+sub connect {
+    my $db_info = shift;
+
+    my $dsn = join(':',
+      'dbi',
+      'mysql',
+      $db_info->{'NAME'},
+      $db_info->{'HOST'},
+      $db_info->{'PORT'},
+    );
+
+    if ($dbh{$dsn}) {
+        $current_dbh = $dbh{$dsn};
+    } else {
+        $current_dbh = $dbh{$dsn} = DBI->connect_cached(
+            $dsn,
+            __PACKAGE__->species_defs->DATABASE_WRITE_USER,
+            __PACKAGE__->species_defs->DATABASE_WRITE_PASS,
+            {
+              RaiseError => 1,
+              PrintError => 1,
+              AutoCommit => 1,
+            }
+        );
+        if (not $current_dbh) {
+            warn "Could not connect to '$dsn' $DBI::errstr";
+            return 0;
+        }
+    }
+    return 1;
+}
+ 
+sub db_Main {
+    my $self = shift;
+    return $current_dbh;
+}
 
 1;
