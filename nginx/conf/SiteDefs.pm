67dd0c0dda159e06f5bb54f21e34abed5c9d0a24
Evgeny Bragin <eb4@sanger.ac.uk>
2009-04-27 11:13:15 +0000

    + nginx plugin

---
+++ nginx/conf/SiteDefs.pm
@@ -0,0 +1,13 @@
+package EnsEMBL::Nginx::SiteDefs;
+
+use strict;
+
+sub update_conf {
+  $SiteDefs::ENSEMBL_PORT       = 8080;
+  $SiteDefs::ENSEMBL_NGINX_PORT = 8015;
+  $SiteDefs::ENSEMBL_PROXY_PORT = 80;
+
+  $SiteDefs::ENSEMBL_NGINX_ROOT = $SiteDefs::ENSEMBL_SERVERROOT."/sanger-plugins/nginx";
+}
+
+1;
