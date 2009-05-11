d121c72d9f807f7d335352c1026e50421ae13187
Evgeny Bragin <eb4@sanger.ac.uk>
2009-05-11 16:45:07 +0000

    *** empty log message ***

--- nginx/conf/SiteDefs.pm
+++ nginx/conf/SiteDefs.pm
@@ -3,9 +3,8 @@ package EnsEMBL::Nginx::SiteDefs;
 use strict;
 
 sub update_conf {
-  $SiteDefs::ENSEMBL_PORT       = 8080;
-  $SiteDefs::ENSEMBL_NGINX_PORT = 8000;
-  $SiteDefs::ENSEMBL_PROXY_PORT = 80;
+  $SiteDefs::ENSEMBL_NGINX_PORT = $SiteDefs::ENSEMBL_PORT;
+  $SiteDefs::ENSEMBL_PORT       = $SiteDefs::ENSEMBL_NGINX_PORT + 5000;
 
   $SiteDefs::ENSEMBL_NGINX_ROOT = $SiteDefs::ENSEMBL_SERVERROOT."/sanger-plugins/nginx";
 }
