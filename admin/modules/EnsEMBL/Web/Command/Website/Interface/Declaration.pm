baf950ec68d09cf7b43b21e64832d403f8c7b6ed
Ensembl Account <ensembl@sanger.ac.uk>
2009-07-22 15:47:58 +0000

    Extended interface to work in both add and edit modes

--- admin/modules/EnsEMBL/Web/Command/Website/Interface/Declaration.pm
+++ admin/modules/EnsEMBL/Web/Command/Website/Interface/Declaration.pm
@@ -25,15 +25,7 @@ sub process {
   $interface->element('caveat', {'type' => 'Information', 'value' => 'Please do not enter text in all capitals, as it then has to be changed back to normal case for use in the news pages!'});
   $interface->modify_element('declaration', {'rows' => 20, 'cols' => 80});
   $interface->modify_element('notes', {'rows' => 5, 'cols' => 80});
-  
-  $interface->extra_data('add_species');
-  my $values = [
-    {'name' => 'All', 'value' => 'All'},
-    {'name' => 'Only some', 'value' => 'Some'},
-  ];
-  $interface->element('add_species', {'type' => 'DropDown', 'name' => 'add_species', 'label' => 'Which species does this declaration affect?', 'values' => $values});
-  $interface->element('species_note', {'type' => 'Information', 'value' => 'N.B. Species can only be added after you have previewed and saved your declaration.'});
-
+ 
   $interface->modify_element('assembly',        {'select' => 'radio', 'label' => 'New assembly'});
   $interface->modify_element('gene_set',        {'select' => 'radio', 'label' => 'New gene set'});
   $interface->modify_element('repeat_masking',  {'select' => 'radio', 'label' => 'New repeat mask'});
@@ -42,7 +34,21 @@ sub process {
   $interface->modify_element('database',        {'select' => 'radio', 'label' => 'Database on ens-staging'});
   $interface->modify_element('release_id', {'type' => 'Hidden', 'value' => $object->species_defs->ENSEMBL_VERSION});
 
-  $interface->element_order(['team', 'title', 'caveat', 'declaration', 'notes', 'add_species', 'species_note', 'assembly', 'gene_set', 'repeat_masking', 'stable_id_mapping', 'affy_mapping', 'database', 'status', 'release_id']);
+  ## This interface has to vary depending on whether the record exists or not, owing to the many-to-many relationship
+  if ($data->id) {
+    $interface->element_order(['team', 'title', 'caveat', 'declaration', 'notes', 'species', 'assembly', 'gene_set', 'repeat_masking', 'stable_id_mapping', 'affy_mapping', 'database', 'status', 'release_id']);
+  }
+  else { 
+    $interface->extra_data('add_species');
+    my $values = [
+      {'name' => 'All', 'value' => 'All'},
+      {'name' => 'Only some', 'value' => 'Some'},
+    ];
+    $interface->element('add_species', {'type' => 'DropDown', 'name' => 'add_species', 'label' => 'Which species does this declaration affect?', 'values' => $values});
+    $interface->element('species_note', {'type' => 'Information', 'value' => 'N.B. Species can only be added after you have previewed and saved your declaration.'});
+    $interface->element_order(['team', 'title', 'caveat', 'declaration', 'notes', 'add_species', 'species_note', 'assembly', 'gene_set', 'repeat_masking', 'stable_id_mapping', 'affy_mapping', 'database', 'status', 'release_id']);
+  }
+
   $interface->dropdown(1);
   $interface->option_columns(['team','declaration']);
   
