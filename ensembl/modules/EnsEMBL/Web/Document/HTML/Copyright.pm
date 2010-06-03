016bc8d3d4815b57497c1ae6bc01757b3ee41519
cvs2git <cvs2git>
2010-06-03 08:49:31 +0000

    This commit was manufactured by cvs2svn to create branch 'branch- ensembl-58'.
    Cherrypick from master 2010-06-03 08:49:29 UTC Simon Brent <sb23@sanger.ac.uk> 'add constant class to links so they aren't affected by url hash change':
        ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
        ensembl/modules/EnsEMBL/Web/Document/HTML/FooterLinks.pm

---
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/Copyright.pm
@@ -0,0 +1,59 @@
+package EnsEMBL::Web::Document::HTML::Copyright;
+
+### Replacement copyright notice for www.ensembl.org
+
+use strict;
+use URI::Escape qw(uri_escape);
+use EnsEMBL::Web::RegObj;
+
+use base qw(EnsEMBL::Web::Document::HTML);
+
+sub new { return shift->SUPER::new('sitename' => '?'); }
+
+sub sitename    :lvalue { $_[0]{'sitename'};   }
+
+sub render {
+  my $self = shift;
+  my @time = localtime();
+  my $year = @time[5] + 1900;
+
+  my $sd = $ENSEMBL_WEB_REGISTRY->species_defs;
+
+  my $you_are_here = $ENV{'REQUEST_URI'};
+  my $stable_URL   = uri_escape('http://' . $sd->ARCHIVE_VERSION . '.archive.ensembl.org');
+
+  $self->printf(
+    q(
+    <div class="twocol-left left unpadded">
+        %s release %d - %s
+      &copy; <span class="print_hide"><a href="http://www.sanger.ac.uk/" class="nowrap constant">WTSI</a> /
+      <a href="http://www.ebi.ac.uk/" class="nowrap constant">EBI</a></span>
+      <span class="screen_hide_inline">WTSI / EBI<br />http://%s</span>
+    ),
+    $sd->ENSEMBL_SITETYPE, $sd->ENSEMBL_VERSION,
+    $sd->ENSEMBL_RELEASE_DATE, $sd->ENSEMBL_SERVERNAME,
+    );
+  
+  $self->printf('<div class="print_hide">'); 
+  unless ($ENV{'ENSEMBL_TYPE'} =~ /Help|Account|UserData/) {
+    $self->print(
+      qq{
+      <br />
+        <a class="modal_link" id="p_link" href="/Help/Permalink?url=$stable_URL">Permanent link</a>
+      }
+    );
+    unless ($you_are_here =~ /html$/ && $you_are_here ne '/index.html') {
+      ## Omit archive links from static content, which tends to change a lot
+      $self->print(
+        '
+         - <a class="modal_link" id="a_link" href="/Help/ArchiveList">View in archive site</a>
+        '
+      );
+    }
+  }
+  $self->print('</div></div>');
+
+}
+
+1;
+
---
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/FooterLinks.pm
@@ -0,0 +1,23 @@
+package EnsEMBL::Web::Document::HTML::FooterLinks;
+
+### Replacement footer links for www.ensembl.org
+
+use strict;
+
+use base qw(EnsEMBL::Web::Document::HTML);
+
+sub render {
+  $_[0]->print( qq(
+    <div class="twocol-right right unpadded print_hide">
+      <a href="http://www.ensembl.org/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
+      <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a> | 
+      <a href="/info/website/help/" class="constant">Help</a> 
+    </div>
+    <div class="twocol-right right unpadded screen_hide_block">
+      helpdesk\@ensembl.org
+    </div>) 
+  );
+}
+
+1;
+
