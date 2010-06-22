ba9acd9ef0b63471a0236e101681c42a83fed48c
Web Group <www-ens@sanger.ac.uk>
2010-06-22 12:10:33 +0000

    Moved in from sanger-plugins/admin

---
+++ admin/modules/EnsEMBL/Web/DbFrontend/Changelog.pm
@@ -0,0 +1,30 @@
+package EnsEMBL::Web::DbFrontend::Changelog;
+
+### NAME: EnsEMBL::Web::DbFrontend::Changelog;
+
+### STATUS: Under development
+
+### DESCRIPTION:
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+use base qw(EnsEMBL::Web::DbFrontend);
+
+sub init {
+  my $self = shift;
+  ## Alter default settings if required
+  $self->{'show_fields'} = [qw(team title species content status assembly gene_set repeat_masking stable_id_mapping affy_mapping db_status notes)];
+  $self->{'show_history'} = 1;
+}
+
+sub modify_form {
+  my ($self, $param, $mode) = @_;
+
+  $param->{'db_status'}{'label'} = 'Database changed';
+  $param->{'release_id'}{'type'} = 'NoEdit';
+  $param->{'release_id'}{'value'} = $self->hub->species_defs->ENSEMBL_VERSION;
+}
+
+1;
