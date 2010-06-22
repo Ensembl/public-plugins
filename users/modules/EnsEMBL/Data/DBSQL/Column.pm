14bc5e9dd4f3e896bbfbd2645213be4e2070f967
Web Group <www-ens@sanger.ac.uk>
2010-06-22 10:20:24 +0000

    Moved into plugin, so core code does not depend on Rose::DB

---
+++ users/modules/EnsEMBL/Data/DBSQL/Column.pm
@@ -0,0 +1,35 @@
+package EnsEMBL::Data::DBSQL::Column;
+
+### NAME: EnsEMBL::Data::DBSQL::Column;
+
+### STATUS: Under development
+
+### DESCRIPTION: A very simple object that mimics a Rose::DB::Object::MetaData::Column
+### Used by DbFrontend code to populate lookups (e.g. dropdown lists) from relationships
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+sub new {
+  my ($class, $args) = @_;
+  if (!$args->{'name'}) {
+    warn "!!! MUST SUPPLY NAME FOR THIS COLUMN!";
+    return;
+  }
+  my $self = {
+    '_name' => $args->{'name'},
+    '_type' => $args->{'type'} || 'scalar',
+    '_values' => $args->{'values'} || [],
+  };
+  bless $self, $class;
+  return $self;
+}
+
+sub name    { return $_[0]->{'_name'}; }
+sub type    { return $_[0]->{'_type'}; }
+sub values  { return $_[0]->{'_values'}; }
+
+sub is_primary_key_member { return 0; }
+
+1;
---
+++ users/modules/EnsEMBL/Data/Manager.pm
@@ -0,0 +1,28 @@
+package EnsEMBL::Data::Manager;
+
+### NAME: EnsEMBL::Data::Manager
+### Base class for a Rose::DB::Object::Manager object 
+
+### STATUS: Under Development
+### You will need to uncomment the use base line in order to test this code!
+
+### DESCRIPTION:
+### This module and its children enable easy instantiation of EnsEMBL::Data
+### objects
+
+### At the moment this base class doesn't really do anything apart from
+### inheritance, but it avoids having to comment out the use lines
+### on multiple modules when running on pre-Lenny boxes...
+
+use strict;
+use warnings;
+
+no warnings qw(uninitialized);
+
+use base qw(Rose::DB::Object::Manager);
+
+## Stub - return appropriate set of objects to populate a lookup widget
+sub get_lookup { return []; }
+
+1;
+
