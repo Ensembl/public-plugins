5df188214cb037f2c1630bb66126bf8a4bd7895b
Web Group <www-ens@sanger.ac.uk>
2010-06-22 12:17:16 +0000

    Added config options needed by db interface

--- admin/conf/SiteDefs.pm
+++ admin/conf/SiteDefs.pm
@@ -4,6 +4,30 @@ package EnsEMBL::Admin::SiteDefs;
 
 sub update_conf {
   $SiteDefs::ENSEMBL_WEBADMIN_ID     = 0;
+
+
+  ## ALLOWABLE DATA OBJECTS
+  $SiteDefs::OBJECT_TO_SCRIPT = {
+    Config      => 'config',
+    Component   => 'component',
+    Zmenu       => 'zmenu',
+
+    Search      => 'action',
+    Changelog   => 'action',
+    Healthcheck => 'action',
+    News        => 'action',
+
+    Account     => 'modal',
+    UserData    => 'modal',
+    Help        => 'modal',
+  };
+
+  $SiteDefs::ENSEMBL_BLAST_ENABLED = 0;
+  $SiteDefs::ENSEMBL_MART_ENABLED = 0;
+  $SiteDefs::ENSEMBL_MEMCACHED = {};
+};
+
+
 }
 
 1;
