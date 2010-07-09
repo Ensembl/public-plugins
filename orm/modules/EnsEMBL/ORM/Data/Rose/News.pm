4b9a7749d9862e01ed2616a6ee93bb696aa44d97
Web Group <www-ens@sanger.ac.uk>
2010-07-09 14:12:06 +0000

    Fixed some namespace issues and added fetch_published

--- orm/modules/EnsEMBL/ORM/Data/Rose/News.pm
+++ orm/modules/EnsEMBL/ORM/Data/Rose/News.pm
@@ -9,25 +9,48 @@ use strict;
 use warnings;
 no warnings qw(uninitialized);
 
-use EnsEMBL::ORM::Rose::Manager::News;
+use EnsEMBL::ORM::Rose::Manager::NewsItem;
 use base qw(EnsEMBL::ORM::Data::Rose);
 
 sub set_classes {
-### Set custom class names, as they are not quite the same as the table name
+### Set custom class names, as they are not quite the same as the URL
   my $self = shift;
   $self->{'_object_class'} = 'EnsEMBL::ORM::Rose::Object::NewsItem';
-  $self->{'_manager_class'} = 'EnsEMBL::ORM::Rose::Manager::News';
+  $self->{'_manager_class'} = 'EnsEMBL::ORM::Rose::Manager::NewsItem';
+}
+
+sub set_primary_keys {
+  my $self = shift;
+  $self->{'_primary_keys'} = [qw(news_item_id)];
 }
 
 sub fetch_all {
-### Custom query - for the changelog output, we normally only want to 
+### Custom query - for the news output, we normally only want to 
 ### see the results for one release at a time
   my $self = shift;
   my $release_id = $self->hub->param('release')
                       || $self->hub->species_defs->ENSEMBL_VERSION;
   my $objects = $self->{'_manager_class'}->get_newsitems(
     query => [
+      release_id  => $release_id,
+      !content    => '', ## not empty
+    ],
+  );
+  $self->data_objects(@$objects);
+  return $objects;
+}
+
+sub fetch_published {
+### Custom query - get only the published news for a release
+### (used for public display, as opposed to CRUD interface)
+  my $self = shift;
+  my $release_id = $self->hub->param('release')
+                      || $self->hub->species_defs->ENSEMBL_VERSION;
+  my $objects = $self->{'_manager_class'}->get_newsitems(
+    query => [
       release_id => $release_id,
+      status     => 'published',
+      !content    => '', ## not empty
     ],
   );
   $self->data_objects(@$objects);
