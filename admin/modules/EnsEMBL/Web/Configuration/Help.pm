33207ae579460ab9fb6dd8276623cf5a18bd61f4
Web Group <www-ens@sanger.ac.uk>
2010-08-06 09:54:24 +0000

    Added call to delete tree before adding CRUD nodes

--- admin/modules/EnsEMBL/Web/Configuration/Help.pm
+++ admin/modules/EnsEMBL/Web/Configuration/Help.pm
@@ -3,8 +3,11 @@ package EnsEMBL::Web::Configuration::Help;
 sub modify_tree {
   my $self = shift;
 
+  ## Remove standard Help tree
+  $self->delete_tree;
+
   ## Add defaults
-  $self->add_dbfrontend_to_tree(['WebAdmin']);
+  $self->add_dbfrontend_to_tree(['WebAdmin'], [qw(View Faq Glossary Movie)]);
 }
 
 1;
