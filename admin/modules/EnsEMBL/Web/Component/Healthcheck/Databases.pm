d58bb72a8beff029fc910971a6e4c54e27f880cf
Ensembl Account <ensembl@sanger.ac.uk>
2010-09-02 12:35:41 +0000

    Added checking against healthcheck db list - needs testing when healthchecks start for next release!

--- admin/modules/EnsEMBL/Web/Component/Healthcheck/Databases.pm
+++ admin/modules/EnsEMBL/Web/Component/Healthcheck/Databases.pm
@@ -18,15 +18,20 @@ sub _init {
 sub content {
   my $self = shift;
   my $html;
+  $html .= '<h1>Database list</h1>';
+  $html .= "<p><strong>Y/N</strong>: is this database being healthchecked?</p>";
 
   my @servers = ('ens-staging1', 'ens-staging2');
+  my @databases = $self->object->database_names;
+  my %db_check = map { $_ => 1 } @databases;
   my %configured_dbs;
 
   my $SD = $self->object->species_defs;
   my @db_types = qw(DATABASE_CORE DATABASE_OTHERFEATURES DATABASE_VARIATION DATABASE_FUNCGEN);
 
   foreach my $hostname (@servers) {
-    $html .= "<h2>$hostname</h2><ul>";
+    $html .= "<h2>$hostname</h2>";
+    $html .= "<ul>";
     my $drh = DBI->install_driver('mysql');
     my @dbs = $drh->func($hostname, '3306', 'ensro', '_ListDBs');
     my $matchstring = '_core|_otherfeatures|_cdna|_variation|_funcgen|_compara';
@@ -47,13 +52,15 @@ sub content {
           $html .= '<ul>';
           $indent = 0;
         }
-        #warn ">>> SPECIES $spname"; 
         if ($indent) {
-          $html .= "<li><strong>$dbname</strong></li>";
+          $html .= "<li><strong>$dbname</strong>";
         }
         else {
-          $html .= "<li>$dbname</li>";
+          $html .= "<li>$dbname";
         }
+        $html .= $db_check{$dbname} ? ' <span style="color:red;font-weight:bold">(Y)</span>' 
+                                    : ' <span style="color:green;font-weight:bold">(N)</span>';
+        $html .= '</li>';
         $previous = $spname;
       }
     }
