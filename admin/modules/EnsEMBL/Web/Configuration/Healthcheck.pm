9a22c3b9e4f849c33f2b281f11db0216a004f3d6
Web Group <www-ens@sanger.ac.uk>
2010-11-16 10:05:59 +0000

    Config changes for new Healthcheck

--- admin/modules/EnsEMBL/Web/Configuration/Healthcheck.pm
+++ admin/modules/EnsEMBL/Web/Configuration/Healthcheck.pm
@@ -1,44 +1,63 @@
 package EnsEMBL::Web::Configuration::Healthcheck;
 
 use strict;
-
+use warnings;
 use base qw(EnsEMBL::Web::Configuration);
 
 sub set_default_action {
-  my $self = shift;
-  $self->{'_data'}{'default'} = 'Summary';
+  my $self                = shift;
+  $self->{_data}{default} = 'Summary';
 }
 
-sub modify_page_elements {
-  my $self = shift;
-  my $page = $self->page;
-  $page->remove_body_element('tabs');
-  $page->remove_body_element('summary');
-}
+sub global_context { return undef; }
+sub ajax_content   { return undef; }
+sub local_context  { return $_[0]->_local_context; }
+sub local_tools    { return $_[0]->_local_tools; }
+sub context_panel  { return undef; }
+sub content_panel  { return $_[0]->_content_panel;  }
+
+sub caption { return '';}
 
 sub populate_tree {
-  my $self         = shift;
-  my $species_defs = $self->object->species_defs;
-  my $release_id   = $species_defs->ENSEMBL_VERSION;
+  my $self = shift;
+  my $hub = $self->hub;
+  my $species_defs = $hub->species_defs;
 
-  $self->create_node( 'Summary', "Healthcheck Summary - Release $release_id",
+  $self->create_node( 'Summary', "Summary",
     [qw(
-      session_info    EnsEMBL::Admin::Component::Healthcheck::SessionInfo
       failure_summary EnsEMBL::Admin::Component::Healthcheck::FailureSummary
+      session_info    EnsEMBL::Admin::Component::Healthcheck::SessionInfo
     )],
     { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
   );
- $self->create_node( 'Details', "Healthcheck Details",
+ $self->create_node( 'DBType', "Details (by DB Type)",
     [qw(
-      results         EnsEMBL::Admin::Component::Healthcheck::DetailsSummary
-      species_failure EnsEMBL::Admin::Component::Healthcheck::FailureReports
+      database_report EnsEMBL::Admin::Component::Healthcheck::Details::DBType
     )],
     { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
   );
-  $self->create_node( 'Databases', "Databases",
+ $self->create_node( 'Species', "Details (by Species)",
     [qw(
-      databases EnsEMBL::Web::Component::Healthcheck::Databases
-    )], 
+      species_report EnsEMBL::Admin::Component::Healthcheck::Details::Species
+    )],
+    { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
+  );
+ $self->create_node( 'Testcase', "Details (by Testcase)",
+    [qw(
+      testcase_report EnsEMBL::Admin::Component::Healthcheck::Details::Testcase
+    )],
+    { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
+  );
+ $self->create_node( 'Database', "Details (by Database)",
+    [qw(
+      database_report EnsEMBL::Admin::Component::Healthcheck::Details::Database
+    )],
+    { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
+  );
+  $self->create_node( 'DBList', "Database List",
+    [qw(
+      directory EnsEMBL::Admin::Component::Healthcheck::DatabaseList
+    )],
     { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
   );
   $self->create_node( 'UserDirectory', "User Directory",
@@ -48,18 +67,18 @@ sub populate_tree {
     { 'availability' => 1, 'filters' => [qw(WebAdmin)]}
   );
 
-  $self->create_node( 'Annotation', '', [],
-      { 'command' => 'EnsEMBL::Admin::Command::Healthcheck::Interface::Annotation',
-         'no_menu_entry' => 1, 'filters' => [qw(WebAdmin)]}
-  );
-  $self->create_node( 'MultiAnnotate', '',
-    [qw(multi_annotate  EnsEMBL::Admin::Component::Healthcheck::MultiAnnotate)],
-      {'no_menu_entry' => 1, 'filters' => [qw(WebAdmin)]}
-  );
-  $self->create_node( 'MultiSave', '', [],
-      { 'command' => 'EnsEMBL::Admin::Command::Healthcheck::MultiSave',
+  $self->create_node( 'Annotation', '',
+    [qw(
+      annotation EnsEMBL::Admin::Component::Healthcheck::Annotation
+    )],
+    { 'no_menu_entry' => 1, 'filters' => [qw(WebAdmin)]}
+  );  
+
+  $self->create_node( 'AnnotationSave', '', [],
+      { 'command' => 'EnsEMBL::Admin::Command::Healthcheck::Annotation',
          'no_menu_entry' => 1, 'filters' => [qw(WebAdmin)]}
   );
 }
 
 1;
+                  
