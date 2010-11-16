dff88512ce42274170ea613b4399849b4f548cfa
Web Group <www-ens@sanger.ac.uk>
2010-11-16 10:29:36 +0000

    New object for Healthchecks

---
+++ admin/modules/EnsEMBL/Web/Object/Healthcheck.pm
@@ -0,0 +1,23 @@
+package EnsEMBL::Web::Object::Healthcheck;
+
+use strict;
+
+use EnsEMBL::Admin::Data::Rose::Report;
+use EnsEMBL::Admin::Data::Rose::Session;
+use EnsEMBL::ORM::Data::Rose::User;
+
+use base qw(EnsEMBL::Web::Object);
+
+sub new {
+  my $class = shift;
+  return bless { 'hub'  => @_ }, $class;
+}
+
+sub data_interface {
+  my ($self, $type) = @_;
+
+  my $module = sprintf 'EnsEMBL::%s::Data::Rose::%s', $type eq 'User' ? 'ORM' : 'Admin', $type;
+  return $self->{$type} ||= $module->new($self->{'hub'});
+}
+
+1;
