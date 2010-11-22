88ceef33ef3025437160b3b2610c322f013b1849
Web Group <www-ens@sanger.ac.uk>
2010-11-22 12:38:50 +0000

    fetch_by_group method added

--- orm/modules/EnsEMBL/ORM/Data/Rose/User.pm
+++ orm/modules/EnsEMBL/ORM/Data/Rose/User.pm
@@ -3,8 +3,6 @@ package EnsEMBL::ORM::Data::Rose::User;
 ### NAME: EnsEMBL::ORM::Data::Rose::User;
 ### Wrapper for one or more EnsEMBL::ORM::Rose::Object::User objects
 
-### DESCRIPTION:
-
 use strict;
 use warnings;
 no warnings qw(uninitialized);
@@ -15,6 +13,22 @@ use base qw(EnsEMBL::ORM::Data::Rose);
 sub set_primary_keys {
   my $self = shift;
   $self->{'_primary_keys'} = [qw(user_id)];
+}
+
+#data mining methods
+sub fetch_by_group {
+  my ($self, $group_id) = @_;
+  return [] unless $group_id;
+  
+  my $users = $self->manager_class->get_users(
+    with_objects    => 'membership',
+    query => [
+      'membership.webgroup_id'    => $group_id,
+    ],
+    sort_by        => 'name',
+  );
+  $self->data_objects(@$users);
+  return $users || [];
 }
 
 1;
