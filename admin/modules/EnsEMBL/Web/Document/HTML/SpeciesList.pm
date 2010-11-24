f95edfe127c5654880092d573cb13b1fc16e781f
Ensembl Account <ensembl@sanger.ac.uk>
2010-11-24 17:08:37 +0000

    correct destination for db list

--- admin/modules/EnsEMBL/Web/Document/HTML/SpeciesList.pm
+++ admin/modules/EnsEMBL/Web/Document/HTML/SpeciesList.pm
@@ -16,7 +16,7 @@ sub render {
 <h2>Healthchecks</h2>
 <ul>
   <li><a href="/Healthcheck/Summary">Healthcheck summary</a></li>
-  <li><a href="/Healthcheck/Databases">List of databases</a></li>
+  <li><a href="/Healthcheck/DBList">List of databases</a></li>
 </ul>
 );
   }
