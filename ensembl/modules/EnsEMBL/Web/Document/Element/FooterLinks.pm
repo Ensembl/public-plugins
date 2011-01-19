f102f2d9f441f213711fd2a0f03c555d4c5fa467
Anne Parker <ap5@sanger.ac.uk>
2011-01-19 13:31:33 +0000

    Moved template elements into correct directory - again!

--- ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -1,13 +1,13 @@
-package EnsEMBL::Web::Document::HTML::FooterLinks;
+package EnsEMBL::Web::Document::Element::FooterLinks;
 
 ### Replacement footer links for www.ensembl.org
 
 use strict;
 
-use base qw(EnsEMBL::Web::Document::HTML);
+use base qw(EnsEMBL::Web::Document::Element);
 
-sub render {
-  $_[0]->print( qq(
+sub content {
+  return qq(
     <div class="twocol-right right unpadded print_hide">
       <a href="http://www.ensembl.org/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
       <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a> | 
@@ -15,7 +15,7 @@ sub render {
     </div>
     <div class="twocol-right right unpadded screen_hide_block">
       helpdesk\@ensembl.org
-    </div>) 
+    </div>
   );
 }
 
