f57b1ca89129bb99055a019987e17f9cabae7cb6
Harpreet Riat <hr5@sanger.ac.uk>
2011-02-03 12:02:28 +0000

    admin plugin for Ensembl JS base

---
+++ admin/htdocs/components/01_Admin_Ensembl.js
@@ -0,0 +1,18 @@
+Ensembl.extend({
+  initialize: function () {
+    this.base();
+
+    //JS for healthcheck pages
+    this.HCManager.initialize();
+
+    //JS for changelog (TinyMCE)
+    $('.tinymce').tinymce({
+      script_url: '/tiny_mce/jscripts/tiny_mce/tiny_mce.js',
+      theme: "advanced",
+      theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect",
+      theme_advanced_buttons2: "cut,copy,paste,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,code,|,removeformat,visualaid,|,sub,sup,|,charmap",
+      theme_advanced_buttons3: "",
+      theme_advanced_toolbar_location: "top"
+    });
+  }
+});
