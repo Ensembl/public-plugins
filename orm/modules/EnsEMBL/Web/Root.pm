737d7cd0cb6f3b690a3c86dab79c649df4f1bcd8
Harpreet Riat <hr5@sanger.ac.uk>
2011-02-03 15:05:42 +0000

    use strict

--- orm/modules/EnsEMBL/Web/Root.pm
+++ orm/modules/EnsEMBL/Web/Root.pm
@@ -2,6 +2,8 @@ package EnsEMBL::Web::Root;
 
 ### Extends E::W::Root by adding a method needed by CRUD Components, Commands, etc
 
+use strict;
+
 sub get_frontend_config {
 ### Instantiates the config module for the Ensembl CRUD frontend
   my $self = shift;
