95b91740b527f3d2d690499ec8633fee34b41e74
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-08 13:56:11 +0000

    web object generation made similar to as in web code

--- admin/modules/EnsEMBL/Web/Factory/Healthcheck.pm
+++ admin/modules/EnsEMBL/Web/Factory/Healthcheck.pm
@@ -2,17 +2,16 @@ package EnsEMBL::Web::Factory::Healthcheck;
 
 ### NAME: EnsEMBL::Web::Factory::Healthcheck
 
-### STATUS: Under Development
+### STATUS: Stable
 
 use strict;
 
 use base qw(EnsEMBL::Web::Factory);
-use EnsEMBL::Web::Object::Healthcheck;
 
 sub createObjects {
   my $self = shift;
-
-  $self->DataObjects(EnsEMBL::Web::Object::Healthcheck->new($self->hub));
+  
+  $self->DataObjects($self->new_object('Healthcheck', undef, $self->__data));
 }
 
 1;
