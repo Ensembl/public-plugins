ed32f99fe1ff5c40f2fef667c21a2005dffcfb8d
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-23 14:05:00 +0000

    added some useful methods to rose object

--- orm/modules/EnsEMBL/ORM/Rose/Object.pm
+++ orm/modules/EnsEMBL/ORM/Rose/Object.pm
@@ -6,10 +6,6 @@ package EnsEMBL::ORM::Rose::Object;
 ### DESCRIPTION:
 ### This module's children provide access to non-genomic databases, using the Rose::DB::Object suite
 
-### Tip: When debugging, change the value of the error_mode (below) 
-### from 'return' to 'carp'/'cluck'/'confess'/'croak' to produce
-### the desired Carp behaviour in your data objects
-
 use strict;
 use warnings;
 
@@ -21,21 +17,51 @@ use base qw(Rose::DB::Object);
 use constant {                        ## Override in child class
   ROSE_DB_NAME        => undef,       ## Name of the database connection as registered with Rose::DB
   TITLE_COLUMN        => '',          ## Column which contains the 'title' of the record (if any)
-  INACTIVE_FLAG       => '',          ## Column that indicates that the row is to be considered deleted if set value as in INACTIVE_FLAG_VALUE
+  INACTIVE_FLAG       => '',          ## Column that indicates that the row is to be considered deleted if its value is set as INACTIVE_FLAG_VALUE
   INACTIVE_FLAG_VALUE => '',          ## Value of the INACTIVE_FLAG
-  
+
   LINKED_USERS_KEY    => '__linked_users'
 };
+
+__PACKAGE__->meta->error_mode('return'); ## When debugging, change from 'return' to 'carp'/'cluck'/'confess'/'croak' to produce the desired Carp behaviour
 
 sub meta_setup {
   ## Wrapper around meta->setup method
   return shift->meta->setup(@_);
 }
 
+sub primary_key {
+  ## Returns the name of the primary key (no composite keys)
+  ## @return String
+  return shift->meta->primary_key_column_names->[0];
+}
+
 sub init_db {
-  ## Creats connection to database
+  ## Method called by Rose to creat connection to database
   ## Override ROSE_DB_NAME constant instead of this method
   EnsEMBL::ORM::Rose::DbConnection->new(shift->ROSE_DB_NAME);
+}
+
+sub get_title {
+  ## Returns the name of the object as defined by TITLE_COLUMN
+  ## @return String
+  my $self  = shift;
+  my $title = $self->TITLE_COLUMN || $self->primary_key;
+  
+  return $self->$title;
+}
+
+sub get_primary_key_value {
+  ## Gets the values of the primary key column
+  my $self = shift;
+  my $key  = $self->primary_key;
+  return $key ? $self->$key || undef : undef;
+}
+
+sub is_trackable {
+  ## Returns true if Object contains the trackable fields (created_by, modified_by etc)
+  ## @return 0/1 accordingly
+  return 0;
 }
 
 sub get_user {
@@ -65,7 +91,5 @@ sub set_user {
   $self->$column_name($user ? $user->user_id : 0);
   $self->{$self->LINKED_USERS_KEY}{$column_name} = $user;
 }
-
-__PACKAGE__->meta->error_mode('return');
 
 1;
