f2482f47f5abec14a877c75530d40e08b8ffba17
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-23 13:42:27 +0000

    do just as much as required

--- admin/modules/EnsEMBL/Admin/Command/Healthcheck/Annotation.pm
+++ admin/modules/EnsEMBL/Admin/Command/Healthcheck/Annotation.pm
@@ -7,56 +7,23 @@ use base qw(EnsEMBL::Web::Command);
 sub process {
   my $self = shift;
   
-  my $report_db_interface = $self->object->data_interface('Report');
-  my $hub = $self->hub;
-
-  my $report_id = $hub->param('rid'); 
-  $report_id    = [ split ',', $report_id ], #in case multiple reports
-  my $reports   = $report_db_interface->fetch_by_id($report_id, 1); #fetch_report_id(ArrayRef $report_id, Boolean $failed_only)
-
-  unless (scalar $reports) {
-    warn 'No report found to annotate. (Invalid report id)';
-    return;
-  }
-  
-  my $current_user_id = $hub->user->user_id;
-  my ($sec, $min, $hour, $day, $mon, $year) = localtime();
-  my $now = (1900+$year).'-'.sprintf('%02d', $mon+1).'-'.sprintf('%02d', $day).' '
-              .sprintf('%02d', $hour).':'.sprintf('%02d', $min).':'.sprintf('%02d', $sec);
+  my $hub     = $self->hub;
+  my $object  = $self->object;
+  my $reports = $object->rose_objects;
+  my $anchor;
 
   for (@$reports) {
-    
-    my $annotation = undef;
-    if ($_->annotation) {
-      #edit existing annotation
-      $annotation = $_->annotation;
-      $annotation->modified_by($current_user_id);
-      $annotation->modified_at($now);
-    }
-    else {
-      #add new if no annotation found linked to the report
-      $annotation = $report_db_interface->create_empty_object('EnsEMBL::Admin::Rose::Object::Annotation'); 
-      $annotation->created_by($current_user_id);
-      $annotation->created_at($now);
-      $annotation->report_id($_->report_id); #don't forget to link it to the report
-    }
+
+    my $annotation = $_->annotation || $_->annotation($object->rose_manager('Annotation')->create_empty_object);
 
     $annotation->comment($hub->param('comment'));
     $annotation->action($hub->param('action'));
-    $annotation->save;
+    $annotation->save('user' => $hub->user);
+    
+    $anchor ||= $_->database_name;
+  }
 
-  }
-  
-  my $urn   = '/Healthcheck/Summary'; 
-  my $vars  = {};
-  if ($hub->param('referrer')) {
-    $urn      = [split (/\?/, $hub->param('referrer'))];
-    my $uri   = $urn->[1] ? [split /\&|\;/, $urn->[1]] : [];
-    for (@$uri) {
-      $vars->{ $1 } = $2 if $_ =~ /([^\=]+)\=([^\;\&]+)/;
-    }
-  }
-  $self->ajax_redirect($urn->[0], $vars, $reports->[0]->database_name || '');
+  $self->ajax_redirect($hub->param('referrer') || $hub->url({'action' => 'Summary'}), {}, $anchor || '');
 }
 
 1;
