23766f85a655d8587b1639bb2946ac05cc567492
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-23 14:03:51 +0000

    automatically add user and time to trackable fields while saving

--- orm/modules/EnsEMBL/ORM/Rose/Object/Trackable.pm
+++ orm/modules/EnsEMBL/ORM/Rose/Object/Trackable.pm
@@ -8,6 +8,8 @@ package EnsEMBL::ORM::Rose::Object::Trackable;
 use strict;
 use warnings;
 
+use Rose::DateTime::Util qw(parse_date);
+
 use base qw(EnsEMBL::ORM::Rose::Object);
 
 sub meta_setup {
@@ -53,6 +55,27 @@ sub meta_setup {
   }
   
   return $class->meta->setup(%params);
+}
+
+sub save {
+  ## @overrides
+  ## Adds the trackable info to the record before saving it, iff user provided in the hash argument
+  ## @params Hash with an extra key 'user' containing current user (Rose object) along with keys as accepted by Rose::DB::Object->save
+  my ($self, %params) = @_;
+
+  if (my $user = delete $params{'user'}) {
+    my $key = $self->get_primary_key_value ? 'modified' : 'created';
+    my $by  = "${key}_by";
+    my $at  = "${key}_at";
+    $self->$by($user->user_id);
+    $self->$at(parse_date('now'));
+  }
+  return $self->SUPER::save(%params);
+}
+
+sub is_trackable {
+  ## @overrides
+  return 1;
 }
 
 1;
