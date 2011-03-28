c5ae9db1a5cda4977d7f94e507e7ca82cfca596e
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-28 10:24:36 +0000

    modularised into smaller methods to make it easy to override or extend in child classes

--- orm/modules/EnsEMBL/ORM/Component/DbFrontend/Display.pm
+++ orm/modules/EnsEMBL/ORM/Component/DbFrontend/Display.pm
@@ -26,52 +26,60 @@ sub content_tree {
   my $content = $self->dom->create_element('div', {'class' => $object->content_css});
   my $page    = $object->get_page_number;
   my $links   = defined $page ? $content->append_child($self->content_pagination_tree(scalar @$records)) : undef;
-
-  for my $record (@$records) {
   
-    my $record_div  = $self->dom->create_element('div', {'class' => 'dbf-record'});
-    my $primary_key = $record->get_primary_key_value;
-    $record_div->set_flag('primary_key', $primary_key);
-
-    my @bg = qw(bg1 bg2);
-    my $fields  = $object->show_fields;
-
-    while (my $field_name = shift @$fields) {
-    
-      my $label = shift @$fields;
-      $label    = exists $label->{'label'} ? $label->{'label'} : '';
-      my $value = $record->$field_name;
-
-      my $row = $record_div->append_child($self->dom->create_element('div', {'class' => "dbf-row $bg[0]"}));
-      $row->set_flag($field_name);
-      $row->append_children(
-        $self->dom->create_element('div', {
-          'class'       => 'dbf-row-left',
-          'inner_HTML'  => $label
-        }),
-        $self->dom->create_element('div', {
-          'class'       => 'dbf-row-right',
-          'inner_HTML'  =>  $self->_display_field_value($value) || ''
-        })
-      );
-      @bg = reverse @bg;
-    }
-    $record_div->append_child($self->dom->create_element('div', {
-      'class'       => "dbf-row-buttons",
-      'inner_HTML'  => sprintf('<a href="%s">Edit</a>%s', $hub->url({'action' => 'Edit', 'id' => $primary_key}), $object->permit_delete ? sprintf('<a href="%s">Delete</a>', $hub->url({'action' => 'Confirm', 'id' => $primary_key})) : '')
-    }));
-    $content->append_child($record_div);
-  }
+  $content->append_child($self->record_tree($_)) for @$records;
 
   $content->append_child($links->clone_node(1)) if $links; ## bottom pagination
   
   return $content;
 }
 
-sub _display_field_value {
+sub record_tree {
+  ## Generates a DOM tree for each database record
+  ## Override this one in the child class and do the DOM manipulation on the DOM tree if required
+  ## Flags are set on required HTML elements for 'selection and manipulation' purposes in child classes (get_nodes_by_flag)
+  my ($self, $record) = @_;
+  my $object = $self->object;
+  
+  my $record_div  = $self->dom->create_element('div', {'class' => 'dbf-record'});
+  my $primary_key = $record->get_primary_key_value;
+  $record_div->set_flag('primary_key', $primary_key);
+
+  my @bg = qw(bg1 bg2);
+  my $fields  = $object->show_fields;
+
+  while (my $field_name = shift @$fields) {
+
+    my $label = shift @$fields;
+    $label    = exists $label->{'label'} ? $label->{'label'} : '';
+    my $value = $record->$field_name;
+
+    my $row = $record_div->append_child($self->dom->create_element('div', {'class' => "dbf-row $bg[0]"}));
+    $row->set_flag($field_name);
+    $row->append_children(
+      $self->dom->create_element('div', {
+        'class'       => 'dbf-row-left',
+        'inner_HTML'  => $label
+      }),
+      $self->dom->create_element('div', {
+        'class'       => 'dbf-row-right',
+        'inner_HTML'  =>  $self->display_field_value($value) || ''
+      })
+    );
+    @bg = reverse @bg;
+  }
+  $record_div->append_child($self->dom->create_element('div', {
+    'class'       => "dbf-row-buttons",
+    'inner_HTML'  => sprintf('<a href="%s">Edit</a>%s', $self->hub->url({'action' => 'Edit', 'id' => $primary_key}), $object->permit_delete ? sprintf('<a href="%s">Delete</a>', $self->hub->url({'action' => 'Confirm', 'id' => $primary_key})) : '')
+  }));
+  return $record_div;
+}
+
+sub display_field_value {
   ## Converts the field value into displayable form
-  ## Value, as returned by the rose's method call, can be a string, rose object or an arrayref of rose objects ;)
-  my ($self, $value) = @_;
+  ## @param Value, as returned by the rose's method call, can be a string, rose object or an arrayref of rose objects ;)
+  ## @param delimiter, to be used in join if multiple values
+  my ($self, $value, $delimiter) = @_;
 
   ## if nothing
   return '' unless defined $value;
@@ -82,8 +90,8 @@ sub _display_field_value {
   ## if it's an arrayref
   if (ref $value eq 'ARRAY') {
     my @return;
-    push @return, $self->_display_field_value($_) for @$value;
-    return @return ? sprintf('<ul><li>%s</li></ul>', join('</li><li>', @return)) : '';
+    push @return, $self->display_field_value($_) for @$value;
+    return @return ? sprintf($delimiter ? '%s' : '<ul><li>%s</li></ul>', join($delimiter || '</li><li>', @return)) : '';
   }
 
   ## if it's DateTime (rose returns DateTime for datetime mysql type)
