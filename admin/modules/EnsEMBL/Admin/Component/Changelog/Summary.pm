76b39f50c5f85c5034d5d827aa31f26d956b9390
Harpreet Riat <hr5@sanger.ac.uk>
2011-03-30 13:18:39 +0000

    hide some info if user is not logged in

--- admin/modules/EnsEMBL/Admin/Component/Changelog/Summary.pm
+++ admin/modules/EnsEMBL/Admin/Component/Changelog/Summary.pm
@@ -22,7 +22,6 @@ sub content_tree {
     $toc->append_child($dom->create_element('p', {'inner_HTML' => qq(<a href="#team_$team">$team</a>)}));
   }
 
-  $_->remove for @{$content->get_nodes_by_flag('pagination_links')};
   return $content;
 }
 
@@ -37,6 +36,16 @@ sub record_tree {
 
   my $team = $record->team;
   $record_div->set_flag('team_name', $self->{'__previous_team'} = $team) if !exists $self->{'__previous_team'} || $self->{'__previous_team'} ne $team;
+  
+  my @for_logged_in_user = ();
+  if ($self->hub->user) {
+    @for_logged_in_user = (
+      $dom->create_element('span', {'inner_HTML' => 'Declared by:', 'class' => 'cl-field-title'}),
+      $dom->create_element('span', {'inner_HTML' => $self->display_field_value($record->created_by_user), 'class' => 'cl-field-value'}),
+      $dom->create_element('span', {'inner_HTML' => 'Last updated:', 'class' => 'cl-field-title'}),
+      $dom->create_element('span', {'inner_HTML' => $self->display_field_value($record->modified_at ? $record->modified_at : $record->created_at), 'class' => 'cl-field-value'}),
+    );
+  }
 
   $record_div->append_children(
     $dom->create_element('h3',   {'inner_HTML' => $record->title }),
@@ -45,20 +54,17 @@ sub record_tree {
     $dom->create_element('span', {'inner_HTML' => $self->display_field_value((my $a = $record->species), ', ') || 'All Species', 'class' => 'cl-field-value'}),
     $dom->create_element('span', {'inner_HTML' => 'Status:', 'class' => 'cl-field-title'}),
     $dom->create_element('span', {'inner_HTML' => $self->display_field_value($record->status), 'class' => 'cl-field-value cl-fv-'.$record->status}),
-    $dom->create_element('span', {'inner_HTML' => 'Declared by:', 'class' => 'cl-field-title'}),
-    $dom->create_element('span', {'inner_HTML' => $self->display_field_value($record->created_by_user), 'class' => 'cl-field-value'}),
-    $dom->create_element('span', {'inner_HTML' => 'Last updated:', 'class' => 'cl-field-title'}),
-    $dom->create_element('span', {'inner_HTML' => $self->display_field_value($record->modified_at ? $record->modified_at : $record->created_at), 'class' => 'cl-field-value'}),
+    @for_logged_in_user
   );
 
   $record_div->append_child($dom->create_element('div', {
     'class'       => "dbf-row-buttons",
-    'inner_HTML'  => sprintf(
+    'inner_HTML'  => scalar @for_logged_in_user ? sprintf(
       '<a href="%s">View</a><a href="%s">Edit</a>%s',
       $self->hub->url({'action' => 'Display', 'id' => $primary_key}),
       $self->hub->url({'action' => 'Edit', 'id' => $primary_key}),
       $object->permit_delete ? sprintf('<a href="%s">Delete</a>', $self->hub->url({'action' => 'Confirm', 'id' => $primary_key})) : ''
-    )
+    ) : ''
   }));
   return $record_div;
 
