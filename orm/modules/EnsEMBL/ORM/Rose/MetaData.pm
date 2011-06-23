4bb93d89002201b45bc535b1abd77f3902560f14
Harpreet Riat <hr5@sanger.ac.uk>
2011-06-23 16:36:53 +0000

    new meta class for rose objects

---
+++ orm/modules/EnsEMBL/ORM/Rose/MetaData.pm
@@ -0,0 +1,10 @@
+package EnsEMBL::ORM::Rose::MetaData;
+
+## Name: EnsEMBL::ORM::Rose::MetaData
+## MetaData class for all Rose Objects
+
+use strict;
+
+use base qw(Rose::DB::Object::Metadata);
+
+1;
