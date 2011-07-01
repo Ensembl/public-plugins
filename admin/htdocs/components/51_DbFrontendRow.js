780fd70a05b0c758f12aa12dd976383dcd252f5d
Harpreet Riat <hr5@sanger.ac.uk>
2011-07-01 12:34:11 +0000

    show the hidden heading if new team added

--- admin/htdocs/components/51_DbFrontendRow.js
+++ admin/htdocs/components/51_DbFrontendRow.js
@@ -1,11 +1,13 @@
 /* overrides the default afterResponse method of DbFrontendRow to sort the rows in case of changelog */
+// TODO - hide any heading and link if record moved to another team
 
 Ensembl.Panel.DbFrontendRow.prototype.afterResponse = function(success) {
-  if (success && window.location.href.match('Changelog')) {
+  if (success && window.location.pathname.match('Changelog')) {
 
     var team = $('input._cl_team_name', this.target).val();
     if ($(this.el).prev('._cl_team_heading').attr('id') != 'team_' + team) {
-      $('#team_' + team).after(this.target, this.target.next());
+      $('#team_' + team).removeClass('hidden').after(this.target, this.target.next());
+      $('#_cl_link_' + team).removeClass('hidden');
     }
   }
   this.scrollIn({margin: 5});
