e61cbdb5043a07244fca05eb4f4d07e0a05b4cef
Harpreet Riat <hr5@sanger.ac.uk>
2011-07-04 09:50:57 +0000

    dont display 'Add New' button if user is not logged in; some minor code formatting

--- admin/modules/EnsEMBL/Admin/Component/Changelog/Summary.pm
+++ admin/modules/EnsEMBL/Admin/Component/Changelog/Summary.pm
@@ -13,7 +13,7 @@ sub content_tree {
   my $content = $self->SUPER::content_tree;
   my $toc     = $content->prepend_child('div', {'class' => 'cl-toc'});
 
-  $_->remove for @{$content->get_nodes_by_flag('pagination_div')};
+  $_->remove for @{$content->get_nodes_by_flag(['pagination_div', $self->hub->user ? () : 'add_new_button'])};
 
   $content->prepend_child('h1', {'inner_HTML' => sprintf('Changelog for release %d', $self->object->requested_release)});
 
@@ -42,22 +42,13 @@ sub record_tree {
   ## @overrides
   my ($self, $record) = @_;
   my $object = $self->object;
+  my $hub    = $self->hub;
 
   my $record_div  = $self->dom->create_element('div');
   my $primary_key = $record->get_primary_key_value;
 
   my $team = $record->team;
   $record_div->set_flag('team_name', $self->{'__previous_team'} = $team) unless exists $self->{'__previous_team'} && $self->{'__previous_team'} eq $team;
-  
-  my @for_logged_in_user = ();
-  if ($self->hub->user) {
-    @for_logged_in_user = (
-      {'node_name' => 'span', 'inner_HTML' => 'Declared by:', 'class' => 'cl-field-title'},
-      {'node_name' => 'span', 'inner_HTML' => $self->display_field_value($record->created_by_user), 'class' => 'cl-field-value'},
-      {'node_name' => 'span', 'inner_HTML' => 'Last updated:', 'class' => 'cl-field-title'},
-      {'node_name' => 'span', 'inner_HTML' => $self->display_field_value($record->modified_at ? $record->modified_at : $record->created_at), 'class' => 'cl-field-value'},
-    );
-  }
 
   $record_div->append_children(
     {'node_name' => 'input','class' => '_cl_team_name',   'value'      => $team, 'type' => 'hidden'},
@@ -69,21 +60,21 @@ sub record_tree {
     {'node_name' => 'span', 'class' => 'cl-field-value',  'inner_HTML' => $self->display_field_value((my $a = $record->species), {'delimiter' => ', '}) || 'All Species'},
     {'node_name' => 'span', 'class' => 'cl-field-title',  'inner_HTML' => 'Status:'},
     {'node_name' => 'span', 'class' => 'cl-field-value cl-fv-'.$record->status, 'inner_HTML' => $self->display_field_value($record->status)},
-    @for_logged_in_user
+    $hub->user ? (
+    {'node_name' => 'span', 'inner_HTML' => 'Declared by:', 'class' => 'cl-field-title'},
+    {'node_name' => 'span', 'inner_HTML' => $self->display_field_value($record->created_by_user), 'class' => 'cl-field-value'},
+    {'node_name' => 'span', 'inner_HTML' => 'Last updated:', 'class' => 'cl-field-title'},
+    {'node_name' => 'span', 'inner_HTML' => $self->display_field_value($record->modified_at ? $record->modified_at : $record->created_at), 'class' => 'cl-field-value'},
+    {'node_name' => 'div',  'inner_HTML' => sprintf(
+      '<a href="%s">View</a><a href="%s" class="%s">Edit</a>%s',
+      $hub->url({'action' => 'Display', 'id' => $primary_key}),
+      $hub->url({'action' => 'Edit', 'id' => $primary_key}),
+      $self->_JS_CLASS_EDIT_BUTTON,
+      $object->permit_delete ? sprintf('<a class="%s" href="%s">Delete</a>', $self->_JS_CLASS_EDIT_BUTTON, $hub->url({'action' => 'Confirm', 'id' => $primary_key})) : ''
+    )}) : ()
   );
 
-  $record_div->append_child('div', {
-    'class'       => "dbf-row-buttons",
-    'inner_HTML'  => scalar @for_logged_in_user ? sprintf(
-      '<a href="%s">View</a><a href="%s" class="%s">Edit</a>%s',
-      $self->hub->url({'action' => 'Display', 'id' => $primary_key}),
-      $self->hub->url({'action' => 'Edit', 'id' => $primary_key}),
-      $self->_JS_CLASS_EDIT_BUTTON,
-      $object->permit_delete ? sprintf('<a class="%s" href="%s">Delete</a>', $self->_JS_CLASS_EDIT_BUTTON, $self->hub->url({'action' => 'Confirm', 'id' => $primary_key})) : ''
-    ) : ''
-  });
   return $record_div;
-
 }
 
 1;
