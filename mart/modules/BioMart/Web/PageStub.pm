f7b3a45496f1d7a5cd589c40259c424691f29150
Simon Brent <sb23@sanger.ac.uk>
2011-07-04 14:05:58 +0000

    set doc type in a place it won't cause firefox to leave massive spaces all over the place

--- mart/modules/BioMart/Web/PageStub.pm
+++ mart/modules/BioMart/Web/PageStub.pm
@@ -27,7 +27,7 @@ sub new {
     $page->include_navigation(0);
     $page->initialize;
     $page->remove_body_element('breadcrumbs');
-    $page->set_doc_type('XHTML', '1.0 Trans');
+    $page->set_doc_type('none', 'none');
     
     my $elements = $page->elements;
     my @order    = map $_->[0], @{$page->head_order}, @{$page->body_order};
@@ -102,7 +102,8 @@ sub end {
 sub render_start {
   my $self = shift;
   my $page = $self->{'page'};
-
+  
+  $page->set_doc_type('XHTML',  '1.0 Trans');
   $page->add_body_attr('id',    'ensembl-webpage');
   $page->add_body_attr('class', 'mac')                               if $ENV{'HTTP_USER_AGENT'} =~ /Macintosh/;
   $page->add_body_attr('class', "ie ie$2" . ($2 < 8 ? ' ie67' : '')) if $ENV{'HTTP_USER_AGENT'} =~ /MSIE( (\d))?/ && $2 < 9; # Assumes that IE 9 will have support for everything we need. LOL.
