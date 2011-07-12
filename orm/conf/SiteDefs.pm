f6e307d47a0ea1c4d7d9d5932fc53d79b0bd3c40
Harpreet Riat <hr5@sanger.ac.uk>
2011-07-12 15:57:40 +0000

    some readme stuff added

--- orm/conf/SiteDefs.pm
+++ orm/conf/SiteDefs.pm
@@ -1,7 +1,7 @@
 package EnsEMBL::ORM::SiteDefs;
 
 ### This module adds a new variable ROSE_DB_DATABASES, a hashref, to site defs
-### View DbConnection.pm for more info
+### View modules/EnsEMBL/ORM/Rose/DbConnection.pm for more info
 
 use strict;
 
