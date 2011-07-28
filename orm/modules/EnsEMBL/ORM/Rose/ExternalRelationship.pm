0748927d15792cfa4d881082262f8ccb5f4f9943
Harpreet Riat <hr5@sanger.ac.uk>
2011-07-28 10:50:21 +0000

    new class for representing relationships among rose objects residing on different db server hosts

---
+++ orm/modules/EnsEMBL/ORM/Rose/ExternalRelationship.pm
@@ -0,0 +1,31 @@
+package EnsEMBL::ORM::Rose::ExternalRelationship;
+
+## Name: EnsEMBL::ORM::Rose::ExternalRelationship
+## A class for defining relationship of rose object to another rose object while the corrosponding db tables being on differnt hosts
+## The class contains some essential methods as in Rose::DB::Object::Metadata::Relationship
+## ExternalRelationship DOES NOT allow many to many relationship
+
+use strict;
+
+use base qw(EnsEMBL::Web::Root);
+
+sub new {
+  ## @constructor
+  ## @param Hashref with keys:
+  ##  - name        Relationship name
+  ##  - type        Type of relationship - 'one to one' etc
+  ##  - column_map  Hashref {internal_column => external_column} defining the link between relationship
+  ##  - class       Class name of the object mapped
+  my ($class, $params) = @_;
+  
+  $class->dynamic_use($params->{'class'}) or warn sprintf("Error: External relationship mapping class '%s' could not be found.", $params->{'class'}) and return;
+  return bless $params, $class;
+}
+
+sub class       { return shift->{'class'}; }
+sub name        { return shift->{'name'}; }
+sub type        { return shift->{'type'}; }
+sub column_map  { return shift->{'column_map'}; }
+sub is_singular { return shift->type =~ /to one$/ ? 1 : undef; }
+
+1;
