a0f6512e46f83c2acaa6b1941d193a172706c83f
Harpreet Riat <hr5@sanger.ac.uk>
2011-07-28 10:17:28 +0000

    extra functionality moved to new metadata class EnsEMBL::ORM::Rose::MetaData::Trackable

--- orm/modules/EnsEMBL/ORM/Rose/Object/Trackable.pm
+++ orm/modules/EnsEMBL/ORM/Rose/Object/Trackable.pm
@@ -9,52 +9,12 @@ use strict;
 use warnings;
 
 use Rose::DateTime::Util qw(parse_date);
+use EnsEMBL::ORM::Rose::MetaData::Trackable;
 
 use base qw(EnsEMBL::ORM::Rose::Object);
 
-sub meta_setup {
-  ## Wrapper around meta's setup method to automatically add trackable fields (and relationships if applicable).
-  ## If object's table is not residing in the user db, methods camouflaged as relationship methods are created
-  ## Use __PACKAGE__->meta_setup instead of __PACKAGE__->meta->setup to setup schema in child classes
-  ## @overrides
-  ## @param Same hash as for meta->setup, but with an extra flag at key 'user_db', kept on if user table is on same database as the object
-  my ($class, %params) = @_;
-  
-  push @{$params{'columns'}}, (
-    'created_by',  { type => 'integer'  },
-    'created_at',  { type => 'datetime' },
-    'modified_by', { type => 'integer'  },
-    'modified_at', { type => 'datetime' }
-  );
-
-  # make actual relationships if same db as user table, otherwise camouflage methods as relationship method
-  if (delete $params{'user_db'}) {
-    push @{$params{'relationships'}}, (
-      created_by_user => {
-        'type'        => 'one to one',
-        'class'       => 'EnsEMBL::ORM::Rose::Object::User',
-        'column_map'  => {'created_by' => 'user_id'}
-      },
-      modified_by_user => {
-        'type'        => 'one to one',
-        'class'       => 'EnsEMBL::ORM::Rose::Object::User',
-        'column_map'  => {'modified_by' => 'user_id'}
-      }  
-    );
-  }
-  else {
-    no strict 'refs';
-    foreach my $subroutine (qw(created_by_user modified_by_user)) {
-      *{"${class}::$subroutine"} = sub {
-        my $this    = shift;
-        (my $column = $subroutine) =~ s/_user$//;
-        $this->set_user($column, shift) if @_;
-        return $this->get_user($column);
-      };
-    }
-  }
-  
-  return $class->meta->setup(%params);
+sub meta_class {
+  return 'EnsEMBL::ORM::Rose::MetaData::Trackable';
 }
 
 sub save {
@@ -67,15 +27,11 @@ sub save {
     my $key = $self->get_primary_key_value ? 'modified' : 'created';
     my $by  = "${key}_by";
     my $at  = "${key}_at";
+
     $self->$by($user->user_id);
     $self->$at(parse_date('now'));
   }
   return $self->SUPER::save(%params);
-}
-
-sub is_trackable {
-  ## @overrides
-  return 1;
 }
 
 1;
