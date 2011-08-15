63cf5cb65b6919ccba32a2bd35ef11d8b4e8f832
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-15 15:32:52 +0000

    datastructure not restricted to textareas anymore

--- orm/htdocs/components/01_ORM_Ensembl.js
+++ orm/htdocs/components/01_ORM_Ensembl.js
@@ -2,7 +2,7 @@ Ensembl.extend({
   initialize: function () {
     this.base();
 
-    $('textarea._datastructure').livequery(function() {
+    $('._datastructure').livequery(function() {
       $(this).datastructure();
     });
   }
