05d6913178c7bbdef2f20d5d1f71bb5d23cd5546
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-15 15:56:07 +0000

    is_datastructure and is_column methods added

--- orm/modules/EnsEMBL/ORM/Rose/Field.pm
+++ orm/modules/EnsEMBL/ORM/Rose/Field.pm
@@ -11,17 +11,19 @@ use strict;
 
 use Rose::DateTime::Util qw(format_date parse_date);
 
-sub name        { return shift->{'_name'};    }
-sub label       { return shift->{'_label'};   }
-sub field_type  { return shift->{'_f_type'};  }
-sub value       { return shift->{'_value'};   }
-sub caption     { return shift->{'_caption'}; }
-sub lookup      { return shift->{'_lookup'};  }
-sub selected    { return shift->{'_values'};  }
-sub multiple    { return shift->{'_multi'};   }
-sub value_type  { return shift->{'_v_type'};  }
-sub is_null     { return shift->{'_is_null'}; }
-sub extras      { return shift->{'_extras'};  }
+sub name              { return shift->{'_name'};    }
+sub label             { return shift->{'_label'};   }
+sub field_type        { return shift->{'_f_type'};  }
+sub value             { return shift->{'_value'};   }
+sub caption           { return shift->{'_caption'}; }
+sub lookup            { return shift->{'_lookup'};  }
+sub selected          { return shift->{'_values'};  }
+sub multiple          { return shift->{'_multi'};   }
+sub value_type        { return shift->{'_v_type'};  }
+sub is_null           { return shift->{'_is_null'}; }
+sub extras            { return shift->{'_extras'};  }
+sub is_datastructure  { return shift->{'_is_ds'};   }
+sub is_column         { return shift->{'_is_col'};  }
 
 sub new {
   my ($class, $params) = @_;
@@ -40,11 +42,13 @@ sub new {
     '_label'   => delete $params->{'label'} || ucfirst $label,
     '_value'   => delete $params->{'value'},
     '_lookup'  => delete $params->{'lookup'} || {},
+    '_is_ds'   => delete $params->{'is_datastructure'} || 0,
     '_v_type'  => delete $params->{'value_type'},
     '_f_type'  => delete $params->{'type'} || 'noedit',
     '_values'  => delete $params->{'selected'} || {},
     '_multi'   => delete $params->{'multiple'} || 0,
     '_is_null' => delete $params->{'is_null'}  || 0,
+    '_is_col'  => delete $params->{'is_column'}  || 0,
     '_caption' => $caption,
     '_extras'  => $params
   }, $class;
