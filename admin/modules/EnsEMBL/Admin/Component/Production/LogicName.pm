7ac4eb8a27603528896d8a56e0996feac963e1ed
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-15 16:09:54 +0000

    content_tree method overridden

--- admin/modules/EnsEMBL/Admin/Component/Production/LogicName.pm
+++ admin/modules/EnsEMBL/Admin/Component/Production/LogicName.pm
@@ -8,7 +8,19 @@ sub caption {
   return '';
 }
 
+sub content_tree {
+  ## @overrides
+  my $self    = shift;
+
+  my $tree = $self->SUPER::content_tree;
+
+  $_->inner_HTML('No analysis web data found in the database for the applied filter.') for @{$tree->get_nodes_by_flag($self->_FLAG_NO_CONTENT)};
+
+  return $tree;
+}
+
 sub record_tree {
+  ## @overrides
   my $record_div = shift->SUPER::record_tree(@_);
 
   $_->inner_HTML($_->inner_HTML ? 'Yes' : 'No')  for @{$record_div->get_nodes_by_flag('displayable')};
