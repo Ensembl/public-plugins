0b163c683def72eb5fa3eca9b619821adeac0770
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-16 09:55:25 +0000

    ignore array

--- admin/modules/EnsEMBL/Admin/Component/Production.pm
+++ admin/modules/EnsEMBL/Admin/Component/Production.pm
@@ -5,8 +5,8 @@ use strict;
 use base qw(EnsEMBL::ORM::Component::DbFrontend);
 
 sub get_printable {
-  my ($self, $value, $default) = @_;
-  return $value ? UNIVERSAL::can($value, 'get_title') ? $value->get_title : $default || (ref $value eq 'ARRAY' ? join ', ', @$value : $value) : $default || '<i>null</i>';
+  my ($self, $value) = @_;
+  return $value ? UNIVERSAL::can($value, 'get_title') ? $value->get_title : $value : '<i>null</i>';
 }
 
 1;
