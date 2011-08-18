ab06f468556e87d734a3fd845e1e0fac59ff59fe
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-18 13:50:21 +0000

    read and print the error passed as GET param

--- orm/modules/EnsEMBL/ORM/Component/DbFrontend/Problem.pm
+++ orm/modules/EnsEMBL/ORM/Component/DbFrontend/Problem.pm
@@ -8,11 +8,13 @@ use warnings;
 use base qw(EnsEMBL::ORM::Component::DbFrontend);
 
 sub content_tree {
-  my $self = shift;
+  my $self  = shift;
+  my $error = $self->hub->param('error');
+     $error = $error ? "Error: $error" : 'Please try again.';
 
   return $self->dom->create_element('div', {
     'class'      => [$self->object->content_css, $self->_JS_CLASS_RESPONSE_ELEMENT],
-    'inner_HTML' => '<p>Sorry, a problem occurred while updating information to the database. Please try again.</p>'
+    'inner_HTML' => qq(<p class="dbf-error">Sorry, an error occurred while updating information to the database.<br />$error</p>)
   });
 }
 
