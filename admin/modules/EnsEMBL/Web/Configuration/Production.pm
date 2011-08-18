5277d2aa2c299d01aef37d5b14b743c2bc594424
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-18 13:58:22 +0000

    two new pages added

--- admin/modules/EnsEMBL/Web/Configuration/Production.pm
+++ admin/modules/EnsEMBL/Web/Configuration/Production.pm
@@ -35,7 +35,7 @@ sub populate_tree {
     'Delete'          => {},
   ]);
   $self->create_multidbfrontend_menu('AnalysisDesc', 'Analysis Description', {'filters' => [qw(WebAdmin)]});
-  $self->create_multidbfrontend_menu($_, $_, {'filters' => [qw(WebAdmin)]}) for qw(Species Metakey Biotype Webdata);
+  $self->create_multidbfrontend_menu($_, $_, {'filters' => [qw(WebAdmin)]}) for qw(Species Metakey Biotype Webdata AttribType ExternalDb);
 }
 
 1;
