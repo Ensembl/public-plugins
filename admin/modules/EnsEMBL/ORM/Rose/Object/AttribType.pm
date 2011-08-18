93cba77ef933a56fe4c15e703ffe4a04b1fc63fd
Harpreet Riat <hr5@sanger.ac.uk>
2011-08-18 14:01:18 +0000

    rose object class for master_attrib_type table in production db

---
+++ admin/modules/EnsEMBL/ORM/Rose/Object/AttribType.pm
@@ -0,0 +1,35 @@
+package EnsEMBL::ORM::Rose::Object::AttribType;
+
+### NAME: EnsEMBL::ORM::Rose::Object::AnalysisDescription
+### ORM class for the master_attrib_type table in ensembl_production
+
+### STATUS: Stable 
+
+use strict;
+use warnings;
+
+use base qw(EnsEMBL::ORM::Rose::Object::Trackable);
+
+use constant {
+  ROSE_DB_NAME        => 'production',
+  TITLE_COLUMN        => 'name',
+  INACTIVE_FLAG       => 'is_current',
+  INACTIVE_FLAG_VALUE => '0',
+};
+
+## Define schema
+__PACKAGE__->meta->setup(
+  table       => 'master_attrib_type',
+
+  columns     => [
+    attrib_type_id  => {type => 'serial', primary_key => 1, not_null => 1},
+    code            => {type => 'varchar', 'length' => 15,  not_null => 1},		
+    name            => {type => 'varchar', 'length' => 255, not_null => 1},			
+    description     => {type => 'text'},
+    is_current      => {type => 'int', 'default' => 1,      not_null => 1}
+  ],
+  
+  unique_key => ['code']
+);
+
+1;
