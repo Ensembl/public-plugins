9de58104b1da29ae9431557baf9baea6aa4c2741
Anne Parker <ap5@sanger.ac.uk>
2011-09-13 11:10:11 +0000

    Simplified 'species set' text and fixed numbers of orthologues

--- ensembl/modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
+++ ensembl/modules/EnsEMBL/Web/Component/Gene/ComparaOrthologs.pm
@@ -11,33 +11,39 @@ sub _species_sets {
   my %orthologue_map = qw(SEED BRH PIP RHS);
 
   my $species_sets = {
-    'primates'  =>  {'title' => 'Primates', 'desc' => '', 'species' => []},
-    'rodents'   =>  {'title' => 'Rodents',  'desc' => '', 'species' => []},
+    'primates'  =>  {'title' => 'Primates', 'desc' => 'Humans and other primates', 'species' => []},
+    'rodents'   =>  {'title' => 'Rodents',  'desc' => 'Rodents, rabbits and related species', 'species' => []},
     'laurasia'  =>  {'title' => 'Laurasiatheria', 'desc' => 'Carnivores, ungulates and insectivores',  'species' => []},
-    'placental' =>  {'title' => 'Placental Mammals', 'desc' => '', 'species' => []},
+    'placental' =>  {'title' => 'Placental Mammals', 'desc' => 'All placental mammals', 'species' => []},
     'sauria'    =>  {'title' => 'Sauropsida', 'desc' => 'Birds and Reptiles', 'species' => []},
-    'fish'      =>  {'title' => 'Fish', 'desc' => '', 'species' => []},
+    'fish'      =>  {'title' => 'Fish', 'desc' => 'Ray-finned fishes', 'species' => []},
     'all'       =>  {'title' => 'All', 'desc' => 'All species, including invertebrates', 'species' => []},
   };
 
   my $sets_by_species = {};
 
   my ($ortho_type);
-  foreach my $species (keys %$orthologue_list) {
+  my @A = keys %$orthologue_list;
+
+  foreach my $species ($species_defs->valid_species) {
     next if $skipped->{$species};
-    my $no_ortho = 0;
     my $group = $species_defs->get_config($species, 'SPECIES_GROUP');
     push @{$species_sets->{'all'}{'species'}}, $species;
     my $sets = [];
-
     my $orthologues = $orthologue_list->{$species} || {};
+    my $no_ortho = 0;
+    if (!$orthologue_list->{$species} && $species ne $self->hub->species) {
+      $no_ortho = 1;
+    }
+
     foreach my $stable_id (keys %$orthologues) {
       my $orth_info = $orthologue_list->{$species}{$stable_id};
       my $orth_desc = ucfirst($orthologue_map{$orth_info->{'homology_desc'}} || $orth_info->{'homology_desc'});
       $species_sets->{'all'}{$orth_desc}++;
       $ortho_type->{$species}{$orth_desc} = 1;
     }
-    if (!$ortho_type->{$species}{'1-to-1'} && !$ortho_type->{$species}{'1-to-many'}
+
+    if ($species ne $self->hub->species && !$ortho_type->{$species}{'1-to-1'} && !$ortho_type->{$species}{'1-to-many'}
           && !$ortho_type->{$species}{'Many-to-many'}) {
       $no_ortho = 1;
       $species_sets->{'all'}{'none'}++;
@@ -92,14 +98,6 @@ sub _species_sets {
     }
     $sets_by_species->{$species} = $sets;
   }
-  $species_sets->{'primates'}{'desc'} = 'Human and '. (scalar(@{$species_sets->{'primates'}{'species'}})-1)
-                                          .' other primates';
-  $species_sets->{'rodents'}{'desc'} = 'Mouse, rat and '. (scalar(@{$species_sets->{'rodents'}{'species'}})-2)
-                                          .' other rodents';
-  $species_sets->{'placental'}{'desc'} = (scalar(@{$species_sets->{'placental'}{'species'}}))
-                                          .' placental mammalian species';
-  $species_sets->{'fish'}{'desc'} = 'Zebrafish and '. (scalar(@{$species_sets->{'fish'}{'species'}})-1)
-                                          .' other ray-finned fish';
 
   return ($species_sets, $sets_by_species, $set_order);
 }
