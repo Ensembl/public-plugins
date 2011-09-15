dd0b83f4456aa4b81abe87423baf7f805c194006
Simon Brent <sb23@sanger.ac.uk>
2011-09-15 10:09:46 +0000

    uri_escape query string. tidy.

--- lucene/modules/EnsEMBL/Lucene/Component/Search/Results.pm
+++ lucene/modules/EnsEMBL/Lucene/Component/Search/Results.pm
@@ -1,12 +1,12 @@
+# $Id$
+
 package EnsEMBL::Lucene::Component::Search::Results;
 
 use strict;
-use warnings;
-no warnings "uninitialized";
-use base qw(EnsEMBL::Web::Component);
 
-use Data::Dumper;
-#$Data::Dumper::Maxdepth=2;
+use URI::Escape qw(uri_escape);
+
+use base qw(EnsEMBL::Web::Component);
 
 sub _init {
   my $self = shift;
@@ -15,148 +15,142 @@ sub _init {
 }
 
 sub content {
-  my $self    = shift;
-  my $object  = $self->object;
-  my $hub     = $self->hub;
-  my $species_defs = $hub->species_defs;
-  my $sitetype = lc($species_defs->ENSEMBL_SITETYPE);
+  my $self             = shift;
+  my $hub              = $self->hub;
+  my $species_defs     = $hub->species_defs;
+  my $sitetype         = lc $species_defs->ENSEMBL_SITETYPE;
+  my $species          = $hub->param('species');
+  my $display_species  = $species eq 'all' ? sprintf('the %s website', ucfirst $sitetype) : $hub->species_defs->get_config($species,'SPECIES_COMMON_NAME');
+  my $q                = uri_escape($hub->param('q'));
+  my $results_by_group = $self->object->groups;
   my $html;
-
-  my $species = $hub->param('species');
-  my $display_species = $species eq 'all' ? "the ".ucfirst($sitetype)." website" : $hub->species_defs->get_config($species,'SPECIES_COMMON_NAME');
-  my $q = $hub->param('q');
-
-  my $results_by_group = $object->groups;
-  my $group_count      = scalar keys %$results_by_group;
-
-  if (   $results_by_group->{'Species'}{'total'} && $results_by_group->{'Feature type'}{'total'} || $results_by_group->{'Help'}{'total'} ) {
-    $html = qq(<h3>Your search of $display_species with '$q' returned the following results:</h3>);
-
-    my @group_classes = ('one-col');
-    if ( $group_count > 1 ) {
-      @group_classes = $group_count > 1
-       ? ( 'threecol-left', 'threecol-middle', 'threecol-right' )
-       : ( 'twocol-left', 'twocol-right' );
-    }
-
-    my $i = 0;
-
-#    foreach my $group_name ( sort { $a cmp $b } keys %$results_by_group ) {
-    foreach my $group_name ( 'Feature type', 'Species', 'Help' ) {
-      my $class = $group_classes[$i];
-      my $group_total = delete $results_by_group->{$group_name}->{total};
+  
+  if ($results_by_group->{'Species'}{'total'} && $results_by_group->{'Feature type'}{'total'} || $results_by_group->{'Help'}{'total'}) {
+    my @group_classes = ('threecol-left', 'threecol-middle', 'threecol-right');
+    my $i             = 0;
+       $html          = "<h3>Your search of $display_species with '$q' returned the following results:</h3>";
+    
+    foreach my $group_name ('Feature type', 'Species', 'Help') {
+      my $group_total = delete $results_by_group->{$group_name}->{'total'};
+      
       next if $group_total < 1;
+      
+      my $class = $group_classes[$i];
+      my $group = $results_by_group->{$group_name}->{'results'};
+      
+      $html .= qq{
+      <div class="$class">
+        <table class="search_results">
+          <tr><th colspan="2">By $group_name</th></tr>
+          <tr><td>Total</td><td>$group_total</td></tr>
+      };
 
-      $html .= qq(<div class="$class">
-<table class="search_results">
-<tr><th colspan="2">By $group_name</th></tr>
-<tr><td>Total</td><td>$group_total</td></tr>
-      );
+      foreach my $child_name (sort { $group->{$a}{'sort_field'} cmp $group->{$b}{'sort_field'} } keys %{$group}) {
+        my $child      = $group->{$child_name};
+           $child_name =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
+        my $display_n  = $child->{'sort_field'};
+           $display_n  =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
+           $display_n  =~  s/_/ /g;
 
-      my $group     = $results_by_group->{$group_name}->{results};
-
-      foreach my $child_name ( sort { $group->{$a}{'sort_field'} cmp $group->{$b}{'sort_field'} } keys %{$group} ) {
-        my $child = $group->{$child_name};
-        $child_name =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
-        my $display_n = $child->{'sort_field'};
-        $display_n =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
-        $display_n =~  s/_/ /g;
-
-        $html .= qq(<tr>
-                              <td>);
-
-        $html .= qq(<a href="#" class="collapsible">$display_n</a><ul class="shut">\n);
-        my $grandchild  = $child->{results};
-        my $child_count = $child->{total};
+        $html .= qq{
+          <tr>
+            <td>
+              <a href="#" class="collapsible">$display_n</a>
+              <ul class="shut">
+        };
+        
+        my $grandchild  = $child->{'results'};
+        my $child_count = $child->{'total'};
         my $g_clipped;
-        foreach my $g_name ( sort { $grandchild->{$a}{'sort_field'} cmp $grandchild->{$b}{'sort_field'} } keys %{$grandchild} ) {
-          my $gchild = $grandchild->{$g_name};
+        
+        foreach my $g_name (sort { $grandchild->{$a}{'sort_field'} cmp $grandchild->{$b}{'sort_field'} } keys %{$grandchild}) {
+          my $gchild    = $grandchild->{$g_name};
           my $display_n = $gchild->{'sort_field'};
-          $display_n =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
-          my $g_count = $gchild->{count};
-          my $clipped = $gchild->{is_clipped_flag};
-          $g_clipped = '>' if $clipped eq '>';
-          $g_name =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
+             $display_n =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
+          my $g_count   = $gchild->{'count'};
+          my $clipped   = $gchild->{'is_clipped_flag'};
+             $g_clipped = '>' if $clipped eq '>';
+             $g_name    =~ s/${sitetype}_(.*)$/ucfirst($1)/e;
 
           # Handle Docs Urls differently
           my $g_url;
-          if ( $g_name =~ /faq|docs|glossary|help/i ) {
-            $g_url = "/Search/Details?species=all;idx=" . ucfirst($g_name) . ';q=' . $hub->param('q');
-          }
-          else {
-            my ( $sp, $idx );
-            ( $idx, $sp ) = $group_name =~ /species/i ? ( $g_name, $child_name ) : ( $child_name, $g_name );
+          
+          if ($g_name =~ /faq|docs|glossary|help/i) {
+            $g_url = sprintf '/Search/Details?species=all;idx=%s;q=%s', ucfirst($g_name), $q;
+          } else {
+            my ($idx, $sp) = $group_name =~ /species/i ? ($g_name, $child_name) : ($child_name, $g_name);
             $sp     =~ s/\s/_/;
             $g_name =~ s/_/ /g;
-            $g_url = "/$sp/Search/Details?species=$sp;idx=$idx;end=$g_count;q=" . $hub->param('q');
+            $g_url  = "/$sp/Search/Details?species=$sp;idx=$idx;end=$g_count;q=" . $q;
           }
+          
           # yet more exceptions for Help and docs
           # change Help -> Page Help, Docs -> Documentation and Faq to FAQ
           # this should be fixed at source i.e. the Search Domain name
           $display_n =~ s/Help/Page Help/;
           $display_n =~ s/Docs/Documentation/;
           $display_n =~ s/Faq/FAQ/;
+          $display_n =~ s/_/ /g;
 
-          $display_n =~  s/_/ /g;
-
-          $html .= qq#<li><a href="$g_url"> $display_n ($clipped$g_count)</a></li>#;
+          $html .= qq{<li><a href="$g_url"> $display_n ($clipped$g_count)</a></li>};
         }
-        $html .= "</ul>\n";
-
-        $html .= qq(</td>
- <td style="width:5em"><a href="#"> $g_clipped$child_count</a>
- </tr>\n);
+        
+        $html .= qq{
+              </ul>
+            </td>
+            <td style="width:5em"><a href="#"> $g_clipped$child_count</a></td>
+          </tr>
+        };
       }
 
-      $html .= qq(</table>\n</div>\n\n);
+      $html .= qq{
+        </table>
+      </div>};
+      
       $i++;
     }
+  } else {
+    $html = $self->re_search($q);
   }
-  else {
-    $html = $self->re_search;
-  }
+  
   return $html;
 }
 
 sub re_search {
-  my $self   = shift;
-  my $hub = $self->hub;
-  my $sitetype = ucfirst(lc($hub->species_defs->ENSEMBL_SITETYPE));
-  my $html;
-
-  my $species = $hub->param('species');
+  my ($self, $q)      = @_;
+  my $hub             = $self->hub;
+  my $sitetype        = ucfirst lc $hub->species_defs->ENSEMBL_SITETYPE;
+  my $species         = $hub->param('species');
   my $display_species = $species eq 'all' ? 'all species' : $hub->species_defs->get_config($species,'SPECIES_COMMON_NAME');
-  my $q      = $hub->param('q');
-  my $q_name = $q;
-
-  my $do_search = 0;
-  if ( $q =~ /^(\S+?)(\d+)/ ) {
-    my $ENS = $1;
+  
+  if ($q =~ /^(\S+?)(\d+)/) {
+    my $ens = $1;
     my $dig = $2;
-    if ( ( $ENS =~ /ENS|OTT/ ) && ( length($dig) != 11 ) && ( $ENS !~ /ENSFM|ENSSNP/ ) ) {
-      $do_search = 1;
-      my $newq = $ENS . sprintf( "%011d", $dig );
-      $html = qq(<h2><p>Your search of $display_species with '$q' returned no results</p></h2>);
-      my $url =
-        '/' . $hub->species . "/Search/Results?species=$species;idx=" . $hub->param('idx') . ';q=' . $newq;
-      $html .=
-        sprintf qq(<h3>Would you like to <a href="%s">search using $newq</a> (note number of digits)?</h3>), $url;
-      return $html;
+    
+    if ($ens =~ /ENS|OTT/ && length $dig != 11 && $ens !~ /ENSFM|ENSSNP/) {
+      my $newq = $ens . sprintf "%011d", $dig;
+      my $url  = '/' . $hub->species . "/Search/Results?species=$species;idx=" . $hub->param('idx') . ';q=' . $newq;
+      
+      return qq{
+        <h2>Your search of $display_species with '$q' returned no results</h2>
+        <h3>Would you like to <a href="$url">search using $newq</a> (note number of digits)?</h3>
+      };
     }
   }
-  if ( !$do_search && ( $species ne 'all' ) ) {
-    $species =~ s/_/ /g;
-    $html = qq(<h2>Your search of annotated features from $display_species for the term '$q' returned no results</h2>);
+  
+  if ($species ne 'all') {
     my $url = '/' . $hub->species . '/Search/Results?species=all;idx=' . $hub->param('idx') . ';q=' . $q;
-    $html .= sprintf qq(<h3>Would you like to search <a href="%s">the rest of the website</a> with this term ?</h3>), $url;
-    return $html;
+    
+    return qq{
+      <h2>Your search of annotated features from $display_species for the term '$q' returned no results</h2>
+      <h3>Would you like to search <a href="$url">the rest of the website</a> with this term?</h3>
+    };
   }
 
-  $html = qq(<h2>Your search of the $sitetype website for the term '$q' returned no results</h2>);
-  $html .=
-qq(<h3>If you are expecting to find features with this search term and think the failure to do so is an error, please <a href="/Help/Contact" class="popup">contact helpdesk</a> and let us know</h3>);
-  warn "Found nothing when searching for \"$q\"";
-  return $html;
+  return qq{
+    <h2>Your search of the $sitetype website for the term '$q' returned no results</h2>
+    <h3>If you are expecting to find features with this search term and think the failure to do so is an error, please <a href="/Help/Contact" class="popup">contact helpdesk</a> and let us know</h3>
+  };
 }
 
 1;
