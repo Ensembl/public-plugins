a340a1503e436002b22a23707dea6f00eaff779a
Harpreet Riat <hr5@sanger.ac.uk>
2011-10-03 15:54:10 +0000

    modify_methods method added

--- orm/modules/EnsEMBL/ORM/Rose/DataStructure.pm
+++ orm/modules/EnsEMBL/ORM/Rose/DataStructure.pm
@@ -5,10 +5,62 @@ package EnsEMBL::ORM::Rose::DataStructure;
 
 use strict;
 
+use EnsEMBL::ORM::Rose::DataStructureValue;
+
 use base qw(Rose::DB::Object::Metadata::Column::Text);
 
+sub value_class {
+  ## Returns the name of the class that will be instantiated to represent value of this column
+  return 'EnsEMBL::ORM::Rose::DataStructureValue';
+}
+
 sub type {
+  ## Returns the type of the column (polymorphic method)
   return 'datastructure';
+}
+
+sub modify_methods {
+  ## @constructor
+  ## modifies the get/set methods for a datastructure column
+  my $self  = shift;
+  my $class = $self->parent->class;
+
+  # copy the old methods first
+  no strict qw(refs);
+  my $get_method = $self->accessor_method_name;
+  my $set_method = $self->mutator_method_name;
+  *{"${class}::_ensorm_old_$_"} = \&{"${class}::${_}"} for $get_method, $set_method;
+
+  # create new methods
+  my $new_accessor_method = sub {
+    # modified method calls the actual accessor method to get the stringfied hash/array, then converts into hashref/arrayref before returning it
+    my $object = shift;
+    return $self->value_class->new(map($object->$_, "_ensorm_old_$get_method"), $object);
+  };
+  my $new_mutator_method  = sub {
+    # modified method stringifies the hashref/arrayref before calling the actual mutator method
+    my ($object, $value) = @_;
+    $value = $self->value_class->new($value, $object) unless UNIVERSAL::isa($value, $self->value_class);
+    map($object->$_("$value"), "_ensorm_old_$set_method");
+    return &$new_accessor_method($object);
+  };
+  my $new_accessor_mutator_method = sub {
+    # method that sets the value in arrayref/hashref form, if provided before returning the new set value, using the existing methods
+    my $object = shift;
+    return &$new_mutator_method($object, shift) if @_;
+    return &$new_accessor_method($object);
+  };
+
+  # replace the old methods with new ones
+  if ($get_method eq $set_method) {
+    *{"${class}::${get_method}"} = $new_accessor_mutator_method;
+  }
+  else {
+    *{"${class}::${get_method}"} = $new_accessor_method;
+    *{"${class}::${set_method}"} = $new_mutator_method;
+  }
+  
+  return $self;
 }
 
 1;
