b9643c16ee2644f41c9d48a62ba489b17b266fb0
Harpreet Riat <hr5@sanger.ac.uk>
2011-10-03 16:23:18 +0000

    rolling back accidental commit

--- admin/modules/EnsEMBL/Web/Filter/WebAdmin.pm
+++ admin/modules/EnsEMBL/Web/Filter/WebAdmin.pm
@@ -6,21 +6,21 @@ use base qw(EnsEMBL::Web::Filter);
 
 sub init {
   my $self = shift;
-  my $sd   = $self->hub->species_defs;
 
+  $self->redirect = '/Account/Login?popup=no';
   $self->messages = {
-    restricted => sprintf('These pages are restricted to members of the %s webadmin group. If you require access, please contact the %1$s Web Team.', $sd->ENSEMBL_SITETYPE),
+    not_member => 'These pages are restricted to members of the Ensembl webadmin group. If you require access, please contact the web team.',
   };
 }
+
 
 sub catch {
   my $self = shift;
   my $hub  = $self->hub;
   my $user = $hub->user;
-  my $sd   = $hub->species_defs;
-
-  $self->redirect   = sprintf('%s%s%s', '/Account/Login?then=', $sd->ENSEMBL_BASE_URL, $hub->url);
-  $self->error_code = 'restricted' unless $user && $user->is_member_of($sd->ENSEMBL_WEBADMIN_ID);
+  unless ($user && $user->is_member_of($hub->species_defs->ENSEMBL_WEBADMIN_ID)) {
+    $self->error_code = 'not_member';
+  }
 }
 
 1;
