4412504e07e606d9bca1cf0ff75a574d19e6fa62
Harpreet Riat <hr5@sanger.ac.uk>
2011-10-03 16:25:34 +0000

    manager class for HelpRecord

---
+++ admin/modules/EnsEMBL/ORM/Rose/Manager/HelpRecord.pm
@@ -0,0 +1,15 @@
+package EnsEMBL::ORM::Rose::Manager::HelpRecord;
+
+### NAME: EnsEMBL::ORM::Rose::Manager::HelpRecord
+### Module to handle multiple HelpGlossary entries 
+
+use strict;
+use warnings;
+
+use EnsEMBL::ORM::Rose::Object::HelpRecord;
+
+use base qw(EnsEMBL::ORM::Rose::Manager::Trackable);
+
+sub object_class { 'EnsEMBL::ORM::Rose::Object::HelpRecord' }
+
+1;
