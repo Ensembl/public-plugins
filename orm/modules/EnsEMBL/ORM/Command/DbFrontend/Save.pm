d9f57e15088b6678de410c08e6654bb78ccca83c
Harpreet Riat <hr5@sanger.ac.uk>
2011-10-04 10:29:34 +0000

    retain 'function' part of the url

--- orm/modules/EnsEMBL/ORM/Command/DbFrontend/Save.pm
+++ orm/modules/EnsEMBL/ORM/Command/DbFrontend/Save.pm
@@ -16,11 +16,12 @@ use base qw(EnsEMBL::Web::Command);
 sub process {
   my $self = shift;
 
+  my $hub    = $self->hub;
   my $object = $self->object;
   my $done   = $object->save;
   
-  $self->ajax_redirect($self->hub->url($done && @$done
-    ? $object->is_ajax_request ? {'action' => 'Display', 'id' => $object->rose_object->get_primary_key_value} : {'action' => $object->default_action}
+  $self->ajax_redirect($hub->url($done && @$done
+    ? $object->is_ajax_request ? {'action' => 'Display', 'id' => $object->rose_object->get_primary_key_value} : {'action' => $object->default_action, 'function' => $hub->function}
     : {'action' => 'Problem', 'error' => $object->rose_error}
   ));
 }
