69cfc07d20968c1f38a3e6deb505bc3a2d568274
Harpreet Riat <hr5@sanger.ac.uk>
2011-10-11 12:01:50 +0000

    manager class for User object

---
+++ users/modules/EnsEMBL/ORM/Rose/Manager/User.pm
@@ -0,0 +1,36 @@
+package EnsEMBL::ORM::Rose::Manager::User;
+
+### NAME: EnsEMBL::ORM::Rose::Manager::User
+### Module to handle multiple User entries 
+
+### DESCRIPTION:
+### Enables fetching, counting, etc., of multiple Rose::Object::User objects
+
+use strict;
+use warnings;
+
+use base qw(EnsEMBL::ORM::Rose::Manager::Trackable);
+
+use EnsEMBL::ORM::Rose::Object::User;
+
+sub object_class { 'EnsEMBL::ORM::Rose::Object::User' }
+
+sub get_by_id {
+  ## Gets user by id
+  ## @param String id
+  ## @return User object
+  my ($self, $id) = @_;
+  
+  return $id ? $self->fetch_by_primary_key($id) : undef;
+}
+
+sub get_by_email {
+  ## Gets user by email
+  ## @param String email
+  ## @return User object
+  my ($self, $email) = @_;
+
+  return shift @{$email ? $self->get_objects('query' => [ 'email', $email ]) : []};
+}
+
+1;
