2264df81bbaff15f49a946abb5124f4e2e5565a5
Harpreet Riat <hr5@sanger.ac.uk>
2011-11-03 13:03:12 +0000

    removed method add_active_only_query

--- users/modules/EnsEMBL/ORM/Rose/Manager/Membership.pm
+++ users/modules/EnsEMBL/ORM/Rose/Manager/Membership.pm
@@ -15,11 +15,4 @@ use base qw(EnsEMBL::ORM::Rose::Manager::Trackable);
 
 sub object_class { 'EnsEMBL::ORM::Rose::Object::Membership' }
 
-sub add_active_only_query {
-  ## @overrides
-  my ($self, $params) = @_;
-  return $self->SUPER::add_active_only_query($params) if $params->{'object_class'} && $params->{'object_class'} ne $self->object_class;  #override only for Membership objects
-  push @{$params->{'query'}},  qw(member_status active status active);
-}
-
 1;
