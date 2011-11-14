51bd0c6ddfdb1d676fbf2031421807ae38081d98
Ensembl Account <ensembl@sanger.ac.uk>
2011-11-14 11:31:11 +0000

    changes for release 65

--- ensembl/conf/SiteDefs.pm
+++ ensembl/conf/SiteDefs.pm
@@ -61,6 +61,7 @@ sub update_conf {
   $SiteDefs::__species_aliases{ 'Oryzias_latipes'          } = [qw(ol olat medaka)];
   $SiteDefs::__species_aliases{ 'Takifugu_rubripes'        } = [qw(fr trub ffish fugu takifugu f_rubripes fugu_rubripes)];
   $SiteDefs::__species_aliases{ 'Tetraodon_nigroviridis'   } = [qw(tn tnig tetraodon)];
+  $SiteDefs::__species_aliases{ 'Gadus_morhua'             } = [qw(gm gmor gadus cod)];
 #-------------------- amphibians
   $SiteDefs::__species_aliases{ 'Xenopus_tropicalis'             } = [qw(xt xtro xenopus frog)];
 #-------------------- reptiles
