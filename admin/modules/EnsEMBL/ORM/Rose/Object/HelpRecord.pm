efff8ea83871c0a842e86263824ca7c79090e00e
Harpreet Riat <hr5@sanger.ac.uk>
2011-11-22 13:25:50 +0000

    trusted key added

--- admin/modules/EnsEMBL/ORM/Rose/Object/HelpRecord.pm
+++ admin/modules/EnsEMBL/ORM/Rose/Object/HelpRecord.pm
@@ -20,7 +20,7 @@ __PACKAGE__->meta->setup(
     help_record_id => {type => 'serial',                    not_null => 1, primary_key => 1},
     type           => {type => 'varchar',  'length' => 255, not_null => 1},
     keyword        => {type => 'text'},
-    data           => {type => 'datamap',                   not_null => 1, 'keys' => [qw(question answer category word expanded meaning list_position length youtube_id title ensembl_object ensembl_action content)]},
+    data           => {type => 'datamap',  'trusted' => 1,  not_null => 1, 'keys' => [qw(question answer category word expanded meaning list_position length youtube_id title ensembl_object ensembl_action content)]},
     status         => {type => 'enum',     'values' => [qw(draft live dead)] },
     helpful        => {type => 'int',      'length' => 11},
     not_helpful    => {type => 'int',      'length' => 11}
