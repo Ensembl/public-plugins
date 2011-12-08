fba9371b1ee581dbce80bad460dc90a396cfac96
Harpreet Riat <hr5@sanger.ac.uk>
2011-12-08 17:20:36 +0000

    wrong namespace for JavaScript; target.next does not always need to be moved

--- admin/htdocs/components/51_DbFrontendRow.js
+++ admin/htdocs/components/51_DbFrontendRow.js
@@ -1,14 +1,22 @@
 /* overrides the default afterResponse method of DbFrontendRow to sort the rows in case of changelog */
 // TODO - hide any heading and link if record moved to another team
 
-Ensembl.Panel.DbFrontendRow.prototype.afterResponse = function(success) {
+Ensembl.DbFrontendRow.prototype.afterResponse = function(success) {
   if (success && window.location.pathname.match('Changelog')) {
 
     var team = $('input._cl_team_name', this.target).val();
-    if ($(this.el).prev('._cl_team_heading').attr('id') != 'team_' + team) {
-      $('#team_' + team).removeClass('hidden').after(this.target, this.target.next());
+
+    if ($(this.el).prev('._cl_team_heading').first().attr('id') != 'team_' + team) {
+
+      var form = this.target.next().hasClass('_cl_team_heading') ? false : this.target.next();
+
+      $('#team_' + team).removeClass('hidden').after(this.target);
+      if (form) {
+        this.target.after(form);
+      }
+
       $('#_cl_link_' + team).removeClass('hidden');
     }
+    window.location.hash = 'team_' + team;
   }
-  this.scrollIn({margin: 5});
 };
