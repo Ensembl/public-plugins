36c5eb60c702033d3b325b66a3700012387dc561
Harpreet Riat <hr5@sanger.ac.uk>
2012-03-12 14:45:21 +0000

    html changed; page not displayed for non-admin user anymore

--- admin/modules/EnsEMBL/Web/Document/HTML/SpeciesList.pm
+++ admin/modules/EnsEMBL/Web/Document/HTML/SpeciesList.pm
@@ -9,10 +9,14 @@ sub render {
   my $species_defs = $self->species_defs;
   my @species      = sort $species_defs->valid_species;
   my $healthchecks = $species_defs->databases->{'DATABASE_HEALTHCHECK'};
-  my $html;
+  my $user         = $self->{'user'};
+
+  return '' unless $user && $user->is_member_of($species_defs->ENSEMBL_WEBADMIN_ID);
+
+  my $html         = '<div class="plain-box admin-right-box">';
 
   if ($healthchecks) {
-    $html = qq(
+    $html .= qq(
 <h2>Healthchecks</h2>
 <ul>
   <li><a href="/Healthcheck/Summary">Healthcheck summary</a>
@@ -29,59 +33,22 @@ sub render {
 );
   }
 
-  $html .= qq(<h3>Species</h3>
-<table style="width:100%">
-  <tr>
-  );
+  $html .= qq(<h3>Species</h3><div class="admin-species">);
 
-  my $columns = 2;
-  my $column_width = int((100 / $columns) - 8);
-  my $total = @species;
-  my $break = int($total / $columns);
-  $break++ if $total % $columns;
-  ## Reset total to number of cells required for a complete table
-  $total = $break * $columns;
+  for (@species) {
+    (my $name = $_) =~ s/_/ /g;
 
-  my $row = -1;
-  for (my $i=0; $i < $total; $i++) {
-    my $col = int($i % $columns);
-    if ($col == 0 && $i < $total - 1) {
-       $html .= qq(</tr>\n<tr>);
+    $html .= qq(<div class="admin-species-grid">);
+    $html .= qq(<img height="40" width="40" src="/img/species/thumb_$_.png" alt="$name" /><div><span>$name</span>);
+    if ($healthchecks) {
+      $html .= qq(<br /><a href="/$_/Healthcheck/Details/Species">Healthcheck</a> 
+          | <a href="http://staging.ensembl.org/$_/">View on staging</a>);
     }
-    $row++ if $col == 0;
-    my $j = $row + $break * $col;
-    
-    my $dir = $species[$j];
-    (my $name = $dir) =~ s/_/ /g;
-
-    $html .= qq(<td style="width:8%;text-align:right;padding-bottom:1em">);
-    if ($dir) {
-      $html .= qq(<img height="40" width="40" src="/img/species/thumb_$dir.png" alt="$name">);
-    }
-    else {
-      $html .= '&nbsp;';
-    }
-    $html .= qq(</td>\n<td style="width:$column_width%;padding:2px;padding-bottom:1em">);
-    if ($dir) {
-      if ($healthchecks) {
-        $html .= qq(<span style="font-weight:bold;font-size:1.1em">$name</span><br /><a href="/$dir/Healthcheck/Details/Species">Healthcheck</a> 
-          | <a href="http://staging.ensembl.org/$dir/">View on staging</a>);
-      }
-      else {
-        $html .= qq(<a href="/$dir/" style="font-weight:bold;font-size:1.1em;text-decoration:none">$name</a>);
-      }
-    }
-    else {
-      $html .= '&nbsp;';
-    }
-    $html .= '</td>';
+    $html .= qq(</div></div>);
   }
 
-  $html .= qq(
-</tr>
-</table>);
   
-  return $html;
+  return $html.'</div>';
 }
 
 1;
