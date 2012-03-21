1d94fffadd0c55f60ce1e2930dc5ce7479a45d4b
Anne Parker <ap5@sanger.ac.uk>
2012-03-21 17:02:32 +0000

    Added link to privacy policy, and made all links point to live site

--- ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -7,11 +7,12 @@ use strict;
 use base qw(EnsEMBL::Web::Document::Element);
 
 sub content {
+  ## Note that these links always go to the live site so that the information is up-to-date
   return qq(
     <div class="twocol-right right unpadded print_hide">
-      <a href="/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
-      <a href="/info/about/contact/" class="constant">Contact&nbsp;Us</a> | 
-      <a href="/info/website/help/" class="constant">Help</a> 
+      <a href="http://www.ensembl.org/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
+      <a href="http://www.ensembl.org/info/about/legal/privacy.html" class="constant">Privacy&nbsp;Policy</a> | 
+      <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a>
     </div>
     <div class="twocol-right right unpadded screen_hide_block">
       helpdesk\@ensembl.org
