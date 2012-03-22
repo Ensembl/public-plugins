c8d527d4dfbfb20bf7e04b9a7ea3c3bae87a8a94
Web Admin <w3adm@sanger.ac.uk>
2012-03-22 09:46:18 +0000

    Added link to privacy policy

--- ensembl/modules/EnsEMBL/Web/Document/HTML/FooterLinks.pm
+++ ensembl/modules/EnsEMBL/Web/Document/HTML/FooterLinks.pm
@@ -11,9 +11,9 @@ use base qw(EnsEMBL::Web::Document::HTML);
 sub render {
   $_[0]->print( qq(
     <div class="twocol-right right unpadded print_hide">
-      <a href="http://www.ensembl.org/info/about/intro.html">About&nbsp;Ensembl</a> | 
-      <a href="http://www.ensembl.org/info/about/contact/">Contact&nbsp;Us</a> | 
-      <a href="/info/website/help/">Help</a> 
+      <a href="http://www.ensembl.org/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
+      <a href="http://www.ensembl.org/info/about/legal/privacy.html" class="constant">Privacy&nbsp;Policy</a> | 
+      <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a>      
     </div>
     <div class="twocol-right right unpadded screen_hide_block">
       helpdesk\@ensembl.org
