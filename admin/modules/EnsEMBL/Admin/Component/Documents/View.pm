714cd59af5b42f769626e6889fb8f3a9f6453134
Harpreet Riat <hr5@sanger.ac.uk>
2012-03-28 09:23:48 +0000

    now inherits E::A::Component::Documents

--- admin/modules/EnsEMBL/Admin/Component/Documents/View.pm
+++ admin/modules/EnsEMBL/Admin/Component/Documents/View.pm
@@ -2,35 +2,21 @@ package EnsEMBL::Admin::Component::Documents::View;
 
 use strict;
 
-use base qw(EnsEMBL::Web::Component);
-
-use EnsEMBL::Admin::Tools::DocumentParser qw(file_to_htmlnodes);
-
-sub caption       { ''; }
-sub short_caption { ''; }
+use base qw(EnsEMBL::Admin::Component::Documents);
 
 sub content {
   my $self    = shift;
   my $object  = $self->object;
   my $hub     = $self->hub;
-  my $dom     = $self->dom;
-  my $file    = $object->get_parsed_file;
-  my $message = $object->header_message;
-  my $html    = '';
 
-  if ($hub->function) {
-    $html .= $dom->create_element('div', {'class' => 'embedded-box tinted-box', 'inner_HTML' => $message})->render if $message;
-    $html .= file_to_htmlnodes($file, $dom)->render if $file;
-  } else {
+  return $self->get_display_html if $hub->function;
 
-    my $docs = $object->available_documents;
-    while (my ($function, $title) = splice @$docs, 0, 2) {
-      $html .= sprintf('<li><a href="%s">%s</a></li>', $hub->url({'function' => $function}), $title);
-    }
-    $html = $html ? "<h2>Document list</h2><p>Please select a document below to view:<ul>$html</ul></p>" : '<h2>No document found</h2><p>There is no document to display</p>';
+  my $docs    = $object->available_documents;
+  my $list    = '';
+  while (my ($function, $title) = splice @$docs, 0, 2) {
+    $list .= sprintf('<li><a href="%s">%s</a></li>', $hub->url({'action' => 'View', 'function' => $function}), $title);
   }
-
-  return $html;
+  return $list ? "<h2>Document list</h2><p>Please select a document below to view:<ul>$list</ul></p>" : '<h2>No document found</h2><p>There is no document to display</p>';
 }
 
 1;
