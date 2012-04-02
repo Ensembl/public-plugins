8455c042cb4a7262f0e95f65b88239a4f8659453
Harpreet Riat <hr5@sanger.ac.uk>
2012-04-02 13:53:15 +0000

    class added for javascript

--- orm/modules/EnsEMBL/ORM/Component/DbFrontend/List.pm
+++ orm/modules/EnsEMBL/ORM/Component/DbFrontend/List.pm
@@ -142,7 +142,7 @@ sub _display_column_value {
     if ($value->isa('EnsEMBL::ORM::Rose::Object::User') && $self->object->show_user_email && !$is_title) {
       return sprintf('<a href="mailto:%s">%s</a>', $value->email, $title);
     }
-    return sprintf('%s<a class="dbf-list-view" title="View related %s" href="%s"></a>', $title, $label, $self->hub->url({'type' => $relation, 'action' => 'Display', 'id' => $value->get_primary_key_value})) if $relation;
+    return sprintf('%s<a class="dbf-list-view _dbf_list_view" title="View related %s" href="%s"></a>', $title, $label, $self->hub->url({'type' => $relation, 'action' => 'Display', 'id' => $value->get_primary_key_value})) if $relation;
     return $title;
   }
 
