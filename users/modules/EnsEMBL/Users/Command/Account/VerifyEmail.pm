a018434fc88f99d1111de9f546eb09d9a16d84f5
Harpreet Riat <hr5@sanger.ac.uk>
2012-04-13 10:59:06 +0000

    command module to redirect user to appropriate page after verification of the email

---
+++ users/modules/EnsEMBL/Users/Command/Account/VerifyEmail.pm
@@ -0,0 +1,14 @@
+package EnsEMBL::Users::Command::Account::VerifyEmail;
+
+use strict;
+use warnings;
+
+use base qw(EnsEMBL::Users::Command::Account);
+
+sub process {
+  my $self = shift;
+
+  return $self->ajax_redirect($self->hub->url({'action' => 'Message', 'function' => $self->object->activate_user ? 'Verified' : 'VerificationFailed'}));
+}
+
+1;
