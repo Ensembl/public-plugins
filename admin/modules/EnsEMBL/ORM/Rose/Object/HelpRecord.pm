dacabaea649b9cfac5cc5438b1aae12ec018c158
Harpreet Riat <hr5@sanger.ac.uk>
2012-04-13 09:15:51 +0000

    virtual columns added

--- admin/modules/EnsEMBL/ORM/Rose/Object/HelpRecord.pm
+++ admin/modules/EnsEMBL/ORM/Rose/Object/HelpRecord.pm
@@ -14,21 +14,37 @@ use constant ROSE_DB_NAME => 'website';
 
 ## Define schema
 __PACKAGE__->meta->setup(
-  table         => 'help_record',
+  table           => 'help_record',
 
-  columns       => [
+  columns         => [
     help_record_id  => {type => 'serial',                    not_null => 1, primary_key => 1},
     type            => {type => 'varchar',  'length' => 255, not_null => 1},
     keyword         => {type => 'text'},
-    data            => {type => 'datamap',  'trusted' => 1,  not_null => 1, 'keys' => [qw(question answer category word expanded meaning list_position length youtube_id title ensembl_object ensembl_action content)]},
+    data            => {type => 'datamap',  'trusted' => 0,  not_null => 1},
     status          => {type => 'enum',     'values' => [qw(draft live dead)] },
     helpful         => {type => 'int',      'length' => 11},
     not_helpful     => {type => 'int',      'length' => 11}
   ],
-  
-  title_column  => 'data.content', # for help pages only - ie. if help_record.type == 'view'
 
-  relationships => [
+  virtual_columns => [
+    question        => {'column' => 'data'},
+    answer          => {'column' => 'data'},
+    category        => {'column' => 'data'},
+    word            => {'column' => 'data'},
+    expanded        => {'column' => 'data'},
+    meaning         => {'column' => 'data'},
+    list_position   => {'column' => 'data'},
+    length          => {'column' => 'data'},
+    youtube_id      => {'column' => 'data'},
+    title           => {'column' => 'data'},
+    ensembl_object  => {'column' => 'data'},
+    ensembl_action  => {'column' => 'data'},
+    content         => {'column' => 'data'},
+  ],
+
+  title_column    => 'content', # for help pages only - ie. if help_record.type == 'view'
+
+  relationships   => [
     help_links      => {  # this relation only exists if help_record.type == 'view'
       'type'          => 'one to many',
       'class'         => 'EnsEMBL::ORM::Rose::Object::HelpLink',
