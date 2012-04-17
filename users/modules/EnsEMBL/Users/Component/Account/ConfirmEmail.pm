1dd3d3c12635496f94d64a4cf894846e2a471f54
Harpreet Riat <hr5@sanger.ac.uk>
2012-04-17 13:59:35 +0000

    fixed a couple of syntax errors

--- users/modules/EnsEMBL/Users/Component/Account/ConfirmEmail.pm
+++ users/modules/EnsEMBL/Users/Component/Account/ConfirmEmail.pm
@@ -1,6 +1,6 @@
 package EnsEMBL::Users::Component::Account::ConfirmEmail;
 
-### Component for user to confirm his email and choose a password for login
+### Component for the user to confirm his email and choose a password for login
 
 use strict;
 
@@ -16,7 +16,7 @@ sub content {
   return $self->render_message($object->MESSAGE_CONFIRMATION_FAILED, {'error' => 1}) unless $login;
 
   my $user    = $login->user;
-  my $form    = $content->append_child($self->new_form({'action' => $hub->url({'action' => 'AddUser/Confirmed'})}));
+  my $form    = $content->append_child($self->new_form({'action' => $hub->url({'action' => 'AddUser', 'function' => 'Confirmed'})}));
   my $fset    = $form->add_fieldset(sprintf('Register with %s', $self->site_name));
 
   $fset->add_hidden({'name' => 'code', 'value' => $login->get_url_code});
@@ -25,7 +25,7 @@ sub content {
     {'label'  => 'Name',              'type' => 'noedit',   'value' => $login->name || $user->name, 'no_input'  => 1                                    },
     {'label'  => 'Email Address',     'type' => 'noedit',   'value' => $user->email,                'no_input'  => 1                                    },
     {'label'  => 'Password',          'type' => 'password', 'name'  => 'password',                  'required'  => 1, 'notes' => 'at least 6 characters'},
-    {'label'  => 'Confirm password',  'type' => 'password'  'name'  => 'confirm_password',          'required'  => 1                                    }
+    {'label'  => 'Confirm password',  'type' => 'password', 'name'  => 'confirm_password',          'required'  => 1                                    }
   ]);
 
   $fset->add_button({'value' => 'Activate account'});
