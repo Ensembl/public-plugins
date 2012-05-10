3224d95cc73b649f85cdef022088790226aecf06
Harpreet Riat <hr5@sanger.ac.uk>
2012-05-10 13:38:36 +0000

    take height and width configs for the editor from the element's className

--- admin/htdocs/components/01_Admin_Ensembl.js
+++ admin/htdocs/components/01_Admin_Ensembl.js
@@ -7,14 +7,23 @@ Ensembl.extend({
 
     //JS for changelog (TinyMCE)
     $('textarea._tinymce').livequery(function() {
-      $(this).tinymce({
+      var configs = {
         script_url: '/tiny_mce/jscripts/tiny_mce/tiny_mce.js',
         theme: "advanced",
-        theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect",
-        theme_advanced_buttons2: "cut,copy,paste,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,code,|,removeformat,visualaid,|,sub,sup,|,charmap",
+        theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,|,cut,copy,paste,|,charmap",
+        theme_advanced_buttons2: "bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,code,|,removeformat,visualaid,|,sub,sup,",
         theme_advanced_buttons3: "",
         theme_advanced_toolbar_location: "top"
-      });
+      };
+      var height = this.className.match(/_tinymce_h_([0-9]+)/);
+      var width  = this.className.match(/_tinymce_w_([0-9]+)/);
+      if (height) {
+        configs.height = height[1];
+      }
+      if (width) {
+        configs.width = width[1];
+      }
+      $(this).tinymce(configs);
     });
   }
 });
