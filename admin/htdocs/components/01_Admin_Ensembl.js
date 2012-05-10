484a6e6e4e774d647f711b3026dc814c05bacc50
Harpreet Riat <hr5@sanger.ac.uk>
2012-05-10 13:51:44 +0000

    default height and width for the tinymce textarea

--- admin/htdocs/components/01_Admin_Ensembl.js
+++ admin/htdocs/components/01_Admin_Ensembl.js
@@ -7,23 +7,18 @@ Ensembl.extend({
 
     //JS for changelog (TinyMCE)
     $('textarea._tinymce').livequery(function() {
-      var configs = {
+      var height = this.className.match(/_tinymce_h_([0-9]+)/);
+      var width  = this.className.match(/_tinymce_w_([0-9]+)/);
+      $(this).tinymce({
         script_url: '/tiny_mce/jscripts/tiny_mce/tiny_mce.js',
         theme: "advanced",
         theme_advanced_buttons1: "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,|,cut,copy,paste,|,charmap",
         theme_advanced_buttons2: "bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,cleanup,code,|,removeformat,visualaid,|,sub,sup,",
         theme_advanced_buttons3: "",
-        theme_advanced_toolbar_location: "top"
-      };
-      var height = this.className.match(/_tinymce_h_([0-9]+)/);
-      var width  = this.className.match(/_tinymce_w_([0-9]+)/);
-      if (height) {
-        configs.height = height[1];
-      }
-      if (width) {
-        configs.width = width[1];
-      }
-      $(this).tinymce(configs);
+        theme_advanced_toolbar_location: "top",
+        height: height ? height[1] : 300,
+        width: width ? width[1] : 500
+      });
     });
   }
 });
