3cea50963d0cddec07a2fa77e6a5dbe87e9561c7
Harpreet Riat <hr5@sanger.ac.uk>
2012-06-26 13:54:48 +0000

    virtaul columns declared

--- users/modules/EnsEMBL/ORM/Rose/Object/UserRecord.pm
+++ users/modules/EnsEMBL/ORM/Rose/Object/UserRecord.pm
@@ -8,25 +8,29 @@ use warnings;
 
 use base qw(EnsEMBL::ORM::Rose::Object::Trackable);
 
-use constant {
-  ROSE_DB_NAME => 'user',
-  TABLE_NAME   => 'user_record'
-};
+use constant ROSE_DB_NAME => 'user';
 
 ## Define schema
 __PACKAGE__->meta->setup(
-  table         => __PACKAGE__->TABLE_NAME,
-  columns       => [
-    user_record_id    => {'type' => 'serial',  'primary_key' => 1, 'not_null' => 1},
-    user_id           => {'type' => 'integer', 'length' => 11,     'not_null' => 1},
-    type              => {'type' => 'varchar', 'length' => 255},
-    data              => {'type' => 'datamap', 'trusted' => 1, 'keys' => [qw(url click name species)] }
+  table           => 'user_record',
+  columns         => [
+    user_record_id  => {'type' => 'serial',  'primary_key'  => 1,   'not_null' => 1 },
+    user_id         => {'type' => 'integer', 'length'       => 11,  'not_null' => 1 },
+    type            => {'type' => 'varchar', 'length'       => 255                  },
+    data            => {'type' => 'datamap', 'trusted'      => 1                    }
   ],
-  relationships => [
-    user              => {
-      'type'            => 'many to one',
-      'class'           => 'EnsEMBL::ORM::Rose::Object::User',
-      'column_map'      => {'user_id' => 'user_id'},
+  virtual_columns => [
+    url             => {'column' => 'data'},
+    name            => {'column' => 'data'},
+    description     => {'column' => 'data'},
+    click           => {'column' => 'data'},
+    species         => {'column' => 'data'}
+  ],
+  relationships   => [
+    user            => {
+      'type'          => 'many to one',
+      'class'         => 'EnsEMBL::ORM::Rose::Object::User',
+      'column_map'    => {'user_id' => 'user_id'},
     }
   ]
 );
