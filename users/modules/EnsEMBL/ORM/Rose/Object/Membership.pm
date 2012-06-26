b6e2fe627000e57c263344dbdb62c9d705e7cc36
Harpreet Riat <hr5@sanger.ac.uk>
2012-06-26 14:02:34 +0000

    status and membership status description changed; virtual columns added; superuser level removed; webgroup_id aliased as group_id

--- users/modules/EnsEMBL/ORM/Rose/Object/Membership.pm
+++ users/modules/EnsEMBL/ORM/Rose/Object/Membership.pm
@@ -4,21 +4,22 @@ package EnsEMBL::ORM::Rose::Object::Membership;
 ### ORM class for the group_member table in ensembl_web_user_db
 
 ### Status of the membership is decided by two columns: status and member_status
-### status: active    member_status: active   Membership is active
-### status: active    member_status: inactive User removed himself from the group - To join the group again, he can send a request or can receive an invitation
-### status: active    member_status: pending  Invitation is waiting user's approval
-### status: active    member_status: barred   User blocked the group - he will not receive any invitations but can still send a request to join the group - he can also unblock the group be setting member_status to inactive
-### member_status: active   status: inactive  Admin removed user from the group - To join the group again, user can be sent an invitation again or can send a request for membership approval
-### member_status: active   status: pending   User sent a request to join the group for admin's approval
-### member_status: active   status: barred    User blocked by the group's admin. User can not send any request for joining group - Admin can send an invitation to the user, or can unblock the user by setting status to inactive
-### any other combinations should not be allowed - ie. one of the statuses has to be active at all times
+### status      member_status
+### -------------------------
+### active      active          Membership is active
+### active      pending         Invitation is waiting user's approval
+### active      barred          User blocked the group - he will not receive any invitations but can still send a request to join the group - he can also unblock the group be setting member_status to inactive
+### pending     active          User sent a request to join the group for admin's approval
+### barred      active          User blocked by the group's admin. User can not send any request for joining group - Admin can send an invitation to the user, or can unblock the user by setting status to inactive
+### inactive    inactive        Either user himself, or the admin removed the user from the group - user can still send a request or recieve an invitation
+
+### any other combinations should not be allowed
 
 ### A membership record once created never gets removed from the db - either of the status or member_status can be inactive for the record to appear removed
 
 ### level column can contain three values:
-### member: A normal member of the group
+### member: A normal member of the group - can share stuff with the group like bookmarks, annotations etc
 ### administrator: Can send invitations, accept/reject requests, edit the group and in-activate/re-activate the group
-### superuser: Can do everything an administrator can do, except inactivating the group
 
 use strict;
 use warnings;
@@ -32,24 +33,31 @@ __PACKAGE__->meta->setup(
   table                 => 'group_member',
 
   columns               => [
-    group_member_id   => {type => 'serial', primary_key => 1, not_null => 1},
-    webgroup_id       => {type => 'integer'},
-    user_id           => {type => 'integer'},
-    level             => {type => 'enum', 'values' => [qw(member administrator superuser)]},
-    status            => {type => 'enum', 'values' => [qw(active inactive pending barred)], 'default' => 'active'  },  #status set by the admin
-    member_status     => {type => 'enum', 'values' => [qw(active inactive pending barred)], 'default' => 'inactive'},  #status set by the user
+    group_member_id       => {type => 'serial', primary_key => 1, not_null => 1},
+    webgroup_id           => {type => 'integer', 'alias' => 'group_id'},
+    user_id               => {type => 'integer'},
+    level                 => {type => 'enum', 'values' => [qw(member administrator)]},
+    status                => {type => 'enum', 'values' => [qw(active inactive pending barred)], 'default' => 'active'  },  #status set by the admin
+    member_status         => {type => 'enum', 'values' => [qw(active inactive pending barred)], 'default' => 'inactive'},  #status set by the user
+    data                  => {type => 'datamap'}
+  ],
+
+  virtual_columns       => [
+    notify_edit           => {column => 'data'},
+    notify_share          => {column => 'data'},
+    notify_join           => {column => 'data'},
   ],
 
   relationships         => [
-    user => {
-      'type'        => 'many to one',
-      'class'       => 'EnsEMBL::ORM::Rose::Object::User',
-      'column_map'  => {'user_id' => 'user_id'},
+    user                  => {
+      'type'                => 'many to one',
+      'class'               => 'EnsEMBL::ORM::Rose::Object::User',
+      'column_map'          => {'user_id' => 'user_id'},
     },
-    group => {
-      'type'        => 'many to one',
-      'class'       => 'EnsEMBL::ORM::Rose::Object::Group',
-      'column_map'  => {'webgroup_id' => 'webgroup_id'},
+    group                 => {
+      'type'                => 'many to one',
+      'class'               => 'EnsEMBL::ORM::Rose::Object::Group',
+      'column_map'          => {'webgroup_id' => 'webgroup_id'},
     }
   ]
 );
@@ -112,18 +120,10 @@ sub activate {
   $self->level('member');
 }
 
-sub inactivate_user {
+sub inactivate {
   ## Modifies the status and member_status to make user status inactive
   my $self = shift;
   $self->status('inactive');
-  $self->member_status('active');
-  $self->level('member');
-}
-
-sub inactivate_group {
-  ## Modifies the status and member_status to make group status inactive
-  my $self = shift;
-  $self->status('active');
   $self->member_status('inactive');
   $self->level('member');
 }
