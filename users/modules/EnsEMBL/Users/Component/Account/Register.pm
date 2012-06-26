31a407a2726e0fc4340cee7082885bfac399d723
Harpreet Riat <hr5@sanger.ac.uk>
2012-06-26 12:18:01 +0000

    make use of a general method add_user_details_fields

--- users/modules/EnsEMBL/Users/Component/Account/Register.pm
+++ users/modules/EnsEMBL/Users/Component/Account/Register.pm
@@ -1,6 +1,7 @@
 package EnsEMBL::Users::Component::Account::Register;
 
 ### Component for new user Registeration page
+### @author hr5
 
 use strict;
 
@@ -13,44 +14,15 @@ sub caption {
 sub content {
   my $self    = shift;
   my $hub     = $self->hub;
-  my $content = $self->get_wrapper_div;
-  my $form    = $content->append_child($self->new_form({'action' => $hub->url({'action' => 'AddUser'})}));
-  my $lists   = $hub->species_defs->SUBSCRIPTION_EMAIL_LISTS;
+  my $content = $self->wrapper_div;
+  my $form    = $content->append_child($self->new_form({'action' => $hub->url({qw(action User function Add)})}));
 
-  $form->add_field({
-    'label'     => 'Name',
-    'name'      => 'name',
-    'type'      => 'string',
-    'required'  => 1,
-  });
+  $form->add_field({qw(type honeypot name title   label Title)});      # honeypot fields for catching bots
+  $form->add_field({qw(type honeypot name surname label Surname)});
 
-  $form->add_field({
-    'label'     => 'Email Address',
-    'name'      => 'email',
-    'type'      => 'email',
-    'required'  => 1,
-    'notes'     => sprintf("You'll use this to log in to %s.", $self->site_name),
-  });
+  $self->add_user_details_fields($form, {'email_notes' => sprintf("You'll use this to log in to %s.", $self->site_name)});
 
-  $form->add_field([ map {'type' => 'honeypot', 'name' => lc $_, 'label' => $_}, qw(Surname Address) ]); # honeypot fields for catching bots
-
-  $form->add_field({
-    'label'     => 'Organisation',
-    'name'      => 'organisation',
-    'type'      => 'string',
-  });
-
-  $form->add_field({
-    'label'   => 'Ensembl news list subscription',
-    'type'    => 'checklist',
-    'name'    => 'subscription',
-    'notes'   => 'Tick the box corresponding to the email list you would wish to subscribe to',
-    'values'  => [ map {'value' => $_, 'caption' => $_, 'checked' => 1}, @$lists ]
-  }) if @$lists;
-
-  $form->add_button({'value' => 'Register'});
-
-  return $content->render . $self->render_openid_buttons;
+  return $content->render;
 }
 
 1;
