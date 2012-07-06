d6941353bdb478c196e23278b0a63a25a2c08470
Harpreet Riat <hr5@sanger.ac.uk>
2012-07-06 12:06:32 +0000

    unwanted methods removed - these methods where implimenting 'business logic', so shouldn't be in this file; make use of rose's inbuilt find_objects and add_objects inbuilt methods

--- users/modules/EnsEMBL/ORM/Rose/Object/User.pm
+++ users/modules/EnsEMBL/ORM/Rose/Object/User.pm
@@ -64,7 +64,7 @@ __PACKAGE__->meta->setup(
 ############################
 
 sub activate_login {
-  ## Activates the given login object after copying the information like name, organisation, country to the user and linking both login to the user (does not save to the database afterwards)
+  ## Activates the given login object after copying the information like name, organisation, country to the user and linking login object to the user object (does not save to the database afterwards)
   ## @param Login object to be linked an activated
   my ($self, $login) = @_;
 
@@ -76,8 +76,8 @@ sub activate_login {
 
 sub get_local_login {
   ## Gets the local login object related to the user
-  my $self = shift;
-  return shift @{$self->find_logins('query' => ['type' => 'local'])};
+  ## @return Login object if found
+  return shift @{shift->find_logins('query' => ['type' => 'local'])};
 }
 
 ############################
@@ -93,8 +93,7 @@ sub get_membership_object {
   ## @return Membership object, if found, undef otherwise
   my ($self, $group, $level) = @_;
   my $group_id = ref $group ? $group->group_id : $group or return undef;
-  $self->load('with' => 'memberships');
-  $_->group_id eq $group_id and return !$level || $level && $level eq $_->level ? $_ : undef for $self->memberships;
+  return shift @{$self->find_memberships('query' => ['group_id' => $group_id, 'status' => 'active', 'member_status' => 'active', $level ? ('level' => $level) : ()])};
 }
 
 sub admin_memberships {
@@ -112,7 +111,7 @@ sub active_memberships {
   return shift->find_memberships('query' => ['status' => 'active', 'member_status' => 'active']);
 }
 
-sub create_group_membership {
+sub create_membership_object {
   ## Creates a membership and group with the given details
   ## @param Hashref with keys as column (and relationships) for the membership object
   ## @return Memberhsip object with a new group (not yet saved to the database)
@@ -120,7 +119,7 @@ sub create_group_membership {
 
   return $self->meta->relationship('memberships')->class->new(
     'level'         => 'administrator',
-    'user_id'       => $self->user_id,  # many to one relation do not save objects, so have to provide the foreign key value
+    'user_id'       => $self->user_id,  # many to one relation does not save related objects, so have to provide the foreign key value
     'status'        => 'active',
     'member_status' => 'active',
     'group'         => {
@@ -143,7 +142,8 @@ sub is_member_of {
   ## @param Group rose object or id of the group
   ## @return 1 or undef accordingly
   my ($self, $group) = @_;
-  return $self->get_membership_object($group) ? 1 : undef;
+  my $membership = $self->get_membership_object($group);
+  return $membership && $membership->is_active ? 1 : undef;
 }
 
 sub is_admin_of {
@@ -151,8 +151,8 @@ sub is_admin_of {
   ## @param Group rose object or id of the group
   ## @return 1 or undef accordingly
   my ($self, $group) = @_;
-  my $membership = $self->get_membership_object($group);
-  return $membership && $membership->level eq 'administrator' ? 1 : undef;
+  my $membership = $self->get_membership_object($group, 'administrator');
+  return $membership && $membership->is_active ? 1 : undef;
 }
 
 sub is_nonadminmember_of {
@@ -160,95 +160,8 @@ sub is_nonadminmember_of {
   ## @param Group rose object or id of the group
   ## @return 1 or undef accordingly
   my ($self, $group) = @_;
-  my $membership = $self->get_membership_object($group);
-  return $membership && $membership->level eq 'member' ? 1 : undef;
-}
-
-sub request_membership {
-  ## Creates a request for a group
-  ## This adds a new row in the membership's table, but only if no existing row is found with same user_id and group_id
-  ## @param Rose Group object to be sent request for
-  ## @return
-  ##   1  if request is created (or already exists)
-  ##   -1 if user joined the group now (open group) or was already an active member of group
-  ##   0  if user is blocked from the group
-  my ($self, $group) = @_;
-
-  my $membership = $group->membership($self, 'member');
-
-  return 0  if $membership->is_user_blocked;
-  return -1 if $membership->is_active;
-
-  my $return = -1;
-  if ($group->type eq 'open') {
-    $membership->activate;
-  } else {
-    $membership->make_request unless $membership->is_pending_request;
-    $return = 1;
-  }
-  $membership->save(user => $self);
-
-  return $return;
-}
-
-sub block_group {
-  ## Blocks a group from sending any further invitations
-  ## @param Group to be blocked - Rose Group object
-  ## @return 1 if group blocked successfully (or was already blocked), -1 if user is not a member of the group or 0 if user is admin of the group
-  my ($self, $group) = @_;
-
-  if (my $membership = $group->get_membership($self)) {
-    return 0 if $membership->level ne 'member';
-    $membership->block_group;
-    $membership->save(user => $self);
-    return 1;
-  }
-  return -1;
-}
-
-sub unblock_group {
-  ## Unblocks a previously blocked group
-  ## @param Group to be unblocked - Rose Group object
-  ## @return 1 if unblocked successfully, 0 if user not a member of this group, -1 if group not blocked
-  my ($self, $group) = @_;
-
-  if (my $membership = $group->get_membership($self)) {
-    return -1 unless $membership->is_group_blocked;
-    $membership->inactivate_group;
-    $membership->save(user => $self);
-    return 1;
-  }
-  return 0;
-}
-
-sub accept_request {
-  ## Makes the status of the user active - only if membership is a pending invitation for user
-  ## @param Rose Group object or id of the group
-  ## @return 1 if user set active successfully, 0 if user not a member of this group, -1 if current status is anything other than pending request from user
-  my ($self, $group) = @_;
-
-  if (my $membership = $self->get_membership_object($group)) {
-    return -1 unless $membership->is_pending_invitation;
-    $membership->activate;
-    $membership->save(user => $self);
-    return 1;
-  }
-  return 0;
-}
-
-sub inactivate_group {
-  ## Makes the status of the group inactive - only an active user or a pending invitation or a pending request can be inactivated
-  ## @param Group whose membership is to be set as inactive - Rose Group object or id of the group
-  ## @return 1 if user set inactive successfully, 0 if user not a member of this group, -1 if user membership is neither active nor is the membership status a pending invitation or request
-  my ($self, $group) = @_;
-
-  if (my $membership = $self->get_membership_object($group)) {
-    return -1 unless $membership->is_active || $membership->is_pending_invitation || $membership->is_pending_request;
-    $membership->inactivate_group;
-    $membership->save(user => $self);
-    return 1;
-  }
-  return 0;
+  my $membership = $self->get_membership_object($group, 'member');
+  return $membership && $membership->is_active ? 1 : undef;
 }
 
 #########################
@@ -263,11 +176,11 @@ sub create_record   {
   ## @param Hashref of name value pair for columns of the new object (optional)
   my ($self, $type, $params) = @_;
 
-  return $self->meta->relationship('records')->class->new(
+  return ($self->add_records([{
     'user_id'       => $self->user_id,
     'type'          => $type,
     %{$params || {}}
-  );
+  }]))[0];
 }
 
 
