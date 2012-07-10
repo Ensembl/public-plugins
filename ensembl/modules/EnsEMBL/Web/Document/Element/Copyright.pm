07048477717a322a52cb5b146f4acc1b05fbfb94
Harpreet Riat <hr5@sanger.ac.uk>
2012-07-10 15:34:56 +0000

    footer layout based in two column css

--- ensembl/modules/EnsEMBL/Web/Document/Element/Copyright.pm
+++ ensembl/modules/EnsEMBL/Web/Document/Element/Copyright.pm
@@ -3,6 +3,7 @@ package EnsEMBL::Web::Document::Element::Copyright;
 ### Replacement copyright notice for www.ensembl.org
 
 use strict;
+
 use URI::Escape qw(uri_escape);
 
 use base qw(EnsEMBL::Web::Document::Element);
@@ -29,17 +30,18 @@ sub content {
 
   $html .= sprintf(
     q(
-    <div class="twocol-left left unpadded">
-        %s release %d - %s
-      &copy; <span class="print_hide"><a href="http://www.sanger.ac.uk/" class="nowrap constant">WTSI</a> /
-      <a href="http://www.ebi.ac.uk/" class="nowrap constant">EBI</a></span>
-      <span class="screen_hide_inline">WTSI / EBI<br />http://%s</span>
+    <div class="column-two left">
+      <p class="no-top-margin">%s release %d - %s &copy;
+        <span class="print_hide"><a href="http://www.sanger.ac.uk/" class="nowrap constant">WTSI</a> /
+        <a href="http://www.ebi.ac.uk/" class="nowrap constant">EBI</a></span>
+        <span class="screen_hide_inline">WTSI / EBI<br />http://%s</span>
+      </p>
     ),
     $sd->ENSEMBL_SITETYPE, $sd->ENSEMBL_VERSION,
     $sd->ENSEMBL_RELEASE_DATE, $sd->ENSEMBL_SERVERNAME,
     );
   
-  $html .= '<div class="print_hide">'; 
+  $html .= '<p class="print_hide">'; 
   unless ($ENV{'ENSEMBL_TYPE'} =~ /Help|Account|UserData/) {
     $html .= qq{
         <a class="modal_link" id="p_link" href="/Help/Permalink?url=$stable_URL">Permanent link</a>
@@ -49,7 +51,7 @@ sub content {
       $html .= ' - <a class="modal_link" id="a_link" href="/Help/ArchiveList">View in archive site</a>';
     }
   }
-  $html .= '</div></div>';
+  $html .= '</p></div>';
   return $html;
 }
 
--- ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -9,13 +9,15 @@ use base qw(EnsEMBL::Web::Document::Element);
 sub content {
   ## Note that these links always go to the live site so that the information is up-to-date
   return qq(
-    <div class="twocol-right right unpadded print_hide">
-      <a href="http://www.ensembl.org/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
-      <a href="http://www.ensembl.org/info/about/legal/privacy.html" class="constant">Privacy&nbsp;Policy</a> | 
-      <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a>
+    <div class="column-two right print_hide">
+      <p class="no-top-margin">
+        <a href="http://www.ensembl.org/info/about/intro.html" class="constant">About&nbsp;Ensembl</a> | 
+        <a href="http://www.ensembl.org/info/about/legal/privacy.html" class="constant">Privacy&nbsp;Policy</a> | 
+        <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a>
+      </p>
     </div>
-    <div class="twocol-right right unpadded screen_hide_block">
-      helpdesk\@ensembl.org
+    <div class="column-two right screen_hide_block">
+      <p class="no-top-margin">helpdesk\@ensembl.org</p>
     </div>
   );
 }
