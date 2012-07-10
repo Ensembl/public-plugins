fbd24608bba4e8b10e2cda318721e80c94e44952
Harpreet Riat <hr5@sanger.ac.uk>
2012-07-10 12:50:15 +0000

    messages improved; 'get_' methods changed to 'fetch_'

--- users/modules/EnsEMBL/Web/Object/Account.pm
+++ users/modules/EnsEMBL/Web/Object/Account.pm
@@ -17,51 +17,72 @@ use EnsEMBL::ORM::Rose::Manager::Membership;
 use base qw(EnsEMBL::Web::Object);
 
 sub _messages {## TODO change message texts
-  MESSAGE_OPENID_CANCELLED      => 135 => sub { sprintf q(Your request to login via %s was cancelled. Please try again, or use one of the alternative login options below.), shift->hub->param('provider') || 'OpenID' },
-  MESSAGE_OPENID_INVALID        => 153 => '_message__OPENID_INVALID',
+  my $self  = shift;
+  my $hub   = $self->hub;
+
+  # Message error constants    => code => message string
+  MESSAGE_OPENID_CANCELLED      => 104 => sprintf('Your request to login via %s was cancelled. Please try again, or use one of the alternative login options below.', $hub->param('provider') || 'OpenID'),
+  MESSAGE_OPENID_INVALID        => 128 => '_message__OPENID_INVALID',
   MESSAGE_OPENID_SETUP_NEEDED   => 167 => '_message__OPENID_SETUP_NEEDED',
-  MESSAGE_OPENID_ERROR          => 278 => '_message__OPENID_ERROR',
-  MESSAGE_OPENID_EMAIL_MISSING  => 233 => '_message__OPENID_EMAIL_MISSING',
-  MESSAGE_EMAIL_NOT_FOUND       => 256 => 'The email address provided is not recognised. Please try again.',
-  MESSAGE_PASSWORD_WRONG        => 297 => 'The password provided is invalid. Please try again.',
-  MESSAGE_ALREADY_REGISTERED    => 345 => sub { sprintf q(The email address provided seems to be already registered. Please try to login with the email, or request to <a href="%s">retrieve your password</a> if you have lost one.), $_[0]->hub->url({'action' => 'Password', 'function' => 'Lost', 'email' => $_[0]->hub->param('email')}) },
-  MESSAGE_CONFIRMATION_FAILED   => 354 => 'The email address could not be confirmed.',
-  MESSAGE_VERIFICATION_FAILED   => 367 => 'The email address could not be verified.',
-  MESSAGE_INVALID_PASSWORD      => 374 => 'Password needs to be atleast 6 characters long.',
-  MESSAGE_PASSWORD_MISMATCH     => 457 => 'The passwords do not match. Please try again.',
+  MESSAGE_OPENID_ERROR          => 178 => '_message__OPENID_ERROR',
+  MESSAGE_OPENID_EMAIL_MISSING  => 189 => '_message__OPENID_EMAIL_MISSING',
+  MESSAGE_EMAIL_NOT_FOUND       => 206 => 'The email address provided is not recognised. Please try again.',
+  MESSAGE_PASSWORD_WRONG        => 247 => 'The password provided is invalid. Please try again.',
+  MESSAGE_PASSWORD_INVALID      => 272 => 'Password needs to be atleast 6 characters long.',
+  MESSAGE_PASSWORD_MISMATCH     => 289 => 'The passwords do not match. Please try again.',
+  MESSAGE_ALREADY_REGISTERED    => 297 => sprintf('The email address provided seems to be already registered. Please try to login with the email, or request to <a href="%s">retrieve your password</a> if you have lost one.', $hub->url({'action' => 'Password', 'function' => 'Lost', 'email' => $hub->param('email')})),
+  MESSAGE_CONFIRMATION_FAILED   => 314 => 'The email address could not be confirmed.',
+  MESSAGE_VERIFICATION_FAILED   => 337 => 'The email address could not be verified.',
+  MESSAGE_VERIFICATION_PENDING  => 351 => '_message__VERIFICATION_PENDING',
   MESSAGE_LOGIN_MISSING         => 436 => '_message__LOGIN_MISSING',
   MESSAGE_EMAIL_INVALID         => 492 => 'Please enter a valid email address',
+  MESSAGE_EMAILS_INVALID        => 510 => sprintf('Invalid email address: %s', $hub->param('invalids') || ''),
   MESSAGE_NAME_MISSING          => 543 => 'Please provide a name',
   MESSAGE_ACCOUNT_BLOCKED       => 563 => 'Your account seems to be blocked. Please contact the helpdesk in case you need any help.',
-  MESSAGE_VERIFICATION_SENT     => 528 => sub { sprintf q(A verification email has been sent to the email address '%s'. Please go to your inbox and click on the link provided in the email.), shift->hub->param('email') },
-  MESSAGE_PASSWORD_EMAIL_SENT   => 602 => sub { sprintf q(An email has been sent to the email address '%s'. Please go to your inbox and follow the instructions to reset your password provided in the email.), shift->hub->param('email') },
-  MESSAGE_EMAIL_CHANGED         => 645 => sub { sprintf q(You email address on our records has been successfully changed. Please <a href="%s">login</a> with your new email address to continue.), shift->hub->url({'action' => 'Preferences'})},
+  MESSAGE_VERIFICATION_SENT     => 528 => sprintf(q(A verification email has been sent to the email address '%s'. Please go to your inbox and click on the link provided in the email.), $hub->param('email')),
+  MESSAGE_PASSWORD_EMAIL_SENT   => 602 => sprintf(q(An email has been sent to the email address '%s'. Please go to your inbox and follow the instructions to reset your password provided in the email.), $hub->param('email')),
+  MESSAGE_EMAIL_CHANGED         => 645 => sprintf(q(You email address on our records has been successfully changed. Please <a href="%s">%s</a> to continue.), $hub->user ? 'click here' : 'login', $hub->url({'action' => 'Preferences'})),
   MESSAGE_CANT_DELETE_LOGIN     => 691 => 'You can not delete the only login option you have to access your account.',
-  MESSAGE_GROUP_NOT_FOUND       => 713 => 'Sorry, we could not find any group.',
-  MESSAGE_MEMBER_BLOCKED        => 735 => '_message_MESSAGE_MEMBER_BLOCKED',
+  MESSAGE_GROUP_NOT_FOUND       => 713 => 'Sorry, we could not find the specified group. Either the group does not exist or is inactive or is inaccessible to you for the action selected.',
+  MESSAGE_GROUP_INVITATION_SENT => 722 => sprintf(q{Invitation for the group sent successfully to the following email(s): %s}, $hub->param('emails')),
+  MESSAGE_CANT_DEMOTE_ADMIN     => 754 => 'Sorry, you can not demote yourself as you seem to be the only administrator of this group.',
   MESSAGE_BOOKMARK_NOT_FOUND    => 782 => '_message_BOOKMARK_NOT_FOUND',
-  MESSAGE_UNKNOWN_ERROR         => 952 => 'An unknown error occurred. Please try again or contact the help desk.',
+  MESSAGE_UNKNOWN_ERROR         => 952 => 'An unknown error occurred. Please try again or contact the help desk.'
 }
 
-sub caption               { return 'Your Account';                                                      }
-sub short_caption         { return 'Your Account';                                                      }
-sub default_action        { return $_[0]->hub->user ? 'Preferences' : 'Login'                           }
+sub caption               { return 'Your Account';                                                        }
+sub short_caption         { return 'Your Account';                                                        }
+sub default_action        { return $_[0]->hub->user ? 'Preferences' : 'Login'                             }
 
-sub openid_providers      { return [ map {$_} @{shift->hub->species_defs->OPENID_PROVIDERS} ];          } ## TODO do I need to map?
+sub openid_providers      { return [ map {$_} @{shift->hub->species_defs->OPENID_PROVIDERS} ];            } ## TODO do I need to map?
+sub get_root_url          { return $_[0]->{'_root_url'} ||= $_[0]->hub->species_defs->ENSEMBL_BASE_URL;   }
 
-sub get_root_url          { return $_[0]->{'_root_url'} ||= $_[0]->hub->species_defs->ENSEMBL_BASE_URL; }
-sub get_user_by_id        { return EnsEMBL::ORM::Rose::Manager::User->get_by_id($_[1]);                 }
-sub get_user_by_email     { return EnsEMBL::ORM::Rose::Manager::User->get_by_email($_[1]);              }
-sub new_login_account     { return EnsEMBL::ORM::Rose::Manager::Login->create_empty_object($_[1]);      }
-sub get_login_account     { return EnsEMBL::ORM::Rose::Manager::Login->get_with_user($_[1]);            }
-sub get_group             { return EnsEMBL::ORM::Rose::Manager::Group->fetch_by_primary_key($_[1]);       }
-sub get_all_groups        { return EnsEMBL::ORM::Rose::Manager::Group->get_objects;                       }
+sub new_login_account     { return EnsEMBL::ORM::Rose::Manager::Login->create_empty_object($_[1]);        }
+sub fetch_login_account   { return EnsEMBL::ORM::Rose::Manager::Login->get_with_user($_[1]);              }
+sub fetch_user_by_email   { return EnsEMBL::ORM::Rose::Manager::User->get_by_email($_[1]);                }
 
-## TODO - change above methods to 'fetch'
+sub fetch_membership {
+  ## Fetches a membership object with the given id
+  ## Wrapper around fetch_by_primary_key of the manager class
+  ## @params As accepted by fetch_by_primary_key method
+  return EnsEMBL::ORM::Rose::Manager::Membership->fetch_by_primary_key(splice @_, 1);
+}
 
-sub fetch_membership      { return EnsEMBL::ORM::Rose::Manager::Membership->fetch_by_primary_key($_[1]);  }
+sub fetch_group {
+  ## Fetches a group object with the given id
+  ## Wrapper around fetch_by_primary_key of the manager class
+  ## @params As accepted by fetch_by_primary_key method
+  return EnsEMBL::ORM::Rose::Manager::Group->fetch_by_primary_key(splice @_, 1);
+}
 
-sub get_login_from_url_code {
+sub fetch_groups {
+  ## Fetches group objects
+  ## Wrapper around get_objects of the manager class
+  ## @param Reference of a hash as accepted by get_objects method
+  return EnsEMBL::ORM::Rose::Manager::Group->get_objects(%{$_[1] || {}});
+}
+
+sub fetch_login_from_url_code {
   ## Fetches and returns a login object by parsing the code parameter in the url
   ## @param Flag if on, will ignore checking a valid user object related to the login object
   ## @return Login object for matching salt, login id and user id, undef otherwise
@@ -82,6 +103,32 @@ sub get_login_from_url_code {
   }
 
   return undef;
+}
+
+sub fetch_accessible_membership_for_user {
+  ## Fetches a membership object for the user for a given group
+  ## Membership object fetch is active and linked to an active group unless the user is admin - ie. it will only get an inactive group membership for an admin of the group
+  ## @param User object
+  ## @param Group object or group id
+  ## @param Optional hashref to be passed to find_memberships method of user object
+  ## @return Membership object if found
+  my ($self, $user, $group, $params) = @_;
+  push @{$params->{'query'} ||= []}, ('or' => ['level' => 'administrator', 'group.status' => 'active'], 'status' => 'active', 'member_status' => 'active');
+  push @{$params->{'with_objects'} ||= []}, 'group';
+  return $user->get_membership_object($group, {%$params, 'multi_many_ok' => 1});
+}
+
+sub fetch_active_membership_for_user {
+  ## Fetches a membership object for the user for a given group
+  ## Membership object fetch is active and linked to an active group
+  ## @param User object
+  ## @param Group object or group id
+  ## @param Optional hashref to be passed to find_memberships method of user object
+  ## @return Membership object if found
+  my ($self, $user, $group, $params) = @_;
+  push @{$params->{'query'} ||= []}, ('group.status' => 'active', 'status' => 'active', 'member_status' => 'active');
+  push @{$params->{'with_objects'} ||= []}, 'group';
+  return $user->get_membership_object($group, {%$params, 'multi_many_ok' => 1});
 }
 
 sub new_user_account {
