a799b48ded949196c69bdd6fecc632b3e309af9b
Harpreet Riat <hr5@sanger.ac.uk>
2012-08-20 16:44:02 +0000

    links not displayed in the accounts dropdown if no bookmark if found

--- users/modules/EnsEMBL/Web/Document/Element/AccountLinks.pm
+++ users/modules/EnsEMBL/Web/Document/Element/AccountLinks.pm
@@ -8,19 +8,16 @@ use strict;
 
 
 sub content {
-  my $self  = shift;
-  my $hub   = $self->hub;
-  my $user  = $hub->user;
+  my $self      = shift;
+  my $hub       = $self->hub;
+  my $user      = $hub->user;
+  my $bookmarks = $user ? $user->bookmarks : [];
 
   return $user
     ? sprintf('<a class="constant _accounts_link account-link" href="%s"><span class="acc-email">%s</span><span class="acc-arrow"><span>&#9660;</span><span class="selected">&#9650;</span></a>
         <div class="_accounts_dropdown accounts-dropdown">
           <h4>Bookmarks</h4>
-          <div>%s</div>
-          <div>
-            <p><a href="%s" class="modal_link" title="User Account">Bookmark this page</a></p>
-            <p><a href="%s" class="modal_link" title="User Account">View all bookmarks</a></p>
-          </div>
+          <div>%s</div>%s
           <h4>Account</h4>
           <div>
             <p><a href="%1$s" class="modal_link" title="User Account">Edit Settings</a></p>
@@ -28,15 +25,26 @@ sub content {
             <p><strong><a href="%s">Logout</a></strong></p>
           </div>
         </div>',
-        $hub->url({qw(species Multi type Account action Preferences)}),
+        $hub->url({qw(type Account action Preferences)}),
         $user->email,
-        join('', map {sprintf '<p><a href="%s" title="%s: %1$s"><span>%2$s</span><span class="acc-bookmark-overflow">&#133;</span></a></p>', $_->url, $_->name} @{$user->bookmarks}) || '<p><i>No bookmark added</i></p>',
-        $hub->url({qw(species Multi type Account action Bookmark function Add)}),
-        $hub->url({qw(species Multi type Account action Bookmark function View)}),
-        $hub->url({qw(species Multi type Account action ChangePassword)}),
-        $hub->url({qw(species Multi type Account action Logout)})
+        join('', map {
+          sprintf '<p><a href="%s" title="%s: %s"><span>%2$s</span><span class="acc-bookmark-overflow">&#133;</span></a></p>',
+            $hub->url({'species' => '', 'type' => 'Account', 'action' => 'Bookmark', 'function' => 'Use', 'id' => $_->record_id, '__clear' => 1}),
+            $_->shortname || $_->name,
+            $_->url
+        } @$bookmarks) || '<p><i>No bookmark added</i></p>',
+        @$bookmarks ? sprintf('
+          <div>
+            <!--p><a href="%s" class="modal_link" title="User Account">Bookmark this page</a></p-->
+            <p><a href="%s" class="modal_link" title="User Account">View all bookmarks</a></p>
+          </div>',
+          $hub->url({qw(type Account action Bookmark function Add)}),
+          $hub->url({qw(type Account action Bookmark function View)})
+        ) : '',
+        $hub->url({qw(type Account action ChangePassword)}),
+        $hub->url({qw(type Account action Logout)})
       )
-    : sprintf('<a class="constant modal_link account-link" href="%s" title="Login/Register">Login/Register</a>',  $hub->url({qw(species Multi type Account action Login)}))
+    : sprintf('<a class="constant modal_link account-link" href="%s" title="Login/Register">Login/Register</a>',  $hub->url({qw(type Account action Login)}))
   ;
 }
 
