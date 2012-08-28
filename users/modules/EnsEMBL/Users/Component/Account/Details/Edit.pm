d903c078108b0c01b59ff4fa756a9c550e6b776d
Harpreet Riat <hr5@sanger.ac.uk>
2012-08-28 15:41:11 +0000

    display a link 'Change' for changing email instead of always showing an input

--- users/modules/EnsEMBL/Users/Component/Account/Details/Edit.pm
+++ users/modules/EnsEMBL/Users/Component/Account/Details/Edit.pm
@@ -8,27 +8,48 @@ use warnings;
 
 use base qw(EnsEMBL::Users::Component::Account);
 
+use constant JS_CLASS_CHANGE_EMAIL => '_change_email';
+
 sub content {
-  my $self        = shift;
-  my $hub         = $self->hub;
-  my $user        = $hub->user;
-  my $email       = $user->email;
-  my $form        = $self->new_form({'action' => {qw(action Details function Save)}});
-  my $email_note  = '';
+  my $self            = shift;
+  my $hub             = $self->hub;
+  my $user            = $hub->user;
+  my $email           = $user->email;
+  my $form            = $self->new_form({'action' => {qw(action Details function Save)}});
+  my $js_change_email = $self->JS_CLASS_CHANGE_EMAIL;
+  my $email_note      = '';
 
   $_->type eq 'local' && $_->identity eq $email and $email_note = sprintf('You use this email to login to %s.<br />', $self->site_name) and last for @{$user->logins};
 
+  $form->add_field({
+    'label'         => 'Email Address',
+    'notes'         => qq($email_note<div class="hidden $js_change_email">An email will be sent to the new address for verification purposes if email address is changed.</div>),
+    'elements'      => [{
+      'type'          => 'noedit',
+      'element_class' => $js_change_email,
+      'value'         => qq($email <a href="#ChangeEmail" class="small $js_change_email">Change</a>),
+      'no_input'      => 1,
+      'is_html'       => 1
+    }, {
+      'type'          => 'email',
+      'name'          => 'email',
+      'value'         => $email,
+      'no_asterisk'   => 1,
+      'shortnote'     => qq(<a href="#Cancel" class="small $js_change_email hidden">Cancel</a>),
+      'element_class' => qq($js_change_email hidden)
+    }]
+  });
+
   $self->add_user_details_fields($form, {
-    'email'         => $email,
     'name'          => $user->name,
     'organisation'  => $user->organisation,
     'country'       => $user->country,
     'no_list'       => 1,
+    'no_email'      => 1,
     'button'        => 'Save',
-    'email_notes'   => $email_note . 'If email address is changed, an email will be sent to the new address for verification purposes.'
   });
 
-  return $self->js_section({'id' => 'edit_details', 'subsections' => [ $form->render ]});
+  return $self->js_section({'id' => 'edit_details', 'subsections' => [ $form->render ], 'js_panel' => 'AccountForm'});
 }
 
 1;
