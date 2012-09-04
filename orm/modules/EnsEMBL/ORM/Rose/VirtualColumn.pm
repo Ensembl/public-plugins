7c14e4480397e6fc30928bf5f67a977dad7e2a83
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-04 10:19:20 +0000

    make use of MethodMaker's add_method; feature added for setting default value of the virtual column

--- orm/modules/EnsEMBL/ORM/Rose/VirtualColumn.pm
+++ orm/modules/EnsEMBL/ORM/Rose/VirtualColumn.pm
@@ -6,38 +6,46 @@ package EnsEMBL::ORM::Rose::VirtualColumn;
 use strict;
 use warnings;
 
-use overload ('""'  => 'name');
+use EnsEMBL::Web::Tools::MethodMaker qw(add_method);
+
+use overload qw("" name);
 
 sub new {
   ## @constructor
   ## @params Hashref with keys:
-  ##  - name:   actual key name
-  ##  - column: column name that created the hash saved as string
-  ##  - alias:  alternative name to be used as a method name for rose object if key name is reserved in rose
-  ##  - parent: Meta class of the rose object
+  ##  - name:     actual key name
+  ##  - column:   column name that created the hash saved as string
+  ##  - alias:    alternative name to be used as a method name for rose object if key name is reserved in rose
+  ##  - parent:   Meta class of the rose object
+  ##  - default:  Default value
   my ($class, $params) = @_;
   return bless $params, $class;
 }
 
-sub type                  { return 'virtual';                           }
-sub name                  { return shift->{'name'};                     }
-sub column                { return shift->{'column'};                   }
-sub alias                 { return shift->{'alias'};                    }
-sub parent                { return shift->{'parent'};                   }
-sub accessor_method_name  { return $_[0]->{'alias'} || $_[0]->{'name'}; }
-sub mutator_method_name   { return shift->accessor_method_name;         }
+sub type                  { return 'virtual';                                               }
+sub name                  { return shift->{'name'};                                         }
+sub column                { return shift->{'column'};                                       }
+sub alias                 { return shift->{'alias'};                                        }
+sub parent                { return shift->{'parent'};                                       }
+sub default_exists        { return exists $_[0]->{'default'} && defined $_[0]->{'default'}; }
+sub delete_default        { return shift->{'default'} = undef;                              }
+sub accessor_method_name  { return $_[0]->{'alias'} || $_[0]->{'name'};                     }
+sub mutator_method_name   { return shift->accessor_method_name;                             }
+
+sub default {
+  ## Sets/Gets the default value of the virtual column
+  my $self = shift;
+  $self->{'default'} = shift if @_;
+  return $self->{'default'};
+}
 
 sub make_methods {
   ## Creates the method to access/modify the value from the rose object
-  ## @param Hash with key target_class - rose object class name
-  my ($self, %params) = @_;
-  my $object_class = $params{'target_class'};
-  my $method_name  = $self->accessor_method_name;
+  my $self = shift;
 
-  no strict qw(refs);
-  *{"${object_class}::${method_name}"} = sub {
+  add_method($self->parent->class, $self->accessor_method_name, sub {
     return shift->virtual_column_value($self, @_);
-  };
+  });
 }
 
 1;
