16d9142b9820e18ba55717b4d7c4b759ac14e575
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-04 13:06:48 +0000

    new membership created should be pending invitation instead of active one

--- users/modules/EnsEMBL/Users/Command/Account/Membership/Create.pm
+++ users/modules/EnsEMBL/Users/Command/Account/Membership/Create.pm
@@ -1,39 +1,40 @@
 package EnsEMBL::Users::Command::Account::Membership::Create;
 
 ### Creates the membership object for an invitation sent to a new user (saved in user records)
+### Membership created is a "pending" invitation
 
 use strict;
 use warnings;
+
+use EnsEMBL::Users::Messages qw(MESSAGE_GROUP_NOT_FOUND MESSAGE_URL_EXPIRED);
 
 use base qw(EnsEMBL::Users::Command::Account::Membership);
 
 sub modify_membership {
   my ($self, $membership) = @_;
 
-  my $user        = $self->hub->user->rose_object;
-  my $invitation  = $self->object->fetch_invitation_record_from_url_code;
+  my %redirect_url  = qw(type Account action Groups);
+  my $return        = undef;
+  my $invitation    = $self->object->fetch_invitation_record_from_url_code;
 
   if ($invitation) {
-    my $email_matching = grep {$_ eq $invitation->email} ($user->email, map {$_->status eq 'active' ? $_->identity : ()} ($user->get_local_login || ()));
-  
-    if ($email_matching) {
-      my $group = $invitation->group;
-      if ($group && $group->status eq 'active') {
-        my $group_id = $group->group_id;
-        $membership->group_id($group_id);
-        $invitation->delete;
-        $self->redirect_url({'action' => 'Groups', 'function' => 'View', 'id' => $group_id});
-        return 1;
-      } else {
-        ## TODO - error no group found
-      }
+
+    # we do not check if the email to which the invitation was sent is same as user's email as user can have a different registered email
+    my $group = $invitation->group;
+    if ($group && $group->status eq 'active') {
+      my $group_id = $group->group_id;
+      $membership->group_id($group_id);
+      $membership->make_invitation;
+      $invitation->delete;
+      $return = 1;
     } else {
-      ## TODO - redirect a page asking for email verification
+      $redirect_url{'err'} = MESSAGE_GROUP_NOT_FOUND;
     }
-    return undef;
   } else {
-    ## TODO - could not find invitation
+    $redirect_url{'err'} = MESSAGE_URL_EXPIRED;
   }
+  $self->redirect_url(\%redirect_url);
+  return $return;
 }
 
 1;
