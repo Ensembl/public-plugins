b0ea171b07b7778563e4145185e87cdebe581932
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-14 15:48:42 +0000

    save as a new bookmark if user is not the admin of the group, or creator of the bookmark

--- users/modules/EnsEMBL/Users/Command/Account/Bookmark/Save.pm
+++ users/modules/EnsEMBL/Users/Command/Account/Bookmark/Save.pm
@@ -10,14 +10,22 @@ sub process {
   my $self        = shift;
   my $object      = $self->object;
   my $hub         = $self->hub;
+  my $user        = $hub->user;
   my $bookmark_id = $hub->param('id');
 
-  if (my ($bookmark, $owner) = $object->fetch_bookmark_with_owner( $bookmark_id ? ($bookmark_id, $hub->param('group')) : 0 )) {
-    $bookmark->$_($hub->param($_)) for qw(name shortname url object);
-    $bookmark->save('user' => $hub->user);
+  if (my ($bookmark, $record_owner) = $object->fetch_bookmark_with_owner( $bookmark_id ? ($bookmark_id, $hub->param('group')) : 0 )) {
 
-    return $self->ajax_redirect($hub->url($owner->RECORD_TYPE eq 'group'
-      ? {'action' => 'Groups',    'function' => 'View', 'id' => $owner->group_id}
+    # if we need to save a new copy of the bookmark
+    if ($record_owner->RECORD_TYPE eq 'group' && ($hub->param('save_new') || !($user->is_admin_of($record_owner) || $bookmark->created_by eq $user->user_id))) {
+      $bookmark = $bookmark->clone_and_reset;
+      $bookmark->click(0);
+    }
+
+    $bookmark->$_($hub->param($_)) for qw(name shortname url object);
+    $bookmark->save('user' => $user);
+
+    return $self->ajax_redirect($hub->url($record_owner->RECORD_TYPE eq 'group'
+      ? {'action' => 'Groups',    'function' => 'View', 'id' => $record_owner->group_id}
       : {'action' => 'Bookmark',  'function' => ''}
     ));
 
