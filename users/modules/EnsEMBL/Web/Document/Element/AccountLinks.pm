d303804e8ce60941dd272484e5923f2dc0a7c5fe
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-17 16:46:28 +0000

    some url changes

--- users/modules/EnsEMBL/Web/Document/Element/AccountLinks.pm
+++ users/modules/EnsEMBL/Web/Document/Element/AccountLinks.pm
@@ -21,11 +21,10 @@ sub content {
           <h4>Account</h4>
           <div>
             <p><a href="%1$s" class="modal_link" title="User Account">Edit Settings</a></p>
-            <p><a href="%s" class="modal_link" title="User Account">Change Password</a></p>
             <p><strong><a href="%s">Logout</a></strong></p>
           </div>
         </div>',
-        $hub->url({qw(type Account action Preferences)}),
+        $hub->url({qw(species common type Account action Preferences)}),
         $user->email,
         join('', map {
           sprintf '<p><a href="%s" title="%s: %s"><span>%2$s</span><span class="acc-bookmark-overflow">&#133;</span></a></p>',
@@ -38,13 +37,12 @@ sub content {
             <!--p><a href="%s" class="modal_link" title="User Account">Bookmark this page</a></p-->
             <p><a href="%s" class="modal_link" title="User Account">View all bookmarks</a></p>
           </div>',
-          $hub->url({qw(type Account action Bookmark function Add)}),
-          $hub->url({qw(type Account action Bookmark function View)})
+          $hub->url({qw(species common type Account action Bookmark function Add)}),
+          $hub->url({qw(species common type Account action Bookmark function View)})
         ) : '',
-        $hub->url({qw(type Account action ChangePassword)}),
-        $hub->url({qw(type Account action Logout)})
+        $hub->url({qw(species common type Account action Logout)})
       )
-    : sprintf('<a class="constant modal_link account-link" href="%s" title="Login/Register">Login/Register</a>',  $hub->url({qw(type Account action Login)}))
+    : sprintf('<a class="constant modal_link account-link" href="%s" title="Login/Register">Login/Register</a>',  $hub->url({qw(species common type Account action Login)}))
   ;
 }
 
