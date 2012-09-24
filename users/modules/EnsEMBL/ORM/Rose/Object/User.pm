98e4eb071c226da93f6af9c551fb662710a46022
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-24 09:29:41 +0000

    admin privilage stuff not working yet

--- users/modules/EnsEMBL/ORM/Rose/Object/User.pm
+++ users/modules/EnsEMBL/ORM/Rose/Object/User.pm
@@ -30,7 +30,7 @@ __PACKAGE__->meta->setup(
   relationships         => [
     logins                => { 'type' => 'one to many', 'class' => 'EnsEMBL::ORM::Rose::Object::Login',           'column_map' => {'user_id' => 'user_id'}  },
     memberships           => { 'type' => 'one to many', 'class' => 'EnsEMBL::ORM::Rose::Object::Membership',      'column_map' => {'user_id' => 'user_id'}, 'methods' => { map {$_, undef} qw(add_on_save count find get_set_on_save)} },
-    admin_privilege       => { 'type' => 'one to one',  'class' => 'EnsEMBL::ORM::Rose::Object::AdminPrivilage',  'column_map' => {'user_id' => 'user_id'}  },
+#    admin_privilege       => { 'type' => 'one to one',  'class' => 'EnsEMBL::ORM::Rose::Object::AdminPrivilage',  'column_map' => {'user_id' => 'user_id'}  },
     records               => __PACKAGE__->record_relationship_params('user_id')
   ],
 
