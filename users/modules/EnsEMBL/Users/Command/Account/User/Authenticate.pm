5d48e589609475807bb5b553c3da5ca697c5f5aa
Harpreet Riat <hr5@sanger.ac.uk>
2012-09-25 15:32:58 +0000

    provide email to registration link\ killing 'Odd number of elements in anonymous hash'

--- users/modules/EnsEMBL/Users/Command/Account/User/Authenticate.pm
+++ users/modules/EnsEMBL/Users/Command/Account/User/Authenticate.pm
@@ -19,12 +19,13 @@ sub process {
   my $self    = shift;
   my $object  = $self->object;
   my $hub     = $self->hub;
-  my $login   = $object->fetch_login_account($hub->param('email'));
+  my $email   = $hub->param('email') || '';
+  my $login   = $object->fetch_login_account($email);
 
-  return $self->redirect_login(MESSAGE_EMAIL_NOT_FOUND)                                   unless $login;
-  return $self->redirect_login(MESSAGE_VERIFICATION_PENDING)                              unless $login->status eq 'active';
-  return $self->redirect_login(MESSAGE_ACCOUNT_BLOCKED)                                   unless $login->user->status eq 'active';
-  return $self->redirect_login(MESSAGE_PASSWORD_WRONG, {'email' => $hub->param('email')}) unless $login->verify_password($hub->param('password'));
+  return $self->redirect_login(MESSAGE_EMAIL_NOT_FOUND, {'email' => $email})  unless $login;
+  return $self->redirect_login(MESSAGE_VERIFICATION_PENDING)                  unless $login->status eq 'active';
+  return $self->redirect_login(MESSAGE_ACCOUNT_BLOCKED)                       unless $login->user->status eq 'active';
+  return $self->redirect_login(MESSAGE_PASSWORD_WRONG, {'email' => $email})   unless $login->verify_password($hub->param('password') || '');
 
   return $self->redirect_after_login($login->user);
 }
