0004bc0d22700c3f1c7b721e930218ca7ec71998
Harpreet Riat <hr5@sanger.ac.uk>
2012-11-06 11:14:23 +0000

    modifications done to make it easy to add a customised page by inheriting this module

--- orm/modules/EnsEMBL/ORM/Component/DbFrontend/Input.pm
+++ orm/modules/EnsEMBL/ORM/Component/DbFrontend/Input.pm
@@ -15,40 +15,36 @@ sub content_tree {
   ## Generates a DOM tree for content HTML
   ## Override this one in the child class and do the DOM manipulation on the DOM tree if required
   ## Flags are set on required HTML elements for 'selection and manipulation' purposes in child classes (get_nodes_by_flag)
-  my $self    = shift;
-  my $object  = $self->object;
-  my $hub     = $self->hub;
-  my $action  = $hub->action;
+  my $self        = shift;
+  my $url_params  = shift || $self->get_url_params;
+  my $object      = $self->object;
+  my $hub         = $self->hub;
+  my $action      = $hub->action;
 
-  my $record  = $object->rose_object or return $self->dom->create_element('p', {'inner_HTML' => sprintf('No %s selected to edit.', $object->record_name->{'singular'})});
-  my $is_ajax = $object->is_ajax_request;
-  my $preview = $object->show_preview && $action ne 'Preview' && $is_ajax ne 'list';
-  my $serial  = $record->get_primary_key_value;
+  my $record      = $object->rose_object or return $self->dom->create_element('p', {'inner_HTML' => sprintf('No %s selected to edit.', $object->record_name->{'singular'})});
+  my $is_ajax     = $object->is_ajax_request;
+  my $preview     = $object->show_preview && $action ne 'Preview' && $is_ajax ne 'list';
+  my $serial      = $record->get_primary_key_value;
+  my $content     = $self->content_div;
 
-  my $content = $self->dom->create_element('div', {
-    'class'     => [$object->content_css, $self->_JS_CLASS_RESPONSE_ELEMENT],
-    'children'  => [$is_ajax ? {'node_name' => 'h3', 'class' => 'dbf-padded no-bottom-margin', 'inner_HTML' => sprintf('%s %s:', $object->action, $object->record_name->{'singular'})} : ()]
-  });
-  my $form    = $content->append_child($self->new_form({
-    'action' => $hub->url({'action' => $preview ? 'Preview' : 'Save', 'function' => $hub->function}),
-    'class'  => [ !$preview ? $serial ? $self->_JS_CLASS_SAVE_FORM : $self->_JS_CLASS_ADD_FORM : $self->_JS_CLASS_PREVIEW_FORM, 'dbf-padded' ]
+  my $form        = $content->append_child($self->new_form({
+    'action'        => $hub->url({'action' => $preview ? 'Preview' : 'Save', 'function' => $hub->function}),
+    'class'         => [ !$preview ? $serial ? $self->_JS_CLASS_SAVE_FORM : $self->_JS_CLASS_ADD_FORM : $self->_JS_CLASS_PREVIEW_FORM, 'dbf-padded' ]
   }));
 
-  my $url_params  = { map { my @vals = $hub->param($_); @vals > 1 ? ($_ => \@vals) : ($_ => shift @vals) } $hub->param };
   my $fields      = $self->unpack_rose_object($record, $url_params);
 
   # print fields  
   foreach my $field (@$fields) {
 
-    my $field_extras = $field->extras;
-    my $field_params = {'label' => $field->label, 'elements' => []};
-    $field_params->{$_} = $field_extras->{$_} for keys %$field_extras;
+    my $field_extras    = $field->extras;
+    my $field_params    = {'label' => $field->label, 'elements' => [], %$field_extras};
 
-    my $name   = $field->name;
-    my $lookup = {};
-    my $value  = '';
-    my $f_type = $field->field_type;
-    my $select = $f_type =~ /^(dropdown|checklist|radiolist)$/ ? 1 : 0;
+    my $name            = $field->name;
+    my $lookup          = {};
+    my $value           = '';
+    my $f_type          = $field->field_type;
+    my $select          = $f_type =~ /^(dropdown|checklist|radiolist)$/ ? 1 : 0;
 
     map {delete $field_params->{$_}} qw(notes shortnote) if $action eq 'Preview'; # don't show notes in form fields while previewing
     exists $url_params->{$name} and delete $url_params->{$name}; # remove keys included in fields so they do not repeat as hidden inputs later
@@ -142,6 +138,38 @@ sub content_tree {
   ]})->set_flag('buttons');
 
   return $content;
+}
+
+sub error_content_tree {
+  ## Returns the node tree to display some error
+  my ($self, $error)  = @_;
+  my $content         = $self->content_div;
+  my $form            = $content->append_child($self->new_form({'action' => '#', 'class' => 'dbf-padded'}));
+
+  $form->add_notes({'location' => 'head', 'text' => $error, 'class' => 'error', 'heading' => 'An error occurred while processing your request'});
+  $form->add_button({'type' => 'reset', 'value' => 'Back', 'class' => $self->_JS_CLASS_CANCEL_BUTTON})->set_flag('buttons') if $self->object->is_ajax_request;
+
+  return $content;
+}
+
+sub get_url_params {
+  ## Gets url param from hub
+  ## @return Hashref of key as name and value as value of the params (arrayref in case of multiple values)
+  my $self  = shift;
+  my $hub   = $self->hub;
+  
+  return { map { my @vals = $hub->param($_); @vals > 1 ? ($_ => \@vals) : ($_ => shift @vals) } $hub->param };
+}
+
+sub content_div {
+  ## Returns a div element for wrapping the content
+  my $self    = shift;
+  my $object  = $self->object;
+
+  return $self->dom->create_element('div', {
+    'class'         => [$object->content_css, $self->_JS_CLASS_RESPONSE_ELEMENT],
+    'children'      => [$object->is_ajax_request ? {'node_name' => 'h3', 'class' => 'dbf-padded no-bottom-margin', 'inner_HTML' => sprintf('%s %s:', $object->action, $object->record_name->{'singular'})} : ()]
+  });
 }
 
 1;
