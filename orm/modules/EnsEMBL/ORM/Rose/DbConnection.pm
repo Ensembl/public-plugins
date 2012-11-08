17887a7b74b35efc7992db3bd7e5fef53f455012
Dan Sheppard <ds23@sanger.ac.uk>
2012-11-08 09:31:11 +0000

    Force ROSE and Class::DBI to use different pools. Don't allow parent/child connection sharing in ROSE to avoid premature cleanup.
    Two further attempts to rectify admin lost connection issue. Improvements,
    but are they the current issue?

--- orm/modules/EnsEMBL/ORM/Rose/DbConnection.pm
+++ orm/modules/EnsEMBL/ORM/Rose/DbConnection.pm
@@ -24,12 +24,24 @@ my $species_defs = EnsEMBL::Web::SpeciesDefs->new;
 __PACKAGE__->use_private_registry;
 
 ## Something to do with rose's db cache
-__PACKAGE__->use_cache_during_apache_startup(1);
+__PACKAGE__->use_cache_during_apache_startup(0);
 
-## Set the default domain
+## Set the default domain & type
 __PACKAGE__->default_domain('ensembl');
+__PACKAGE__->default_type('user');
 
-## Register data sources from site defs
+## Register data source for users
+__PACKAGE__->register_db(
+  type      => 'user',
+  driver    => 'mysql',
+  database  => $species_defs->ENSEMBL_USERDB_NAME,
+  host      => $species_defs->ENSEMBL_USERDB_HOST,
+  port      => $species_defs->ENSEMBL_USERDB_PORT,
+  username  => $species_defs->ENSEMBL_USERDB_USER || $species_defs->DATABASE_WRITE_USER,
+  password  => $species_defs->ENSEMBL_USERDB_PASS || $species_defs->DATABASE_WRITE_PASS,
+);
+
+## Register other data sources from site defs
 while (my ($key, $details) = each %{$SiteDefs::ROSE_DB_DATABASES}) {
 
   my $params = $details;
