985d489a3fc2b2afd130fae3df19584a2a8e3e6e
Harpreet Riat <hr5@sanger.ac.uk>
2012-11-23 13:27:14 +0000

    'View on YouKu' link added

--- admin/modules/EnsEMBL/Admin/Component/HelpRecord/Display.pm
+++ admin/modules/EnsEMBL/Admin/Component/HelpRecord/Display.pm
@@ -13,8 +13,10 @@ sub record_tree {
   ## Overrides the default one to print corresponding youtube video link for youtube_id field
   my $self        = shift;
   my $record_div  = $self->SUPER::record_tree(@_);
-
   my $youtube_div = $record_div->get_nodes_by_flag('data.youtube_id');
+  my $youku_div   = $record_div->get_nodes_by_flag('data.youku_id');
+  my $html_div    = $record_div->get_nodes_by_flag(['data.answer', 'data.content']);
+
   if (@$youtube_div) {
     $youtube_div = $youtube_div->[0]->last_child;
     if (my $youtube_id = $youtube_div->inner_HTML) {
@@ -22,7 +24,13 @@ sub record_tree {
     }
   }
 
-  my $html_div    = $record_div->get_nodes_by_flag(['data.answer', 'data.content']);
+  if (@$youku_div) {
+    $youku_div = $youku_div->[0]->last_child;
+    if (my $youku_id = $youku_div->inner_HTML) {
+      $youku_div->inner_HTML(sprintf '%s (<a href="http://v.youku.com/v_show/id_%1$s.html" target="_blank">View on YouKu</a>)', $youku_id);
+    }
+  }
+
   if (@$html_div) {
     $html_div = $html_div->[0]->last_child;
     $html_div->inner_HTML($self->parse_help_html($html_div->inner_HTML, $self->object));
