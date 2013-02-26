dd7c186e3db1125ae73fc693a94746a23ad354a8
Harpreet Riat <hr5@sanger.ac.uk>
2013-02-26 11:01:28 +0000

    'target' key removed

--- users/modules/EnsEMBL/Users/Component/Account/Groups/Notifications.pm
+++ users/modules/EnsEMBL/Users/Component/Account/Groups/Notifications.pm
@@ -17,9 +17,9 @@ sub content {
     ? sprintf(q{<li><i>%s (%s)</i> is inviting you to join the group <i>%s</i><br />%s &middot; %s &middot; %s</li>},
         map({ $self->html_encode($_->name), $_->email } $_->modified_by_user || $_->created_by_user),
         $self->html_encode($_->group->name),
-        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Accept',     'id' => $_->group_member_id}, 'target' => 'page', 'caption' => 'Accept'}),
-        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Decline',    'id' => $_->group_member_id}, 'target' => 'page', 'caption' => 'Decline'}),
-        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'BlockGroup', 'id' => $_->group_member_id}, 'target' => 'page', 'caption' => 'Block group from sending further invitations'})
+        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Accept',     'id' => $_->group_member_id}, 'caption' => 'Accept'}),
+        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Decline',    'id' => $_->group_member_id}, 'caption' => 'Decline'}),
+        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'BlockGroup', 'id' => $_->group_member_id}, 'caption' => 'Block group from sending further invitations'})
       )
     : ()
     } @{$user->memberships}
@@ -29,10 +29,10 @@ sub content {
   my @requests = map({ $_->is_pending_request
     ? sprintf(q{<li><i>%s (%s)</i> would like to join your group <i>%s</i><br />%s &middot; %s &middot; %s</li>},
         map({ $self->html_encode($_->name), $_->email } $_->user),
-        $self->js_link({'href' => {'action' => 'Groups',     'function' => 'View',       'id' => $_->group->group_id}, 'target' => 'page', 'caption' => $self->html_encode($_->group->name)}),
-        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Allow',      'id' => $_->group_member_id}, 'target' => 'page', 'caption' => 'Allow'}),
-        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Ignore',     'id' => $_->group_member_id}, 'target' => 'page', 'caption' => 'Ignore'}),
-        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'BlockUser',  'id' => $_->group_member_id}, 'target' => 'page', 'caption' => 'Block user from sending further requests'})
+        $self->js_link({'href' => {'action' => 'Groups',     'function' => 'View',       'id' => $_->group->group_id}, 'caption' => $self->html_encode($_->group->name)}),
+        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Allow',      'id' => $_->group_member_id}, 'caption' => 'Allow'}),
+        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'Ignore',     'id' => $_->group_member_id}, 'caption' => 'Ignore'}),
+        $self->js_link({'href' => {'action' => 'Membership', 'function' => 'BlockUser',  'id' => $_->group_member_id}, 'caption' => 'Block user from sending further requests'})
       )
     : ()
     } map {@{$_->group->memberships}} @{$user->admin_memberships}
@@ -41,6 +41,6 @@ sub content {
   return sprintf '%s%s',
     @invitations ? $self->_info($self->append_s_to_plural('Invitation', @invitations > 1), sprintf('<ul>%s</ul>', join('', @invitations)), '100%') : '',
     @requests    ? $self->_info($self->append_s_to_plural('Request',    @requests    > 1), sprintf('<ul>%s</ul>', join('', @requests   )), '100%') : ''
-  ;    
+  ;
 }
 1;
