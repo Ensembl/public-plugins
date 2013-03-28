9efd7219328ea2ee65fbc583378c0c28751b28ba
Simon Brent <sb23@sanger.ac.uk>
2013-03-28 16:14:40 +0000

    setting start/end correctly in core code now

--- genoverse/modules/Bio/EnsEMBL/GlyphSet/contig.pm
+++ genoverse/modules/Bio/EnsEMBL/GlyphSet/contig.pm
@@ -3,15 +3,11 @@ package Bio::EnsEMBL::GlyphSet::contig;
 use strict;
 
 sub genoverse_attributes {
-  my $slice_start = $_[0]{'container'}->start - 1;
-  my $start       = $_[1]{'from_start'} + $slice_start;
-  
   return (
-    id         => "$_[1]{'name'}:$start",
-    start      => $start,
-    end        => $_[1]{'from_end'} + $slice_start,
+    id         => "$_[1]{'name'}:$_[1]{'start'}",
     labelColor => '#FFFFFF',
   );
 }
+
 
 1;
