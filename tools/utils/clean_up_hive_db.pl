364ff41e07ad80cccc57fc42e7f5b713e50e2141
Web Group <www-ens@sanger.ac.uk>
2013-04-01 17:03:28 +0000

    set up environment correctly

--- tools/utils/clean_up_hive_db.pl
+++ tools/utils/clean_up_hive_db.pl
@@ -1,14 +1,5 @@
 #!/usr/local/bin/perl
 
-# Script that is run by cron to create and remove paritions from the tools database tables
-# this works by calling a stored procedure on the mysql server hosting the database
-# procedure should be called with the following options:
-# database_name (db_name), table_name (t_name), number of previous days to keep partions for (days_past), number of days 
-# into the future to create partions for (days_future).
-# Command called should look like:
-# 
-# "CALL db_name.UpdatePartitions('db_name', 't_name', days_past, days_future);"
-
 use strict;
 
 use DBI;
@@ -40,5 +31,7 @@ my $url = "mysql://$user:$pass".'@'."$host:$port/$db";
 my $days = 8; # delete jobs older than 8 days
 
 my $command = $serverroot ."ensembl-hive/scripts/hoover_pipeline.pl -url " . $url . " -days_ago " . $days;
+my $lib = join ':', @INC;
+$ENV{PERL5LIB} = $lib;
 system ($command);
 
