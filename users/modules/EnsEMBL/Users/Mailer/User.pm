45f8719f9972cbb7342145ee4a069e376cfa20fe
Harpreet Riat <hr5@sanger.ac.uk>
2013-04-05 11:09:45 +0000

    methods modified so they don't need logged-in user as an argument anymore; new method 'send_group_sharing_notification_email' added

--- users/modules/EnsEMBL/Users/Mailer/User.pm
+++ users/modules/EnsEMBL/Users/Mailer/User.pm
@@ -105,119 +105,152 @@ sub send_change_email_confirmation_email {
 }
 
 sub send_group_sharing_notification_email {
-  ## Sends email to a given member about records shared with a group
-  ##TODO
-}
-
-sub send_group_editing_notification_email {
-  ## Sends an email to the a given admin of the group about the changes done to a group
-  ## @param Logged-in user (Admin who made the changes)
-  ## @param Admin to send email to
-  ## @param Group that's been changed
-  ## @param Changes made (string)
-  my ($self, $user, $admin, $group, $changes) = @_;
+  ## Sends emails to the given members about records shared with a group
+  ## @param Members to send emails to (ArrayRef)
+  ## @param Group object
+  ## @param Items shared (string)
+  my ($self, $members, $group, $shared_items) = @_;
 
   my $sitename    = $self->site_name;
   my $footer      = $self->email_footer;
   my $url         = $self->url({
-    'species'       => '',
     'type'          => 'Account',
     'action'        => 'Groups',
     'function'      => 'View',
     'id'            => $group->group_id
   });
-  my $to_name     = $admin->name;
+  my $user        = $self->hub->user;
   my $by_name     = $user->name;
-  my $group_name  = $group->name;
   my $by_email    = $user->email;
-
-  $self->to       = $admin->email;
-  $self->subject  = qq($sitename: Changes made to group "$group_name");
-  $self->message  = qq{Dear $to_name,\n\nThis email is to notify you that the following changes have been made to the group "$group_name" by administrator $by_name ($by_email):}
-                   .qq(\n\n$changes\n\nYou have received this email since you have opted to get notified when someone makes any change to this group. To view the changes made, )
+  my $group_name  = $group->name;
+  my $message     = qq{Dear %s,\n\nThis email is to notify you that $by_name has shared the following items with the group "$group_name"}
+                   .qq(\n\n$shared_items\n\nYou have received this email since you have opted to get notified when someone shares something with this group. To view the shared items, )
                    .qq(or to change your email preferences, please go to the link below:\n\n$url\n$footer);
 
-  $self->send;
+  $self->subject  = qq($sitename: Items shared to group "$group_name");
+
+  for (@$members) {
+    $self->message  = sprintf $message, $_->name;
+    $self->to       = $_->email;
+    $self->send;
+  }
 }
 
-sub send_group_deletion_notification_email {
-  ## Sends an email to the given admin notifying him about the deletion of the group
-  ## @param Logged-in user (admin who deleted the group)
-  ## @param Admin to send email to
-  ## @param Name of the deleted group
-  my ($self, $user, $admin, $group_name) = @_;
+sub send_group_editing_notification_email {
+  ## Sends emails to the given admins of the group about the changes done to a group
+  ## @param Admins to send emails to (ArrayRef)
+  ## @param Group that's been changed
+  ## @param Changes made (string)
+  my ($self, $admins, $group, $changes) = @_;
 
   my $sitename    = $self->site_name;
   my $footer      = $self->email_footer;
   my $url         = $self->url({
-    'species'       => '',
+    'type'          => 'Account',
+    'action'        => 'Groups',
+    'function'      => 'View',
+    'id'            => $group->group_id
+  });
+  my $user        = $self->hub->user;
+  my $by_name     = $user->name;
+  my $by_email    = $user->email;
+  my $group_name  = $group->name;
+  my $message     = qq{Dear %s,\n\nThis email is to notify you that the following changes have been made to the group "$group_name" by administrator $by_name ($by_email):}
+                   .qq(\n\n$changes\n\nYou have received this email since you have opted to get notified when someone makes any change to this group. To view the changes made, )
+                   .qq(or to change your email preferences, please go to the link below:\n\n$url\n$footer);
+
+  $self->subject  = qq($sitename: Changes made to group "$group_name");
+
+  for (@$admins) {
+    $self->message  = sprintf $message, $_->name;
+    $self->to       = $_->email;
+    $self->send;
+  }
+}
+
+sub send_group_deletion_notification_email {
+  ## Sends emails to the given admins notifying them about the deletion of the group
+  ## @param Admins to send email to (Arrayref)
+  ## @param Name of the deleted group
+  my ($self, $admins, $group_name) = @_;
+
+  my $sitename    = $self->site_name;
+  my $footer      = $self->email_footer;
+  my $url         = $self->url({
     'type'          => 'Account',
     'action'        => 'Groups',
     'function'      => ''
   });
-  my $to_name     = $admin->name;
+  my $user        = $self->hub->user;
   my $by_name     = $user->name;
   my $by_email    = $user->email;
-
-  $self->to       = $admin->email;
-  $self->subject  = qq($sitename: Group "$group_name" deleted);
-  $self->message  = qq{Dear $to_name,\n\nThis email is to notify you that the group "$group_name" have been deleted by administrator $by_name ($by_email).\n\n}
+  my $message     = qq{Dear %s,\n\nThis email is to notify you that the group "$group_name" have been deleted by administrator $by_name ($by_email).\n\n}
                    .qq(You have received this email since you have opted to get notified when someone makes any change to this group. To view your other groups, )
                    .qq(or to change your email preferences for other groups, please go to the link below:\n\n$url\n$footer);
 
-  $self->send;
+  $self->subject  = qq($sitename: Group "$group_name" deleted);
+
+  for (@$admins) {
+    $self->message  = sprintf $message, $_->name;
+    $self->to       = $_->email;
+    $self->send;
+  }
 }
 
 sub send_group_joining_notification_email {
-  ## Send an email to the given admin notifying him about a new joinee
-  my ($self, $user, $admin, $group, $has_joined) = @_;
+  ## Send emails to the given admins notifying them about a new joinee
+  ## @param Admins of the group (ArrayRef)
+  ## @param Group object
+  ## @param Flag to tell whether user joined or requested for membership
+  my ($self, $admins, $group, $has_joined) = @_;
 
   my $sitename    = $self->site_name;
   my $footer      = $self->email_footer;
   my $url         = $self->url({
-    'species'       => '',
     'type'          => 'Account',
     'action'        => 'Groups',
     'function'      => 'View',
     'id'            => $group->group_id
   });
-  my $to_name     = $admin->name;
+  my $user        = $self->hub->user;
   my $by_name     = $user->name;
   my $by_email    = $user->email;
   my $group_name  = $group->name;
   my $action      = $has_joined ? 'has joined' : 'has sent request to join';
   my $action_2    = $has_joined ? '' : 'or to accept or decline the request, ';
-
-  $self->to       = $admin->email;
-  $self->subject  = qq($sitename: "$by_name" $action the group "$group_name");
-  $self->message  = qq{Dear $to_name,\n\nThis email is to notify you that $by_name ($by_email) $action the group "$group_name".\n\n}
+  my $message     = qq{Dear %s,\n\nThis email is to notify you that $by_name ($by_email) $action the group "$group_name".\n\n}
                    .qq(You have received this email since you have opted to get notified when someone joins this group. To view the group, $action_2)
                    .qq(or to change your email preferences, please go to the link below:\n\n$url\n$footer);
 
-  $self->send;
-}
+  $self->subject  = qq($sitename: "$by_name" $action the group "$group_name");
 
+  for (@$admins) {
+    $self->message  = sprintf $message, $_->name;
+    $self->to       = $_->email;
+    $self->send;
+  }
+}
 
 sub send_group_invitation_email_to_existing_user {
   ## Sends an invitation email to the given user for the given group
   ## @param Group object
   ## @param User to be invited
-  ## @param Sender admin user
-  my ($self, $group, $invitee, $sender) = @_;
+  my ($self, $group, $invitee) = @_;
 
   my $sitename    = $self->site_name;
   my $footer      = $self->email_footer;
   my $url         = $self->url({
     'species'       => '',
     'type'          => 'Account',
-    'action'        => 'Groups'
+    'action'        => 'Preferences'
   });
   my $to_name     = $invitee->name;
-  my $by_name     = $sender->name;
+  my $user        = $self->hub->user;
+  my $by_name     = $user->name;
   my $group_name  = $group->name;
 
   $self->to       = $invitee->email;
-  $self->reply    = $sender->email;
+  $self->reply    = $user->email;
   $self->subject  = qq($sitename: Invitation from $by_name to join group "$group_name");
   $self->message  = qq(Dear $to_name,\n\n$by_name would like you to join the group "$group_name". To accept or decline the invitation, )
                    .qq(or to block the group from sending you further invitations, please go to the link below:\n\n$url\n$footer);
@@ -229,10 +262,9 @@ sub send_group_invitation_email_to_existing_user {
 sub send_group_invitation_email_to_new_user {
   ## Sends an invitation email to the given user for the given group
   ## @param Group object
-  ## @param Sender admin user
   ## @param Email of the new user to be invited
   ## @param Invitation object
-  my ($self, $group, $sender, $email, $invitation) = @_;
+  my ($self, $group, $email, $invitation) = @_;
 
   my $sitename        = $self->site_name;
   my $footer          = $self->email_footer;
@@ -252,11 +284,12 @@ sub send_group_invitation_email_to_new_user {
     'action'            => 'Register',
     'then'              => $self->hub->url($invitation_url)
   });
-  my $by_name         = $sender->name;
+  my $user            = $self->hub->user;
+  my $by_name         = $user->name;
   my $group_name      = $group->name;
 
   $self->to           = $email;
-  $self->reply        = $sender->email;
+  $self->reply        = $user->email;
   $self->subject      = qq($sitename: Invitation from $by_name to join group "$group_name" on $sitename);
   $self->message      = qq(Hi,\n\n$by_name would like you to join the group "$group_name" on $sitename. To register with $sitename and join the group, )
                        .qq(please click on the link below:\n\n$url_2\n\nIf you already have an account with $sitename, to accept or decline the invitation, )
@@ -269,13 +302,13 @@ sub send_group_invitation_email_to_new_user {
 
 sub send_mailinglists_subscription_emails {
   ## Sends emails to the address provided to join the selected mailing lists
-  ## @param User object
+  ## @param Login object
   ## @params Email addressed to which emails are to be sent to subscribe the user to the email list
-  my ($self, $user, @subscription_emails) = @_;
+  my ($self, $login, @subscription_emails) = @_;
 
   $self->subject  = "Subscription";
   $self->message  = "Subscription";
-  $self->from     = $user->email;
+  $self->from     = $login->user->email;
 
   for (@subscription_emails) {
     $self->to = $_;
