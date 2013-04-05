f3afb3bea5bba2d81ccf8bd35a784f07ffe82620
Harpreet Riat <hr5@sanger.ac.uk>
2013-04-05 14:43:05 +0000

    accounts dropdown is now fetched via ajax

--- users/htdocs/components/10_Masthead_Users.js
+++ users/htdocs/components/10_Masthead_Users.js
@@ -1,33 +1,55 @@
 //Extension to Ensembl.Panel.Masthead to add some dynamic behaviour to account links
 
 Ensembl.Panel.Masthead = Ensembl.Panel.Masthead.extend({
+  constructor: function (id) {
+    this.base(id);
+
+    Ensembl.EventManager.register('refreshAccountsDropdown', this, this.refreshAccountsDropdown);
+  },
+
   init: function () {
-    var panel = this;
     this.base();
+
+    this.elLk.accountHolder = this.el.find('div._account_holder');
+    
+    this.refreshAccountsDropdown();
+  },
+  
+  refreshAccountsDropdown: function() {
+    var panel = this;
+
+    this.elLk.accountHolder.empty();
 
     var hideDropdown = function() {
       panel.toggleAccountsDropdown(false);
       $(document).off('click', hideDropdown);
     }
 
-    this.elLk.accountLink = this.el.find('._accounts_link').on({
-      'click': function(event) {
-        event.preventDefault();
-        if (!$(this).hasClass('selected')) {
-          event.stopPropagation();
-          panel.toggleAccountsDropdown(true);
-          $(document).on('click', hideDropdown);
-        }
-      }
+    $.ajax('/Ajax/accounts_dropdown', {
+      'context': this,
+      'success': function(html) {
+        this.elLk.accountHolder.html(html);
+        this.elLk.accountLink = this.el.find('._accounts_link').on({
+          'click': function(event) {
+            event.preventDefault();
+            if (!$(this).hasClass('selected')) {
+              event.stopPropagation();
+              panel.toggleAccountsDropdown(true);
+              $(document).on('click', hideDropdown);
+            }
+          }
+        });
+    
+        this.elLk.accountDropdown = this.el.find('._accounts_dropdown').on({
+          'click': function(event) {
+            if (event.target.nodeName !== 'A') {
+              event.stopPropagation();
+            }
+          }
+        }).find('a').on('click', hideDropdown).end();
+      },
+      'dataType': 'html'
     });
-
-    this.elLk.accountDropdown = this.el.find('._accounts_dropdown').on({
-      'click': function(event) {
-        if (event.target.nodeName !== 'A') {
-          event.stopPropagation();
-        }
-      }
-    }).find('a').on('click', hideDropdown).end();
   },
 
   toggleAccountsDropdown: function(flag) {
