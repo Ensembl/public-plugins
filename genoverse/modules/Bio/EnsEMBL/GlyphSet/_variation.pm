188d9282bcea9e2160d437783ee691bbb6ad08bc
Simon Brent <sb23@sanger.ac.uk>
2013-04-23 08:22:18 +0000

    overwrite depth function

--- genoverse/modules/Bio/EnsEMBL/GlyphSet/_variation.pm
+++ genoverse/modules/Bio/EnsEMBL/GlyphSet/_variation.pm
@@ -2,6 +2,9 @@ package Bio::EnsEMBL::GlyphSet::_variation;
 
 use strict;
 
+use EnsEMBL::Web::Tools::MethodMaker(copy => [ 'depth', '_depth' ]);
+
 sub genoverse_attributes { return ( legend => $_[0]->my_colour($_[0]->colour_key($_[1]), 'text') ); }
+sub depth                { return $_[0]->_depth if $_[0]{'container'}; }
 
 1;
