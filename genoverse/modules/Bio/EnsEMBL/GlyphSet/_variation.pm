da68e1807e339811e1c71c018eba0aeda46ec121
Simon Brent <sb23@sanger.ac.uk>
2013-04-23 11:12:47 +0000

    scalex function

--- genoverse/modules/Bio/EnsEMBL/GlyphSet/_variation.pm
+++ genoverse/modules/Bio/EnsEMBL/GlyphSet/_variation.pm
@@ -6,5 +6,6 @@ use EnsEMBL::Web::Tools::MethodMaker(copy => [ 'depth', '_depth' ]);
 
 sub genoverse_attributes { return ( legend => $_[0]->my_colour($_[0]->colour_key($_[1]), 'text') ); }
 sub depth                { return $_[0]->_depth if $_[0]{'container'}; }
+sub scalex               { return $_[0]{'config'}{'transform'} ? $_[0]->SUPER::scalex : 1; }
 
 1;
