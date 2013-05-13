ccf2ce62620d1b0ec6021876e6b5a34a426b908b
Harpreet Riat <hr5@sanger.ac.uk>
2013-05-13 16:11:00 +0000

    group relationship name changed from 'membership' to 'memberships'; encode the utf8 name strings

--- admin/modules/EnsEMBL/Admin/Component/UserDirectory.pm
+++ admin/modules/EnsEMBL/Admin/Component/UserDirectory.pm
@@ -4,24 +4,27 @@ use strict;
 
 use base qw(EnsEMBL::Web::Component);
 
+use Encode;
+
 sub caption { ''; }
 
 sub content {
   my $self = shift;
   
-  my $admin_group = $self->object->rose_object;
+  my $admin_group   = $self->object->rose_object;
+  my $admin_members = $admin_group ? $admin_group->memberships : [];
 
-  return '<p>No User found.</p>'  unless $admin_group && scalar @{$admin_group->membership};
+  return '<p>No User found.</p>' unless scalar @$admin_members;
 
   my $table = $self->new_table([], [], {'class' => 'tint'});
   $table->add_columns(
     {'key' => 'name',   'title' => 'Name',    'width' => '30%'},
     {'key' => 'email',  'title' => 'Email',   'width' => '60%'},
   );
-  for (@{$admin_group->membership}) {
+  for (@$admin_members) {
     $_ = $_->user or next;
     $table->add_row({
-      'name'  => $_->name,
+      'name'  => encode("utf8", $_->name),
       'email' => '<a href="mailto:'.$_->email.'">'.$_->email.'</a>',
     });
   }
