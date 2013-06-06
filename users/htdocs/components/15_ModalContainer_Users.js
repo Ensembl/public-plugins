0cfc44f65ce0ff39ecd510da1c91e43a1fd15110
Simon Brent <sb23@sanger.ac.uk>
2013-06-06 10:12:38 +0000

    stop error where el and elLk references have been nullified by a redirect

--- users/htdocs/components/15_ModalContainer_Users.js
+++ users/htdocs/components/15_ModalContainer_Users.js
@@ -4,7 +4,8 @@ Ensembl.Panel.ModalContainer = Ensembl.Panel.ModalContainer.extend({
   hide: function () {
     this.base.apply(this, arguments);
 
-    if (this.el.find('._needs_refresh_on_hide').removeClass('_needs_refresh_on_hide').length) {
+    // updateConfiguration can cause a redirect from MultiSelector, destroying this.el, so check that this hasn't happened before continuing
+    if (this.el && this.el.find('._needs_refresh_on_hide').removeClass('_needs_refresh_on_hide').length) {
       Ensembl.EventManager.trigger('refreshAccountsDropdown');
     }
   }
