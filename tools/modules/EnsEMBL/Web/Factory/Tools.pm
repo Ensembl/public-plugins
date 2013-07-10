c871fffddeb9a547982871370078b44ea180993d
Harpreet Riat <hr5@sanger.ac.uk>
2013-07-10 13:48:54 +0000

    considering 'url action' for the object is liked by other parts of the code

--- tools/modules/EnsEMBL/Web/Factory/Tools.pm
+++ tools/modules/EnsEMBL/Web/Factory/Tools.pm
@@ -13,7 +13,7 @@ sub createObjects {
   my $ticket_name = $self->param('tk');
   my $tools_data  = $ticket_name ? {'_ticket' => ORM::EnsEMBL::DB::Tools::Manager::Ticket->fetch_ticket_by_name($ticket_name)} : {};
 
-  $self->DataObjects($self->new_object($self->hub->action || 'Tools', $tools_data, $data) || $self->new_object('Tools', $tools_data, $data));
+  $self->DataObjects($self->new_object('Tools', $tools_data, $data));
 }
 
 1;
