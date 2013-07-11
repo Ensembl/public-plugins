0dd5165a93edfc9025a290a27d0c5d6e32f2733b
William McLaren <wm2@ebi.ac.uk>
2013-07-11 16:03:19 +0000

    add toggleContent to draw charts when revealed

--- tools/htdocs/components/15_VEPResultsSummary.js
+++ tools/htdocs/components/15_VEPResultsSummary.js
@@ -40,5 +40,17 @@ Ensembl.Panel.VEPResultsSummary = Ensembl.Panel.Piechart.extend({
     };
     
     this.base();
+  },
+  
+  toggleContent: function (el) {
+    if (el.hasClass('open') && !el.data('done')) {
+      this.base(el);
+      this.makeGraphs($('.pie_chart > div', '.' + el.attr('rel')).map(function () { return this.id.replace('graphHolder', ''); }).toArray());
+      el.data('done', true);
+    } else {
+      this.base(el);
+    }
+    
+    el = null;
   }
 });
