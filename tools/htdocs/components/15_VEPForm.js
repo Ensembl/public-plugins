38d4fcf92a9852fb08b95742263419f59bc76bcf
William McLaren <wm2@ebi.ac.uk>
2013-07-22 13:09:27 +0000

    updates for form validation

--- tools/htdocs/components/15_VEPForm.js
+++ tools/htdocs/components/15_VEPForm.js
@@ -1,4 +1,9 @@
 Ensembl.Panel.VEPForm = Ensembl.Panel.Content.extend({
+  constructor: function () {
+    this.base.apply(this, arguments);
+    Ensembl.EventManager.register('indicateInputError', this, this.indicateInputError);
+  },
+
   init: function () {
     this.base();
     var panel = this;
@@ -8,11 +13,18 @@ Ensembl.Panel.VEPForm = Ensembl.Panel.Content.extend({
       panel.elLk.vepForm.addClass('overlay_blast');
       $('input.submit_vep', panel.elLk.vepForm).addClass('disabled').prop('value', 'Processing');
       $('.vep_input', panel.elLk.vepForm).attr('disabled', 'disabled');
+      
+      // make FormData to allow file upload to go through AJAX
+      var formData = new FormData(this);
+      
       $.ajax({
         url: this.action,
-        data: $(this).serialize(),
+        data: formData,
         dataType: 'json',
         type: this.method,
+        cache: false,
+        processData: false,
+        contentType: false,
         success: function (json) {
 
           Ensembl.EventManager.trigger(json.functionName, json.functionData);
@@ -29,5 +41,29 @@ Ensembl.Panel.VEPForm = Ensembl.Panel.Content.extend({
       });
       return false;
     });
+    
+    $('.select_on_focus').on('click', function() { $(this).select(); });
+  },
+
+  indicateInputError: function (errors) {
+    var panel = this;
+    panel.elLk.vepForm.addClass('check');
+
+    var display_errors = { rules: {}, message: {} };
+
+    var failed = $.map(errors, function (message, error_class) {
+      var tmp_errors = { rules: {}, messages: {} };
+      tmp_errors.rules[error_class] = function (val) {
+        return !this.inputs.filter('._' + error_class).hasClass('failed');
+      };
+      tmp_errors.messages[error_class] = message;
+      $.extend(true, display_errors, tmp_errors);
+      $("." + error_class +":last", panel.elLk.vepForm).prop('title', message).addClass('_' + error_class + ' failed');
+      return;
+    });
+
+    panel.elLk.vepForm.validate( display_errors, 'showError');
+    $.each(failed, function () { this.removeClass('failed valid'); });
+    failed = null;
   }
 });
