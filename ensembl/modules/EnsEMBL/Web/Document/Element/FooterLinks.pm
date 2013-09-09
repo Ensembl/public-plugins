32885baa555b029ba5f82c09eac22f487a932543
Simon Brent <sb23@sanger.ac.uk>
2013-09-09 14:36:11 +0000

    relative links so that we can get to the content before it goes live

--- ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
+++ ensembl/modules/EnsEMBL/Web/Document/Element/FooterLinks.pm
@@ -7,13 +7,12 @@ use strict;
 use base qw(EnsEMBL::Web::Document::Element);
 
 sub content {
-  ## Note that these links always go to the live site so that the information is up-to-date
   return qq(
     <div class="column-two right print_hide">
       <p>
-        <a href="http://www.ensembl.org/info/about/index.html" class="constant">About&nbsp;Ensembl</a> | 
-        <a href="http://www.ensembl.org/info/about/legal/privacy.html" class="constant">Privacy&nbsp;Policy</a> | 
-        <a href="http://www.ensembl.org/info/about/contact/" class="constant">Contact&nbsp;Us</a>
+        <a href="/info/about/index.html" class="constant">About&nbsp;Ensembl</a> | 
+        <a href="/info/about/legal/privacy.html" class="constant">Privacy&nbsp;Policy</a> | 
+        <a href="/info/about/contact/" class="constant">Contact&nbsp;Us</a>
       </p>
     </div>
     <div class="column-two right screen_hide_block">
