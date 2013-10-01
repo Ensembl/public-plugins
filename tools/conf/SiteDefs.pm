ef2c243008a4b58519620e9d97812f4262bddba5
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-01 10:21:04 +0000

    some more configs for tools

--- tools/conf/SiteDefs.pm
+++ tools/conf/SiteDefs.pm
@@ -6,7 +6,13 @@ use strict;
 
 sub update_conf {
 
-  $SiteDefs::ENSEMBL_ORM_DATABASES->{'ticket'} = 'DATABASE_WEB_TOOLS';
+  $SiteDefs::ENSEMBL_ORM_DATABASES->{'ticket'}  = 'DATABASE_WEB_TOOLS';
+  $SiteDefs::ENSEMBL_TOOLS_SHARED_LOCATION      = '~/tools_hive_code_sync'; # set 0 to avoid copying, in case the hive jobs are run on the same machine as web server
+  $SiteDefs::ENSEMBL_TOOLS_PIPELINE_PACKAGE     = 'EnsEMBL::Web::PipeConfig::Tools_conf';
+
+  $SiteDefs::ENSEMBL_BLAST_LSF_QUEUE            = 'blasttest';
+
+  $ENV{'EHIVE_ROOT_DIR'}                        = $SiteDefs::ENSEMBL_SERVERROOT.'/ensembl-hive/';
 }
 
 1;
