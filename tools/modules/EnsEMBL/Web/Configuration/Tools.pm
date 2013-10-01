0064b5e661be36a92d8c952b954bec3c8b2df439
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-01 11:30:55 +0000

    temporary commit - excludes everything except blast

--- tools/modules/EnsEMBL/Web/Configuration/Tools.pm
+++ tools/modules/EnsEMBL/Web/Configuration/Tools.pm
@@ -25,125 +25,116 @@ sub populate_tree {
   my $self = shift;
 
   $self->create_node('Summary', 'Activity Summary',
-    [qw( 
-      summary   EnsEMBL::Web::Component::Tools::JobsList 
-      details   EnsEMBL::Web::Component::Tools::TicketDetails          
+    [qw(
+      tickets         EnsEMBL::Web::Component::Tools::TicketsList
     )],
     { 'availability' => 1, 'concise' => 'Activity Summary' }
   );
-  $self->create_node('Blast', 'BLAST/BLAT search',
+
+  my $blast_node = $self->create_node('Blast', 'BLAST/BLAT search',
     [qw(
-      jobs         EnsEMBL::Web::Component::Tools::JobsList 
-      sequence     EnsEMBL::Web::Component::Tools::BlastInputForm
+      tickets         EnsEMBL::Web::Component::Tools::Blast::TicketsList
+      sequence        EnsEMBL::Web::Component::Tools::Blast::InputForm
     )],
     { 'availability' => 1, 'concise' => 'BLAST/BLAT search' }
   );
 
-  my $blast_node = $self->tree->get_node('Blast');
-  my $title = $self->object ? $self->object->long_caption : '';
-  $blast_node->append($self->create_subnode('BlastResults', 'Results',
+  $blast_node->append($self->create_node('Blast/Summary', 'Activity Summary',
     [qw(
-      results     EnsEMBL::Web::Component::Tools::BlastResults
-      jobs         EnsEMBL::Web::Component::Tools::JobsList 
-      karyotype   EnsEMBL::Web::Component::Tools::Karyotype
-      hsps        EnsEMBL::Web::Component::Tools::HspQueryPlot
-      table     EnsEMBL::Web::Component::Tools::BlastResultsTable
+      tickets         EnsEMBL::Web::Component::Tools::Blast::TicketsList
+      details         EnsEMBL::Web::Component::Tools::Blast::TicketDetails
     )],
-    { 'availability' => 1, 'concise' => $title } 
+    { 'availability' => 1, 'concise' => 'BLAST/BLAT Activity Summary'}
   ));
-  $blast_node->append($self->create_subnode('BlastAlignment', 'Alignment',
+
+  $blast_node->append($self->create_subnode('Blast/Results', 'Results',
     [qw(
-      hit       EnsEMBL::Web::Component::Tools::BlastHitSummary
-      alignment     EnsEMBL::Web::Component::Tools::BlastAlignment
+      results         EnsEMBL::Web::Component::Tools::Blast::JobDetails
+      karyotype       EnsEMBL::Web::Component::Tools::Blast::Karyotype
+      hsps            EnsEMBL::Web::Component::Tools::Blast::HspQueryPlot
+      table           EnsEMBL::Web::Component::Tools::Blast::ResultsTable
     )],
-    { 'availability' => 1, 'concise' => 'BLAST/BLAT Alignment', 'no_menu_entry' => 1 }
+    { 'availability' => 1, 'concise' => $self->object ? $self->object->long_caption : '' }
   ));
-  $blast_node->append($self->create_subnode('BlastAlignmentTranscript', 'Alignment',
+  $blast_node->append($self->create_subnode('Blast/Alignment', 'Alignment',
     [qw(
-      hit             EnsEMBL::Web::Component::Tools::BlastHitSummary
-      transalignment  EnsEMBL::Web::Component::Tools::BlastAlignmentTranscript
+      hit             EnsEMBL::Web::Component::Tools::Blast::HitSummary
+      alignment       EnsEMBL::Web::Component::Tools::Blast::Alignment
     )],
     { 'availability' => 1, 'concise' => 'BLAST/BLAT Alignment', 'no_menu_entry' => 1 }
   ));
-  $blast_node->append($self->create_subnode('BlastAlignmentProtein', 'Alignment',
+  $blast_node->append($self->create_subnode('Blast/QuerySeq', 'Query Sequence',
     [qw(
-      hit             EnsEMBL::Web::Component::Tools::BlastHitSummary
-      protalignment   EnsEMBL::Web::Component::Tools::BlastAlignmentProtein
-    )],
-    { 'availability' => 1, 'concise' => 'BLAST/BLAT Alignment', 'no_menu_entry' => 1 }
-  ));
-  $blast_node->append($self->create_subnode('BlastQuerySeq', 'Query Sequence',
-    [qw(
-      hit       EnsEMBL::Web::Component::Tools::BlastHitSummary
-      query     EnsEMBL::Web::Component::Tools::BlastQuerySeq
+      hit             EnsEMBL::Web::Component::Tools::Blast::HitSummary
+      query           EnsEMBL::Web::Component::Tools::Blast::QuerySeq
     )],
     { 'availability' => 1, 'concise' => 'BLAST/BLAT Query Sequence', 'no_menu_entry' => 1 }
   ));
-  $blast_node->append($self->create_subnode('BlastGenomicSeq', 'Genomic Sequence',
+  $blast_node->append($self->create_subnode('Blast/GenomicSeq', 'Genomic Sequence',
     [qw(
-      hit       EnsEMBL::Web::Component::Tools::BlastHitSummary
-      genomic     EnsEMBL::Web::Component::Tools::BlastGenomicSeq
+      hit             EnsEMBL::Web::Component::Tools::Blast::HitSummary
+      genomic         EnsEMBL::Web::Component::Tools::Blast::GenomicSeq
     )],
     { 'availability' => 1, 'concise' => 'BLAST/BLAT Genomic Sequence', 'no_menu_entry' => 1 }
   ));
 
-  $self->create_node( 'Submit', '', [],
-    {'command' => 'EnsEMBL::Web::Command::Tools::Submit',
-     'availability' => 1, 'no_menu_entry' => 1 },
-  );
-  $self->create_node( 'Download', 'Download', 
-    [qw( download EnsEMBL::Web::Component::Tools::Download )],
-    { 'availability' => 1, 'no_menu_entry' => 1 },
-  );
-
-=cut
-  $self->create_node('GeneTree', 'Gene Tree Mapping',
-    [qw(  
-      genetree EnsEMBL::Web::Component::Tools::GeneTreeInput
-    )],
-    {'availability' => 1, 'concise' => 'Gene Tree Mapping'}
-  );
-
-  my $custom_tracks_menu = $self->create_submenu('custom', 'Custom tracks');
-  $custom_tracks_menu->append($self->create_node('Upload', 'Upload a file',
-    [qw( upload         EnsEMBL::Web::Component::Tools::Blank )],
-    { 'availability' => 1, 'concise' => 'Upload a file' }
-  ));
-  $custom_tracks_menu->append($self->create_node('UrlData', 'Attach URL data',
-    [qw( url         EnsEMBL::Web::Component::Tools::Blank )],
-    { 'availability' => 1, 'concise' => 'Attach URL data' }
-  ));
-  $custom_tracks_menu->append($self->create_node('DAS', 'Attach a DAS source',
-    [qw( das         EnsEMBL::Web::Component::Tools::Blank )],
-    { 'availability' => 1, 'concise' => 'Attach DAS source' }
-  ));
-=cut
-  my $data_conversion_menu = $self->create_submenu('convert', 'Data Conversion');
-  $data_conversion_menu->append($self->create_node('AssemblyConverter', 'Assembly converter',
-    [qw( assembly         EnsEMBL::Web::Component::Tools::Blank )],
-    { 'availability' => 1, 'concise' => 'Assembly Converter' }
-  ));
-  $data_conversion_menu->append($self->create_node('IDMapper', 'ID History Converter',
-    [qw( idhistory         EnsEMBL::Web::Component::Tools::Blank )],
-    { 'availability' => 1, 'concise' => 'ID History Converter' }
-  ));
-  
-  
-  my $vep_node = $self->create_node('VEP', 'Variation Effect Predictor',
-    [qw( 
-      jobs         EnsEMBL::Web::Component::Tools::JobsList
-      vepeffect    EnsEMBL::Web::Component::Tools::VEPInputForm
-    )],
-    { 'availability' => 1, 'concise' => 'Variation Effect Predictor<span style="float:right;"><img src="/img/vep_logo.png"></span>' }
-  );
-  
-  $vep_node->append($self->create_subnode('VEPResults', 'Results',
-    [qw(
-      summary     EnsEMBL::Web::Component::Tools::VEPResultsSummary
-      results     EnsEMBL::Web::Component::Tools::VEPResults
-    )],
-    { 'availability' => 1, 'concise' => 'Variant Effect Predictor results<span style="float:right"><img src="/img/vep_logo.png"></span>' } 
-  ));
+#   $self->create_node( 'Submit', '', [],
+#     {'command' => 'EnsEMBL::Web::Command::Tools::Submit',
+#      'availability' => 1, 'no_menu_entry' => 1 },
+#   );
+#   $self->create_node( 'Download', 'Download', 
+#     [qw( download EnsEMBL::Web::Component::Tools::Download )],
+#     { 'availability' => 1, 'no_menu_entry' => 1 },
+#   );
+# 
+# =cut
+#   $self->create_node('GeneTree', 'Gene Tree Mapping',
+#     [qw(  
+#       genetree EnsEMBL::Web::Component::Tools::GeneTreeInput
+#     )],
+#     {'availability' => 1, 'concise' => 'Gene Tree Mapping'}
+#   );
+# 
+#   my $custom_tracks_menu = $self->create_submenu('custom', 'Custom tracks');
+#   $custom_tracks_menu->append($self->create_node('Upload', 'Upload a file',
+#     [qw( upload         EnsEMBL::Web::Component::Tools::Blank )],
+#     { 'availability' => 1, 'concise' => 'Upload a file' }
+#   ));
+#   $custom_tracks_menu->append($self->create_node('UrlData', 'Attach URL data',
+#     [qw( url         EnsEMBL::Web::Component::Tools::Blank )],
+#     { 'availability' => 1, 'concise' => 'Attach URL data' }
+#   ));
+#   $custom_tracks_menu->append($self->create_node('DAS', 'Attach a DAS source',
+#     [qw( das         EnsEMBL::Web::Component::Tools::Blank )],
+#     { 'availability' => 1, 'concise' => 'Attach DAS source' }
+#   ));
+# =cut
+#   my $data_conversion_menu = $self->create_submenu('convert', 'Data Conversion');
+#   $data_conversion_menu->append($self->create_node('AssemblyConverter', 'Assembly converter',
+#     [qw( assembly         EnsEMBL::Web::Component::Tools::Blank )],
+#     { 'availability' => 1, 'concise' => 'Assembly Converter' }
+#   ));
+#   $data_conversion_menu->append($self->create_node('IDMapper', 'ID History Converter',
+#     [qw( idhistory         EnsEMBL::Web::Component::Tools::Blank )],
+#     { 'availability' => 1, 'concise' => 'ID History Converter' }
+#   ));
+#   
+#   
+#   my $vep_node = $self->create_node('VEP', 'Variation Effect Predictor',
+#     [qw( 
+#       jobs         EnsEMBL::Web::Component::Tools::JobsList
+#       vepeffect    EnsEMBL::Web::Component::Tools::VEPInputForm
+#     )],
+#     { 'availability' => 1, 'concise' => 'Variation Effect Predictor<span style="float:right;"><img src="/img/vep_logo.png"></span>' }
+#   );
+#   
+#   $vep_node->append($self->create_subnode('VEPResults', 'Results',
+#     [qw(
+#       summary     EnsEMBL::Web::Component::Tools::VEPResultsSummary
+#       results     EnsEMBL::Web::Component::Tools::VEPResults
+#     )],
+#     { 'availability' => 1, 'concise' => 'Variant Effect Predictor results<span style="float:right"><img src="/img/vep_logo.png"></span>' } 
+#   ));
 }
 
 sub user_populate_tree {
