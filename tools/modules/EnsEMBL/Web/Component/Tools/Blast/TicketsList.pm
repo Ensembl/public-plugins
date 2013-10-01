fe9797ff6f4e0687f009599081a9dc4af50e04b9
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-01 11:20:13 +0000

    blast ticket list page

---
+++ tools/modules/EnsEMBL/Web/Component/Tools/Blast/TicketsList.pm
@@ -0,0 +1,11 @@
+package EnsEMBL::Web::Component::Tools::Blast::TicketsList;
+
+use strict;
+use warnings;
+
+use base qw(
+  EnsEMBL::Web::Component::Tools::TicketsList
+  EnsEMBL::Web::Component::Tools::Blast
+);
+
+1;
