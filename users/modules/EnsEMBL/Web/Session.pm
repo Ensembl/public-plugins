c5bba0f88080558c2dc974ec971a98e9186e68b9
Simon Brent <sb23@sanger.ac.uk>
2013-10-03 11:01:14 +0000

    better security for sharing user records

--- users/modules/EnsEMBL/Web/Session.pm
+++ users/modules/EnsEMBL/Web/Session.pm
@@ -4,11 +4,14 @@ package EnsEMBL::Web::Session;
 
 use strict;
 
+use Digest::MD5 qw(md5_hex);
+
 use EnsEMBL::Web::Record;
 
 sub receive_shared_user_data {
-  my ($self, $id) = @_; 
-  return EnsEMBL::Web::Record->new($id);
+  my ($self, $id, $checksum) = @_; 
+  my $record = EnsEMBL::Web::Record->new($id);
+  return $record && $record->type =~ /^(url|upload)$/ && $checksum eq md5_hex($record->code) ? $record : undef;
 }
 
 1;
