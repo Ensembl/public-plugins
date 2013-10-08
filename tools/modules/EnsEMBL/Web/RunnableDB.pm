d3aa09404fc55ee1d66d47554239f8d48bbf424d
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-08 10:54:37 +0000

    'save_results' method added to the base runnabledb class to allow sub classes to use it to easily write results to the db

--- tools/modules/EnsEMBL/Web/RunnableDB.pm
+++ tools/modules/EnsEMBL/Web/RunnableDB.pm
@@ -21,4 +21,45 @@ sub param_required {
   return $self->param($param_name);
 }
 
+sub save_results {
+  ## Writes the results to the result table of ticket db
+  ## Use this in 'write_output' to save results
+  ## @param Job id to write results against
+  ## @param Arrayref of hashrefs (each hashref goes in result_data column)
+  my ($self, $job_id, $results) = @_;
+
+  if (@$results) {
+
+    # Serialise and compress results to save them to db
+    my @serialised_results = map {
+      my $serialised = nfreeze($_);
+      my $serialised_gzip;
+      gzip(\$serialised => \$serialised_gzip, -LEVEL => 9) or throw exception('HiveException', "GZIP failed: $GzipError");
+      $serialised_gzip;
+    } @$results;
+
+    my $ticket_db   = $self->param('ticket_db');
+    my $ticket_dbh  = DBI->connect(sprintf('dbi:mysql:%s:%s:%s', $ticket_db->{'-dbname'}, $ticket_db->{'-host'}, $ticket_db->{'-port'}), $ticket_db->{'-user'}, $ticket_db->{'-pass'}, { 'PrintError' => 0 });
+
+    if ($ticket_dbh) {
+      my $now = $self->_get_time_now;
+      my $sth = $ticket_dbh->prepare('INSERT INTO `result` (`job_id`, `result_data`, `created_at`) values ' . join(',', map {'(?,?,?)'} @serialised_results));
+
+      $sth->execute(map {($job_id, $_ || '', $now)} @serialised_results);
+
+    } else {
+
+      ## TODO -- what if ticket connection comes back later?
+      $self->warning("Ticket database: Connection could not be created ($DBI::errstr)");
+    }
+
+    $ticket_dbh->disconnect;
+  }
+}
+
+sub _get_time_now {
+  # @private
+  my ($sec, $min, $hour, $day, $month, $year) = localtime;
+  return sprintf '%d-%02d-%02d %02d:%02d:%02d', $year + 1900, $month + 1, $day, $hour, $min, $sec;
+}
 1;
