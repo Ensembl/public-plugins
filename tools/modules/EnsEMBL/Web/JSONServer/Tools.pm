2997536e64e8df48df0a212cc529589a4070f83e
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-10 09:30:59 +0000

    web object now has it ticket type param

--- tools/modules/EnsEMBL/Web/JSONServer/Tools.pm
+++ tools/modules/EnsEMBL/Web/JSONServer/Tools.pm
@@ -11,16 +11,11 @@ sub json_form_submit {
   my $self          = shift;
   my $hub           = $self->hub;
   my $object        = $self->object;
+  my $jobs_data     = $object->form_inputs_to_jobs_data;
 
-  my $ticket_type   = $object->get_ticket_type_object($hub->param('ensembl_tool'));
-
-  if ($ticket_type) {
-    my $jobs_data = $object->form_inputs_to_jobs_data;
-
-    if ($jobs_data && @$jobs_data) {
-      $object->create_ticket($ticket_type, $jobs_data);
-      return {'panelMethod' => ['refreshJobsList']};
-    }
+  if ($jobs_data && @$jobs_data) {
+    $object->create_ticket($jobs_data);
+    return {'panelMethod' => ['refreshJobsList']};
   }
 
   return {'invalid' => 1};
