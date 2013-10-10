4f78287e8aed60e9ebd223f4628e0d8d7e99381b
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-10 10:45:31 +0000

    code rewrite

--- tools/modules/EnsEMBL/Web/ZMenu/Blast.pm
+++ tools/modules/EnsEMBL/Web/ZMenu/Blast.pm
@@ -1,94 +1,65 @@
 package EnsEMBL::Web::ZMenu::Blast;
 
 use strict;
+use warnings;
 
 use base qw(EnsEMBL::Web::ZMenu);
 
 sub content {
-  my $self = shift;
-  my $hub = $self->hub;
-  my $object = $self->hub->core_objects->{'tools'};
-  my $ticket_name = $hub->param('tk'); 
+  my $self        = shift;
+  my $hub         = $self->hub;
+  my $object      = $self->object->get_sub_object;
   my $coord_range = $hub->param('bin');
-  my $idx = $hub->param('idx') || 0;
-  my $ticket = $object->fetch_ticket_by_name($ticket_name);
-  my $ticket_id = $ticket->{'ticket_id'};  
-  my $species = $hub->param('sp');   
-  my (@results, $hit, $hit_id);
+  my $index       = $hub->param('idx') || 0;
+  my $job         = $object->get_requested_job({'with_requested_result' => 1});
+  my $hits        = {};
 
-  if ($hub->param('hid')){
-    $hit_id = $hub->param('hid');
-    $hit = $object->fetch_blast_hit_by_id($hit_id);
+  my ($hit, $hit_id);
+
+  if ($job) {
+
+    my $result = $job->result->[0];
+    $hit_id = $result->result_id;
+    $hit    = $result->result_data;
+
   } else {
-    @results = @{$object->get_ticket_hits_by_coords($coord_range, $ticket_id, $species)};
-    my @sorted_results = sort { $b->[1]->{'score'} <=> $a->[1]->{'score'}}  @results;
-    $hit = $sorted_results[$idx]->[1]; 
-    $hit_id = $sorted_results[$idx]->[0];
-  }
 
-  my $query_location = $hit->{'qid'}.':'.$hit->{'qstart'}.'-'.$hit->{'qend'};
-  my $genomic_location = $hit->{'gid'}.':'.$hit->{'gstart'}.'-'.$hit->{'gend'};
+    $job    = $object->get_requested_job; # ignore the result here since it did not return anything in the previous attempt
+    $hits   = $object->get_all_hits_by_coords($job) if $job;
+
+    return unless $job && keys %$hits;
+
+    $hit_id = [ sort { $hits->{$b}->{'score'} <=> $hits->{$a}->{'score'} } keys %$hits ]->[ $index ];
+    $hit    = $hits->{$hit_id};
+  }
 
   $self->caption('Blast/Blat Hit');
-  $self->highlight('hsp_'. $hit_id);
+  $self->highlight("hsp_$hit_id");
 
-  $self->add_entry({
-    type  => 'Query bp',
-    label => $query_location
-  });
+  $self->add_entry({ 'type' => 'Query bp',    'label' => sprintf('%s:%s-%s', $hit->{'qid'}, $hit->{'qstart'}, $hit->{'qend'}) });
+  $self->add_entry({ 'type' => 'Target',      'label' => $hit->{'tid'}                                                        }) if $hit->{'db_type'} !~/latest/i;
+  $self->add_entry({ 'type' => 'Genomic bp',  'label' => sprintf('%s:%s-%s', $hit->{'gid'}, $hit->{'gstart'}, $hit->{'gend'}) });
+  $self->add_entry({ 'type' => 'Score',       'label' => $hit->{'score'}                                                      });
+  $self->add_entry({ 'type' => 'E-value',     'label' => $hit->{'evalue'}                                                     });
+  $self->add_entry({ 'type' => '%ID',         'label' => $hit->{'pident'}                                                     });
+  $self->add_entry({ 'type' => 'Length',      'label' => $hit->{'len'}                                                        });
 
-
-  $self->add_entry({
-    type  => 'Target',
-    label => $hit->{'tid'},
-  }) if $hit->{'db_type'} !~/latest/i;
-
-  $self->add_entry({
-    type  => 'Genomic bp',
-    label => $genomic_location
-  });
-
-  $self->add_entry({
-    type  => 'Score',
-    label => $hit->{'score'},
-  });
-
-  $self->add_entry({
-    type  => 'E-value',
-    label => $hit->{'evalue'}  
-  });
-
-  $self->add_entry({
-    type  => '%ID',
-    label => $hit->{'pident'}
-  });
-
-  $self->add_entry({
-    type  => 'Length',
-    label => $hit->{'len'}
-  });
-  
-  if (scalar @results > 1){
-
-    my $count = scalar @results;
-    my $url_template = $hub->url({
-      type    => 'ZMenu', 
-      action  => 'Blast', 
-      tk      => $ticket_name, 
-      bin     => $coord_range, 
-      sp      => $species, 
-      res     => $hit_id, 
-      idx     => 1
-    });
+  if ($hits && keys %$hits > 1) {
 
     $self->pagination({
-      position      => $idx, 
-      total         => $count, 
-      url_template  => $url_template,
+      'position'      => $index,
+      'total'         => keys %$hits,
+      'url_template'  => $hub->url({
+        'type'          => 'ZMenu',
+        'action'        => 'Blast',
+        'function'      => '',
+        'tl'            => $object->create_url_param({'result_id' => $hit_id}),
+        'bin'           => $coord_range,
+        'idx'           => 1
+      })
     });
 
   }
-
 }
 
 1;
