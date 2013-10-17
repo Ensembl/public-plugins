02e3dbd13fddb902cb7b7466def654d679a482f5
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-17 16:04:12 +0000

    plugin to content to handle generic AJAX JSON requests

---
+++ tools/htdocs/components/10_ContentTools.js
@@ -0,0 +1,136 @@
+/*
+ * Plugin to Content panel to make Ajax requests and handle generic JSON responses (as returned by JSON Controller by the Perl backend)
+ */
+
+Ensembl.Panel.ContentTools = Ensembl.Panel.Content.extend({
+
+  ajax: function(configs) {
+  /*
+   * Wrapper arounf jQuery's ajax method
+   * Forces the generic response handling for any resposne recieved
+   */
+    var panel = this;
+
+    $.ajax($.extend({
+      'type'        : 'get'
+    }, configs, {
+      'dataType'    : 'json',
+      'context'     : panel,
+      'success'     : function(json) {
+        this.ajaxSuccess(json);
+      },
+      'error'       : function(jqXHR, textStatus, errorThrown) {
+        this.ajaxError(jqXHR, textStatus, errorThrown);
+      }
+    }));
+  },
+
+  ajaxSuccess: function(json) {
+  /*
+   * Reads and reacts according to the JSON response sent by the Perl backend
+   * To override this method, check it's return type in the child class.
+   */
+    if (!this.parseJSONResponseHeader(json)) {
+      return 'header_status';
+    }
+
+    if (json.panelMethod) {
+      var methodName = json.panelMethod.shift();
+
+      if (methodName in this) {
+        this[methodName].apply(this, json.panelMethod);
+        return 'method_applied';
+      }
+
+      this.showError("Requested method '" + methodName + "' could not be found.", 'Javascript Error');
+      return 'method_missing';
+    }
+
+    return true;
+  },
+
+  ajaxError: function(jqXHR, textStatus, errorThrown) {
+  /*
+   * Reacts according to the error recieved from the Perl backend
+   */
+    var heading = errorThrown ? 'Server error' : 'Error';
+    if (!errorThrown) {
+      switch (textStatus) {
+        case 'timeout':
+          errorThrown = 'The request took longer than expected. The may happen due to an issue with your Internet connection.';
+          break;
+        case 'abort':
+          errorThrown = 'The request was aborted, or the connection was closed unexpectedly.';
+          break;
+        case 'parsererror':
+          errorThrown = 'An error occured while parsing the server response.';
+        default:
+          errorThrown = 'Some unknown error occurred';
+      }
+    }
+    this.showError(errorThrown, heading);
+  },
+
+  parseJSONResponseHeader: function(json) {
+  /*
+   * This parses the header for status as packed inside the JSON object
+   * Not to be confused with the actual HTTP header
+   */
+    var header = json.header || {};
+    switch (parseInt(header.status)) {
+      case 200:
+        // continue with response handling
+        return true;
+      case 302:
+        // handle redirection
+        if (header.location) {
+          this.ajax({'url' : header.location, 'async': false});
+        } else {
+          this.showError('Redirect URL is missing', 'Redirection Error');
+        }
+        return false;
+      case 404:
+        // not found
+        this.showError('The requested page could not be found.', 'Not found');
+        return false;
+      case 500:
+        // server error
+        var exception = json.exception || {};
+        this.showError(exception.message, 'Server Error: ' + exception.type);
+        return false;
+      default:
+        // not likely to come here, but anyway...
+        this.showError();
+        return false;
+    }
+  },
+
+  showError: function(message, heading) {
+  /*
+   * Displays error in the specified errorDiv
+   * To display error in a custom div, create panel.errorDiv before calling this method
+   */
+    heading = heading || (message ? 'Error' : 'Unknown Error');
+    message = message || 'Some unknown error has occoured.';
+    if (!this.errorDiv) {
+      var panel     = this;
+      this.errorDiv = $('<div>')
+        .append('<div class="error-overlay"></div><div class="error-popup error"><h3 class="_error_heading"></h3><div class="error-pad"><p class="_error_message"></p><p class="center"><a href="#" class="_error_hide button">Close</a></p></div></div>')
+        .appendTo(document.body)
+        .find('a._error_hide').on('click', function(event) {
+          event.preventDefault();
+          panel.hideError();
+        }).end();
+    }
+    this.errorDiv.find('._error_heading').html(heading).end().find('._error_message').html(message).end().show();
+  },
+
+  hideError: function() {
+  /*
+   * Hides the errorDiv
+   */
+    if (this.errorDiv) {
+      this.errorDiv.hide();
+    }
+  }
+});
