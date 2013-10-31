bcdc6545456d7f63e9cb724124d2b060b8895fb7
Harpreet Riat <hr5@sanger.ac.uk>
2013-10-31 13:16:20 +0000

    default values

--- tools/conf/SiteDefs.pm
+++ tools/conf/SiteDefs.pm
@@ -10,8 +10,11 @@ sub update_conf {
   $SiteDefs::ENSEMBL_TOOLS_SHARED_LOCATION      = '~/tools_hive_code_sync'; # set 0 to avoid copying, in case the hive jobs are run on the same machine as web server
   $SiteDefs::ENSEMBL_TOOLS_PIPELINE_PACKAGE     = 'EnsEMBL::Web::PipeConfig::Tools_conf';
 
+  $SiteDefs::ENSEMBL_BLAST_ENABLED              = 1;
+  $SiteDefs::ENSEMBL_VEP_ENABLED                = 1;
+
   $SiteDefs::ENSEMBL_BLAST_LSF_QUEUE            = 'blasttest';
-  $SiteDefs::ENSEMBL_VEP_LSF_QUEUE              = 'vep';
+  $SiteDefs::ENSEMBL_VEP_LSF_QUEUE              = 'VEP';
 
   $ENV{'EHIVE_ROOT_DIR'}                        = $SiteDefs::ENSEMBL_SERVERROOT.'/ensembl-hive/';
 }
