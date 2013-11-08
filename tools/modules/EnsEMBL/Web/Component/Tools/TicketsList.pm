8a341468df901df89b45d724a17d5eb8e0f14294
Harpreet Riat <hr5@sanger.ac.uk>
2013-11-08 13:16:24 +0000

    ticket_buttons method added

--- tools/modules/EnsEMBL/Web/Component/Tools/TicketsList.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/TicketsList.pm
@@ -10,7 +10,6 @@ use base qw(EnsEMBL::Web::Component::Tools);
 sub content {
   my $self          = shift;
   my $hub           = $self->hub;
-  my $user          = $hub->user;
   my $object        = $self->object;
   my $tickets       = $object->get_current_tickets;
   my $toggle        = $hub->action ne 'Summary'; # Summary page's table of tickets can not toggled
@@ -49,19 +48,14 @@ sub content {
         ;
       }
 
-      my $owner_is_user = $ticket->owner_type eq 'user';
-      my $created_at    = $ticket->created_at;
-      my $ticket_extras = sprintf '<span class="_ht sprite save_icon%s _ticket_save" title="%s"></span><span class="_ht sprite delete_icon _ticket_delete" title="Delete ticket">',
-        !$user || $owner_is_user ? ' sprite_disabled' : '',
-        !$owner_is_user ? $user ? 'Save to account' : 'Login to save to account' : 'Saved to account'
-      ;
+      my $created_at = $ticket->created_at;
 
       $table->add_row({
         'analysis'  => $ticket->ticket_type->ticket_type_caption,
         'ticket'    => $self->ticket_link($ticket),
         'jobs'      => join('', sort values %$jobs_summary),
         'created'   => sprintf('<span class="hidden">%d</span>%s', $created_at =~ s/[^\d]//gr, $self->format_date($created_at)),
-        'extras'    => $ticket_extras,
+        'extras'    => $self->ticket_buttons($ticket)->render,
         'options'   => {'class' => "_ticket_$ticket_name"}
       });
     }
@@ -74,11 +68,6 @@ sub content {
     'type'        => 'hidden',
     'class'       => 'panel_type',
     'value'       => 'ActivitySummary'
-  }, {
-    'node_name'   => 'input',
-    'type'        => 'hidden',
-    'name'        => '_delete_url',
-    'value'       => $hub->url('Json', {'function' => 'delete', 'tl' => 'TICKET_NAME'})
   }, {
     'node_name'   => 'input',
     'type'        => 'hidden',
@@ -119,6 +108,51 @@ sub ticket_link {
     }),
     $ticket_name
   );
+}
+
+sub ticket_buttons {
+  my ($self, $ticket) = @_;
+  my $hub           = $self->hub;
+  my $user          = $hub->user;
+  my $ticket_name   = $ticket->ticket_name;
+  my $owner_is_user = $ticket->owner_type eq 'user';
+  my $url_param     = $self->object->create_url_param({'ticket_name' => $ticket_name});
+  my $action        = $ticket->ticket_type->ticket_type_name;
+
+  my $save_button   = {
+    'node_name'       => 'span',
+    'class'           => ['_ht', 'sprite', 'save_icon', $user && $owner_is_user ? 'sprite_disabled' : ()],
+    'title'           => $user ? $owner_is_user ? 'Already saved to account' : 'Save to account' : 'Login to save to account'
+  };
+
+  $save_button      = {
+    'node_name'       => 'a',
+    'class'           => [ $user ? '_ticket_save' : 'modal_link' ],
+    'href'            => $user ? $hub->url('Json', {'type' => 'Tools', 'action' => $action, 'function' => 'save', 'tl' => $url_param}) : $hub->url({'type' => 'Account', 'action' => 'Login'}),
+    'children'        => [ $save_button ]
+  } unless $owner_is_user;
+
+  my $buttons       = $self->dom->create_element('div', { 'children' => [ $save_button, {
+    'node_name'       => 'a',
+    'class'           => '_ticket_delete',
+    'href'            => $hub->url('Json', {'action' => $action, 'function' => 'delete', 'tl' => $url_param}),
+    'children'        => [{
+      'node_name'       => 'span',
+      'class'           => [ '_ht', 'sprite', 'delete_icon' ],
+      'title'           => 'Delete ticket'
+    }]
+  }, {
+    'node_name'       => 'a',
+    'class'           => '_ticket_edit',
+    'href'            => $hub->url({'action' => $action, 'function' => '', 'tl' => $url_param, 'edit' => 1}),
+    'children'        => [{
+      'node_name'       => 'span',
+      'class'           => [ '_ht', 'sprite', 'edit_icon' ],
+      'title'           => 'Edit &amp; resubmit ticket'
+    }]
+  }]});
+
+  return $buttons;
 }
 
 1;
