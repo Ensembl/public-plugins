5e1156265eedff691cb4a3933ed6e6e28d65ee93
Simon Brent <sb23@sanger.ac.uk>
2013-11-10 22:49:37 +0000

    fix storing before merging happened

--- genoverse/modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
+++ genoverse/modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
@@ -5,10 +5,10 @@ use strict;
 use previous qw(merge_all);
 
 sub merge_all {
-  PREV::merge_all(@_);
-  
-  my $species_defs = shift;
+  my $species_defs = $_[0];
   $species_defs->{'_storage'}{'GENOVERSE_JS_NAME'} = merge($species_defs, 'js', [split 'modules', __FILE__]->[0] . 'htdocs', 'genoverse');
+  
+  PREV::merge_all(@_);
 }
 
 1;
--- solr/modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
+++ solr/modules/EnsEMBL/Web/Tools/DHTMLmerge.pm
@@ -5,13 +5,13 @@ use strict;
 use previous qw(merge_all);
 
 sub merge_all {
-  PREV::merge_all(@_);
-  
-  my $species_defs = shift;
+  my $species_defs = $_[0];
   my $dir          = [split 'modules', __FILE__]->[0] . 'htdocs';
   
   $species_defs->{'_storage'}{'SOLR_JS_NAME'}  = merge($species_defs, 'js',  $dir, 'solr');
   $species_defs->{'_storage'}{'SOLR_CSS_NAME'} = merge($species_defs, 'css', $dir, 'solr');
+
+  PREV::merge_all(@_);
 }
 
 1;
