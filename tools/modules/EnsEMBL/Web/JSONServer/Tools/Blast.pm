df871d7fc22db42875a24eeb61c07548facea40b
Harpreet Riat <hr5@sanger.ac.uk>
2013-11-12 10:25:21 +0000

    take file size from MAX_SEQUENCE_LENGTH and MAX_NUM_SEQUENCES

--- tools/modules/EnsEMBL/Web/JSONServer/Tools/Blast.pm
+++ tools/modules/EnsEMBL/Web/JSONServer/Tools/Blast.pm
@@ -3,6 +3,8 @@ package EnsEMBL::Web::JSONServer::Tools::Blast;
 use strict;
 use warnings;
 
+use EnsEMBL::Web::BlastConstants;
+
 use base qw(EnsEMBL::Web::JSONServer::Tools);
 
 sub object_type { 'Blast' }
@@ -15,7 +17,7 @@ sub json_read_file {
   my $self        = shift;
   my $hub         = $self->hub;
   my $cgi         = $hub->input;
-  my $max_size    = $hub->species_defs->TOOLS_MAX_FILE_UPLOAD_CHARS || 100 * 1024;
+  my $max_size    = int(MAX_SEQUENCE_LENGTH() * MAX_NUM_SEQUENCES() * 1.1);
   my $filename    = $cgi->param('query_file');
 
   if ($filename =~ /\.(fa|txt)$/ && $cgi->uploadInfo($filename)->{'Content-Type'} =~ /^(application|text)/) {
@@ -26,7 +28,13 @@ sub json_read_file {
     while (<$filehandle>) {
       $filecontent .= $_;
       if (length($filecontent) > $max_size) { # FIXME - find an alternative!
-        return {'file_error' => sprintf 'Uploaded file should not be more than %s Kb.', $max_size / 1024};
+        my $limit = $max_size / 1024;
+        my $unit  = 'KB';
+        if ($limit > 1024) {
+          $limit  = $limit / 1024;
+          $unit   = 'MB';
+        }
+        return {'file_error' => sprintf 'Uploaded file should not be more than %s %s.', int($limit), $unit};
       }
     }
 
