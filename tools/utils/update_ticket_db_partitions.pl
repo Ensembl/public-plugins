2b91f227e466e23db49ca118103f61ed821622cf
Simon Brent <sb23@sanger.ac.uk>
2013-11-28 10:53:49 +0000

    Add Apache 2 license to code

--- tools/utils/update_ticket_db_partitions.pl
+++ tools/utils/update_ticket_db_partitions.pl
@@ -1,4 +1,18 @@
 #!/usr/local/bin/perl
+# Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#      http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # Script that is run by cron to create and remove paritions from the tools database tables
 # this works by calling a stored procedure on the mysql server hosting the database
