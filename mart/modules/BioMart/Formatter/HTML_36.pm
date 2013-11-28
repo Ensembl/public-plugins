9a10dc9037ae63c9d5fc58e85958a9e33203405d
Simon Brent <sb23@sanger.ac.uk>
2013-11-28 10:53:39 +0000

    Add Apache 2 license to code

--- mart/modules/BioMart/Formatter/HTML_36.pm
+++ mart/modules/BioMart/Formatter/HTML_36.pm
@@ -1,9 +1,6 @@
 # $Id$
 #
 # BioMart module for BioMart::Formatter::HTML_36
-#
-# You may distribute this module under the same terms as perl
-# itself.
 
 # POD documentation - main docs before the code.
 
@@ -26,6 +23,22 @@ templates are defined for the attributes in the Dataset's ConfigurationTree
 then appropiate hyperlinks will be calculated for each cell of the table.
 Addition of any extra attributes to the Query that may be required for this
 hyperlink formatting is handled in this Formatter
+
+=head1 LICENSE
+
+Copyright [1999-2013] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 =head1 AUTHORS
 
