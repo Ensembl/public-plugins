1745ac9abe8c33df46da20cb7453bd57ec8fcf24
Anne Parker <ap5@sanger.ac.uk>
2014-01-23 15:45:15 +0000

    Improved formatting and added key

--- ensembl/modules/EnsEMBL/Ensembl/Document/HTML/AssemblyTable.pm
+++ ensembl/modules/EnsEMBL/Ensembl/Document/HTML/AssemblyTable.pm
@@ -26,6 +26,14 @@ use EnsEMBL::Web::DBSQL::ArchiveAdaptor;
 sub render {
   my ($class, $request) = @_;
 
+  my $html = qq(
+<p><b>Key</b>: 
+<span class="hilite" style="padding:0 10px;border:1px solid #999;">&nbsp;</span> New species
+<span class="bg2" style="padding:0 10px;margin-left:2em;border:1px solid #999;">&nbsp;</span> or
+<span class="bg4" style="padding:0 10px;border:1px solid #999;">&nbsp;</span> Species present in archive 
+<span style="padding:0 10px;margin-left:2em;border:1px solid #999;">&nbsp;</span> Species not in this version of Ensembl 
+);
+
   my $hub = EnsEMBL::Web::Hub->new;
 
   my $SD = $hub->species_defs;
@@ -58,25 +66,28 @@ sub render {
     $release_to_species->{$_->{'species_id'}}{$_->{'id'}} = $_->{'assembly'};
   }
 
-  return render_assembly_table($split_releases->[0], $species, $release_to_species).render_assembly_table($split_releases->[1], $species, $release_to_species);
+  $html .= render_assembly_table($split_releases->[0], $species, $release_to_species, 1).render_assembly_table($split_releases->[1], $species, $release_to_species, 2);
+  return $html;
 }
 
 sub render_assembly_table {
-  my ($releases, $species, $release_species) = @_;
+  my ($releases, $species, $release_species, $table_count) = @_;
   return unless @$releases;
 
   my $header = '<tr>
-    <th style="width:20%">Species</th>
+    <th style="width:20%">&nbsp;</th>
   ';
   my $body = "";
 
-  my ($date, $version, $order, $species_name, $row, $rs, $cells, $assembly_name, $current_name, $online, $class);
+  my ($date, $version, $order, $species_name, $common, $row, $rs, $cells, $assembly_name, $current_name, $online, $class);
 
   my $c = { -1 => 'bg4', 1 => 'bg2', x => 1 }; # CSS class flip-flop for tds
 
   my $style = sprintf( ' style="width:%0.3f%%"', 80 / @$releases );
   foreach my $rel (@$releases) {
-    $date = $rel->{'online'} eq 'Y' ? qq{<a href="http://} . $rel->{'archive'} . qq{.archive.ensembl.org">} . $rel->{'date'} . "</a>" : $rel->{'date'};
+    my $short_date = $rel->{'archive'};
+    $short_date =~ s/20/ 20/;
+    $date = $rel->{'online'} eq 'Y' ? qq{<a href="http://} . $rel->{'archive'} . qq{.archive.ensembl.org">} . $short_date . "</a>" : $short_date;
 
     $header .= "<th$style>$date<br />v".$rel->{'id'}."</th>";
   }
@@ -85,17 +96,23 @@ sub render_assembly_table {
   
   my @rows = ();
  
+  my $top_border = 'style="border-style:solid;border-color:#ccc;border-width:1px 0 0 0"';
+  my $two_borders = 'style="border-style:solid;border-color:#ccc;border-width:1px 0 0 1px"';
   foreach my $s (sort { $a->{'name'} cmp $b->{'name'} } @$species) {
     ($species_name = $s->{'name'}) =~ s/_/ /g;
+    $common = $s->{'common_name'};
     $cells = {};
     $assembly_name = "";
     $current_name = "";
-    $online = $s->{'online'} || 'N';
     $order = 1;
     
     $c->{'x'} = 1; # Reset the flip-flop
 
-    $row = "<tr><th>" . ( $online eq 'Y' ? qq{<a href="http://www.ensembl.org/} . $s->{'name'} . qq{"><i>$species_name</i></a>} : "<i>$species_name</i>" ) . "</th>";
+    my $name_string = "<i>$species_name</i>";
+    $name_string .= " ($common)" unless $common =~ /\./;
+    $row = sprintf('<tr><th><a href="/%s/"><i>%s</i></a>', $s->{'name'}, $species_name);
+    $row .= " ($common)" unless $common =~ /\./;
+    $row .= '</th>';
 
     foreach my $r (@$releases)  {  
       $assembly_name = $release_species->{$s->{'id'}}->{$r->{'id'}} || 'none';
@@ -111,9 +128,23 @@ sub render_assembly_table {
     # Don't print empty row
     next if !$cells->{$order}->{'name'} && $cells->{$order}->{'count'} == scalar @$releases;
 
+    my $i = 0;
     foreach my $td (sort keys %$cells) {
-      $class = $cells->{$td}->{'name'} ? $c->{$c->{'x'}*=-1} : "";
-      $row .= qq(<td colspan="$cells->{$td}->{'count'}" class="$class">$cells->{$td}->{'name'}</td>);
+      my $border = $i > 0 ? $two_borders : $top_border;
+      if ($table_count == 1
+            && $i == 0 
+            && !$cells->{$td+1}->{'name'} 
+            && $cells->{$td}->{'count'} == 1) {
+        $class = 'hilite'; ## new species
+      }
+      elsif ($cells->{$td}->{'name'}) {
+        $class = $c->{$c->{'x'}*=-1};
+      }
+      else {
+        $class = '';
+      }
+      $row .= qq(<td colspan="$cells->{$td}->{'count'}" class="$class"$border>$cells->{$td}->{'name'}</td>);
+      $i++;
     }
  
     $row .= "</tr>\n";
@@ -135,7 +166,7 @@ sub render_assembly_table {
   $body = join ('', @rows); 
   $body .= $header if $species_total % $divisor;
 
-  return qq{\n<table class="ss">\n$header\n$body\n</table>};
+  return qq{\n<table class="ss" style="margin-bottom:2em">\n$header\n$body\n</table>};
 }
 
 1;
