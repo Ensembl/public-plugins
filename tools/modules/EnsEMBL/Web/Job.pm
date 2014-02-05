6d28fa5af930ca4b29035b7440afc70e3a8a693a
Harpreet Riat <harpreet@ebi.ac.uk>
2014-02-05 17:53:35 +0000

    New object classes for Ticket & Job
    New object classes to represent an unsubmitted ticket ot job

---
+++ tools/modules/EnsEMBL/Web/Job.pm
@@ -0,0 +1,117 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Job;
+
+### Wrapper around ORM::EnsEMBL::DB::Tools::Job
+
+use strict;
+use warnings;
+
+use EnsEMBL::Web::Exceptions;
+use EnsEMBL::Web::Tools::FileSystem qw(create_path copy_files);
+
+sub object  { return shift->{'_object'};  }
+sub hub     { return shift->{'_hub'};     }
+
+sub new {
+  ## @constructor
+  ## @param Web Ticket object
+  ## @param Hashref of key-value pairs for columns of the job table row
+  ## @param Hashref of input files (destined file name => file temp location)
+  my ($class, $web_ticket, $params, $files) = @_;
+
+  return bless {
+    '_params'       => $params || {},
+    '_object'       => $web_ticket->object,
+    '_hub'          => $web_ticket->hub,
+    '_io_files'     => $files || {},
+    '_rose_object'  => undef
+  }, $class;
+}
+
+sub get_param {
+  ## Gets a param value (column value in the table) for the job
+  ## @param Valid column name for tools.job table
+  my ($self, $param_name) = @_;
+  my $rose_job = $self->rose_object;
+  return $rose_job ? $rose_job->$param_name : $self->{'_params'}{$param_name};
+}
+
+sub set_params {
+  ## Sets params values for the job
+  ## @param Hashref with keys as valid column name for tools.job table to their corresponding values being set
+  my ($self, $params) = @_;
+  my $rose_job = $self->rose_object;
+  for (keys %$params) {
+    $rose_job->$_($params->{$_}) if $rose_job;
+    $self->{'_params'}{$_} = $params->{$_};
+  }
+}
+
+sub rose_object {
+  ## Gets the linked rose object for the job (will create a new unsaved one if not already existing)
+  ## @return ORM::EnsEMBL::DB::Tools::Job object
+  my $self = shift;
+  return $self->{'_rose_object'} ||= $self->object->rose_manager((qw(Tools Job)))->object_class->new(%{$self->{'_params'}});
+}
+
+sub params {
+  ## Gets all param for the job 
+  ## @return Hashref with keys as column names, and values as (potential) column values
+  my $self = shift;
+  return $self->{'_params'};
+}
+
+sub process_for_hive_submission {
+  ## Processes the job data to make it ready to be submitted to hive db
+  ## Override to make some manipulation to job data before submitting it
+  ## The manipulated data then gets saved as hive_job_data in the same job object, along with being saved to hive job table as input_id
+  ## @return Hashref as to be passed to hive job as input id
+  return shift->rose_object->job_data->raw;
+}
+
+sub save {
+  ## Saves the linked rose object to the database
+  ## @params As accepted by Rose::DB::Object->save
+  my $self      = shift;
+  my $rose_job  = $self->rose_object or throw exception('DataObjectMissing', 'Call to save() without existance of mapped rose object');
+  return $rose_job->save(@_);
+}
+
+sub create_work_dir {
+  ## Creates the directory path for the job, and move it's input files to that directory
+  ## @param Hashref with following keys:
+  ##  - ticket_type : Ticket type name
+  ##  - ticket_name : Ticket name
+  ##  - persistent  : Flag to tell whether the ticket dir will be persistent, or will need to be cleaned after some age
+  ##  - job_number  : Unique number given to the job to differentiate from other jobs
+  ## @return Absolute path to the work dir
+  my ($self, $params) = @_;
+
+  my $files = $self->{'_io_files'};
+
+  my $dir = join '/', $self->hub->species_defs->ENSEMBL_TOOLS_TMP_DIR, ($params->{'persistent'} ? 'persistent' : 'temporary'), $params->{'ticket_type'}, ($params->{'ticket_name'} =~ /.{1,3}/g), $params->{'job_number'};
+
+  create_path($dir);
+  copy_files({ map {$files->{$_} => "$dir/$_"} keys %$files }) if keys %$files;
+
+  return $dir;
+}
+
+1;
---
+++ tools/modules/EnsEMBL/Web/Job/VEP.pm
@@ -0,0 +1,106 @@
+=head1 LICENSE
+
+Copyright [1999-2014] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Job::VEP;
+
+use strict;
+use warnings;
+
+use base qw(EnsEMBL::Web::Job);
+
+sub process_for_hive_submission {
+  ## @override
+  my $self        = shift;
+  my $rose_object = $self->rose_object;
+  my $job_data    = $rose_object->job_data;
+  my $species     = $job_data->{'species'};
+  my $vep_configs = {};
+
+  $vep_configs->{'format'}  = $job_data->{'format'};
+  $vep_configs->{'species'} = lc $species;
+
+  # refseq
+  $vep_configs->{'refseq'}  = 'yes' if ($job_data->{"core_type_$species"} // '') eq 'refseq';
+
+  # filters
+  my $frequency_filtering = $job_data->{'frequency'};
+  
+  if ($species eq 'Homo_sapiens') {
+
+    if ($frequency_filtering eq 'common') {
+      $vep_configs->{'filter_common'} = 'yes';
+    } elsif($frequency_filtering eq 'advanced') {
+      $vep_configs->{'check_frequency'} = 'yes';
+      $vep_configs->{$_} = $job_data->{$_} for qw(freq_pop freq_freq freq_gt_lt freq_filter);
+    }
+  }
+
+  my $summary = $job_data->{'summary'};
+  if ($summary ne 'no') {
+    $vep_configs->{$summary} = 'yes';
+  }
+
+  # species-dependent
+  for (qw(regulatory sift polyphen)) {
+    my $value = $job_data->{"${_}_$species"};
+    $vep_configs->{$_} = $value if $value && $value ne 'no';
+  }
+
+  # regulatory
+  if($vep_configs->{'regulatory'}) {
+    
+    # cell types
+    if($vep_configs->{'regulatory'} eq 'cell') {
+      my @cell_types = grep { length $_ } ref $job_data->{'cell_type'} ? @{$job_data->{'cell_type'}} : $job_data->{'cell_type'};
+      $vep_configs->{'cell_type'} = join ",", @cell_types if scalar @cell_types;
+    }
+
+    $vep_configs->{'regulatory'} = 'yes';
+  }
+
+  # check existing
+  my $check_ex = $job_data->{"check_existing_$species"};
+
+  if ($check_ex) {
+    if($check_ex eq 'check') {
+      $vep_configs->{'check_existing'} = 'yes';
+    } elsif ($check_ex eq 'allele') {
+      $vep_configs->{'check_existing'} = 'yes';
+      $vep_configs->{'check_alleles'} = 'yes';
+    }
+    
+    # MAFs in human
+    if ($species eq 'Homo_sapiens') {
+      $vep_configs->{'gmaf'}    = 'yes' if ($job_data->{'gmaf_'.$check_ex}    // '') eq 'yes';
+      $vep_configs->{'maf_1kg'} = 'yes' if ($job_data->{'maf_1kg_'.$check_ex} // '') eq 'yes';
+      $vep_configs->{'maf_esp'} = 'yes' if ($job_data->{'maf_esp_'.$check_ex} // '') eq 'yes';
+    }
+  }
+
+  # i/o files
+  $vep_configs->{'input_file'}  = 'input.txt';
+  $vep_configs->{'output_file'} = 'output.vcf';
+  $vep_configs->{'stats_file'}  = 'stats.txt';
+
+  # extra and identifiers
+  $job_data->{$_} and $vep_configs->{$_} = $job_data->{$_} for qw(numbers canonical domains biotype symbol ccds protein hgvs coding_only);
+
+  return { 'species' => $vep_configs->{'species'}, 'work_dir' => $rose_object->job_dir, 'config' => $vep_configs };
+}
+
+1;
