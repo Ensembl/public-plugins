84ccfda8432ea1f294019a44da86d925e59b9af8
Harpreet Riat <harpreet@ebi.ac.uk>
2014-03-20 14:38:10 +0000

    Bug fix: irrelevant arguments need to be filtered
    Only preserve those arguments that are valid for the selected species, otherwise they can cause problems with the VEP script.

--- tools/modules/EnsEMBL/Web/Job/VEP.pm
+++ tools/modules/EnsEMBL/Web/Job/VEP.pm
@@ -29,17 +29,18 @@ sub process_for_hive_submission {
   my $rose_object = $self->rose_object;
   my $job_data    = $rose_object->job_data;
   my $species     = $job_data->{'species'};
+  my $sp_details  = $self->_species_details($species);
   my $vep_configs = {};
 
   $vep_configs->{'format'}  = $job_data->{'format'};
   $vep_configs->{'species'} = lc $species;
 
   # refseq
-  $vep_configs->{'refseq'}  = 'yes' if ($job_data->{'core_type'} // '') eq 'refseq';
+  $vep_configs->{'refseq'}  = 'yes' if $sp_details->{'refseq'} && ($job_data->{'core_type'} // '') eq 'refseq';
 
   # filters
   my $frequency_filtering = $job_data->{'frequency'};
-  
+
   if ($species eq 'Homo_sapiens') {
 
     if ($frequency_filtering eq 'common') {
@@ -55,14 +56,14 @@ sub process_for_hive_submission {
     $vep_configs->{$summary} = 'yes';
   }
 
-  for (qw(regulatory sift polyphen)) {
+  for (grep $sp_details->{$_}, qw(regulatory sift polyphen)) {
     my $value = $job_data->{$_ . ($_ eq 'regulatory' ? "_$species" : '')};
     $vep_configs->{$_} = $value if $value && $value ne 'no';
   }
 
   # regulatory
-  if($vep_configs->{'regulatory'}) {
-    
+  if($sp_details->{'regulatory'} && $vep_configs->{'regulatory'}) {
+
     # cell types
     if($vep_configs->{'regulatory'} eq 'cell') {
       my @cell_types = grep { length $_ } ref $job_data->{"cell_type_$species"} ? @{$job_data->{"cell_type_$species"}} : $job_data->{"cell_type_$species"};
@@ -82,7 +83,7 @@ sub process_for_hive_submission {
       $vep_configs->{'check_existing'} = 'yes';
       $vep_configs->{'check_alleles'} = 'yes';
     }
-    
+
     # MAFs in human
     if ($species eq 'Homo_sapiens') {
       ($job_data->{'gmaf'} // '') eq 'yes' and $vep_configs->{$_} = 'yes' for qw(gmaf maf_1kg maf_esp);
@@ -103,6 +104,24 @@ sub process_for_hive_submission {
   }
 
   return { 'species' => $vep_configs->{'species'}, 'work_dir' => $rose_object->job_dir, 'config' => $vep_configs };
+}
+
+sub _species_details {
+  ## @private
+  my ($self, $species) = @_;
+
+  my $sd        = $self->hub->species_defs;
+  my $db_config = $sd->get_config($species, 'databases');
+
+  return {
+    'sift'        => $db_config->{'DATABASE_VARIATION'}{'SIFT'},
+    'polyphen'    => $db_config->{'DATABASE_VARIATION'}{'POLYPHEN'},
+    'regulatory'  => $sd->get_config($species, 'REGULATORY_BUILD'),
+
+    # at the moment only human, chicken and mouse have RefSeqs in their otherfeatures DB
+    # there's no config for this currently so species are listed manually
+    'refseq'      => grep({ $_ eq $species } qw(Gallus_gallus Homo_sapiens Mus_musculus)) && $db_config->{'DATABASE_OTHERFEATURES'}
+  };
 }
 
 1;
