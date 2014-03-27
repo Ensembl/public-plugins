29d9583a7bd39210ae86ef0b7f1444961a88cbfe
Harpreet Riat <harpreet@ebi.ac.uk>
2014-03-27 18:47:51 +0000

    New version of beekeeper
    This is designed to be run every few seconds by a cronjob.
    If the beekeeper thread is already running, it will get not launch
    a new beekeeper.

--- tools/utils/beekeeper.pl
+++ tools/utils/beekeeper.pl
@@ -13,14 +13,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script wraps the actual hive beekeeper script to get the url
+# param from SiteDefs instead of having to provide it in command line.
+
+# This accepts an extra arguments '--no_cache_config' to ignore the cached
+# db name saved in the beekeeper.cmd file.
 
 use strict;
-use warnings;
 
-use FindBin qw($Bin);
+use Data::Dumper;
 use DBI;
+use FindBin qw($Bin);
 
-BEGIN {
+my $config_file = "$Bin/beekeeper.config";
+my $no_cache    = grep { $_ eq '--no_cache_config' } @ARGV;
+my $config;
+
+if (!$no_cache && -e $config_file && open CONF, "<$config_file") {
+  $config = <CONF>;
+  $config = eval "$config";
+  $config = undef if $@ || ref $config ne 'HASH';
+  close CONF;
+}
+
+if (!$config) {
   my $code_path = "$Bin/../../..";
   unshift @INC, "$code_path/ensembl-webcode/conf";
   eval {
@@ -30,31 +46,50 @@ BEGIN {
     print "Can't use SiteDefs - $@\n";
     exit;
   }
-  unshift @INC, "$code_path/sanger-plugins/tools/modules/"; # TEMP - while tools code in in sanger-plugins
-#  unshift @INC, "$code_path/public-plugins/tools/modules/";
-  unshift @INC, $_ for @SiteDefs::ENSEMBL_LIB_DIRS;
-  $ENV{'PERL5LIB'} = join ':', $ENV{'PERL5LIB'}, @INC;
-  
+  unshift @INC, reverse ("$code_path/sanger-plugins/tools/modules/", @{SiteDefs::ENSEMBL_LIB_DIRS});
+  $ENV{'PERL5LIB'} = join ':', $ENV{'PERL5LIB'} || (), @INC;
+
   require EnsEMBL::Web::SpeciesDefs;
+
+  my $sd  = EnsEMBL::Web::SpeciesDefs->new();
+  $config = {
+    'EHIVE_ROOT_DIR'  => $ENV{'EHIVE_ROOT_DIR'},
+    'inc'             => \@INC,
+    'db'              => {
+      'host'            =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'HOST'},
+      'port'            =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'PORT'},
+      'user'            =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'USER'} || $sd->DATABASE_WRITE_USER,
+      'pass'            =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'PASS'} || $sd->DATABASE_WRITE_PASS,
+      'name'            =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'NAME'}
+    }
+  };
+
+  if (open CONF, ">$config_file") {
+    print CONF Data::Dumper->new([$config])->Sortkeys(1)->Useqq(1)->Terse(1)->Indent(0)->Dump;
+    close CONF;
+  } else {
+    print "Can't save db configs: $!";
+    exit;
+  }
+} else {
+  @INC = @{$config->{'inc'}};
+  $ENV{'PERL5LIB'} = join ':', $ENV{'PERL5LIB'} || (), @INC;
 }
 
-my $sd = EnsEMBL::Web::SpeciesDefs->new();
-my $db = {
-  '-host'   =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'HOST'},
-  '-port'   =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'PORT'}, 
-  '-user'   =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'USER'} || $sd->DATABASE_WRITE_USER,
-  '-pass'   =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'PASS'} || $sd->DATABASE_WRITE_PASS,
-  '-dbname' =>  $sd->multidb->{'DATABASE_WEB_HIVE'}{'NAME'},
-};
-
-my $command     = "-url mysql://$db->{'-user'}:$db->{'-pass'}\@$db->{'-host'}:$db->{'-port'}/$db->{'-dbname'}";
+my $command     = "-url mysql://$config->{'db'}{'user'}:$config->{'db'}{'pass'}\@$config->{'db'}{'host'}:$config->{'db'}{'port'}/$config->{'db'}{'name'}";
 my $script_name = 'beekeeper.pl';
-my $dbh         = DBI->connect(sprintf('dbi:mysql:%s:%s:%s', $db->{'-dbname'}, $db->{'-host'}, $db->{'-port'}), $db->{'-user'}, $db->{'-pass'}, { 'PrintError' => 0 });
+
+if (my $pid = `ps -eo pid,cmd | grep "perl $script_name $command" | grep -v grep | sed -r 's/^\\s+//' | cut -d ' ' -f 1`) {
+  print "Beekeeper already running with PID $pid\n";
+  exit;
+}
+
+my $dbh = DBI->connect(sprintf('dbi:mysql:%s:%s:%s', $config->{'db'}{'name'}, $config->{'db'}{'host'}, $config->{'db'}{'port'}), $config->{'db'}{'user'}, $config->{'db'}{'pass'}, { 'PrintError' => 0 });
 
 die "Database connection to hive db could not be created. Please make sure the pipiline is initialised.\nError: $DBI::errstr\n"   unless $dbh;
-die "ENV variable EHIVE_ROOT_DIR is not set. Please set it to the location containg HIVE code.\n"                                 unless $ENV{'EHIVE_ROOT_DIR'};
-die "Could not find location of the $script_name script.\n"                                                                       unless chdir "$ENV{'EHIVE_ROOT_DIR'}/scripts/";
+die "ENV variable EHIVE_ROOT_DIR is not set. Please set it to the location containg HIVE code.\n"                                 unless $config->{'EHIVE_ROOT_DIR'};
+die "Could not find location of the $script_name script.\n"                                                                       unless chdir "$config->{'EHIVE_ROOT_DIR'}/scripts/";
 
-system(join ' ', 'perl', $script_name, $command, @ARGV);
+system(join ' ', 'perl', $script_name, $command, grep { $_ ne '--no_cache_config' } @ARGV);
 
 1;
