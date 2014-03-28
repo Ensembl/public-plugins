eb68a355cd15f83b7ce7bbc843a8f4cb12abb0c0
Harpreet Riat <harpreet@ebi.ac.uk>
2014-03-28 10:58:15 +0000

    Beekeeper optionally can redirect output
    Beekeeper can now optionally redirect output to log files if '--redirect_output' option is provided.
    This is to allow debug info to go into log files if script is running via cron

--- tools/utils/beekeeper.pl
+++ tools/utils/beekeeper.pl
@@ -16,24 +16,44 @@
 # This script wraps the actual hive beekeeper script to get the url
 # param from SiteDefs instead of having to provide it in command line.
 
-# This accepts an extra arguments '--no_cache_config' to ignore the cached
-# configurations saved in the beekeeper.config file.
+# This accepts two extra arguments:
+# --no_cache_config: to ignore the cached configurations saved in
+#   the beekeeper.config file.
+# --redirect_output: flag provided if this script is being run by cron.
+#   The output is then redirected to log files.
 
 use strict;
 
 use Data::Dumper;
 use DBI;
 use FindBin qw($Bin);
+use Time::localtime;
 
-my $config_file = "$Bin/beekeeper.config";
-my $no_cache    = grep { $_ eq '--no_cache_config' } @ARGV;
+my $config_file   = "$Bin/beekeeper.config";
+my $log_file      = "$Bin/beekeeper.log";
+my $cron_log_file = "$Bin/beekeeper.cronlog";
+my $script_name   = 'beekeeper.pl';
+my $no_cache      = grep { $_ eq '--no_cache_config' } @ARGV;
+my $redirect_out  = grep { $_ eq '--redirect_output' } @ARGV;
+my $sleep_time    = grep({ $_ =~ /^\-\-sleep/ } @ARGV) ? undef : "0.5";
 my $config;
 
-if (!$no_cache && -e $config_file && open CONF, "<$config_file") {
-  $config = <CONF>;
-  $config = eval "$config";
-  $config = undef if $@ || ref $config ne 'HASH';
-  close CONF;
+if ($redirect_out) {
+  open STDERR, ">>$cron_log_file";
+  warn sprintf "\n%s\n%s\n", ctime(), '-' x 24;
+}
+
+if (!$no_cache && -e $config_file) {
+  if (open CONF, "<$config_file") {
+    warn "Retrieving cached conf from $config_file\n";
+    $config = <CONF>;
+    $config = eval "$config";
+    $config = undef if $@ || ref $config ne 'HASH';
+    close CONF;
+  } else {
+    warn "Couldn't open $config_file file to read configs: $!\n";
+    warn "Continuing to read configs from SiteDefs.\n";
+  }
 }
 
 if (!$config) {
@@ -43,8 +63,7 @@ if (!$config) {
     require SiteDefs;
   };
   if ($@) {
-    print "Can't use SiteDefs - $@\n";
-    exit;
+    die "Can't use SiteDefs - $@\n";
   }
   unshift @INC, reverse ("$code_path/sanger-plugins/tools/modules/", @{SiteDefs::ENSEMBL_LIB_DIRS});
   $ENV{'PERL5LIB'} = join ':', $ENV{'PERL5LIB'} || (), @INC;
@@ -64,23 +83,19 @@ if (!$config) {
     }
   };
 
-  if (open CONF, ">$config_file") {
-    print CONF Data::Dumper->new([$config])->Sortkeys(1)->Useqq(1)->Terse(1)->Indent(0)->Dump;
-    close CONF;
-  } else {
-    print "Can't save db configs: $!";
-    exit;
-  }
+  open(CONF, ">$config_file") or die "Couldn't open $config_file file to write configs: $!";
+  print CONF Data::Dumper->new([$config])->Sortkeys(1)->Useqq(1)->Terse(1)->Indent(0)->Dump;
+  close CONF;
+
 } else {
   @INC = @{$config->{'inc'}};
   $ENV{'PERL5LIB'} = join ':', $ENV{'PERL5LIB'} || (), @INC;
 }
 
-my $command     = "-url mysql://$config->{'db'}{'user'}:$config->{'db'}{'pass'}\@$config->{'db'}{'host'}:$config->{'db'}{'port'}/$config->{'db'}{'name'}";
-my $script_name = 'beekeeper.pl';
+my $command = "perl $script_name -url mysql://$config->{'db'}{'user'}:$config->{'db'}{'pass'}\@$config->{'db'}{'host'}:$config->{'db'}{'port'}/$config->{'db'}{'name'}";
 
-if (my $pid = `ps -eo pid,cmd | grep "perl $script_name $command" | grep -v grep | sed -r 's/^\\s+//' | cut -d ' ' -f 1`) {
-  print "Beekeeper already running with PID $pid\n";
+if (my $pid = `ps -eo pid,cmd | sed -r 's/^\\s+//' | grep "$command" | grep -v grep | cut -d ' ' -f 1 | head -1`) {
+  warn "Beekeeper already running with PID $pid\n";
   exit;
 }
 
@@ -88,8 +103,9 @@ my $dbh = DBI->connect(sprintf('dbi:mysql:%s:%s:%s', $config->{'db'}{'name'}, $c
 
 die "Database connection to hive db could not be created. Please make sure the pipiline is initialised.\nError: $DBI::errstr\n"   unless $dbh;
 die "ENV variable EHIVE_ROOT_DIR is not set. Please set it to the location containg HIVE code.\n"                                 unless $config->{'EHIVE_ROOT_DIR'};
-die "Could not find location of the $script_name script.\n"                                                                       unless chdir "$config->{'EHIVE_ROOT_DIR'}/scripts/";
+die "Could not find location of the hive $script_name script.\n"                                                                  unless chdir "$config->{'EHIVE_ROOT_DIR'}/scripts/";
 
-system(join ' ', 'perl', $script_name, $command, grep { $_ ne '--no_cache_config' } @ARGV);
+warn "Running beekeeper\n";
+system(join ' ', $command, $sleep_time ? ("--sleep=$sleep_time") : (), grep($_ !~ /^\-\-(redirect_output|no_cache_config)$/, @ARGV), $redirect_out ? ('>&', $log_file) : ());
 
 1;
