6d079d0393b5447f10833070830e7a146cc5bf4c
Anne Lyle <annelyle@ebi.ac.uk>
2014-05-02 15:38:37 +0100

    Added a method for retrieving stored assembly name

--- users/modules/EnsEMBL/Web/Record.pm
+++ users/modules/EnsEMBL/Web/Record.pm
@@ -73,6 +73,10 @@ sub label { # if this record is a das record, the webcode expects it to have thi
   return shift->data->{'label'};
 }
 
+sub assembly { # We store assembly ID in an arbitrary field
+  return shift->data->{'module_version'};
+}
+
 sub logic_name {  # if this record is a das record, the webcode expects it to have this method
   return shift->data->{'logic_name'};
 }
