c840793b75158a85d97a43171bc1efd2f87d4e5e
Harpreet Riat <harpreet@ebi.ac.uk>
2014-05-12 12:14:59 +0100

    HSP_plot and HSP_query_plot merged together

--- tools/modules/EnsEMBL/Draw/GlyphSet/HSP_query_plot.pm
+++ tools/modules/EnsEMBL/Draw/GlyphSet/HSP_query_plot.pm
@@ -19,15 +19,76 @@ limitations under the License.
 package EnsEMBL::Draw::GlyphSet::HSP_query_plot;
 
 use strict;
+use warnings;
 
-use parent qw(EnsEMBL::Draw::GlyphSet::HSP_plot);
+use Sanger::Graphics::Bump;
+
+use parent qw(EnsEMBL::Draw::GlyphSet);
+
+sub _init {
+  my ($self)        = @_;
+  my $container     = $self->{'container'};
+  my $config        = $self->{'config'};
+  my $mode          = $self->my_config('mode') || "byhit";
+  my $colours       = $container->colours;
+  my $opts          = {
+    'pix_per_bp'      => $config->transform->{'scalex'},
+    'bitmap_length'   => int($container->length() * $config->transform->{'scalex'}),
+    'id'              => $container->name,
+    'db'              => $container->{'database'},
+    'dep'             => $self->my_config('dep') || 10,
+    'bitmap'          => [],
+    'tally'           => {},
+  };
+
+  my @all_hsps      = ();
+  my $ori           = $self->strand;
+  foreach my $hsp (@{$container->hsps}) {
+    my $qori = $hsp->{'q_ori'} || 1;
+    my $hori = $hsp->{'g_ori'} || 1;
+    next if $qori * $hori != $ori;
+    push @all_hsps, $hsp;
+  }
+
+  $self->hsp($_, $opts, $colours) for sort { $b->{'pident'} <=> $a->{'pident'} } @all_hsps;
+}
+
+sub hsp {
+  my ($self, $hsp, $opts, $colours) = @_;
+
+  my ($hspstart, $hspend) = $self->region($hsp);
+  my $colour              = $colours->[int(((@$colours - 1) * $hsp->{'pident'} / 100) + 0.5)]; # round
+  my $height              = 5;
+  my $glyph               = Sanger::Graphics::Glyph::Rect->new({
+    'x'                     => $hspstart,
+    'y'                     => 0,
+    'width'                 => $hspend - $hspstart,
+    'height'                => $height,
+    'colour'                => $colour,
+    'bordercolour'          => 'black',
+    'href'                  => $self->_url({
+      'species' => $self->species,
+      'action'  => 'Blast',
+      'tl'      => $hsp->{'tl'}
+    })
+  });
+
+  my $bump_start          = int($glyph->x() * $opts->{'pix_per_bp'});
+     $bump_start          = 0 if $bump_start < 0;
+  my $bump_end            = $bump_start + int($glyph->width() * $opts->{'pix_per_bp'}) + 1;
+     $bump_end            = $opts->{'bitmap_length'} if $bump_end > $opts->{'bitmap_length'};
+  my $row                 = &Sanger::Graphics::Bump::bump_row($bump_start, $bump_end, $opts->{'bitmap_length'}, $opts->{'bitmap'});
+
+  return if $opts->{'dep'} != 0 && $row >= $opts->{'dep'};
+  $glyph->y($glyph->y() - (1.6 * $row * $height * $self->strand()));
+  $self->push($glyph);
+}
 
 sub region {
   my ($self, $hsp) = @_;
-
   my $start = $hsp->{'qstart'};
   my $end   = $hsp->{'qend'};
-  return ($start, $end);
+  return $end < $start ? ($end, $start) : ($start, $end);
 }
 
 1;
