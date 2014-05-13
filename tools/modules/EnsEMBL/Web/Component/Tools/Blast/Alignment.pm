6c0fece802ae4d9ae10be4e6f40b9b4c56dfce11
Harpreet Riat <harpreet@ebi.ac.uk>
2014-05-13 17:50:32 +0100

    hit_id method removed + code tidy
    hit_id method replaced with key 'result_id' according to Object::Blast::get_all_hits

--- tools/modules/EnsEMBL/Web/Component/Tools/Blast/Alignment.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/Blast/Alignment.pm
@@ -43,16 +43,16 @@ sub initialize {
   for (qw(align_display exon_display snp_display hide_long_snps line_numbering title_display)) {
     $config->{$_} = $hub->param($_) unless $hub->param($_) =~ /^(off|no)$/;
   }
-  
+
   $config->{'slices'}     = $slices || $self->get_slice($config);
   $config->{'end_number'} = $config->{'number'} = 1 if $config->{'line_numbering'};
-  
+
   if (!$config->{'align_display'}) {
     splice @{$config->{'slices'}}, 1, 1;
   } elsif ($config->{'align_display'} eq 'dot') {
     $config->{'slices'}[1]{'seq'} =~ s/\|/\./g;
   }
-  
+
   my ($sequence, $markup) = $self->get_sequence_data($config->{'slices'}, $config);
 
   $self->markup_exons($sequence, $markup, $config)     if $config->{'exon_display'};
@@ -65,11 +65,11 @@ sub initialize {
 
 sub content {
   my $self = shift;
-  
+
   return '' unless $self->job;
-  
+
   my ($sequence, $config) = $self->initialize;
-  
+
   return sprintf '%s<div class="sequence_key">%s</div>%s', $self->tool_buttons, $self->get_key($config), $self->build_sequence($sequence, $config);
 }
 
@@ -80,11 +80,11 @@ sub get_slice {
   my $hit             = $self->hit;
   my $species         = $hit->{'species'};
   my $blast_method    = $self->blast_method;
-  my $query_seq       = $self->query_sequence($job, $hit, $blast_method, $species); 
+  my $query_seq       = $self->query_sequence($job, $hit, $blast_method, $species);
   my $hit_seq         = $query_seq;
   my $aln_summary     = $hit->{'aln'} =~ s/\s+//rg;
   my $homology_string = '|' x $hit->{'len'};
-  
+
   # take into account alignments that do not start with a match
   $aln_summary = $aln_summary =~ /^\d+/ ? $aln_summary : "0$aln_summary";
 
@@ -92,32 +92,32 @@ sub get_slice {
     # process BTOP alignment to allow for gaps and missmatches
     $aln_summary =~ s/(\d+)/:$1:/g;
     $aln_summary =~ s/^:|:$//g;
-    
+
     my @alignment_features = split ':', $aln_summary;
     my $index              = 0;
 
-    while (scalar @alignment_features > 1) {  
+    while (scalar @alignment_features > 1) {
       my $seq_index   = shift @alignment_features;
       my $edit_string = shift @alignment_features;
       my @edits       = split '', $edit_string;
-      
+
       $index += $seq_index;
-      
+
       while (@edits) {
         my $query_edit = shift @edits;
         my $hit_edit   = shift @edits;
         my $length     = $query_edit eq '-' ? 0 : 1;
            $length     = $hit_edit   eq '-' ? 0 : 1 if $blast_method =~ /blastx/ && $hit->{'qori'} == -1;
-        
+
         substr($query_seq,       $index, $length) = $query_edit;
         substr($hit_seq,         $index, $length) = $hit_edit;
         substr($homology_string, $index, 1)       = ' ';
-        
+
         $index++;
       }
     }
   }
-  
+
   my $query_slice = $self->create_slice($hit->{'qid'}, $hit->{'qstart'}, $hit->{'qori'}, $query_seq);
   my $hit_slice   = $self->create_slice($hit->{'tid'}, $hit->{'tstart'}, $hit->{'tori'}, $hit_seq);
   my @slices;
@@ -132,46 +132,46 @@ sub get_slice {
     my $ref_slice     = $object->get_hit_genomic_slice($hit, $species);
     my $target_object = $object->get_target_object($hit, $job->job_data->{'source'});
     my $orientation   = $hit->{'gori'};
-    
+
     if ($target_object) {
       my $object_strand = $target_object->isa('Bio::EnsEMBL::Translation') ? $target_object->start_Exon->strand : $target_object->strand;
-         $orientation   = $hit->{'tori'} eq $object_strand ? 1 : -1; 
+         $orientation   = $hit->{'tori'} eq $object_strand ? 1 : -1;
     }
-    
+
     my $mapped_slice_container = $self->get_mapped_slice($hit_slice, $hit_seq, $ref_slice, $orientation);
     my $mapped_slices          = shift @{$mapped_slice_container->get_all_MappedSlices};
     my ($slice, $mapper)       = @{$mapped_slices->get_all_Slice_Mapper_pairs->[0]};
-    
+
     @slices = (
-      { name => 'Query',   slice => $query_slice,     no_markup => 1 },    
+      { name => 'Query',   slice => $query_slice,     no_markup => 1 },
       { name => '',        seq   => $homology_string, no_markup => 1 },
       { name => 'Subject', slice => $slice, seq => $hit_slice->seq   },
     );
-    
+
     $config->{'mapper'}          = $mapper;
     $config->{'ref_slice_start'} = $ref_slice->start;
     $config->{'ref_slice_end'}   = $ref_slice->end;
-  } 
-  
+  }
+
   return \@slices;
 }
 
 sub get_mapped_slice {
   my ($self, $hit_slice, $hit_seq, $reference_slice, $mapping_orientation) = @_;
-  my $object            = $self->object; 
+  my $object            = $self->object;
   my $hub               = $self->hub;
   my $hit               = $self->hit;
   my $species           = $self->job->species;
   my $sr_name           = $reference_slice->seq_region_name;
   my $msc               = Bio::EnsEMBL::MappedSliceContainer->new(-SLICE => $reference_slice, -EXPANDED => 1);
   my $ms                = Bio::EnsEMBL::MappedSlice->new(-adaptor => $reference_slice->adaptor, -name => 'test_map', -container => $msc);
-  my $mapper            = Bio::EnsEMBL::Mapper->new('mapped_slice', 'ref_slice');  
-  my $end               = $reference_slice->end; 
+  my $mapper            = Bio::EnsEMBL::Mapper->new('mapped_slice', 'ref_slice');
+  my $end               = $reference_slice->end;
   my $indel_flag        = 0;
   my $total_length_diff = 0;
 
   # Process genomic btop string to map coordiniates
-  my $aln = $hit->{'db_type'} =~ /cdna/i ?  $object->map_btop_to_genomic_coords($hit, $self->hit_id) : $hit->{'aln'};
+  my $aln = $hit->{'db_type'} =~ /cdna/i ?  $object->map_btop_to_genomic_coords($hit, $hit->{'result_id'}) : $hit->{'aln'};
      $aln = $object->reverse_btop($aln) if $hit->{'gori'} ne '1' && $hit->{'db_type'}=~ /latest/i;
      $aln =~ s/(\d+)/:$1:/g;
      $aln  =~ s/^:|:$//g;
@@ -188,31 +188,31 @@ sub get_mapped_slice {
   my $seq_index     = 0;
   my $last_edit_pos = 0;
   my %edits;
-  
-  while (scalar @aln_features) { 
+
+  while (scalar @aln_features) {
     my $matching_bp = shift @aln_features;
     my $edit_string = shift @aln_features;
        $seq_index  += $matching_bp;
-       
+
     last unless $edit_string;
-    
+
     my $edit_len = length($edit_string) / 2;
     my ($type, $diffs);
-    
+
     if ($edit_len > 1) {
       my @base_edits = split //, $edit_string;
       my $count      = 0;
       my $previous_state;
-      
+
       while (my $query_base = $rev_flag ? pop @base_edits : shift @base_edits) {
         my $target_base = $rev_flag ? pop @base_edits : shift @base_edits;
         ($target_base, $query_base) = ($query_base, $target_base) if $rev_flag;
-        
+
         my @bases = ($query_base, $target_base);
         my $state = $target_base eq '-' && $query_base  ne '-' ? 'insert_query' :
                     $query_base  eq '-' && $target_base ne '-' ? 'insert_hit'   :
                     $query_base =~ /[ACTG]/i && $target_base =~ /[ACTG]/i ? 'missmatch' : 'gap';
-        
+
         if (!$previous_state) {
           $diffs->[0] = \@bases;
         } elsif ($state eq $previous_state) {
@@ -221,56 +221,56 @@ sub get_mapped_slice {
           $count++;
           $diffs->[$count] = \@bases;
         }
-        
+
         $previous_state = $state;
       }
     } else {
       my @differences = split '', $edit_string;
          $diffs->[0]  = \@differences;
     }
-    
+
     my @edit_info = [ $seq_index ];
-    
+
     foreach my $d (@$diffs) {
       my @differences = @{$d};
       my $length      = scalar @differences / 2;
-      my $query_base  = shift @differences; 
+      my $query_base  = shift @differences;
       my $target_base = shift @differences;
-      my $type        = $target_base eq '-' && $query_base  ne '-' ? 'insert_query' : 
+      my $type        = $target_base eq '-' && $query_base  ne '-' ? 'insert_query' :
                         $query_base  eq '-' && $target_base ne '-' ? 'insert_hit'   :
                         ($query_base =~ /[ACTG]/i && $target_base =~ /[ACTG]/i) ? 'missmatch' : 'gap';
-      
+
       if ($type ne 'missmatch') {
         my $pos = $seq_index - $last_edit_pos;
-        
+
         $edits{$seq_index} = [ $pos, $type, $length ];
         $last_edit_pos     = $seq_index;
-        $last_edit_pos     = $type ne 'gap' ? $last_edit_pos += $length : $last_edit_pos + 1; 
+        $last_edit_pos     = $type ne 'gap' ? $last_edit_pos += $length : $last_edit_pos + 1;
       }
-      
+
       $seq_index = $type eq 'gap' ? $seq_index + 1 : $seq_index + $length;
     }
   }
-  
+
   my $ref_start         = $hit->{'gstart'};
   my $ref_strand        = $hit->{'gori'};
   my $ms_start          = $rev_flag ? $hit->{'len'} : 1;
   my $total_insert_size = 0;
-  
+
   foreach my $edit_position (sort { $a <=> $b } keys %edits) {
-    my $num_matching_bp = $edits{$edit_position}->[0];     
+    my $num_matching_bp = $edits{$edit_position}->[0];
     my $edit_type       = $edits{$edit_position}->[1];
     my $edit_length     = $edits{$edit_position}->[2];
     my $ms_end          = $rev_flag ? $ms_start - $num_matching_bp + 1 : $ms_start + $num_matching_bp - 1;
     my $ref_end         = $ref_start + $num_matching_bp -1;
-    
-    if ($edit_type eq 'insert_hit') { 
+
+    if ($edit_type eq 'insert_hit') {
       $ms_end   = $rev_flag ? $ms_end - $edit_length : $ms_end + $edit_length;
-      $ref_end += $edit_length; 
+      $ref_end += $edit_length;
     }
-    
+
     ($ms_start, $ms_end) = ($ms_end, $ms_start) if $ms_start > $ms_end;
-    
+
     # first add match block
     $mapper->add_map_coordinates(
       'mapped_slice',
@@ -281,25 +281,25 @@ sub get_mapped_slice {
       $ref_start,
       $ref_end,
     );
-    
+
     if ($edit_type eq 'insert_query') {
       $ms_start  = $rev_flag ? $ms_start - 1 - $edit_length : $ms_end + 1 + $edit_length;
       $ref_start = $ref_end + 1;
     } elsif ($edit_type eq 'insert_hit') {
       $ms_start           = $rev_flag ? $ms_start - 1 : $ms_end + 1;
       $ref_start          = $ref_end + 1;
-      $total_insert_size += $edit_length;  
+      $total_insert_size += $edit_length;
     } else {
       $ms_start  = $rev_flag ? $ms_start - 1 : $ms_end + 1;
       $ref_start = $ref_end + $edit_length + 1;
     }
-  }   
-  
+  }
+
   if (($mapping_orientation == 1 && $ms_start < $seq_index) || ($mapping_orientation != 1 && $ms_start > 0)) {
     my $ms_end = $rev_flag ? 1 : $end  - $ref_start + $ms_start;
-    
+
     ($ms_start, $ms_end) = ($ms_end, $ms_start) if $ms_start > $ms_end;
-    
+
     $mapper->add_map_coordinates(
      'mapped_slice',
       $ms_start,
@@ -310,17 +310,17 @@ sub get_mapped_slice {
       $end,
     );
   }
-  
+
   $reference_slice = $reference_slice->expand(undef, $total_insert_size, 1);
   $ms->add_Slice_Mapper_pair($reference_slice, $mapper);
   $msc->{'mapped_slices'} = [ $ms ];
-  
+
   return $msc;
 }
 
 sub create_slice {
   my ($self, $name, $start, $strand, $seq) = @_;
-  
+
   return Bio::EnsEMBL::Slice->new(
     -seq_region_name => $name,
     -start           => $start,
@@ -341,43 +341,43 @@ sub query_sequence {
   my $end            = $hit->{'qend'};
   my $length         = $end - $start + 1;
   my $full_length;
-  
+
   if ($blast_method =~ /blastx/i) {
     my $codon_table_id = 1;
-    my $frame          = $hit->{'qframe'}; 
+    my $frame          = $hit->{'qframe'};
        $length         = $length / 3;
-    
+
     if ($frame =~ /\-/) {
       my $strand        = $hit->{'gori'};
       my $slice_adaptor = $self->hub->get_adaptor('get_SliceAdaptor', 'core', $species);
       my $temp_seq;
-      
+
       $frame = 0;
-      
-      foreach (@{$hit->{'g_coords'}}) { 
+
+      foreach (@{$hit->{'g_coords'}}) {
         my $slice       = $slice_adaptor->fetch_by_toplevel_location(sprintf '%s:%s-%s:%s', $hit->{'gid'}, $start, $end, $$hit->{'gori'});
            $temp_seq   .= $slice->seq;
            $full_length = 1;
       }
-      
+
       $query_sequence = $temp_seq;
     }
-    
+
     $frame = $frame > 0 ? $frame -= 1 : $frame;
-    
+
     my $peptide = Bio::Seq->new(
       -seq      => $query_sequence,
-      -moltype  => 'dna', 
+      -moltype  => 'dna',
       -alphabet => 'dna',
       -id       => 'sequence'
     );
-    
+
     $query_sequence = $peptide->translate(undef, undef, $frame, $codon_table_id)->seq;
     $query_sequence =~ s/\*$//;
     $start          = ($hit->{'qstart'} / 3) + 1;
     $start          = length($query_sequence) - $start if $hit->{'qori'} =~ /\-/;
   }
-  
+
   return $full_length ? $query_sequence : substr($query_sequence, $start - 1, $length);
 }
 
@@ -393,9 +393,9 @@ sub set_variations {
 
 sub markup_line_numbers {
   my ($self, $sequence, $config) = @_;
-  
+
   $self->SUPER::markup_line_numbers($sequence, $config);
-  
+
   foreach (map @$_, values %{$config->{'line_numbers'}}) {
     $config->{'padding'}{'pre_number'} = length $_->{'label'} if length $_->{'label'} > $config->{'padding'}{'pre_number'};
   }
--- tools/modules/EnsEMBL/Web/Component/Tools/Blast/TextSequence.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/Blast/TextSequence.pm
@@ -20,13 +20,15 @@ package EnsEMBL::Web::Component::Tools::Blast::TextSequence;
 
 use strict;
 
-use parent qw(EnsEMBL::Web::Component::TextSequence EnsEMBL::Web::Component::Tools::Blast);
+use parent qw(
+  EnsEMBL::Web::Component::TextSequence
+  EnsEMBL::Web::Component::Tools::Blast
+);
 
-sub job            { return $_[0]{'_job'};                        } # @return The cached requested job object
-sub hit_id         { return $_[0]{'_hit_id'};                     } # @return ID of the result object containing the hit
-sub hit            { return $_[0]{'_hit'};                        } # @return Result hit (hashref)
-sub blast_method   { return $_[0]{'_blast_method'};               } # @return Blast method chosen for the given job
-sub is_protein     { return $_[0]{'_is_protein'};                 } # @return Flag whether the sequence is protein or not
+sub job            { return $_[0]{'_job'};                        } ## @return The cached requested job object
+sub hit            { return $_[0]{'_hit'};                        } ## @return Result hit (hashref)
+sub blast_method   { return $_[0]{'_blast_method'};               } ## @return Blast method chosen for the given job
+sub is_protein     { return $_[0]{'_is_protein'};                 } ## @return Flag whether the sequence is protein or not
 sub object         { return $_[0]->SUPER::object->get_sub_object; } ## Gets the actual blast object instead of the Tools object
 sub get_slice_name { return $_[1]->name;                          }
 
@@ -35,16 +37,16 @@ sub new {
   ## Adds hsp_display as a key param, info about the requested job and the blast method, and adds some extra keys to the objects after instantiating it
   my $self   = shift->SUPER::new(@_);
   my $object = $self->object;
-  
-  $self->{'_job'}          = $object->get_requested_job({ with_requsted_result => 1 }) or return $self;
-  $self->{'_hit_id'}       = $self->{'_job'}->result->[0]->result_id;
-  $self->{'_hit'}          = $self->{'_job'}->result->[0]->result_data;
-  $self->{'_blast_method'} = $object->parse_search_type($self->{'_job'}->job_data->{'search_type'}, 'search_method');
-  $self->{'_is_protein'}   = $self->{'_blast_method'} =~ /^(blastx|blastp)$/i ? 1 : 0;
-  
+
+  $self->{'_job'}               = $object->get_requested_job({ with_requsted_result => 1 }) or return $self;
+  $self->{'_blast_method'}      = $object->parse_search_type($self->{'_job'}->job_data->{'search_type'}, 'search_method');
+  $self->{'_is_protein'}        = $self->{'_blast_method'} =~ /^(blastx|blastp)$/i ? 1 : 0;
+  $self->{'_hit'}               = $self->{'_job'}->result->[0]->result_data->raw;
+  $self->{'_hit'}{'result_id'}  = $self->{'_job'}->result->[0]->result_id;
+
   push @{$self->{'key_types'}},  'hsp';
   push @{$self->{'key_params'}}, 'hsp_display';
-  
+
   return $self;
 }
 
@@ -54,9 +56,9 @@ sub _init {
   my $self    = shift;
   my $hub     = $self->hub;
   my @package = split '::', ref $self;
-  
+
   $self->{'view_config'} = $hub->get_viewconfig($package[-1], $package[-2], 'cache');
-  
+
   $self->SUPER::_init(5000);
   $self->cacheable(0);
 }
@@ -66,11 +68,11 @@ sub get_sequence_data {
   ## Add HSPs to the sequence data
   my ($self, $slices, $config) = @_;
   my ($sequence, $markup) = $self->SUPER::get_sequence_data($slices, $config);
-  
+
   if ($config->{'hsp_display'}) {
     $self->set_hsps($config, $slices->[$_], $markup->[$_]) for 0..$#$slices;
   }
-  
+
   return ($sequence, $markup);
 }
 
@@ -86,39 +88,38 @@ sub set_hsps {
   my $slice_length  = $slice->length;
   my $ori           = $self->hub->param('orientation'); # TODO provide a default value to this somewhere!
   my $hits          = [];
-  
+
   if ($config->{'hsp_display'} eq 'all') {
     $hits = $slice_type eq 'g' ? $object->get_all_hits_in_slice_region($job, $slice) : $object->get_all_hits($job);
   } elsif ($config->{'hsp_display'} eq 'sel') {
-    $hits = { $self->hit_id => $self->hit };
+    $hits = [ $self->hit ];
   }
-  
-  foreach (sort { $a <=> $b } keys %$hits) {
-    my $hit         = $hits->{$_};
-    my $type        = $_ == $self->hit_id ? 'sel' : 'other';
+
+  foreach my $hit (@$hits) {
+    my $type        = $hit->{'result_id'} == $self->hit->{'result_id'} ? 'sel' : 'other';
     my $g_ori       = $hit->{'gori'};
     my @coords      = $source_type !~ /LATEST/ && $slice_type eq 'g' ? @{$hit->{'g_coords'}} : { start => $hit->{$slice_type . 'start'}, end => $hit->{$slice_type . 'end'} };
     my $invert_flag = $ori eq 'fa' && $g_ori eq '-1' ? 1
                     : $ori eq 'fc' && $slice->strand eq '-1' ? 1
                     : $ori eq 'rc' && $slice->strand eq '-1' ? 1
                     : undef;
-    
+
     foreach (@coords) {
       my $start = $_->{'start'} - $slice_start;
-      my $end   = $_->{'end'}   - $slice_start; 
-      
-      if ($invert_flag) { 
+      my $end   = $_->{'end'}   - $slice_start;
+
+      if ($invert_flag) {
         $start = $slice_end - $_->{'start'};
         $end   = $slice_end - $_->{'end'};
       }
-      
+
       ($start, $end) = ($end, $start) if $start > $end;
       $start = 0                      if $start < 0;
       $end   = $slice_length - 1      if $end >= $slice_length;
-      
+
       push @{$markup->{'hsps'}{$_}{'type'}}, $type for $start..$end;
     }
-  } 
+  }
 }
 
 sub markup_hsp {
@@ -128,22 +129,22 @@ sub markup_hsp {
 
   foreach my $data (@$markup) {
     my $seq = $sequence->[$i];
-    
+
     foreach (sort { $a <=> $b } keys %{$data->{'hsps'}}) {
       my $hsp = $data->{'hsps'}{$_};
-      
+
       next unless $hsp->{'type'};
-      
+
       my %types = map { $_ => 1 } @{$hsp->{'type'}};
       my $type  = $types{'sel'} ? 'sel' : 'other'; # Both types are denoted by foreground colour, so only mark the more important type
-      
+
       $seq->[$_]{'class'} .= "hsp_$type " unless $seq->[$_]{'class'} =~ /\bhsp_$type\b/;
       $hsp_types{$type}    = 1;
     }
-    
+
     $i++;
   }
-  
+
   $config->{'key'}{'hsp'}{$_} = 1 for keys %hsp_types;
 }
 
@@ -151,17 +152,17 @@ sub class_to_style {
   ## @override
   ## Add two more styles for HSPs
   my $self = shift;
-  
+
   if (!$self->{'class_to_style'}) {
     $self->SUPER::class_to_style(@_); # this will set $self->{'class_to_style'}
-    
+
     my $styles  = $self->hub->species_defs->colour('sequence_markup');
     my $counter = scalar keys %{$self->{'class_to_style'}};
-    
+
     $self->{'class_to_style'}{'hsp_other'} = [ ++$counter, { 'font-weight' => 'bold', color => "#$styles->{'SEQ_HSP_OTHER'}{'default'}" }];
     $self->{'class_to_style'}{'hsp_sel'}   = [ ++$counter, { 'font-weight' => 'bold', color => "#$styles->{'SEQ_HSP_SEL'}{'default'}"   }]; # selected is more important, so goes after other
   }
-  
+
   return $self->{'class_to_style'};
 }
 
@@ -171,7 +172,7 @@ sub tool_buttons {
   return unless $self->html_format;
 
   my $hub = $self->hub;
-  
+
   return sprintf('
     <div class="other_tool">
       <p><a class="seq_export export" href="%s">Download view as RTF</a></p>
@@ -182,16 +183,16 @@ sub tool_buttons {
 
 sub content {
   my $self    = shift;
-  my $slice   = $self->get_slice; 
+  my $slice   = $self->get_slice;
   my $length  = $slice->length;
   my $html    = $self->tool_buttons;
-  
+
   if ($length >= $self->{'subslice_length'}) {
     $html .= '<div class="sequence_key"></div>' . $self->chunked_content($length, $self->{'subslice_length'}, { length => $length, name => $slice->name });
   } else {
     $html .= $self->content_sub_slice($slice); # Direct call if the sequence length is short enough
   }
-  
+
   return $html;
 }
 
@@ -204,16 +205,16 @@ sub content_sub_slice {
      $slice ||= $self->get_slice;
   my $name    = $self->get_slice_name($slice);
      $slice   = $slice->sub_Slice($start, $end) if $start && $end;
-  
+
   my ($sequence, $config) = $self->initialize($slice, $start, $end);
-  
+
   my $template = '<pre class="text_sequence%s">%s%%s</pre><p class="invisible">.</p>';
      $template = sprintf('<div class="sequence_key">%s</div>', $self->get_key($config)) . $template unless $start;
-     
+
   $config->{'html_template'} = sprintf $template, ($start == 1) || ($end && $end != $length) ? ' no-bottom-margin' : '', $start == 1 || !$start ? "&gt;$name\n" : '';
-  
-  $self->id(''); 
-  
+
+  $self->id('');
+
   return $self->build_sequence($sequence, $config);
 }
 
