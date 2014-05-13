c3a77f4bfe043b955045de869c4b89e21aecea20
Harpreet Riat <harpreet@ebi.ac.uk>
2014-05-13 13:42:22 +0100

    HSP_Coverage modified to work with new Blast
    Max coverage shown by dark grey
    No zmenu on coverage

--- tools/modules/EnsEMBL/Draw/GlyphSet/HSP_coverage.pm
+++ tools/modules/EnsEMBL/Draw/GlyphSet/HSP_coverage.pm
@@ -19,39 +19,36 @@ limitations under the License.
 package EnsEMBL::Draw::GlyphSet::HSP_coverage;
 
 use strict;
-
-use Sanger::Graphics::Bump;
+use warnings;
 
 use parent qw(EnsEMBL::Draw::GlyphSet);
 
 sub _init {
-  my ($self)       = @_;
-  my $container    = $self->{'container'};
-  my $config       = $self->{'config'};
-  my $sample_size  = int($container->length() / 1000) || 1;
-  my @all_hsps     = $container->hsps;
-  my $distribution = {};
+  my $self          = shift;
+  my $container     = $self->{'container'};
+  my $config        = $self->{'config'};
+  my $sample_size   = int($container->length() / 1000) || 1;
+  my @all_hsps      = sort {$a->{'qstart'} <=> $b->{'qstart'} || $a->{'qend'} <=> $b->{'qend'} } @{$container->hsps};
+  my $distribution  = {};
 
-  return if(scalar @all_hsps < 2);
-
-  @all_hsps = sort {$a->{'qstart'} <=> $b->{'qstart'} || $a->{'qend'} <=> $b->{'qend'} } @all_hsps;
+  return if scalar @all_hsps < 2;
 
   foreach my $hsp (@all_hsps) {
     my $sample_sskip = $hsp->{'qstart'} % $sample_size;
     my $sample_start = $hsp->{'qstart'} - $sample_sskip;
     my $sample_eskip = $hsp->{'qend'}   % $sample_size;
     my $sample_end   = $hsp->{'qend'};
-    $sample_end     += $sample_size if($sample_eskip != 0);
+    $sample_end     += $sample_size if $sample_eskip != 0;
 
-    for (my $i = $sample_start; $i <= $sample_end; $i+=$sample_size) {
-      for (my $j = $i; $j<$i+$sample_size; $j++) {
+    for (my $i = $sample_start; $i <= $sample_end; $i += $sample_size) {
+      for (my $j = $i; $j < $i + $sample_size; $j++) {
         $distribution->{$i}++;
       }
     }
   }
-  my $max = (sort {$b <=> $a} values %$distribution)[0];
+  my ($max) = sort {$b <=> $a} values %$distribution;
 
-  return if($max == 0);
+  return if $max == 0;
 
   my $smax = 50;
 
@@ -63,31 +60,17 @@ sub _init {
       'y'      => $smax/3 - $sval/3,
       'width'  => $sample_size,
       'height' => $sval/3,
-      'colour' => $val == $max ? 'red' : 'grey'.int(100 - $sval),
-      'href'   => $self->href()  
-  }));
+      'colour' => $val == $max ? 'grey20' : 'grey'.int(100 - $sval)
+    }));
   }
 
   $self->push($self->Rect({
     'x'      => 0,
     'y'      => $smax/3,
-    'width'  => $container->length(),
+    'width'  => $container->length,
     'height' => 0,
-    'colour' => "white",
+    'colour' => 'white',
   }));
 }
 
-sub href {
-  my ( $self, $hsp, $type ) = @_;
-
-
-  my $href = $self->_url({
-    'species' => $self->species,
-    'type'    => 'Blast',
-  });
-
-  return $href;
-}
-
 1;
-
