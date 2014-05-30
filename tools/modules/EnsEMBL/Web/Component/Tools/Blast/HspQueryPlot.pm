705c36c3614ad50334328cacd35ad1dc8a4bfd2e
Harpreet Riat <harpreet@ebi.ac.uk>
2014-05-30 17:57:26 +0100

    HSP swatch added to legend, removed from table

--- tools/modules/EnsEMBL/Web/Component/Tools/Blast/HspQueryPlot.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/Blast/HspQueryPlot.pm
@@ -45,30 +45,11 @@ sub content {
   $image->imagemap            = 'yes';
   $image->set_button('drag');
 
-  # Add colour key info table
-  my $steps         = 5; # we need to display 1 + 5 labels (0, 20 .. 100)
-  my %labels        = map { int($_ * (@pointer_cols - 1) / $steps) => int($_ * 100 / $steps) } 0 .. $steps;
-  my $swatch        = join '', map { sprintf '<div style="background:#%s">%s</div>', $pointer_cols[$_], $labels{$_} // '' } 0 .. $#pointer_cols;
-  my $legend        = sprintf '<div class="swatch-legend">Lower %%ID  &#9668;<span>%s</span>&#9658; Higher %%ID </div>', ' ' x 33;
-
-  my $swatch_table  = $self->new_table(
-    [
-      {'key' => 'ftype',  'title' => 'Feature type'},
-      {'key' => 'colour', 'title' => 'Colour'},
-    ], [{
-      'ftype'  => {'value' => $object->parse_search_type($job->job_data->{'search_type'}, 'blast_type') eq 'BLAT' ? 'BLAT hit' : 'BLAST hit'},
-      'colour' => {'value' => qq(<div class="swatch-wrapper"><div class="swatch">$swatch</div>$legend</div>)},
-    }], {}
-  );
-
   # final HTML
   return sprintf('
     <h3><a rel="_blast_queryplot" class="toggle set_cookie open" href="#">HSP distribution on query sequence:</a></h3>
-    <div class="_blast_queryplot toggleable">%s</div>
-    <h3><a rel="_blast_key_table" class="toggle set_cookie open" href="#">Key</a></h3>
-    <div class="_blast_key_table toggleable">%s</div>',
+    <div class="_blast_queryplot toggleable">%s</div>',
     $image->render,
-    $swatch_table->render
   );
 }
 
