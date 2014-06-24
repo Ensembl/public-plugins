c12f26971cd2f1dcac0b121eb5d1ecfdf65fca5e
Harpreet Riat <harpreet@ebi.ac.uk>
2014-06-24 13:41:53 +0100

    minor change - label changed

--- tools/modules/EnsEMBL/Web/Component/Tools/Blast.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/Blast.pm
@@ -54,7 +54,7 @@ sub job_details_table {
     $result_link->parent_node->insert_after($download_link, $result_link);
   }
 
-  $two_col->add_row('Job summary',    $job_summary->render);
+  $two_col->add_row('Job name',       $job_summary->render);
   $two_col->add_row('Species',        sprintf('<img class="job-species" src="%sspecies/16/%s.png" alt="" height="16" width="16">%s', $self->img_url, $species, $sd->species_label($species, 1)));
   $two_col->add_row('Search type',    $object->get_param_value_caption('search_type', $job_data->{'search_type'}));
   $two_col->add_row('Sequence',       sprintf('<div class="input-seq">&gt;%s</div>', join("\n", $sequence->{'display_id'} || '', ($sequence->{'sequence'} =~ /.{1,60}/g))));
