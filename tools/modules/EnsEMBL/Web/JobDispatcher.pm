085df583440450d7569bfc93bb1f3b13d02fa614
Anne Lyle <annelyle@ebi.ac.uk>
2014-07-22 13:12:03 +0100

    More idiomatic standard English

--- tools/modules/EnsEMBL/Web/JobDispatcher.pm
+++ tools/modules/EnsEMBL/Web/JobDispatcher.pm
@@ -59,7 +59,7 @@ sub update_jobs {
 
 sub default_error_message {
   ## Default message to be displayed if the job has failed
-  return 'Some error occurred while running the job.';
+  return 'An error occurred while running the job.';
 }
 
 
