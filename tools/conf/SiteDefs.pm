3ae73e8150fec850b8e1b800dc258986ecce0203
Harpreet Riat <harpreet@ebi.ac.uk>
2014-07-25 12:17:04 +0100

    Code tidy + ENSEMBL_TOOLS_CGI_POST_MAX added

--- tools/conf/SiteDefs.pm
+++ tools/conf/SiteDefs.pm
@@ -22,26 +22,45 @@ use strict;
 
 sub update_conf {
 
-  $SiteDefs::OBJECT_TO_SCRIPT->{'Tools'}        = 'Page';
+  $SiteDefs::OBJECT_TO_SCRIPT->{'Tools'} = 'Page';
 
-  $SiteDefs::ENSEMBL_ORM_DATABASES->{'ticket'}  = 'DATABASE_WEB_TOOLS';                             # Database key name for tools db as defined in MULTI.ini
+  # Database key name for tools db as defined in MULTI.ini
+  $SiteDefs::ENSEMBL_ORM_DATABASES->{'ticket'} = 'DATABASE_WEB_TOOLS';
 
-  $SiteDefs::ENSEMBL_TOOLS_LIST                 = [ 'Blast' => 'BLAST/BLAT', 'VEP' => 'Variant Effect Predictor', 'AssemblyConverter' => 'Assembly Converter' ];
-                                                                                                    # Entries as added to the tools db ticket_type_name table
-  $SiteDefs::ENSEMBL_TOOLS_JOB_DISPATCHER       = { 'Blast' => '', 'VEP' => '', 'AssemblyConverter' => '' };                   # Which dispatcher to be used for the jobs
+  # Entries as added to the tools db ticket_type_name table
+  $SiteDefs::ENSEMBL_TOOLS_LIST = [ 'Blast' => 'BLAST/BLAT', 'VEP' => 'Variant Effect Predictor', 'AssemblyConverter' => 'Assembly Converter' ];
 
-  $SiteDefs::ENSEMBL_TMP_DIR_TOOLS              = defer { $SiteDefs::ENSEMBL_TMP_DIR.'/tools' };    # tmp directory for jobs i/o files
-  $SiteDefs::ENSEMBL_BLAST_ENABLED              = 1;                                                # Flag to enable/disable BLAST
-  $SiteDefs::ENSEMBL_BLAT_BIN_PATH              = '/localsw/bin/gfClient';                          # Path to BLAT client
-  $SiteDefs::ENSEMBL_BLAT_BTOP_SCRIPT           = 'sanger-plugins/tools/utils/BLAT_alignments_to_BTOP.pl';  # Script to convert BLAT alignments to BTOP
-  $SiteDefs::ASSEMBLY_CONVERTER_BIN_PATH        = '/localsw/CrossMap-0.1.3/usr/local/bin/CrossMap.py';                          # Path to CrossMap
-  $SiteDefs::ENSEMBL_VEP_ENABLED                = 1;                                                # Flag to enable/disable VEP
-  $SiteDefs::ENSEMBL_AC_ENABLED                 = 1;                                                # Flag to enable/disable Assembly Converter
-  $SiteDefs::ENSEMBL_VEP_CGI_POST_MAX           = 52428800;                                         # 50MB limit for VEP input files
-  $SiteDefs::ENSEMBL_AC_CGI_POST_MAX            = 52428800;                                         # 50MB limit for Assembly Converter input files
-  $SiteDefs::ENSEMBL_VEP_FILTER_SCRIPT          = 'ensembl-tools/scripts/variant_effect_predictor/filter_vep.pl';
-                                                                                                    # location of the VEP filter script accessible to the web machine for filtering Results pages output
-  $SiteDefs::ENSEMBL_VEP_FILTER_SCRIPT_OPTIONS  = {                                                 # Command line options for VEP filter script
+  # Which dispatcher to be used for the jobs (provide the appropriate values in your plugins)
+  $SiteDefs::ENSEMBL_TOOLS_JOB_DISPATCHER = { 'Blast' => '', 'VEP' => '', 'AssemblyConverter' => '' };
+
+  # tmp directory for jobs i/o files
+  $SiteDefs::ENSEMBL_TMP_DIR_TOOLS = defer { $SiteDefs::ENSEMBL_TMP_DIR.'/tools' };
+
+  # Flag to enable/disable BLAST, VEP, Assembly Converter
+  $SiteDefs::ENSEMBL_BLAST_ENABLED  = 1;
+  $SiteDefs::ENSEMBL_VEP_ENABLED    = 1;
+  $SiteDefs::ENSEMBL_AC_ENABLED     = 1;
+
+  # Path to BLAT client
+  $SiteDefs::ENSEMBL_BLAT_BIN_PATH = '/localsw/bin/gfClient';
+
+  # Script to convert BLAT alignment strings to BTOP
+  $SiteDefs::ENSEMBL_BLAT_BTOP_SCRIPT = 'sanger-plugins/tools/utils/BLAT_alignments_to_BTOP.pl';
+
+  # Path to CrossMap
+  $SiteDefs::ASSEMBLY_CONVERTER_BIN_PATH = '/localsw/CrossMap-0.1.3/usr/local/bin/CrossMap.py';
+
+  # Upload file size limits
+  $SiteDefs::ENSEMBL_TOOLS_CGI_POST_MAX = {
+    'VEP'               =>  50 * 1024 * 1024,
+    'AssemblyConverter' =>  50 * 1024 * 1024,
+  };
+
+  # location of the VEP filter script accessible to the web machine for filtering Results pages output
+  $SiteDefs::ENSEMBL_VEP_FILTER_SCRIPT = 'ensembl-tools/scripts/variant_effect_predictor/filter_vep.pl';
+
+  # Command line options for VEP filter script
+  $SiteDefs::ENSEMBL_VEP_FILTER_SCRIPT_OPTIONS = {
     '-host'         => undef,
     '-user'         => undef,
     '-port'         => undef,
