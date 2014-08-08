d419c477c1e68956ea269ea3fcf9e8ba443ae40a
Harpreet Riat <harpreet@ebi.ac.uk>
2014-08-08 10:58:46 +0100

    Show alternative assembly website link (VEP/BLAST)

--- tools/modules/EnsEMBL/Web/Component/Tools/Blast/InputForm.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/Blast/InputForm.pm
@@ -309,8 +309,9 @@ sub content {
   $form =~ s/SPECIES_SELECTOR/$species_selector->render/e;
   $form =~ s/BUTTONS_FIELDSET/$buttons_fieldset->render/e;
 
-  return sprintf('<div class="hidden _tool_new"><p><a class="button _change_location" href="%s">New Search</a></p></div><div class="hidden _tool_form_div"><h2>Create new ticket:</h2><input type="hidden" class="panel_type" value="BlastForm" />%s</div>',
+  return sprintf('<div class="hidden _tool_new"><p><a class="button _change_location" href="%s">New Search</a></p></div><div class="hidden _tool_form_div"><h2>Create new ticket:</h2><input type="hidden" class="panel_type" value="BlastForm" />%s%s</div>',
     $hub->url({'function' => ''}),
+    $self->alt_assembly_info($species, 'BLAST/BLAT', 'Blast'),
     $form
   );
 }
