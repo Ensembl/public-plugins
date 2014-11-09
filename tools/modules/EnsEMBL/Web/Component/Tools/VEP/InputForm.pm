f5c9f5b70391d47afcb92ae7fa9556e5494a0777
William McLaren <wm2@ebi.ac.uk>
2014-11-09 05:54:49 +0000

    change button name

--- tools/modules/EnsEMBL/Web/Component/Tools/VEP/InputForm.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/VEP/InputForm.pm
@@ -104,7 +104,7 @@ sub content {
           'type'            => 'button',
           'name'            => 'preview',
           'class'           => 'hidden',
-          'value'           => 'Preview results for first variant &rsaquo;',
+          'value'           => 'Quick results for first variant &rsaquo;',
           'helptip'         => 'See a quick preview of results for data pasted above',
         }
       ]
