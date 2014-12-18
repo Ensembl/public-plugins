d7e8d6284e56c87ae30d4aca1eab54fd9181b8b1
Harpreet Riat <harpreet@ebi.ac.uk>
2014-12-18 16:35:37 +0000

    Moving RUN_LOCAL config to tools_hive
    It is a hive specific config.
    + reinstating the queues configs

--- tools/conf/SiteDefs.pm
+++ tools/conf/SiteDefs.pm
@@ -42,12 +42,6 @@ sub update_conf {
   $SiteDefs::ENSEMBL_VEP_ENABLED    = 1;
   $SiteDefs::ENSEMBL_AC_ENABLED     = 1;
 
-  # Run all jobs locally by default
-  $SiteDefs::ENSEMBL_BLAST_RUN_LOCAL  = 1; 
-  $SiteDefs::ENSEMBL_BLAT_RUN_LOCAL   = 1; 
-  $SiteDefs::ENSEMBL_VEP_RUN_LOCAL    = 1; 
-  $SiteDefs::ENSEMBL_AC_RUN_LOCAL     = 1; 
-
   # Path to BLAT client
   $SiteDefs::ENSEMBL_BLAT_BIN_PATH = '/path/to/gfClient';
 
