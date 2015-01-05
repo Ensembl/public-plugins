8a53794fa5d02c60cd79a9a64b63a131487b60e5
Harpreet Riat <harpreet@ebi.ac.uk>
2015-01-05 16:03:11 +0000

    Improved usability for sequence selectn for BLAST

--- tools/htdocs/components/16_TextSequence.js
+++ tools/htdocs/components/16_TextSequence.js
@@ -33,28 +33,15 @@ Ensembl.Panel.TextSequence = Ensembl.Panel.TextSequence.extend({
     this.el.find('span._seq').on({
 
       'mousedown': function() {
+        panel.blastSeq = '';
+        panel.resetSelection();
+        panel.toggleBlastPopup(false);
         $(document).on('mouseup.TextSequence', function(e) {
-          var seq;
-
-          if (document.selection && document.selection.createRange) {
-            seq = $('<div>').html(document.selection.createRange().htmlText).find('._seq').text();
-          } else if (window.getSelection) {
-            var selection = window.getSelection();
-            if (selection.rangeCount > 0) {
-              if ($(selection.anchorNode).parents('._seq')[0] === $(selection.focusNode).parents('._seq')[0]) {
-                seq = $('<div>').append(selection.getRangeAt(0).cloneContents()).text();
-              } else {
-                seq = $('<div>').append(selection.getRangeAt(0).cloneContents()).find('._seq').text();
-              }
-            }
-            selection = null;
-          }
-
-          panel.showBlastPopup(seq, e);
-          $(document).off('mouseup.TextSequence');
+          panel.blastSeq = panel.getSelection();
+          panel.toggleBlastPopup(!!panel.blastSeq, e);
         });
       },
-      'click': function(e) {
+      'mouseup': function(e) {
         if (panel.blastSeq) {
           e.stopPropagation(); // prevent firing click event on the document which will close the popup as soon as it opens
         }
@@ -67,20 +54,50 @@ Ensembl.Panel.TextSequence = Ensembl.Panel.TextSequence.extend({
     });
   },
 
-  showBlastPopup: function(seq, event) {
+  toggleBlastPopup: function(flag, event) {
 
     var panel = this;
 
-    this.blastSeq = seq;
+    $(document).off('mouseup.TextSequence');
 
-    if (this.blastSeq) {
+    if (flag) {
 
       this.elLk.blastPopup.css({ left: event.pageX, top: event.pageY }).show();
 
-      $(document).on('click.TextSequence', function() {
-        panel.elLk.blastPopup.hide();
-        $(document).off('click.TextSequence');
+      $(document).on('mouseup.TextSequence', function() {
+        panel.toggleBlastPopup(false);
       });
+
+    } else {
+      this.elLk.blastPopup.hide();
     }
+  },
+
+  resetSelection: function() {
+    if (document.selection && document.selection.empty) {
+      document.selection.empty();
+    } else if (window.getSelection) {
+      window.getSelection().removeAllRanges();
+    }
+  },
+
+  getSelection: function() {
+    var seq;
+
+    if (document.selection && document.selection.createRange) {
+      seq = $('<div>').html(document.selection.createRange().htmlText).find('._seq').text();
+    } else if (window.getSelection) {
+      var selection = window.getSelection();
+      if (selection.rangeCount > 0) {
+        if ($(selection.anchorNode).parents('._seq')[0] === $(selection.focusNode).parents('._seq')[0]) {
+          seq = $('<div>').append(selection.getRangeAt(0).cloneContents()).text();
+        } else {
+          seq = $('<div>').append(selection.getRangeAt(0).cloneContents()).find('._seq').text();
+        }
+      }
+      selection = null;
+    }
+
+    return seq;
   }
 });
