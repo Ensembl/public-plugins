dfaa56b49025d282df23eb8b3faf06f3feb5e6d5
Harpreet Riat <harpreet@ebi.ac.uk>
2015-01-15 17:35:13 +0000

    only peptides are Translation

--- tools_hive/modules/EnsEMBL/Web/Parsers/NCBIBLAST.pm
+++ tools_hive/modules/EnsEMBL/Web/Parsers/NCBIBLAST.pm
@@ -91,7 +91,7 @@ sub map_to_genome {
 
   } else {
 
-    my $feature_type  = $source_type =~ /abinitio/i ? 'PredictionTranscript' : $source_type =~ /cdna/i ? 'Transcript' : 'Translation';
+    my $feature_type  = $source_type =~ /abinitio/i ? 'PredictionTranscript' : $source_type =~ /pep/i ? 'Translation' : 'Transcript';
     my $mapper        = $source_type =~ /pep/i ? 'pep2genomic' : 'cdna2genomic';
     my $adaptor       = $dba->get_adaptor($feature_type);
     my $object        = $adaptor->fetch_by_stable_id($hit->{'tid'});
