d9d83025d5503ef86cf615823c6c993b4a901b17
Harpreet Riat <harpreet@ebi.ac.uk>
2015-02-06 10:55:04 +0000

    Tracks are now added in initialize method

--- tools/modules/EnsEMBL/Web/ImageConfig/gene_summary.pm
+++ tools/modules/EnsEMBL/Web/ImageConfig/gene_summary.pm
@@ -30,9 +30,4 @@ sub initialize {
   $self->initialize_tools_tracks;
 }
 
-sub glyphset_configs {
-  ## This plugin adds multiple blast tracks depending upon the number of jobs we have in the current ticket
-  return shift->blast_glyphset_configs(@_);
-}
-
 1;
