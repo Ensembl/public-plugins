e9e05d428ab29d812a92a9641dab11e869ed9202
William McLaren <wm2@ebi.ac.uk>
2015-03-10 09:22:10 +0000

    set max number of warnings that can be shown in VEP output

--- tools_hive/modules/EnsEMBL/Web/RunnableDB/VEP.pm
+++ tools_hive/modules/EnsEMBL/Web/RunnableDB/VEP.pm
@@ -81,14 +81,24 @@ sub run {
   my $command   = EnsEMBL::Web::SystemCommand->new($self, "$perl_bin $script", $options)->execute({'log_file' => $log_file});
   my $m_type    = 'ERROR';
   my $messages  = {$m_type => ['']};
+  my $max_msgs  = 10;
+  my $w_count   = 0;
 
   for (file_get_contents($log_file)) {
     if (m/^(ERROR|WARNING)\s*:\s*/) {
       $m_type = $1;
+      
+      if($m_type eq 'WARNING' && $messages->{$m_type} && scalar @{$messages->{$m_type}} >= $max_msgs) {
+        $w_count++;
+        next;
+      }
+      
       push @{$messages->{$m_type}}, '';
     }
     $messages->{$m_type}[-1] .= $_;
   }
+  
+  $messages->{'WARNING'}[-1] .= $w_count.' more warnings not shown' if $w_count;
 
   # save any warnings to the log table
   $self->tools_warning({ 'message' => $_, 'type' => 'VEPWarning' }) for @{$messages->{'WARNING'}};
