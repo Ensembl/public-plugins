cc739543fea6f974cf70bd3285d486e0a47ee7db
Ridwan Amode <ridwan@ebi.ac.uk>
2015-03-13 17:49:57 +0000

    add mobile site link when viewing www on mobile device

--- ensembl/modules/EnsEMBL/Web/Document/Element/Copyright.pm
+++ ensembl/modules/EnsEMBL/Web/Document/Element/Copyright.pm
@@ -60,7 +60,15 @@ sub content {
     );
   
   $html .= '<p class="print_hide">'; 
+
   unless ($ENV{'ENSEMBL_TYPE'} =~ /Help|Account|UserData|Tools/) {
+
+    # if you are looking at www on a mobile/tablet device, add mobile site link
+    if($ENV{'MOBILE_DEVICE'}) {
+      # not using $you_are_here because not all pages are available on mobile site
+      $html .= qq{<a class="mobile_link" href="http://m.ensembl.org">Mobile site</a> - };
+    }
+
     $html .= qq{
         <a class="modal_link" id="p_link" href="/Help/Permalink?url=$stable_URL">Permanent link</a>
     };
