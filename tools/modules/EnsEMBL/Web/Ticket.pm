8f99fb3b3006e5229d63c6844b0e58cafd0007a0
Harpreet Riat <harpreet@ebi.ac.uk>
2015-05-01 17:01:04 +0100

    Try multiple times to save a ticket if first attempt fails

--- tools/modules/EnsEMBL/Web/Ticket.pm
+++ tools/modules/EnsEMBL/Web/Ticket.pm
@@ -122,7 +122,21 @@ sub submit_to_toolsdb {
     'job'         => [ map { $_->rose_object } @$jobs ]
   });
 
-  $self->{'_rose_object'} = $tools_ticket if $ticket_type->save('changes_only' => 1);
+  # try to save ticket to the tools db, but if it fails, try for another three times before giving up (useful in case deadlocks)
+  foreach my $tries_left (reverse 0..3) {
+
+    try {
+      $ticket_type->save('changes_only' => 1);
+      $tries_left = -1;
+    } catch {
+      throw $_ unless $tries_left;
+      sleep 1;
+    };
+
+    last if $tries_left < 0;
+  }
+
+  $self->{'_rose_object'} = $tools_ticket;
 }
 
 sub dispatch_jobs {
