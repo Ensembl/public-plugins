e8c56b90f9a64e2cb1b050b0e71e5085cb5513d0
William McLaren <wm2@ebi.ac.uk>
2015-07-07 09:32:35 +0100

    use tokenfield for autocomplete-multi

--- tools/htdocs/components/20_VEPForm.js
+++ tools/htdocs/components/20_VEPForm.js
@@ -77,52 +77,61 @@ Ensembl.Panel.VEPForm = Ensembl.Panel.ToolsForm.extend({
     });
 
     // auto complete plugin stuff
-    this.elLk.form.find('input:text.autocomplete-multi, textarea.autocomplete-multi').on('focus', function() {
+    this.elLk.form.find('input:text.autocomplete-multi, textarea.autocomplete-multi').each(function() {
+      $(this).tokenfield({
+        autocomplete: {
+          source: panel.autocompleteData.hasOwnProperty(this.name) ? panel.autocompleteData[this.name] : [],
+          delay: 100
+        },
+        showAutocompleteOnFocus: true
+      })
 
-      var name = this.name;
-
-      var acValues =
-        panel.autocompleteData.hasOwnProperty(name) ? 
-        panel.autocompleteData[name] : [];
-
-      function split( val ) {
-        return val.split( /,\s*/ );
-      }
-      function extractLast( term ) {
-        return split( term ).pop();
-      }
- 
-      $(this)
-        // don't navigate away from the field on tab when selecting an item
-        .bind( "keydown", function( event ) {
-          if ( event.keyCode === $.ui.keyCode.TAB &&
-              $( this ).autocomplete( "instance" ).menu.active ) {
-            event.preventDefault();
-          }
-        })
-        .autocomplete({
-          minLength: 0,
-          source: function( request, response ) {
-            // delegate back to autocomplete, but extract the last term
-            response( $.ui.autocomplete.filter(
-              acValues, extractLast( request.term ) ) );
-          },
-          focus: function() {
-            // prevent value inserted on focus
-            return false;
-          },
-          select: function( event, ui ) {
-            var terms = split( this.value );
-            // remove the current input
-            terms.pop();
-            // add the selected item
-            terms.push( ui.item.value );
-            // add placeholder to get the comma-and-space at the end
-            terms.push( "" );
-            this.value = terms.join( "," );
-            return false;
-          }
+      // don't allow duplcates
+      .on('tokenfield:createtoken', function (event) {
+        var existingTokens = $(this).tokenfield('getTokens');
+        $.each(existingTokens, function(index, token) {
+          if (token.value === event.attrs.value) event.preventDefault();
         });
+      });
+
+      // function split( val ) {
+      //   return val.split( /,\s*/ );
+      // }
+      // function extractLast( term ) {
+      //   return split( term ).pop();
+      // }
+ 
+      // $(this)
+      //   // don't navigate away from the field on tab when selecting an item
+      //   .bind( "keydown", function( event ) {
+      //     if ( event.keyCode === $.ui.keyCode.TAB &&
+      //         $( this ).autocomplete( "instance" ).menu.active ) {
+      //       event.preventDefault();
+      //     }
+      //   })
+      //   .autocomplete({
+      //     minLength: 0,
+      //     source: function( request, response ) {
+      //       // delegate back to autocomplete, but extract the last term
+      //       response( $.ui.autocomplete.filter(
+      //         acValues, extractLast( request.term ) ) );
+      //     },
+      //     focus: function() {
+      //       // prevent value inserted on focus
+      //       return false;
+      //     },
+      //     select: function( event, ui ) {
+      //       var terms = split( this.value );
+      //       // remove the current input
+      //       terms.pop();
+      //       // add the selected item
+      //       terms.push( ui.item.value );
+      //       // add placeholder to get the comma-and-space at the end
+      //       terms.push( "" );
+      //       this.value = terms.join( "," );
+      //       return false;
+      //     }
+      //   });
         // .autocomplete( "instance" )._renderItem = function( ul, item ) {
         //   return $( "<li>" )
         //     .append( "<a>" + item.label + "<br>" + item.desc + "</a>" )
