a02548bba901271ac4a7d00bb6b02022eb00d6d5
William McLaren <wm2@ebi.ac.uk>
2015-09-14 16:40:59 +0100

    Add get_form_details method
    This method returns labels, helptips and value arrays for the VEP
    input form. This content is returned from the object so that it
    can be retrieved both in the InputForm and TicketDetails module.

--- tools/modules/EnsEMBL/Web/Object/VEP.pm
+++ tools/modules/EnsEMBL/Web/Object/VEP.pm
@@ -133,4 +133,239 @@ sub handle_download {
   }
 }
 
+sub get_form_details {
+  my $self = shift;
+
+  if(!exists($self->{_form_details})) {
+
+    # core form
+    $self->{_form_details} = {
+      core_type => {
+        'label'   => 'Transcript database to use',
+        'helptip' =>
+          '<b>Gencode basic:</b> a subset of the Ensembl transcript set; partial and other low quality transcripts are removed<br/>'.
+          '<b>RefSeq:</b> aligned transcripts from NCBI RefSeq',
+        'values'  => [
+          { 'value' => 'core',          'caption' => 'Ensembl transcripts'            },
+          { 'value' => 'gencode_basic', 'caption' => 'Gencode basic transcripts'      },
+          { 'value' => 'refseq',        'caption' => 'RefSeq transcripts'             },
+          { 'value' => 'merged',        'caption' => 'Ensembl and RefSeq transcripts' }
+        ],
+      },
+
+      all_refseq => {
+        'label'   => 'Include additional EST and CCDS transcripts',
+        'helptip' => 'The RefSeq transcript set also contains aligned EST and CCDS transcripts that are excluded by default',
+      },
+
+      # identifiers section
+      symbol => {
+        'label'   => 'Gene symbol',
+        'helptip' => 'Report the gene symbol (e.g. HGNC)',
+      },
+
+      ccds => {
+        'label'   => 'CCDS',
+        'helptip' => 'Report the Consensus CDS identifier where applicable',
+      },
+
+      protein => {
+        'label'   => 'Protein',
+        'helptip' => 'Report the Ensembl protein identifier',
+      },
+
+      uniprot => {
+        'label'   => 'Uniprot',
+        'helptip' => 'Report identifiers from SWISSPROT, TrEMBL and UniParc',
+      },
+
+      hgvs => {
+        'label'   => 'HGVS',
+        'helptip' => 'Report HGVSc (coding sequence) and HGVSp (protein) notations for your variants',
+      },
+
+      # frequency data
+      check_existing => {
+        'label'   => 'Find co-located known variants',
+        'helptip' => "Report known variants from the Ensembl Variation database that are co-located with input. Use 'compare alleles' to only report co-located variants where none of the input variant's alleles are novel",
+        'values'  => [
+          { 'value'     => 'no',      'caption' => 'No'                       },
+          { 'value'     => 'yes',     'caption' => 'Yes'                      },
+          { 'value'     => 'allele',  'caption' => 'Yes and compare alleles'  }
+        ]
+      },
+
+      gmaf => {
+        'label'   => '1000 Genomes global minor allele frequency',
+        'helptip' => 'Report the minor allele frequency for the combined 1000 Genomes Project phase 1 population',
+      },
+
+      maf_1kg => {
+        'label'   => '1000 Genomes continental allele frequencies',
+        'helptip' => 'Report the allele frequencies for the combined 1000 Genomes Project phase 1 continental populations - AFR (African), AMR (American), ASN (Asian) and EUR (European)',
+      },
+
+      maf_esp => {
+        'label'   => 'ESP allele frequencies',
+        'helptip' => 'Report the allele frequencies for the NHLBI Exome Sequencing Project populations - AA (African American) and EA (European American)',
+      },
+
+      pubmed => {
+        'label'   => 'PubMed IDs for citations of co-located variants',
+        'helptip' => 'Report the PubMed IDs of any publications that cite this variant',
+      },
+
+      failed => {
+        'label'   => 'Include flagged variants',
+        'helptip' => 'The Ensembl QC pipeline flags some variants as failed; by default these are not included when searching for known variants',
+      },
+
+      biotype => {
+        'label'   => 'Transcript biotype',
+        'helptip' => 'Report the biotype of overlapped transcripts, e.g. protein_coding, miRNA, psuedogene',
+      },
+
+      domains => {
+        'label'   => 'Protein domains',
+        'helptip' => 'Report overlapping protein domains from Pfam, Prosite and InterPro',
+      },
+
+      numbers => {
+        'label'   => 'Exon and intron numbers',
+        'helptip' => 'For variants that fall in the exon or intron, report the exon or intron number as NUMBER / TOTAL',
+      },
+
+      tsl => {
+        'label'   => 'Transcript support level',
+        'helptip' => 'Report the transcript support level',
+      },
+
+      canonical => {
+        'label'   => 'Identify canonical transcripts',
+        'helptip' => 'Indicate if an affected transcript is the canonical transcript for the gene',
+      },
+
+      sift => {
+        'label'   => 'SIFT',
+        'helptip' => 'Report SIFT scores and/or predictions for missense variants. SIFT is an algorithm to predict whether an amino acid substitution is likely to affect protein function',
+        'values'  => [
+          { 'value'     => 'no',    'caption' => 'No'                   },
+          { 'value'     => 'both',  'caption' => 'Prediction and score' },
+          { 'value'     => 'pred',  'caption' => 'Prediction only'      },
+          { 'value'     => 'score', 'caption' => 'Score only'           }
+        ]
+      },
+
+      polyphen => {
+        'label'   => 'PolyPhen',
+        'helptip' => 'Report PolyPhen scores and/or predictions for missense variants. PolyPhen is an algorithm to predict whether an amino acid substitution is likely to affect protein function',
+        'values'  => [
+          { 'value'     => 'no',    'caption' => 'No'                   },
+          { 'value'     => 'both',  'caption' => 'Prediction and score' },
+          { 'value'     => 'pred',  'caption' => 'Prediction only'      },
+          { 'value'     => 'score', 'caption' => 'Score only'           }
+        ]
+      },
+
+      regulatory => {
+        'label'   => 'Get regulatory region consequences',
+        'helptip' => 'Get consequences for variants that overlap regulatory features and transcription factor binding motifs',
+        'values'  => [
+          { 'value'       => 'no',   'caption' => 'No'                          },
+          { 'value'       => 'reg',  'caption' => 'Yes'                         },
+          { 'value'       => 'cell', 'caption' => 'Yes and limit by cell type'  }
+        ]
+      },
+
+      cell_type => {
+        'label'   => 'Limit to cell type(s)',
+        'helptip' => 'Select one or more cell types to limit regulatory feature results to. Hold Ctrl (Windows) or Cmd (Mac) to select multiple entries.',
+      },
+
+      frequency => {
+        'label'   => 'Filter by frequency',
+        'helptip' => 'Exclude common variants to remove input variants that overlap with known variants that have a minor allele frequency greater than 1% in the 1000 Genomes Phase 1 combined population. Use advanced filtering to change the population, frequency threshold and other parameters',
+        'values'  => [
+          { 'value' => 'no',        'caption' => 'No filtering'             },
+          { 'value' => 'common',    'caption' => 'Exclude common variants'  },
+          { 'value' => 'advanced',  'caption' => 'Advanced filtering'       }
+        ]
+      },
+
+      freq_filter => {
+        'values' => [
+          { 'value' => 'exclude', 'caption' => 'Exclude'      },
+          { 'value' => 'include', 'caption' => 'Include only' }
+        ]
+      },
+
+      freq_gt_lt => {
+        'values' => [
+          { 'value' => 'gt', 'caption' => 'variants with MAF greater than' },
+          { 'value' => 'lt', 'caption' => 'variants with MAF less than'    },
+        ]
+      },
+
+      freq_pop => {
+        'values' => [
+          { 'value' => '1kg_all', 'caption' => 'in 1000 genomes (1KG) combined population' },
+          { 'value' => '1kg_afr', 'caption' => 'in 1KG African combined population'        },
+          { 'value' => '1kg_amr', 'caption' => 'in 1KG American combined population'       },
+          { 'value' => '1kg_eas', 'caption' => 'in 1KG East Asian combined population'     },
+          { 'value' => '1kg_eur', 'caption' => 'in 1KG European combined population'       },
+          { 'value' => '1kg_sas', 'caption' => 'in 1KG South Asian combined population'    },
+          { 'value' => 'esp_aa',  'caption' => 'in ESP African-American population'        },
+          { 'value' => 'esp_ea',  'caption' => 'in ESP European-American population'       },
+        ],
+      },
+
+      coding_only => {
+        'label'   => 'Return results for variants in coding regions only',
+        'helptip' => 'Exclude results in intronic and intergenic regions',
+      },
+
+      summary => {
+        'label'   => 'Restrict results',
+        'helptip' => 'Restrict results by severity of consequence; note that consequence ranks are determined subjectively by Ensembl',
+        'values'  => [
+          { 'value' => 'no',          'caption' => 'Show all results' },
+          { 'value' => 'pick',        'caption' => 'Show one selected consequence per variant'},
+          { 'value' => 'pick_allele', 'caption' => 'Show one selected consequence per variant allele'},
+          { 'value' => 'per_gene',    'caption' => 'Show one selected consequence per gene' },
+          { 'value' => 'summary',     'caption' => 'Show only list of consequences per variant' },
+          { 'value' => 'most_severe', 'caption' => 'Show most severe consequence per variant' },
+        ]
+      },
+    };
+
+
+    # add plugin stuff
+    my $sd  = $self->hub->species_defs;
+    if($sd->ENSEMBL_VEP_PLUGIN_ENABLED) {
+      my $pl = $sd->multi_val('ENSEMBL_VEP_PLUGIN_CONFIG');
+
+      foreach my $plugin(@{$pl->{plugins}}) {
+
+        # each plugin form element has "plugin_" prepended to it
+        $self->{_form_details}->{'plugin_'.$plugin->{key}} = {
+          label => $plugin->{label} || $plugin->{key},   # plugins may not have a label
+          helptip => $plugin->{helptip},
+        };
+
+        # add plugin-specific form elements
+        # e.g. option selector for dbNSFP
+        foreach my $form_el(@{$plugin->{form} || []}) {
+          $self->{_form_details}->{'plugin_'.$plugin->{key}.'_'.$form_el->{name}} = {
+            label => ($plugin->{label} || $plugin->{key}).' '.($form_el->{label} || $form_el->{name}),   # prepend label with plugin label
+            helptip => $form_el->{helptip},
+            values => $form_el->{values}
+          };
+        }
+      }
+    }
+  }
+
+  return $self->{_form_details};
+}
+
 1;
