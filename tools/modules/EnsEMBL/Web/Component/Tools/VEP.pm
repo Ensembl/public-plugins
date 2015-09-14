8d8e7954ef4bf37bd5dc82c5e78e01a8a756b824
William McLaren <wm2@ebi.ac.uk>
2015-09-14 16:42:55 +0100

    add override job_details_table method using get_form_details to show user a summary of the options they selected

--- tools/modules/EnsEMBL/Web/Component/Tools/VEP.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/VEP.pm
@@ -43,4 +43,86 @@ sub job_statistics {
   return $stats;
 }
 
+
+sub job_details_table {
+  ## A two column layout displaying a job's details
+  ## @param Job object
+  ## @param Flag to tell whether user or session owns the ticket or not
+  ## @return DIV node (as returned by new_twocol method)
+  my ($self, $job, $is_owned_ticket) = @_;
+
+  # get basic table from superclass method
+  my $two_col = $self->SUPER::job_details_table($job, $is_owned_ticket);
+  my $opt_two_col = $self->new_twocol({striped => 1});
+
+  # fetch job data and input form field information
+  my $job_data = $job->job_data;
+  my $form_data = $self->object->get_form_details;
+
+  # special case frequency data, we don't want a separate line for each form element
+  if($job_data->{frequency} && $job_data->{frequency} ne 'no') {
+    my @tmp;
+
+    foreach my $freq_key(qw(freq_filter freq_gt_lt freq_freq freq_pop)) {
+      my %values = map { $_->{value} => $_->{caption} } grep {ref($_) eq 'HASH'} @{$form_data->{$freq_key}->{values} || []};
+      push @tmp, $values{$job_data->{$freq_key}} || $job_data->{$freq_key};
+    }
+
+    $job_data->{frequency} = join(' ', @tmp);
+  }
+  delete $job_data->{$_} for qw(freq_filter freq_gt_lt freq_freq freq_pop);
+
+  my (%skip, $have_plugins);
+  
+  # sort by label so at least it appears somewhat logical to user
+  foreach my $opt_key(sort {
+    lc($form_data->{$a} && $form_data->{$a}->{label} ? $form_data->{$a}->{label} : $a)
+    cmp
+    lc($form_data->{$b} && $form_data->{$b}->{label} ? $form_data->{$b}->{label} : $b)
+  } keys %$job_data) {
+
+    # we might need to remove a species suffix
+    my $opt_key_lookup = $opt_key;
+    $opt_key_lookup =~ s/(regulatory|cell_type)(_.+)/$1/;
+
+    my $opt_data = $form_data->{$opt_key_lookup};
+    next unless $opt_data;
+
+    # skip disabled plugins
+    if((grep {$_ =~ /$opt_key\_/} keys %skip) || ($opt_key =~ /^plugin\_/ && $job_data->{$opt_key} ne $opt_key)) {
+      $skip{$opt_key} = 1;
+      next;
+    }
+
+    $have_plugins = 1 if $opt_key =~ /^plugin/;
+
+    # get value labels from form details hash
+    my %values = map { $_->{value} => $_->{caption} } grep {ref($_) eq 'HASH'} @{$opt_data->{values} || []};
+    $values{yes} = 'Enabled';
+    $values{no}  = 'Disabled';
+
+    my $label = $opt_data->{label} || $opt_key;
+    $label =~ s/\f/\&shy;/g;
+
+    $opt_two_col->add_row(
+      sprintf(
+        '<span %s>%s%s:</span>',
+        $opt_data->{helptip} ? 'class="ht _ht" title="'.$opt_data->{helptip}.'"' : '',
+        $label,
+        $opt_key =~ /^plugin/ ? ' *' : '',
+      ),
+
+      # plugin checkboxes have a value the same as the key
+      $values{$job_data->{$opt_key}} || ($job_data->{$opt_key} eq $opt_key ? 'Enabled' : $job_data->{$opt_key})
+    );
+  }
+
+  $two_col->add_row(
+    'Options summary',
+    $opt_two_col->render.($have_plugins ? '<p class="small">* = functionality from <a href="/info/docs/tools/vep/script/vep_plugins.html">VEP plugin</a></p>' : '')
+  );
+
+  return $two_col;
+}
+
 1;
