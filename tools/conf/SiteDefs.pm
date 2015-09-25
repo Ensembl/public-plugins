e13d438a0cb228a14fd94efd285172f8c284950d
Harpreet Riat <harpreet@ebi.ac.uk>
2015-09-25 10:42:54 +0100

    VEP plugins can have multiple config files

--- tools/conf/SiteDefs.pm
+++ tools/conf/SiteDefs.pm
@@ -74,7 +74,11 @@ sub update_conf {
     '-pass'         => undef
   };
 
-  $SiteDefs::ENSEMBL_VEP_PLUGIN_WEB_CONFIG = $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/tools/conf/vep_plugins_web_config.txt';
+  # Config files for VEP plugins (each file overrides the configs in the previous one in the list)
+  $SiteDefs::ENSEMBL_VEP_PLUGIN_CONFIG_FILES  = [
+                                                  $SiteDefs::ENSEMBL_SERVERROOT.'/VEP_plugins/plugin_config.txt', # VEP_plugins is a cloned from github.com/ensembl-variation/VEP_plugins
+                                                  $SiteDefs::ENSEMBL_SERVERROOT.'/public-plugins/tools/conf/vep_plugins_web_config.txt'
+                                                ];
 
   # Tickets will expire after 10 days, and user will warned when less than three days are left
   $SiteDefs::ENSEMBL_TICKETS_VALIDITY         = 10 * 24 * 60 * 60;
