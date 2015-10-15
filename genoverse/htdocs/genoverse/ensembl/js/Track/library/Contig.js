86c436b2af6ad3363b5ec7fc3cc131af36f240bb
Harpreet Riat <harpreet@ebi.ac.uk>
2015-10-15 16:39:27 +0100

    Reinstating old ensembl specific tracks

---
+++ genoverse/htdocs/genoverse/ensembl/js/Track/library/Contig.js
@@ -0,0 +1,42 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Genoverse.Track.Contig = Genoverse.Track.extend({
+  resizable : false,
+  allData   : true,
+  
+  view: Genoverse.Track.View.extend({
+    borderColor   : '#000000',
+    labels        : 'overlay',
+    repeatLabels  : true,
+    featureMargin : { top: 0, right: 0, bottom: 0, left: 0 },
+    
+    positionFeatures: function (features, params) {
+      this.base(features, params);
+      params.featureHeight = params.featureHeight || this.prop('height');
+      return features;
+    },
+    
+    draw: function (features, featureContext, labelContext, scale) {
+      featureContext.fillStyle = this.borderColor;
+      
+      featureContext.fillRect(0, 0,                      this.width, 1);
+      featureContext.fillRect(0, this.featureHeight - 1, this.width, 1); 
+      
+      this.base(features, featureContext, labelContext, scale);
+    }
+  })
+});
---
+++ genoverse/htdocs/genoverse/ensembl/js/Track/library/RegulatoryFeature.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Genoverse.Track.RegulatoryFeature = Genoverse.Track.extend({
+  view: Genoverse.Track.View.extend({
+    bump   : true,
+    labels : false,
+    
+    bumpFeature: function (bounds, feature, scale, tree) {
+      bounds.x = feature.bumpStart * scale;
+      bounds.w = (feature.bumpEnd - feature.bumpStart) * scale + Math.max(scale, 1);
+      this.base(bounds, feature, scale, tree);
+    },
+
+    decorateFeature: function (feature, context, scale) {
+      var position    = feature.position[scale];
+      var startOffset = position.start - position.X;
+      var mid         = position.height / 2;
+      var decoration, end;
+      
+      for (var i = 0; i < feature.decorations.length; i++) {
+        decoration       = feature.decorations[i];
+        decoration.x     = decoration.start * scale - startOffset;
+        decoration.width = (decoration.end - decoration.start) * scale + Math.max(scale, 1);
+        
+        context.fillStyle = decoration.color;
+        
+        if (decoration.x < 0 || decoration.x + decoration.width > this.width) {
+          this.truncateForDrawing(decoration);
+        }
+        
+        if (decoration.style === 'rect') {
+          context.fillRect(decoration.x, position.Y, decoration.width, this.featureHeight);
+        } else if (decoration.style === 'fg_ends') {
+          end = decoration.end * scale - startOffset;
+          
+          if (decoration.x >= 0) {
+            context.fillRect(decoration.x, position.Y, 1, position.height);
+          }
+          
+          if (end <= this.width) {
+            context.fillRect(end, position.Y, 1, position.height);
+          }
+          
+          context.fillRect(decoration.x, position.Y + mid, decoration.width, 1);
+        }
+      }
+    }
+  })
+});
