c761f795a0d9755670b02ec4b1a775060cc2e2eb
Harpreet Riat <harpreet@ebi.ac.uk>
2015-10-27 15:26:53 +0000

    IDMapper Component modules

---
+++ tools/modules/EnsEMBL/Web/Component/Tools/IDMapper/TicketDetails.pm
@@ -0,0 +1,40 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Web::Component::Tools::IDMapper::TicketDetails;
+
+use strict;
+use warnings;
+
+use parent qw(
+  EnsEMBL::Web::Component::Tools::IDMapper
+  EnsEMBL::Web::Component::Tools::TicketDetails
+);
+
+sub content_ticket {
+  ## Abstract method implementation
+  my ($self, $ticket, $jobs, $is_owned_ticket) = @_;
+  my $hub     = $self->hub;
+  my $div     = $self->dom->create_element('div');
+
+  $div->append_child($self->job_details_table($_, $is_owned_ticket))->set_attribute('class', 'plain-box') for @$jobs;
+
+  return $div->render;
+}
+
+1;
