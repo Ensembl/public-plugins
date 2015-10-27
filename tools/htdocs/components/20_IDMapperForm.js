3dbefe041a1151f7647dfd4a2643a68d3e6266f9
Harpreet Riat <harpreet@ebi.ac.uk>
2015-10-27 15:38:47 +0000

    JavaScript for ID Mapper form

---
+++ tools/htdocs/components/20_IDMapperForm.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Ensembl.Panel.IDMapperForm = Ensembl.Panel.ToolsForm.extend({
+
+  init: function() {
+    this.base.apply(this, arguments);
+
+    this.elLk.speciesDropdown = this.elLk.form.find('._sdd');
+
+    this.editExisting();
+    this.resetSpecies();
+  },
+
+  reset: function() {
+    this.base.apply(this, arguments);
+    this.resetSpecies();
+  },
+
+  resetSpecies: function () {
+  /*
+   * Resets the species dropdown to select the given species or simply refresh the dropdown
+   */
+    this.elLk.speciesDropdown.find('input[value=' + this.defaultSpecies + ']').first().click();
+    this.elLk.speciesDropdown.speciesDropdown({refresh: true});
+  }
+});
