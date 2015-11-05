63fea62bcc7f9ffe0b8d25cae2c3c5e1eb657fc6
Dan Sheppard <dan@ebi.ac.uk>
2015-11-05 14:01:32 +0000

    Move preload to correct place.

---
+++ tools/modules/Preload.pm
@@ -0,0 +1,11 @@
+package Preload;
+
+use ORM::EnsEMBL::DB::Tools::Manager::TicketType;
+use ORM::EnsEMBL::DB::Tools::Manager::Ticket;
+use ORM::EnsEMBL::DB::Tools::Object::JobMessage;
+use ORM::EnsEMBL::DB::Tools::Object::Job;
+use ORM::EnsEMBL::DB::Tools::Object::Result;
+use ORM::EnsEMBL::DB::Tools::Object::Ticket;
+use ORM::EnsEMBL::DB::Tools::Object::TicketType;
+
+1;
---
+++ users/modules/Preload.pm
@@ -0,0 +1,10 @@
+package Preload;
+
+use ORM::EnsEMBL::DB::Accounts::Object::User;
+use ORM::EnsEMBL::DB::Accounts::Object::Membership;
+use ORM::EnsEMBL::DB::Accounts::Object::RecordOwner;
+use ORM::EnsEMBL::DB::Accounts::Object::Login;
+use ORM::EnsEMBL::DB::Accounts::Object::Group;
+use ORM::EnsEMBL::DB::Accounts::Object::Record;
+
+1;
