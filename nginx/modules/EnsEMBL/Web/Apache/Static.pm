1980935a7d67f3b2a28cfbb9c04ae2859657cf01
Dan Sheppard <dan@ebi.ac.uk>
2015-11-05 13:12:51 +0000

    Nginx fix.

---
+++ nginx/modules/EnsEMBL/Web/Apache/Static.pm
@@ -0,0 +1,20 @@
+package EnsEMBL::Web::Apache::Static;
+
+use strict;
+use warnings;
+
+use Digest::MD5 qw(md5_hex);
+use SiteDefs;
+use EnsEMBL::Web::Cache;
+
+our $MEMD = EnsEMBL::Web::Cache->new;
+
+sub static_cache_hook {
+  my ($uri,$content) = @_;
+
+  return unless $MEMD and $MEMD->can('set_raw');
+  my $key = "nginx-".md5_hex("$SiteDefs::ENSEMBL_STATIC_BASE_URL$uri");
+  $MEMD->set_raw($key,$content);
+}
+
+1;
