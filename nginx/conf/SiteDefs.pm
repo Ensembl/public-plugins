7c1b16c1692c25ccf1dd62bd8f63f13e2d9a264a
Dan Sheppard <dan@ebi.ac.uk>
2015-11-06 13:48:16 +0000

    Use nginx on test.

--- nginx/conf/SiteDefs.pm
+++ nginx/conf/SiteDefs.pm
@@ -21,8 +21,10 @@ package EnsEMBL::Nginx::SiteDefs;
 use strict;
 
 sub update_conf {
-  $SiteDefs::ENSEMBL_NGINX_PORT = $SiteDefs::ENSEMBL_PORT;
-  $SiteDefs::ENSEMBL_PORT       = $SiteDefs::ENSEMBL_NGINX_PORT + 5000;
+  $SiteDefs::_PORT_FIX = defer {
+    $SiteDefs::ENSEMBL_NGINX_PORT = $SiteDefs::ENSEMBL_PORT;
+    $SiteDefs::ENSEMBL_PORT       = $SiteDefs::ENSEMBL_NGINX_PORT + 5000;
+  };
 
   $SiteDefs::ENSEMBL_NGINX_ROOT = $SiteDefs::ENSEMBL_SERVERROOT."/sanger-plugins/nginx";
 }
