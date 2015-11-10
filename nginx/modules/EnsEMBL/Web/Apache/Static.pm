55a6795eb2e841562ffa1b8e55eb91dc8c116b4a
Dan Sheppard <dan@ebi.ac.uk>
2015-11-10 12:55:25 +0000

    Don't stamp on MEMD from core module.

--- nginx/modules/EnsEMBL/Web/Apache/Static.pm
+++ nginx/modules/EnsEMBL/Web/Apache/Static.pm
@@ -7,16 +7,16 @@ use Digest::MD5 qw(md5_hex);
 use SiteDefs;
 use EnsEMBL::Web::Cache;
 
-our $MEMD = EnsEMBL::Web::Cache->new(
+our $SMEMD = EnsEMBL::Web::Cache->new(
   namespace => $SiteDefs::ENSEMBL_STATIC_BASE_URL
 );
 
 sub static_cache_hook {
   my ($uri,$content) = @_;
 
-  return unless $MEMD and $MEMD->can('set_raw');
+  return unless $SMEMD and $SMEMD->can('set_raw');
   my $key = "nginx-".md5_hex("$SiteDefs::ENSEMBL_STATIC_BASE_URL$uri");
-  $MEMD->set_raw($key,$content);
+  $SMEMD->set_raw($key,$content);
 }
 
 1;
