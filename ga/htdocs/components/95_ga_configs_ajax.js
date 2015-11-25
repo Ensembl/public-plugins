b4c2db344c1cda65a5f5b51ca1a82e1750bde0c8
Harpreet Riat <harpreet@ebi.ac.uk>
2015-11-25 11:28:22 +0000

    GA config to log loading times for Ajax requests

---
+++ ga/htdocs/components/95_ga_configs_ajax.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+Ensembl.extend({
+  initialize: function () {
+
+    this.ajaxLoadTimerConfig = new Ensembl.GA.EventConfig({
+      category        : 'AjaxLoadTimer',
+      action          : function () { return this.getURL(); },
+      label           : function () {
+                                      var url = this.currentOptions.url.replace(/(;|&|\?)(_|time)=[0-9]+&?/g, '$1').replace(/(;|&|\?)$/, '');
+                                      if (url.match(/^http/)) {
+                                        url = url.replace(window.location.protocol + '//' + window.location.host, '');
+                                      }
+                                      return url;
+                                    },
+      nonInteraction  : true,
+      value           : function () { return new Date().getTime() - this.currentOptions._ensGA_StartTime; }
+    });
+
+    $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+      options._ensGA_StartTime  = new Date().getTime();
+    });
+
+    $(document).ajaxComplete(function(e, jqXHR, ajaxOptions) {
+      if (ajaxOptions._ensGA_StartTime) {
+        Ensembl.GA.sendEvent(Ensembl.ajaxLoadTimerConfig, {currentOptions: ajaxOptions});
+      }
+    });
+
+    this.base.apply(this, arguments);
+  }
+});
