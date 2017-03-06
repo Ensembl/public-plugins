7b79c7a10d3d3664e92ae341c96b2af1f1010b7b
Jyothish <joyjoy.nt@gmail.com>
2017-03-06 09:57:01 +0000

    Revert "[ENSWEB-3666] Switch default seq search to BLAST"
    This reverts commit db2f4140e8c087a46e7f3bea9fff8a039ddacaba.

--- tools/htdocs/components/20_BlastForm.js
+++ tools/htdocs/components/20_BlastForm.js
@@ -565,11 +565,6 @@ Ensembl.Panel.BlastForm = Ensembl.Panel.ToolsForm.extend({
       }
     }
 
-    // Make BLASTN default
-    if (!selectedValue && queryType === 'dna') {
-      selectedValue = 'NCBIBLAST_BLASTN';
-    }
-
     // now remove the invalid options for the selected combination of query type, db type and source type
     this.elLk.searchType.empty()
       .append(this.elLk.searchTypeOptions.filter(function() { return valid.indexOf(this.value) >= 0; }).clone())
