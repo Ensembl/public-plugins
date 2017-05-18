605895ab142d24b1fe352286165e7e5d2f4fd13b
Dan Sheppard <dan@ebi.ac.uk>
2017-05-18 16:49:47 +0100

    Rewrite of tools expiry script.

--- tools/utils/delete_expired_jobs.pl
+++ tools/utils/delete_expired_jobs.pl
@@ -18,175 +18,233 @@
 use strict;
 use warnings;
 
-use Cwd;
-use File::Basename;
-use File::Spec;
 use Getopt::Long;
+use Storable qw(retrieve);
 use Fcntl qw(:flock);
+use FindBin qw($Bin);
 
-my ($dry,$force,$limit) = (0,0,0);
-
-GetOptions(
-  "n|dry" => \$dry,
-  "f|force" => \$force,
-  "limit" => \$limit
-);
-
-my $MIN_AGE = 60*60;
-
-alarm 300;
-unless(flock(DATA,LOCK_EX|LOCK_NB)) {
-  warn "Already running. Exiting.\n";
-  exit 0;
-}
-
+# Path setup
+my ($CONF,$SD,$PD);
+my ($force,$dry,$limit,$super);
 BEGIN {
+  ($force,$dry,$limit,$super) = (0,0,-1,0);
+  GetOptions(
+    "n|dry" => \$dry,
+    "f|force" => \$force,
+    "limit=s" => \$limit,
+    "s|super" => \$super,
+  );
 
-  my @dirname   = File::Spec->splitdir(dirname(Cwd::realpath(__FILE__)));
-  my $code_path = File::Spec->catdir(splice @dirname, 0, -3);
-
-  # Load SiteDefs
-  unshift @INC, File::Spec->catdir($code_path, qw(ensembl-webcode conf));
-  eval {
-    require SiteDefs; SiteDefs->import;
-  };
-  if ($@) {
-    print "ERROR: Can't use SiteDefs - $@\n";
-    exit 1;
+  my $path = $ARGV[0] || 'config.sconf';
+  unless($path =~ m!^/!) {
+    my $file = $path;
+    my @path = split('/',$Bin);
+    while(@path) {
+      $path = join('/',@path,$file);
+      last if -e $path;
+      pop @path;
+    }
+    die "Cannot find '$file'\n" unless @path;
   }
 
-  # Check if EnsEMBL::Tools exist in plugins
-  if (!{@{$SiteDefs::ENSEMBL_PLUGINS}}->{'EnsEMBL::Tools'}) {
-    print "ERROR: Tools plugin is not loaded. Please add it to the Plugins.pm file before running this script.\n";
-    exit 1;
-  }
-
-  # Include all code dirs
-  unshift @INC, reverse @{SiteDefs::ENSEMBL_LIB_DIRS};
-  $ENV{'PERL5LIB'} = join ':', $ENV{'PERL5LIB'} || (), @INC;
+  warn "Using $path\n";
+  $CONF = retrieve($path);
+  $SD = $CONF->{'SiteDefs'};
+  $PD = $CONF->{'packed'};
+  unshift @INC,reverse @{$SD->{'ENSEMBL_LIB_DIRS'}};
 }
 
-use ORM::EnsEMBL::Rose::DbConnection;
-use ORM::EnsEMBL::DB::Tools::Manager::Ticket;
-use EnsEMBL::Web::SpeciesDefs;
-use EnsEMBL::Web::Utils::FileSystem qw(remove_empty_path);
+# Cronjob hygiene
+my $MIN_AGE = 60*60;
+my $LOCK_FILE =  "$SD->{'ENSEMBL_TMP_DIR'}/tooks-expiry.lock";
+my $STAMP_FILE = "$SD->{'ENSEMBL_TMP_DIR'}/tools-expiry.stamp";
 
-my $stamp = $SiteDefs::ENSEMBL_LOGDIR."/tool-tidy.stamp";
-
-# Dry run? Too soon?
-if ($dry) {
-  print "INFO: Dry run only, not actually making any changes.\n";
-} elsif(!$force) {
-  if(-e $stamp and time-([stat $stamp]->[9]) < $MIN_AGE) {
-    print "INFO: Not running, too soon.\n";
+alarm 86400;
+unless($force) {
+  open(LOCKFILE,'>>',$LOCK_FILE) or die "Cannot open lockfile '$LOCK_FILE': $!";
+  unless(flock(LOCKFILE,LOCK_EX|LOCK_NB)) {
+    warn "Already running";
     exit 0;
   }
 }
-if(!$dry) {
-  open(STAMP,'>>',$stamp);
-  close STAMP;
+if(not $dry and -e $STAMP_FILE and time-((stat $STAMP_FILE)[9]) < $MIN_AGE) {
+  print "Not running, too soon";
+  exit 0;
 }
+warn "Starting run";
 
-# Limit
-if ($limit && $limit > 0) {
-  print "INFO: Limit applied, only first $limit tickets will be deleted.\n";
-} else {
-  $limit = -1;
-}
+# Database connection
+use ORM::EnsEMBL::Rose::DbConnection;
+use ORM::EnsEMBL::DB::Tools::Manager::Ticket;
+use ORM::EnsEMBL::DB::Tools::Manager::Job;
+use EnsEMBL::Web::Utils::FileSystem qw(remove_empty_path);
 
-# Get db connection
-my $sd  = EnsEMBL::Web::SpeciesDefs->new();
+my $validity  = $SD->{'ENSEMBL_TICKETS_VALIDITY'};
+
+warn "Limiting to $limit tickets" unless $limit == -1;
+
+my $db_pd = $PD->{'MULTI'}{'databases'}{'DATABASE_WEB_TOOLS'};
 my $db  = {
-  'database'  =>  $sd->multidb->{'DATABASE_WEB_TOOLS'}{'NAME'},
-  'host'      =>  $sd->multidb->{'DATABASE_WEB_TOOLS'}{'HOST'},
-  'port'      =>  $sd->multidb->{'DATABASE_WEB_TOOLS'}{'PORT'},
-  'username'  =>  $sd->multidb->{'DATABASE_WEB_TOOLS'}{'USER'} || $sd->DATABASE_WRITE_USER,
-  'password'  =>  $sd->multidb->{'DATABASE_WEB_TOOLS'}{'PASS'} || $sd->DATABASE_WRITE_PASS,
+  'database'  =>  $db_pd->{'NAME'},
+  'host'      =>  $db_pd->{'HOST'},
+  'port'      =>  $db_pd->{'PORT'},
+  'username'  =>  $db_pd->{'USER'} || $SD->{'DATABASE_WRITE_USER'},
+  'password'  =>  $db_pd->{'PASS'} || $SD->{'DATABASE_WRITE_PASS'},
   'type'      => 'ticket',
   'domain'    => 'ensembl',
   'trackable' => 0
 };
-
-my $sub_limit = 1000;
-my $counter   = 0;
-my $validity  = $sd->ENSEMBL_TICKETS_VALIDITY;
-
-print sprintf "INFO: Current time %s\n", scalar localtime;
-print sprintf "INFO: Deleting tickets created before %s\n", scalar localtime(time - $validity);
-print sprintf "INFO: Database %s\n", $db->{'database'};
-print sprintf "INFO: Host %s:%s\n", $db->{'host'}, $db->{'port'};
-
-# Register db with rose api
+warn "using ".join(':',map { $db_pd->{$_} } qw(NAME HOST PORT USER))."\n";
 ORM::EnsEMBL::Rose::DbConnection->register_database($db);
 
-my $num_deleted = 0;
-while ($limit) {
+sub job_iterator {
+  my ($ticket) = @_;
 
-  $sub_limit  = $limit > 0 ? [ $limit, $sub_limit ]->[ $limit > $sub_limit ] : $sub_limit;
-  $limit      = $limit - $sub_limit if $limit > 0;
-
-  print sprintf "INFO: Iteration %s: Fetching %s tickets\n", ++$counter, $sub_limit;
-
-  # Fetch all non-user tickets that are not already marked as deleted limited by $sub_limit
-  my $tickets_iterator = ORM::EnsEMBL::DB::Tools::Manager::Ticket->get_objects_iterator(
-    'query'         => [ 'owner_type' => {'ne' => 'user'}, 'status' => {'ne' => 'Deleted'} ],
-    'with_objects'  => [ 'job' ],
-    'sort_by'       => 'created_at ASC',
-    'multi_many_ok' => 1,
-    'limit'         => $sub_limit, # this is to avoid loading all tickets in memory at once
-#    'debug'         => 1,
-  );
-
-  # Any error?
-  if ($tickets_iterator->error) {
-    print sprintf "ERROR: %s\n", $tickets_iterator->error;
+  my $iter = 
+    ORM::EnsEMBL::DB::Tools::Manager::Job->get_objects_iterator(
+      'query'         => [ 'ticket_id' => $ticket->ticket_id ],
+    );
+  if ($iter->error) {
+    warn "ERROR: ".($iter->error)."\n";
     exit 1;
   }
+  return $iter;
+}
 
-  # For all tickets that have no validity left, mark them as deleted and remove all the related dirs from the file system
-  my $deleted = 0;
-  while (my $ticket = $tickets_iterator->next) {
-    if (!$ticket->calculate_life_left($validity)) {
-      print sprintf "INFO: Deleting ticket %s\n", $ticket->ticket_name;
-      eval {
-        if ($dry || $ticket->mark_deleted) {
-          for (grep $_->job_dir, $ticket->job) {
-            my @dir = File::Spec->splitdir($_->job_dir);
-            my $dir = File::Spec->catdir(splice @dir, 0, -1);
-            print "INFO: Removing $dir\n";
-            if (!$dry && -d $dir && !remove_empty_path($dir, { 'remove_contents' => 1, 'exclude' => [ $ticket->ticket_type_name ], 'no_exception' => 1 })) {
-              print "WARNING: Could not remove ticket directory $dir\n";
-            }
-            last; # ticket dir removed, second attempt not required
-          }
-          $deleted++;
-          $num_deleted++;
-        } else {
-          print sprintf "WARNING: Could not mark ticket %s as deleted.\n", $ticket->ticket_name;
+# Deleting mechanics
+sub we_can_delete {
+  my ($ticket) = @_;
+
+  my $job_iter = job_iterator($ticket);
+  while(my $job = $job_iter->next) {
+    unless(-e $job->job_dir) {
+      warn "cannot delete, cannot find job directory\n";
+      return 0;
+    }
+  }
+  return 1;
+}
+
+sub delete_path {
+  my ($ticket,$path) = @_;
+
+  if($dry) {
+    warn "would delete: $path\n";
+    return;
+  }
+  return unless -d $path;
+  remove_empty_path($path,{
+    remove_contents => 1,
+    exclude => [ $ticket->ticket_type_name ]
+  });
+}
+
+sub do_delete {
+  my ($ticket) = @_;
+
+  my $dt = $ticket->created_at;
+  my $at = $dt->ymd.' '.$dt->hms;
+  warn "deleting ".($ticket->ticket_name)." ($at)\n";
+  my %path;
+  my $job_iter = job_iterator($ticket);
+  while(my $job = $job_iter->next) {
+    my @path = split('/',$job->job_dir);
+    pop @path;
+    $path{join('/',@path)} = 1;
+  }
+  foreach my $path (keys %path) {
+    eval { delete_path($ticket,$path); };
+    if($@) {
+      warn "Error deleting '$path': $@\n";
+      return;
+    }
+  }
+  $ticket->mark_deleted unless $dry or $ticket->status eq 'Deleted';
+}
+
+# Super mode
+if($super) {
+  # ... build list of old tickets
+  my $dir = "$SD->{'ENSEMBL_TMP_DIR_TOOLS'}/temporary/tools";
+  my $valid_days = $validity/24/60/60;
+  my %old_tickets;
+  my $cmd = "find $dir -name info\\* -mtime +$valid_days";
+  open(LIST,"$cmd |") or die "Cannot run $cmd";
+  my $count = 0;
+  while(<LIST>) {
+    chomp;
+    if(open(INFO,'<',$_)) {
+      while(<INFO>) {
+        next unless /^Ticket: (.*)/;
+        $old_tickets{$1}=1;
+      }
+      close INFO;
+    }
+    $count++;
+    last if $limit != -1 and $count > $limit;
+  }
+  if(scalar(keys %old_tickets)) {
+    warn "found ".scalar(keys %old_tickets)." old tickets to check\n";
+    my $iter =
+      ORM::EnsEMBL::DB::Tools::Manager::Ticket->get_objects_iterator(
+        'query' => [ 'owner_type' => { 'ne' => 'user' },
+                     't1.status' => { 'eq' => 'Deleted'},
+		     't1.ticket_name' => [keys %old_tickets] ],
+        'multi_many_ok' => 1,
+        'limit' => $limit,
+      );
+    if ($iter->error) {
+      warn "ERROR: ".($iter->error)."\n";
+      exit 1;
+    }
+    while (my $ticket = $iter->next) {
+      warn "already deleted from db: ".($ticket->ticket_name)."\n";
+      if (!$ticket->calculate_life_left($validity)) {
+        if(we_can_delete($ticket)) {
+          do_delete($ticket);
         }
-      };
-      if ($@) { # some unknown error
-        print sprintf "WARNING: Could not mark ticket %s as deleted.\n%s\n%s\n", $ticket->ticket_name, $@, "-" x 50;
       }
     }
   }
+}
 
-  # Any error?
+# Main loop
+my $iter = 0;
+my $each_time = 1000;
+while($limit>0 or $limit==-1) {
+  $iter++;
+  my $this_time = $each_time;
+  $this_time = $limit if $limit != -1 and $limit < $this_time;
+  $limit -= $this_time if $limit > -1;
+  warn "Iteration $iter, retrieving $this_time tickets\n";
+  my $tickets_iterator =
+    ORM::EnsEMBL::DB::Tools::Manager::Ticket->get_objects_iterator(
+      'query'         => [ 'owner_type' => {'ne' => 'user'},
+			   'status' => {'ne' => 'Deleted'} ],
+      'sort_by'       => 'created_at ASC',
+      'multi_many_ok' => 1,
+      'limit'         => $this_time, # don't fill memory
+      #'debug'         => 1,
+    );
   if ($tickets_iterator->error) {
-    print sprintf "WARNING: %s\n", $tickets_iterator->error;
+    warn "ERROR: ".($tickets_iterator->error)."\n";
+    exit 1;
   }
-
-  # If no ticket was deleted in this request, don't do any further requests
-  if (!$deleted) {
-    last;
+  my $successes = 0;
+  while (my $ticket = $tickets_iterator->next) {
+    if (!$ticket->calculate_life_left($validity)) {
+      if(we_can_delete($ticket)) {
+        do_delete($ticket);
+        $successes++;
+      } else {
+        warn "cannot delete ".($ticket->ticket_name)."\n";
+      }
+    }
   }
-
-  if ($dry) {
-    print "INFO: Only 1 iteration is done in DRY RUN mode.\n";
+  if(!$successes) {
+    warn "no successes, exiting\n";
     last;
   }
 }
 
-print "INFO: DONE ($num_deleted deleted or would be)\n";
-__DATA__
-Keep for locking.
+exit 0;
