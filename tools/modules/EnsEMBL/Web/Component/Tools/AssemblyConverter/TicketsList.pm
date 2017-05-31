c043204feee670e4ea2d7345e36730f91b3b3305
Ridwan Amode <ridwan@ebi.ac.uk>
2017-05-31 12:39:14 +0100

    ENSWEB-3323: assembly converter validation; move check for empty output file from runnable to front end as the fail message was confusing when there was no mapped data

--- tools/modules/EnsEMBL/Web/Component/Tools/AssemblyConverter/TicketsList.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/AssemblyConverter/TicketsList.pm
@@ -32,11 +32,22 @@ sub job_status_tag {
   ## Remove link from the status tag of finished jobs
   my $self    = shift;
   my $status  = $_[1];
+  my $job     = $_[0];  
   my $tag     = $self->SUPER::job_status_tag(@_);
+  my $output  = $job->dispatcher_data->{work_dir}."/".$job->dispatcher_data->{config}->{output_file};
 
   if ($status eq 'done') {
     $tag->{'title'} = q(This job is finished. Please click on the 'Download&nbsp;results' link to download result file.);
     $tag->{'href'}  = '';
+  } 
+
+  # Before the below was in the runnable and the job was reported as failed which was confusing, moved it to here
+  # CrossMap's error reporting is poor, so check it produced actual output which means that either there is no mapped data or the data is invalid. 
+  if ($status eq 'done' && (!(-e $output) || !(-s $output))) {
+    $tag->{'inner_HTML'} = "Done: No data found";
+    $tag->{'class'} = [ 'job-status-noresult', grep { $_ ne 'job-status-done' } @{$tag->{'class'}} ];
+    $tag->{'title'} = 'This job is finished, but no results were obtained. This could either be because there is an error in your input data or because no mapping of coordinates were found.';
+    $tag->{'href'}  = '';    
   }
 
   return $tag;
@@ -47,13 +58,18 @@ sub job_summary_section {
   ## Change text and link of the results link
   my $self      = shift;
   my $ticket    = $_[0];
+  my $output    = $ticket->job->[0]->dispatcher_data->{work_dir}."/".$ticket->job->[0]->dispatcher_data->{config}->{output_file};
   my $summary   = $self->SUPER::job_summary_section(@_);
 
   foreach (@{$summary->get_nodes_by_flag('job_results_link') || []}) {
-    $_->inner_HTML('[Download results]');
-    $_->set_attribute('href', $self->object->download_url($ticket->ticket_name));
+    if(-e $output && -s $output) {
+      $_->inner_HTML('[Download results]');
+      $_->set_attribute('href', $self->object->download_url($ticket->ticket_name));
+    } else {
+      $_->inner_HTML('');
+    }
   }
-
+  
   return $summary;
 }
 
@@ -64,9 +80,10 @@ sub ticket_buttons {
   my $ticket    = $_[0];
   my $buttons   = $self->SUPER::ticket_buttons(@_);
   my ($job)     = $ticket && $ticket->job;
+  my $output    = $job->dispatcher_data->{work_dir}."/".$job->dispatcher_data->{config}->{output_file};
 
-  if ($job && $job->dispatcher_status eq 'done') {
-
+  #only provide the download icon when there is an output file and it is not empty
+  if ($job && $job->dispatcher_status eq 'done' && -e $output && -s $output) {
     $buttons->prepend_child({
       'node_name'   => 'a',
       'class'       => [qw(_download)],
