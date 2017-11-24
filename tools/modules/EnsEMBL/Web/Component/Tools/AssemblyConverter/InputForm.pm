a8dd3a3deeac77506394c21037cbf14d668e200d
Harpreet Riat <harpreet@ebi.ac.uk>
2017-11-24 14:46:15 +0000

    output for wig is returned as wig now

--- tools/modules/EnsEMBL/Web/Component/Tools/AssemblyConverter/InputForm.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/AssemblyConverter/InputForm.pm
@@ -34,8 +34,7 @@ sub form_header_info {
   return '<p class="info">This online tool currently uses <a href="http://crossmap.sourceforge.net">CrossMap</a>,
           which supports a limited number of formats (see our online documentation for
           <a href="/info/website/upload/index.html#formats">details of the individual data formats</a> listed below).
-          CrossMap also discards metadata in files, so track definitions, etc, will be lost on conversion.</p>
-          <p><b>Important note</b>: CrossMap converts WIG files to BedGraph internally for efficiency, and also outputs them in BedGraph format.</p>';
+          CrossMap also discards metadata in files, so track definitions, etc, will be lost on conversion.</p>';
 }
 
 sub get_cacheable_form_node {
