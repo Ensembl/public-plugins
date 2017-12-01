48deaf81e4fd4ac881739577ac03fca2dfb86199
Harpreet Riat <harpreet@ebi.ac.uk>
2017-12-01 11:45:10 +0000

    index_file is already declared

--- tools_hive/modules/EnsEMBL/Web/RunnableDB/AlleleFrequency.pm
+++ tools_hive/modules/EnsEMBL/Web/RunnableDB/AlleleFrequency.pm
@@ -71,8 +71,8 @@ sub fetch_input {
   }
   
   #creating new index file based on splitted file but before removing original index file
-  my $index_file = EnsEMBL::Web::SystemCommand->new($self, "rm $work_dir/$shortname.gz.tbi;cd $work_dir;$tabix -f -p vcf $shortname.gz")->execute();
-  if($index_file->error_code) {
+  my $index_file_cmd = EnsEMBL::Web::SystemCommand->new($self, "rm $work_dir/$shortname.gz.tbi;cd $work_dir;$tabix -f -p vcf $shortname.gz")->execute();
+  if($index_file_cmd->error_code) {
     throw exception('HiveException', "Index file error: ".$index_resp->error_code) unless -s $shortname.".gz.tbi";
   }
 
