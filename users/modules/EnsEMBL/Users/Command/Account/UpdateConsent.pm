ea5fd6d94cfcb70024fbe80890836b64e6aced08
Anne Lyle <annelyle@ebi.ac.uk>
2018-05-11 08:49:28 +0100

    Add next stage of consent mechanism

---
+++ users/modules/EnsEMBL/Users/Command/Account/UpdateConsent.pm
@@ -0,0 +1,47 @@
+=head1 LICENSE
+
+Copyright [1999-2015] Wellcome Trust Sanger Institute and the EMBL-European Bioinformatics Institute
+Copyright [2016-2018] EMBL-European Bioinformatics Institute
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=cut
+
+package EnsEMBL::Users::Command::Account::UpdateConsent;
+
+## If user consents to privacy policy, update account and return to original web page,
+## otherwise redirect to warning that their account will be disabled
+
+use strict;
+use warnings;
+
+use parent qw(EnsEMBL::Users::Command::Account);
+
+sub process {
+  my $self    = shift;
+  my $hub     = $self->hub;
+
+  if ($hub->param('consent_1')) {
+    my $email   = $hub->param('email') || '';
+    my $login   = $self->object->fetch_login_account($email);
+    $login->update_consent($hub->species_defs->GDPR_VERSION);;
+
+    return $self->redirect_after_login($login->user);
+  }
+  else {
+    return $self->ajax_redirect($self->hub->url({'action' => 'ConsentWarning','email' => $hub->param('email')}));
+  }
+
+}
+
+1;
