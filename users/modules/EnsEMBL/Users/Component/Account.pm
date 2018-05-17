9e391d149b0b46bea6a86113cb5db8c47090ff76
Anne Lyle <annelyle@ebi.ac.uk>
2018-05-17 11:17:14 +0100

    Simplify mechanism by disabling button until consent checkbox is ticked

--- users/modules/EnsEMBL/Users/Component/Account.pm
+++ users/modules/EnsEMBL/Users/Component/Account.pm
@@ -140,14 +140,14 @@ sub add_user_details_fields {
 
   if ($self->hub->species_defs->GDPR_ACCOUNTS_VERSION) {
     $form->add_field({
-      'label'   => 'Privacy policy',
-      'type'    => 'checkbox',
-      'name'    => 'accounts_consent',
-      'id'      => 'consent_checkbox',
-      'notes'   => '<b>Please tick to agree</b> to our <a href="" rel="external">privacy policy</a>',
-      'value'   => 1,
+      'label'     => 'Privacy policy',
+      'type'      => 'checkbox',
+      'name'      => 'accounts_consent',
+      'id'        => 'consent_checkbox',
+      'notes'     => '<b>Please tick to agree</b> to our <a href="" rel="external">privacy policy</a>',
+      'value'     => 1,
     });
-    $form->add_button({'type' => 'button', 'id' => 'pre_consent', 'value' => $params->{'button'} || 'Register'});
+    $form->add_button({'type' => 'button', 'id' => 'pre_consent', 'class' => 'disabled', 'value' => $params->{'button'} || 'Register'});
   }
   else {
     $form->add_button({'value' => $params->{'button'} || 'Register'});
