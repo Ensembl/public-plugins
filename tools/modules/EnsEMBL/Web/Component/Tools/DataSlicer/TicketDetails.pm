b29b28ed65497776da272399f15e14bafaf70fe2
Jyothish <joyjoy.nt@gmail.com>
2018-06-21 11:18:22 +0100

    Update species image url

--- tools/modules/EnsEMBL/Web/Component/Tools/DataSlicer/TicketDetails.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/DataSlicer/TicketDetails.pm
@@ -40,7 +40,7 @@ sub job_details_table {
   my $job_summary = $self->get_job_summary($job, $is_owned_ticket);
 
   $two_col->add_row('Job name',       $job_summary->render);
-  $two_col->add_row('Species',        $object->valid_species($species) ? sprintf('<img class="job-species" src="%sspecies/16/%s.png" alt="" height="16" width="16">%s', $self->img_url, $species, $sd->species_label($species, 1)) : $species =~ s/_/ /rg);
+  $two_col->add_row('Species',        $object->valid_species($species) ? sprintf('<img class="job-species" src="%sspecies/%s.png" alt="" height="16" width="16">%s', $self->img_url, $species, $sd->species_label($species, 1)) : $species =~ s/_/ /rg);
   $two_col->add_row('Assembly',       $job->assembly);
   $two_col->add_row('File format',    $job_data->{file_format});
   $two_col->add_row('Region',         $job_data->{region});
--- tools/modules/EnsEMBL/Web/Component/Tools/TicketDetails.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/TicketDetails.pm
@@ -51,7 +51,7 @@ sub job_details_table {
 
   $two_col->add_row('Job summary',  $self->get_job_summary($job, $is_owned_ticket)->render);
   $two_col->add_row('Species',      $object->valid_species($species)
-    ? sprintf('<img class="job-species" src="%sspecies/16/%s.png" alt="" height="16" width="16">%s', $self->img_url, $species, $sd->species_label($species, 1))
+    ? sprintf('<img class="job-species" src="%sspecies/%s.png" alt="" height="16" width="16">%s', $self->img_url, $species, $sd->species_label($species, 1))
     : $species =~ s/_/ /rg
   );
   $two_col->add_row('Assembly',     $job->assembly);
