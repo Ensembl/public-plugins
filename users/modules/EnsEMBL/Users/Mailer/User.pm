da4b8755f4e9543b039c36bbd1824b44d38f8495
Ensembl User <ensembl-webteam@ebi.ac.uk>
2018-09-27 17:17:27 +0100

    fix account bug introduced by 8433fee

--- users/modules/EnsEMBL/Users/Mailer/User.pm
+++ users/modules/EnsEMBL/Users/Mailer/User.pm
@@ -89,7 +89,7 @@ sub send_password_retrieval_email {
   $self->to       = "$name <email>";
   $self->subject  = qq($sitename: Reset your password);
   $self->message  = qq(We received a request to reset your password for $sitename. If this was you, please go to the following url:\n\n\n$url\n\n\nThis will allow you to set a new password and log in to the site again.\n\n)
-                    .qq(If this was not you, please let us know at helpdesk@ensembl.org. You can ignore the link above; your old password will not be changed and you will be able to continue to use it.\n\n);
+                    .qq(If this was not you, please let us know at helpdesk\@ensembl.org. You can ignore the link above; your old password will not be changed and you will be able to continue to use it.\n\n)
                     .$footer;
 
   $self->set_noreply_sender;
