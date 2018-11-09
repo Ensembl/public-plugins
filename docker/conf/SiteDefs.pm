de8d41472a3b5d8a09895cb96821278dacda7fc1
Sanjay Boddu <sboddu@ebi.ac.uk>
2018-11-09 11:06:48 +0000

    Updates to docker configs

--- docker/conf/SiteDefs.pm
+++ docker/conf/SiteDefs.pm
@@ -13,26 +13,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-package EnsEMBL::DockerDemo::SiteDefs;
+package EnsEMBL::Docker::SiteDefs;
 use strict;
 
 # configs for a docker-based Ensembl instance
 
 sub update_conf {
-  # path to linuxbrew work dir
-  $SiteDefs::SHARED_SOFTWARE_PATH = '/home/linuxbrew';
 
   # the internal port to bind to
   $SiteDefs::ENSEMBL_PORT = 8080;
+  # get the server name from the environment (this should match the domain name, e.g. www.ensembl.org)
+  $SiteDefs::ENSEMBL_SERVERNAME = $ENV{ENSEMBL_SERVERNAME} if $ENV{ENSEMBL_SERVERNAME};
 
   # set a dummy hostname - without this, Ensembl will use the system hostname and get confused
   # because in a container the hostname is changing with each docker build step
   $SiteDefs::ENSEMBL_SERVER           = 'dockerhost';
   $SiteDefs::ENSEMBL_SERVER_SIGNATURE = "$SiteDefs::ENSEMBL_SERVER-$SiteDefs::ENSEMBL_SERVERROOT" =~ s/\W+/-/gr; #/
 
-  # get the server name from the environment (this should match the domain name, e.g. www.ensembl.org)
-  $SiteDefs::ENSEMBL_SERVERNAME = $ENV{ENSEMBL_SERVERNAME} if $ENV{ENSEMBL_SERVERNAME};
+
+ ## IMPORTANT - do not use these root paths in code, use the 'DIR' version!
+  $SiteDefs::ENSEMBL_TMP_ROOT               = '/ebi/nobackup';
+  $SiteDefs::ENSEMBL_USERDATA_ROOT          = '/ebi/incoming';
+
+  $SiteDefs::ENSEMBL_TMP_DIR                = defer { $SiteDefs::ENSEMBL_TMP_ROOT };
+  $SiteDefs::ENSEMBL_USERDATA_DIR           = defer { $SiteDefs::ENSEMBL_USERDATA_ROOT };
+
+  # path to linuxbrew work dir
+  $SiteDefs::SHARED_SOFTWARE_PATH = '/home/linuxbrew';
+
+  $SiteDefs::ENSEMBL_EXTERNAL_SEARCHABLE    = 0;
 
   # limit the demo to human and mouse
   $SiteDefs::PRODUCTION_NAMES = [qw(
@@ -42,4 +51,3 @@ sub update_conf {
 }
 
 1;
-
--- linuxbrew/conf/SiteDefs.pm
+++ linuxbrew/conf/SiteDefs.pm
@@ -26,6 +26,16 @@ use Cwd qw(abs_path);
 use List::MoreUtils qw(uniq);
 
 sub update_conf {
+
+
+  ## Apache-writeable locations
+  $SiteDefs::ENSEMBL_SYS_DIR                = defer { "$SiteDefs::ENSEMBL_TMP_DIR/server" };
+  $SiteDefs::ENSEMBL_ROBOTS_TXT_DIR         = defer { $SiteDefs::ENSEMBL_SYS_DIR };
+  $SiteDefs::ENSEMBL_MINIFIED_FILES_PATH    = defer { "$SiteDefs::ENSEMBL_SYS_DIR/minified" };
+  $SiteDefs::ENSEMBL_OPENSEARCH_PATH        = defer { "$SiteDefs::ENSEMBL_SYS_DIR/opensearch" };
+  $SiteDefs::GOOGLE_SITEMAPS_PATH           = defer { "$SiteDefs::ENSEMBL_SYS_DIR/sitemaps" };
+
+
   $SiteDefs::SHARED_SOFTWARE_BIN_PATH       = defer { join ':', uniq($SiteDefs::SHARED_SOFTWARE_PATH.'/linuxbrew/bin', split(':', $ENV{'PATH'} || ())) };
   $SiteDefs::ENSEMBL_SETENV->{'PATH'}       = 'SHARED_SOFTWARE_BIN_PATH';
 
@@ -44,11 +54,15 @@ sub update_conf {
   $SiteDefs::ENSEMBL_EMBOSS_PATH            = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/emboss' };   #AlignView
   $SiteDefs::ENSEMBL_WISE2_PATH             = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/genewise' }; #AlignView
   $SiteDefs::THOUSANDG_TOOLS_DIR            = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/1000G-tools' }; #location of all 1000G tools runnable and scripts 
+
   $SiteDefs::GRAPHIC_TTF_PATH               = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/fonts/' };
+  
   $SiteDefs::ENSEMBL_NCBIBLAST_BIN_PATH     = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/ncbi-blast/' };
   $SiteDefs::ENSEMBL_REPEATMASK_BIN_PATH    = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/RepeatMasker' };
   $SiteDefs::ENSEMBL_BLAT_BIN_PATH          = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/gfClient' };
   $SiteDefs::ASSEMBLY_CONVERTER_BIN_PATH    = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/CrossMap.py' };
+  $SiteDefs::WIGTOBIGWIG_BIN_PATH        = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/wigToBigWig' };
+  $SiteDefs::BIGWIGTOWIG_BIN_PATH        = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/paths/bigWigToWig' };
   $SiteDefs::FILE_CHAMELEON_BIN_PATH        = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/FileChameleon/bin/format_transcriber.pl' };
   $SiteDefs::ALLELE_FREQUENCY_BIN_PATH      = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/1000G-tools/allelefrequency/calculate_allele_frq_from_vcf.pl' };
   $SiteDefs::VCF_PED_BIN_PATH               = defer { $SiteDefs::SHARED_SOFTWARE_PATH.'/1000G-tools/vcftoped/vcftoped.pl' };
