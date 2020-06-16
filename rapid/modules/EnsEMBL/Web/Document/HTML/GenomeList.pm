b881272497de3980b0645c814975bc6e8cad26df
Manoj <manojpandian@ebi.ac.uk>
2020-06-16 08:36:04 +0000

    Remove the edit icon

--- rapid/modules/EnsEMBL/Web/Document/HTML/GenomeList.pm
+++ rapid/modules/EnsEMBL/Web/Document/HTML/GenomeList.pm
@@ -77,4 +77,158 @@ sub _species_list {
 
 
 
+
+sub _get_dom_tree {
+  ## @private
+  my $self      = shift;
+  my $hub       = $self->hub;
+  my $sd        = $hub->species_defs;
+  my $species   = $self->_species_list({'no_user' => 1});
+  my $template  = $self->_fav_template;
+  my $prehtml   = '';
+
+  for (0..$self->SPECIES_DISPLAY_LIMIT-1) {
+    $prehtml .= $template =~ s/\{\{species\.(\w+)}\}/my $replacement = $species->[$_]{$1};/gre if $species->[$_] && $species->[$_]->{'favourite'};
+  }
+
+  ## Needed for autocomplete
+  my $strains = [];
+  foreach my $sp (@$species) {
+    if ($sp->{'strainspage'}) {
+      push @$strains, {
+                      'homepage'  => $sp->{'strainspage'},
+                      'name'      => $sp->{'name'},,
+                      'common'    => (sprintf '%s %s', $sp->{'common'}, $sp->{'strain_type'}),
+                      };
+    }
+  }
+
+  my @ok_species = $sd->valid_species;
+  my $sitename  = $self->hub->species_defs->ENSEMBL_SITETYPE;
+  if (scalar @ok_species > 1) {
+    my $list_html = sprintf qq(<h3>All genomes</h3>
+      %s
+      <h3 class="space-above"></h3>
+      %s
+      <p><a href="%s">View full list of all species</a></p>
+      ), 
+      $self->add_species_dropdown,
+      $self->add_genome_groups, 
+      $self->species_list_url; 
+
+    my $sort_html = qq(<p>For easy access to commonly used genomes, drag from the bottom list to the top one</p>
+        <p><strong>Favourites</strong></p>
+          <ul class="_favourites"></ul>
+        <p><a href="#Done" class="button _list_done">Done</a>
+          <a href="#Reset" class="button _list_reset">Restore default list</a></p>
+        <p><strong>Other available species</strong></p>
+          <ul class="_species"></ul>
+          );
+
+  
+    my %taxon_labels = $sd->multiX('TAXON_LABEL'); 
+    unless (keys %taxon_labels) {
+      %taxon_labels = %{$sd->TAXON_LABEL||{}};
+    }
+
+    return $self->dom->create_element('div', {
+      'class'       => 'column_wrapper',
+      'children'    => [{
+              'node_name'   => 'div',
+              'class'       => 'column-two static_all_species',
+              'inner_HTML'  => $list_html,
+            }, {
+              'node_name'   => 'div',
+              'class'       => 'column-two fave-genomes',
+              'children'    => [{
+                        'node_name'   => 'h3',
+                        'inner_HTML'  => "Favourite genomes",
+                      }, {
+                        'node_name'   => 'div',
+                        'class'       => [qw(_species_sort_container reorder_species clear hidden)],
+                        'inner_HTML'  => $sort_html
+                      }, {
+                        'node_name'   => 'div',
+                        'class'       => [qw(_species_fav_container species-list)],
+                        'inner_HTML'  => $prehtml
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param',
+                        'name'        => 'fav_template',
+                        'value'       => encode_entities($template)
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param',
+                        'name'        => 'list_template',
+                        'value'       => encode_entities($self->_list_template)
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param json',
+                        'name'        => 'species_list',
+                        'value'       => encode_entities(to_json($species))
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param json',
+                        'name'        => 'strains_list',
+                        'value'       => encode_entities(to_json($strains))
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param',
+                        'name'        => 'ajax_refresh_url',
+                        'value'       => encode_entities($self->ajax_url)
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param',
+                        'name'        => 'ajax_save_url',
+                        'value'       => encode_entities($hub->url({qw(type Account action Favourites function Save)}))
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param',
+                        'name'        => 'display_limit',
+                        'value'       => SPECIES_DISPLAY_LIMIT
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param json',
+                        'name'        => 'taxon_labels',
+                        'value'       => encode_entities(to_json(\%taxon_labels))
+                      }, {
+                        'node_name'   => 'inputhidden',
+                        'class'       => 'js_param json',
+                        'name'        => 'taxon_order',
+                        'value'       => encode_entities(to_json($sd->TAXON_ORDER))
+                      }]
+          }]
+    });
+  }
+  else {
+    my $species       = $ok_species[0];
+    my $info          = $hub->get_species_info($species);
+    my $homepage      = $hub->url({'species' => $species, 'type' => 'Info', 'function' => 'Index', '__clear' => 1});
+    my $img_url       = $sd->img_url || '';
+    my $sp_info = {
+      homepage    => $homepage,
+      name        => $info->{'name'},
+      img         => sprintf('%sspecies/%s.png', $img_url, $info->{'image'}),
+      common      => $info->{'common'},
+      assembly    => $info->{'assembly'},
+    };
+    my $species_html = $template =~ s/\{\{species\.(\w+)}\}/my $replacement = $sp_info->{$1};/gre;
+    return $self->dom->create_element('div', {
+      'class'       => 'column_wrapper',
+      'children'    => [{
+                        'node_name'   => 'div',
+                        'class'       => 'column-two fave-genomes',
+                        'children'    => [{
+                                          'node_name'   => 'h3',
+                                          'inner_HTML'  => 'Available genomes'
+                                          }, {
+                                          'node_name'   => 'div',
+                                          'inner_HTML'  => $species_html
+                                        }]
+                        }]
+    });
+  }
+}
+
+
 1;
