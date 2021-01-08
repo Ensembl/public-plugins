dde90b8263b2fe2dcf55a65f83855370c17f9b66
dglemos <dlemos@ebi.ac.uk>
2021-01-08 15:03:24 +0000

    Review javascript code VR

--- tools/htdocs/components/20_VRForm.js
+++ tools/htdocs/components/20_VRForm.js
@@ -41,17 +41,6 @@ Ensembl.Panel.VRForm = Ensembl.Panel.ToolsForm.extend({
       panel.elLk.dataField.val(text).trigger('focus');
     });
 
-    // Preview button
-    // Needed to resubmit job
-    this.elLk.previewButton = panel.elLk.form.find('[name=preview]').hide().on('click', function(e) {
-      e.preventDefault();
-      panel.enablePreviewButton(false);
-      panel.preview($(this).data('currentVal'), { left : e.pageX, top : e.pageY });
-    });
-
-    // Preview div
-    this.elLk.previewDiv = $('<div class="vep-preview-div">').appendTo($(document.body));
-
     // show hide preview button acc to the text in the input field
     this.elLk.dataField = this.elLk.form.find('textarea[name=text]').on({
       'paste keyup click change focus scroll': function(e) {
@@ -59,61 +48,26 @@ Ensembl.Panel.VRForm = Ensembl.Panel.ToolsForm.extend({
       }
     });
 
-    // move preview button after the textarea
-    this.elLk.previewButton.appendTo(this.elLk.dataField.parent());
-
     this.editExisting();
 
     // initialise the selectToToggle fields ie. show or hide the ones as needed
     this.resetSelectToToggle();
   },
 
-  /*dataFieldInteraction: function(eventType) {
-   *
+   dataFieldInteraction: function(eventType) {
+   /*
    * Acts according to the event occurred on the textare input
-   *
+   */
     var panel = this;
     var value = this.elLk.dataField[0].value;
     var bgPos = Ensembl.browser.webkit ? 13 : 15;
 
     this.elLk.dataField.removeClass('focused');
-    this.elLk.previewButton.hide();
-
-    if (this.elLk.dataField.data('previousValue') !== value) {
-      this.elLk.dataField.data('previousValue', value);
-      this.elLk.dataField.data('inputFormat', (function(value) {
-        var format = panel.detectFormat(value.split(/[\r\n]+/)[0]);
-        if (format === 'id' || format === 'vcf' || format === 'ensembl' || format === 'hgvs' || format === 'spdi') {
-          return format;
-        }
-        return false;
-      })(value));
-    }
 
     if (!this.dataFieldHeight) {
       this.dataFieldHeight = this.elLk.dataField.height();
     }
-
-
-    if (value.length) {
-      var format = this.elLk.dataField.data('inputFormat');
-
-      if (format) {
-        if (!this.elLk.fakeDataField) {
-          this.elLk.fakeDataField = $('<div class="vep-input-div">').appendTo(this.elLk.form);
-        }
-        var pos   = this.elLk.dataField.prop('selectionStart');
-        var curr  = value.substr(0, pos).replace(/.+[\r\n]/g, '') + value.substr(pos).replace(/[\r\n].+/g, '');
-
-        if (curr.length) {
-          var height = this.elLk.fakeDataField.html(value.substr(0, pos) + 'x').show().height() - bgPos - this.elLk.dataField.scrollTop();
-          this.elLk.fakeDataField.hide();
-          this.elLk.dataField.addClass('focused').css('background-position', '0 ' + height + 'px');
-          this.elLk.previewButton.show().css('margin-top', Math.max(Math.min(height, this.dataFieldHeight - bgPos), 0) + 1).data('currentVal', curr);
-        }
-      }
-    }
-  },*/
+  },
 
   enablePreviewButton: function(flag) {
   /*
@@ -151,18 +105,16 @@ Ensembl.Panel.VRForm = Ensembl.Panel.ToolsForm.extend({
     }
   },
 
-  /*reset: function() {
-   *
+   reset: function() {
+   /*
    * Resets the form, ready to accept next job input
-   *
+   */
     this.base.apply(this, arguments);
     this.elLk.form.find('._download_link').remove();
     this.elLk.dataField.removeClass('focused');
-    this.elLk.previewDiv.empty().removeClass('active loading');
-    this.elLk.previewButton.hide();
     this.resetSpecies(this.defaultSpecies);
     this.resetSelectToToggle();
-  },*/
+  },
 
   resetSpecies: function (species) {
   /*
