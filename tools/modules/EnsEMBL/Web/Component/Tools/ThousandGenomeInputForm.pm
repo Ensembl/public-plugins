ddbb4dcff64998e0a10ab3f31baa57487104d215
mehrnaz-charkhchi <mehrnaz.charkhchi@gmail.com>
2021-04-07 17:10:43 +0000

    Apply changes in 1000g FTP file names.

--- tools/modules/EnsEMBL/Web/Component/Tools/ThousandGenomeInputForm.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/ThousandGenomeInputForm.pm
@@ -149,7 +149,7 @@ sub common_form {
       'size'          => 30,
       'class'         => 'url',
       'field_class'   => 'hidden _stt_custom',
-      'notes'         => 'e.g: http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
+      'notes'         => 'e.g: http://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/ALL.chr1.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz'
     }),
     $input_fieldset->add_field({
     'type'          => 'radiolist',
