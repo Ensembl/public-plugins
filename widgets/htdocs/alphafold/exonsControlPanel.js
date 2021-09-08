abdb11fafa5b140854b9a7e3a5c4c79be527073b
Andrey Azov <andrey@ebi.ac.uk>
2021-09-08 17:20:32 +0000

    Add styling for the exons control panel

--- widgets/htdocs/alphafold/exonsControlPanel.js
+++ widgets/htdocs/alphafold/exonsControlPanel.js
@@ -6,13 +6,79 @@ export class ExonsControlPanel extends LitElement {
     return css`
       :host {
         display: block;
-        height: 100px;
-        // background: var(--main-v-dark);
-        // color: white;
+        border: 1px solid #ccc;
+      }
+
+      .panel-title {
+        background-color: var(--main-dark);
+        color: white;
+        font-weight: bold;
+        padding: 4px;
+      }
+
+      .panel-summary {
+        display: grid;
+        grid-template-columns: 1fr auto auto auto;
+        column-gap: 8px;
+        align-items: center;
+        font-weight: bold;
+        padding: 8px 4px 8px 6px;
+      }
+
+      .exons-count {
+        min-width: 10px;
+        padding: 2px 6px;
+        font-size: 10px;
+        font-weight: bold;
+        border: 1px solid var(--main-dark);
+        text-align: center;
+        border-radius: 10px;
+      }
+
+      .show-feature, .panel-content-toggle {
+        width: 16px;
+        height: 16px;
+        border: none;
+        outline: none;
+        cursor: pointer;
+      }
+
+      .show-feature_hidden {
+        background: url(/i/16/eye_no.png);
+      }
+
+      .show-feature_visible {
+        background: url(/i/16/eye.png);
+      }
+
+      .toggle-expanded {
+        background: url(/i/16/minus-button.png) no-repeat right center;
+      }
+
+      .toggle-collapsed {
+        background: url(/i/16/plus-button.png) no-repeat right center;      
+      }
+
+      .exon-details-wrapper {
+        padding: 5px 5px 5px 10px;
+      }
+
+      .exon-details {
+        width: 100%;
+        border: 1px solid #ccc;
+      }
+
+      .exon-details th {
+        background-color: #ccc;
+      }
+
+      .exon-details tbody tr td:nth-child(2),
+      .exon-details tbody tr td:nth-child(3) {
+        text-align: center;
       }
 
       .exon-row td:first-child {
-        border-left-width: 2px;
+        border-left-width: 5px;
         border-left-style: solid;
       }
     `;
@@ -20,21 +86,55 @@ export class ExonsControlPanel extends LitElement {
 
   static get properties() {
     return {
-      exons: { attribute: false }
+      exons: { attribute: false },
+      isExpanded: { state: true }
     }
+  }
+
+  areAllExonsVisible() {
+    return true;
+  }
+
+  isExonVisible(exonIndex) {
+
   }
 
   render() {
     return html`
-      <div>
-        <table>
-        <tbody>
-          ${this.exons.map((exon, index) => {
-            return this.renderExonRow(exon, index)
-          })}
-        </tbody>
-        </table>
+      <div class="panel-title">
+        Exons
       </div>
+      <div class="panel-summary">
+        Summary
+        <span class="exons-count">${this.exons.length}</span>
+        <button
+          class="show-feature ${this.areAllExonsVisible() ? 'show-feature_visible' : 'show-feature_hidden'}"
+        ></button>
+        <button
+          class="panel-content-toggle ${this.isExpanded ? 'toggle-expanded' : 'toggle-collapsed'}"
+          @click=${() => this.isExpanded = !this.isExpanded}
+        ></button>
+      </div>
+      ${
+        this.isExpanded ? html`
+          <div class="exon-details-wrapper">
+            <table class="exon-details">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Location</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                ${this.exons.map((exon, index) => {
+                  return this.renderExonRow(exon, index)
+                })}
+              </tbody>
+            </table>
+          </div>
+        ` : null
+      }
     `;
   }
 
@@ -44,7 +144,9 @@ export class ExonsControlPanel extends LitElement {
         <td style="border-color: ${exon.color}">Exon ${index + 1}</td>
         <td>${exon.start}-${exon.end}</td>
         <td>
-          <span>eye</span>
+          <button
+            class="show-feature ${this.areAllExonsVisible() ? 'show-feature_visible' : 'show-feature_hidden'}"
+          ></button>
         </td>
       </tr>
     `;
