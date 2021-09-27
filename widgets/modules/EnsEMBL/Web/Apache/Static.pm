9a163b41eafc0550c734bdafd3cd7b5d327e86bd
Andrey Azov <andrey@ebi.ac.uk>
2021-09-27 15:26:16 +0000

    Update the function used for static file caching

--- widgets/modules/EnsEMBL/Web/Apache/Static.pm
+++ widgets/modules/EnsEMBL/Web/Apache/Static.pm
@@ -2,35 +2,18 @@ package EnsEMBL::Web::Apache::Static;
 
 use strict;
 use warnings;
+use List::Util qw( any );
 
-sub add_caching_headers {
+my @patterns_to_skip_caching = (
+  qr/alphafold\/.*\.js$/ # javascript files in the alphafold folder
+);
+
+sub should_skip_caching {
   my $r = shift;
   my $uri = $r->uri;
 
-  if (should_skip_caching($uri)) {
-    $r->headers_out->set('Cache-Control'  => 'no-store, max-age=0');
-  } else {
-    # default behaviour
-    my $thirty_days = 60 * 60 * 24 * 30;
-    $r->headers_out->set('Cache-Control'  => 'max-age=' . $thirty_days);
-    $r->headers_out->set('Expires'        => HTTP::Date::time2str(time + $thirty_days));
-  }
+  return any { $uri =~ $_ } @patterns_to_skip_caching;
 }
 
-sub should_skip_caching {
-  my $uri = shift;
-  my @patterns = (
-    qr/alphafold\/.*\.js$/ # javascript files in the alphafold folder
-  );
-
-  my $bool;
-  foreach my $pattern (@patterns) {
-    if ($uri =~ /$pattern/) {
-      $bool = 1;
-    }
-  }
-
-  return $bool;
-}
 
 1;
