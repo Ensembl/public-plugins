885b479cd986c7cd1b67b330372bcd5defde6519
Jyothish <joyjoy.nt@gmail.com>
2023-02-07 17:17:34 +0000

    ENSWBSITES-1845: setup hotjar (#657)
    use meaningful variable names

---
+++ ga/htdocs/components/90_hotjar.js
@@ -0,0 +1,20 @@
+Ensembl.HotJar = {
+  init: function() {
+    const hotjarId = this.hotjarIds[window.location.hostname];
+    const version = 6;
+    this.addScript(hotjarId);
+  },
+
+  addScript: (hotjarId, version) => {
+    if (hotjarId && version) {
+      (function(h,o,t,j,id,ver){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:id,hjsv:ver};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=',hotjarId,version);
+    }
+  }
+}
