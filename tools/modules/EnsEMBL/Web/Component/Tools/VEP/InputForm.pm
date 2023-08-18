8212894cfac132f2aa7dcbfea274915722ebcaf9
Nuno Agostinho <nuno@ebi.ac.uk>
2023-08-18 17:28:59 +0100

    Fix empty section

--- tools/modules/EnsEMBL/Web/Component/Tools/VEP/InputForm.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/VEP/InputForm.pm
@@ -669,21 +669,7 @@ sub _build_additional_annotations {
 
   $self->_end_section(\@fieldsets, $fieldset, $current_section);
 
-
-  ## FUNCTIONAL EFFECT
-  $current_section = 'Functional effect';
-  my @func_species = map { ucfirst $_ } uniq map { @{$_->{'species'}} } @{$self->_get_plugins_by_section($current_section)};
-
-  if (@func_species) {
-    my @func_species_classes = map { "_stt_".$_ } @func_species;
-
-    my $func_class = (scalar(@func_species_classes)) ? join(' ',@func_species_classes) : '';
-
-    $fieldset = $form->add_fieldset({'legend' => $current_section, 'no_required_notes' => 1, class => $func_class });
-
-    $self->_end_section(\@fieldsets, $fieldset, $current_section);
-  }
-
+  $self->_add_plugin_sections($form, \@fieldsets, "Functional effect");
 
   ## REGULATORY DATA
   $current_section = 'Regulatory data';
