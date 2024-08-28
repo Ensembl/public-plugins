e268db569cc2f185c2a12d21a313737ba0b08c15
Syed Nakib Hossain <snhossain@ebi.ac.uk>
2024-08-28 16:06:30 +0100

    Use case-insensitive grep
    Co-authored-by: Nuno Agostinho <nuno@ebi.ac.uk>

--- tools/modules/EnsEMBL/Web/Job/VEP.pm
+++ tools/modules/EnsEMBL/Web/Job/VEP.pm
@@ -291,7 +291,7 @@ sub _configure_custom_annotations {
     $cu_key =~ s/^custom\_//;
     my $custom_ann = $custom_config{$cu_key};
     next unless $custom_ann;
-    next if $custom_ann->{species} && !grep(/^$job_data->{'species'}$/, map { ucfirst $_ } @{ref $custom_ann->{species} eq 'ARRAY' ? $custom_ann->{species} : [$custom_ann->{species}]});
+    next if $custom_ann->{species} && !grep(/^$job_data->{'species'}$/i, @{ref $custom_ann->{species} eq 'ARRAY' ? $custom_ann->{species} : [$custom_ann->{species}]});
 
     my $params = $custom_ann->{params};
 
