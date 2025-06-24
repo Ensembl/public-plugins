886becb0f69c0d68ecc135220e5f7af0a3305f0d
Aine Fairbrother-Browne <42773574+ainefairbrother@users.noreply.github.com>
2025-06-24 15:21:04 +0100

    Web MaveDB plug-in to display 1 additional column: `MaveDB_doi` (#877)
    * feat: include MaveDB_doi column in output
    * feat: make doi resolve using doi.org

--- tools/modules/EnsEMBL/Web/Component/Tools/VEP/Results.pm
+++ tools/modules/EnsEMBL/Web/Component/Tools/VEP/Results.pm
@@ -248,6 +248,7 @@ sub content {
     'MaveDB_pro'                => 'MaveDB protein change',
     'MaveDB_score'              => 'MaveDB score',
     'MaveDB_urn'                => 'MaveDB URN',
+    'MaveDB_doi'                => 'MaveDB DOI',
     'PARALOGUE_REGIONS'         => 'Paralogue regions and ClinVar variants',
     'PARALOGUE_VARIANTS'        => 'Paralogue variants',
     'OpenTargets_l2g'           => 'Open Targets Genetics L2G',
@@ -349,6 +350,9 @@ sub content {
         }
         elsif ($header eq 'MaveDB_urn'){
           $row->{$header} = $self->get_items_in_list($row_id, 'MaveDB_urn', 'MaveDB URN', $row->{$header}, $species);
+        }
+        elsif ($header eq 'MaveDB_doi'){
+          $row->{$header} = $self->get_items_in_list($row_id, 'MaveDB_doi', 'MaveDB DOI', $row->{$header}, $species);
         }
         elsif ($header eq 'PARALOGUE_REGIONS'){
           # prepare paralogue variants
@@ -1387,6 +1391,9 @@ sub get_items_in_list {
       }
       elsif ($type eq 'MaveDB_urn') {
         $item_url = $hub->get_ExtURL_link($item, 'MAVEDB', $item);
+      }
+      elsif ($type eq 'MaveDB_doi') {
+        $item_url = $hub->get_ExtURL_link($item, 'DOI', $item);
       }
       elsif ($type eq 'PARALOGUE_REGIONS') {
         my ($chr, $start, $end, $transcript_id, $perc_cov, $perc_pos) = split /:/, $item;
